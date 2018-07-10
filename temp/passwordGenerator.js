ab734ab7866a67c3cd9fb7ea93febd40e76e0df6
minotogna <mino.togna@gmail.com>
2018-07-10 09:10:03 +0200

    added password generator file to temp folder.

---
+++ temp/passwordGenerator.js
@@ -0,0 +1,15 @@
+const {passwordHash} = require('../server/user/userUtils')
+
+if (process.argv.length < 3) {
+  console.log('Password is mandatory. Example usage: ')
+  console.log('node temp/passwordGenerator.js my-password')
+  process.exit()
+}
+
+const generatePasswordHash = async () => {
+  const pwd = process.argv[2]
+  console.log(`Generated hash password of ${pwd} is`)
+  console.log(await passwordHash(pwd))
+}
+
+generatePasswordHash()
