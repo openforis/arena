be417d1bed95cb98fadd324cf16c9cfb0a1c6589
minot <cosimo.togna@fao.org>
2018-07-11 17:39:29 +0200

    renamed survey dashboard view`

--- webapp/app/appView.js
+++ webapp/app/appView.js
@@ -1,12 +1,12 @@
 import './style.scss'
 
 import React from 'react'
-import SurveySummaryView from '../survey/surveySummaryView'
+import SurveyDashboardView from '../survey/surveyDashboardView'
 
 const AppView = (props) =>
   <div className="app__container">
 
-    <SurveySummaryView {...props} />
+    <SurveyDashboardView {...props} />
 
   </div>
 
