fb9e432e9a16941163aea9f0456e8249870971be
minot <cosimo.togna@fao.org>
2018-07-11 20:11:43 +0200

    added icons to modules and status

--- webapp/surveyDashboard/components/surveyInfo.js
+++ webapp/surveyDashboard/components/surveyInfo.js
@@ -8,8 +8,8 @@ class SurveyInfo extends React.Component {
       <input className="text-center" placeholder="Survey name"/>
 
       <div className="survey-info__actions">
-        <div className="survey-info__status">
-          Status: DRAFT!
+        <div className="survey-info__status text-center">
+          <span className="icon icon-warning icon-24px"/>
         </div>
         <button className="btn btn-of-light">
           <span className="icon icon-download3 icon-24px"></span>
--- webapp/surveyDashboard/components/surveyModulesComponent.js
+++ webapp/surveyDashboard/components/surveyModulesComponent.js
@@ -18,7 +18,7 @@ class SurveyModules extends React.Component {
       {/*<div className="survey-dashboard__module-container">*/}
         <div className="survey-modules__module b">
           <div className="flex-center title-of">
-            <span className="icon icon-quill icon-24px icon-left"/>
+            <span className="icon icon-table2 icon-24px icon-left"/>
             <h5>Data Explorer</h5>
           </div>
         </div>
@@ -27,7 +27,7 @@ class SurveyModules extends React.Component {
       {/*<div className="survey-dashboard__module-container">*/}
         <div className="survey-modules__module c">
           <div className="flex-center title-of">
-            <span className="icon icon-quill icon-24px icon-left"/>
+            <span className="icon icon-calculator icon-24px icon-left"/>
             <h5>Data Analysis</h5>
           </div>
         </div>
@@ -36,7 +36,7 @@ class SurveyModules extends React.Component {
       {/*<div className="survey-dashboard__module-container">*/}
         <div className="survey-modules__module d">
           <div className="flex-center title-of">
-            <span className="icon icon-quill icon-24px icon-left"/>
+            <span className="icon icon-users icon-24px icon-left"/>
             <h5>Users</h5>
           </div>
           <Route path={'/app/a'} render={() => <div>aaaa</div>}/>
