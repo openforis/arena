a8672b7c448a39f8a835cff8844c90d5be849392
minot <cosimo.togna@fao.org>
2018-07-11 21:08:06 +0200

    survey dashboard code clean up

--- webapp/surveyDashboard/components/surveyInfo.js
+++ webapp/surveyDashboard/components/surveyInfo.js
@@ -3,23 +3,25 @@ import React from 'react'
 class SurveyInfo extends React.Component {
 
   render () {
-    return <div className="survey-info">
+    return (
+      <div className="survey-info">
 
-      <input className="text-center" placeholder="Survey name"/>
+        <input className="text-center" placeholder="Survey name"/>
 
-      <div className="survey-info__actions">
-        <div className="survey-info__status text-center">
-          <span className="icon icon-warning icon-24px"/>
+        <div className="survey-info__actions">
+          <div className="survey-info__status text-center">
+            <span className="icon icon-warning icon-24px"/>
+          </div>
+          <button className="btn btn-of-light">
+            <span className="icon icon-download3 icon-24px"></span>
+          </button>
+
+          <button className="btn btn-of-light">
+            <span className="icon icon-upload3 icon-24px"></span>
+          </button>
         </div>
-        <button className="btn btn-of-light">
-          <span className="icon icon-download3 icon-24px"></span>
-        </button>
-
-        <button className="btn btn-of-light">
-          <span className="icon icon-upload3 icon-24px"></span>
-        </button>
       </div>
-    </div>
+    )
   }
 }
 
--- webapp/surveyDashboard/components/surveyModules.js
+++ webapp/surveyDashboard/components/surveyModules.js
@@ -4,46 +4,40 @@ import { Route } from 'react-router-dom'
 class SurveyModules extends React.Component {
 
   render () {
-    return <div className="survey-modules">
+    return (
+      <div className="survey-modules">
 
-      {/*<div className="survey-modules__module-container">*/}
-      <div className="survey-modules__module">
-        <div className="flex-center title-of">
-          <span className="icon icon-quill icon-24px icon-left"/>
-          <h5>Survey Designer</h5>
+        <div className="survey-modules__module">
+          <div className="flex-center title-of">
+            <span className="icon icon-quill icon-24px icon-left"/>
+            <h5>Survey Designer</h5>
+          </div>
         </div>
-      </div>
-      {/*</div>*/}
 
-      {/*<div className="survey-dashboard__module-container">*/}
-      <div className="survey-modules__module">
-        <div className="flex-center title-of">
-          <span className="icon icon-table2 icon-24px icon-left"/>
-          <h5>Data Explorer</h5>
+        <div className="survey-modules__module">
+          <div className="flex-center title-of">
+            <span className="icon icon-table2 icon-24px icon-left"/>
+            <h5>Data Explorer</h5>
+          </div>
         </div>
-      </div>
-      {/*</div>*/}
 
-      {/*<div className="survey-dashboard__module-container">*/}
-      <div className="survey-modules__module">
-        <div className="flex-center title-of">
-          <span className="icon icon-calculator icon-24px icon-left"/>
-          <h5>Data Analysis</h5>
+        <div className="survey-modules__module">
+          <div className="flex-center title-of">
+            <span className="icon icon-calculator icon-24px icon-left"/>
+            <h5>Data Analysis</h5>
+          </div>
         </div>
-      </div>
-      {/*</div>*/}
 
-      {/*<div className="survey-dashboard__module-container">*/}
-      <div className="survey-modules__module">
-        <div className="flex-center title-of">
-          <span className="icon icon-users icon-24px icon-left"/>
-          <h5>Users</h5>
+        <div className="survey-modules__module">
+          <div className="flex-center title-of">
+            <span className="icon icon-users icon-24px icon-left"/>
+            <h5>Users</h5>
+          </div>
+          <Route path={'/app/a'} render={() => <div>aaaa</div>}/>
         </div>
-        <Route path={'/app/a'} render={() => <div>aaaa</div>}/>
-      </div>
-      {/*</div>*/}
 
-    </div>
+      </div>
+    )
   }
 }
 
