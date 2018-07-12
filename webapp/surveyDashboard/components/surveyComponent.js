5028a7d0b1a35ec4f2143bdee0a9423669bbcc58
minot <cosimo.togna@fao.org>
2018-07-12 20:25:23 +0200

    added add survey button

--- webapp/surveyDashboard/components/surveyComponent.js
+++ webapp/surveyDashboard/components/surveyComponent.js
@@ -25,16 +25,24 @@ class SurveyComponent extends React.Component {
           </div>
 
           <div className="survey-info__actions">
-            <div className="survey-info__status text-center">
-              <span className="icon icon-warning icon-24px"/>
-            </div>
+
             <button className="btn btn-of-light">
-              <span className="icon icon-download3 icon-24px"></span>
+              <span className="icon icon-warning icon-24px"/>
             </button>
 
             <button className="btn btn-of-light">
-              <span className="icon icon-upload3 icon-24px"></span>
+              <span className="icon icon-download3 icon-24px"/>
             </button>
+
+            <button className="btn btn-of-light">
+              <span className="icon icon-upload3 icon-24px"/>
+            </button>
+
+            <button className="btn btn-of-light">
+              <span className="icon icon-plus icon-20px icon-left"/>
+              Survey
+            </button>
+
           </div>
         </div>
       </DataFetchComponent>
