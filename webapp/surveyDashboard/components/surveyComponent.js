7adba6d760f7fc49591c970cf0713984445a7a1a
minotogna <mino.togna@gmail.com>
2018-07-13 10:24:03 +0200

    surveyComponent layout update

--- webapp/surveyDashboard/components/surveyComponent.js
+++ webapp/surveyDashboard/components/surveyComponent.js
@@ -28,23 +28,23 @@ class SurveyComponent extends React.Component {
           <div className="survey-info__actions">
 
             <button className="btn btn-of-light">
-              <span className="icon icon-warning icon-24px"/>
+              <span className="icon icon-warning icon-20px"/>
             </button>
 
             <button className="btn btn-of-light">
-              <span className="icon icon-download3 icon-24px"/>
+              <span className="icon icon-download3 icon-20px"/>
             </button>
 
             <button className="btn btn-of-light">
-              <span className="icon icon-upload3 icon-24px"/>
+              <span className="icon icon-upload3 icon-20px"/>
             </button>
 
             <button className="btn btn-of-light">
-              <span className="icon icon-bin icon-24px"/>
+              <span className="icon icon-bin icon-20px"/>
             </button>
 
             <button className="btn btn-of-light">
-              <span className="icon icon-plus icon-24px"/>
+              <span className="icon icon-plus icon-20px"/>
             </button>
 
           </div>
