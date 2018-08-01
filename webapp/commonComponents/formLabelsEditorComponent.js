69eecee42d55b47c713f54ba524b335847719059
minotogna <mino.togna@gmail.com>
2018-08-01 15:42:23 +0200

    updated label editor style

--- webapp/commonComponents/formLabelsEditorComponent.js
+++ webapp/commonComponents/formLabelsEditorComponent.js
@@ -5,54 +5,51 @@ import { getLanguageLabel } from '../../common/app/languages'
 
 import { FormInput } from './formInputComponents'
 
-const LabelRow = ({label = '', lang, onChange, displayLang = true}) => {
-  return (
-    <React.Fragment>
-      {
-        displayLang
-          ? <h6 style={{alignSelf: 'end'}}>{
-            R.pipe(
-              getLanguageLabel,
-              R.toUpper
-            )(lang)
-          }</h6>
-          : null
-      }
-      <FormInput value={label}
-                 onChange={e => onChange({
-                     lang,
-                     label: e.target.value
-                   }
-                 )}/>
-    </React.Fragment>
-  )
-}
-
-const FormLabelsEditorComponent = ({languages, labels, onChange}) => {
-  const displayLang = languages.length > 1
-
-  return (
-    <div className="form-item">
-      <label className="form-label">Label(s)</label>
-
-      <div style={{
-        display: 'grid',
-        gridTemplateColumns: displayLang ? '.1fr .9fr' : '1fr',
-        gridRowGap: '.5rem',
-        alignItems: 'center',
+const LabelBadge = ({lang}) => (
+  <h6 className="badge"
+      style={{
+        position: 'absolute',
+        right: '0',
+        textTransform: 'uppercase',
       }}>
-        {
-          languages.map(lang =>
-            <LabelRow key={lang}
-                      lang={lang}
-                      label={R.prop(lang)(labels)}
-                      onChange={onChange}
-                      displayLang={displayLang}/>
-          )
-        }
-      </div>
+    {
+      getLanguageLabel(lang)
+    }
+  </h6>
+)
+
+const LabelRow = ({label = '', lang, onChange}) => (
+  <div style={{position: 'relative'}}>
+
+    <LabelBadge lang={lang}/>
+
+    <FormInput value={label}
+               onChange={e => onChange({
+                   lang,
+                   label: e.target.value
+                 }
+               )}/>
+  </div>
+)
+
+const FormLabelsEditorComponent = ({languages, labels, onChange}) => (
+  <div className="form-item">
+    <label className="form-label">Label(s)</label>
+
+    <div style={{
+      display: 'grid',
+      gridRowGap: '1rem',
+    }}>
+      {
+        languages.map(lang =>
+          <LabelRow key={lang}
+                    lang={lang}
+                    label={R.prop(lang)(labels)}
+                    onChange={onChange}/>
+        )
+      }
     </div>
-  )
-}
+  </div>
+)
 
 export default FormLabelsEditorComponent
