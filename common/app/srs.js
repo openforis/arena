3b2010aa1c4d39679deed7e25aa46d99ac7b1718
fefrain <fefrain@users.noreply.github.com>
2018-08-03 16:58:19 +0200

    srs: include geographic coordinates and add documentation

--- common/app/srs.js
+++ common/app/srs.js
@@ -2,19 +2,38 @@ import * as R from 'ramda'
 
 import codes from '@esri/proj-codes'
 
+/**
+ * Projected coordinate reference systems indexed by EPSG code
+ * format: {epgsCode: {name:"CRS name", wkt: "Well Know Text"}}
+ */
 const projected = require('@esri/proj-codes/projected.json')
+/**
+ * Geographic coordinate reference systems indexed by EPSG code
+ * format: same as projected
+ */
+const geographic = require('@esri/proj-codes/geographic.json') //geographic coordinate reference systems
 
 //TODO move to StringUtils module
 const contains = (a = '', b = '') => R.contains(R.toLower(a), R.toLower(b))
 
 const formatName = (name = '') => R.replace(/_/g, ' ')(name)
 
+/**
+ * Array of all srs.
+ * Every item has this format: {key: epsgCode, value: "Formatted coordinate reference system name"}
+ */
 const srs = R.pipe(
+  R.merge(geographic),
   R.mapObjIndexed((value, key, obj) => {return {key, value: formatName(value.name)}}),
   R.values,
   R.sortBy(R.prop('value'))
 )(projected)
 
+/**
+ * Finds a list of srs whose name or code matches the specified parameter
+ * @param codeOrName
+ * @returns {Array}
+ */
 const findSrs = (codeOrName) => {
   const matchingItems = []
 
@@ -28,8 +47,18 @@ const findSrs = (codeOrName) => {
   return matchingItems
 }
 
+/**
+ * Returns the name of a srs having the specified EPSG code
+ * @param code
+ * @returns string
+ */
 const getSrsName = (code) => lookupSrs(code).name
 
+/**
+ * returns a srs having the specified EPSG code
+ * @param code
+ * @returns {{code: number, name: string, wkt: string}}
+ */
 const lookupSrs = (code) => {
   const item = codes.lookup(code)
   return {code, name: formatName(item.name), wkt: item.wkt}
