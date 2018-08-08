f2d5a44dd656b42eedb13ebb129e738448db7ce8
minotogna <mino.togna@gmail.com>
2018-08-08 14:46:04 +0200

    trying to render entity table

--- webapp/survey/nodeDef/components/nodeDefEntity.js
+++ webapp/survey/nodeDef/components/nodeDefEntity.js
@@ -15,6 +15,8 @@ import {
   filterInnerPageChildren,
   getLayout,
   getNoColumns,
+  isRenderForm,
+  isRenderTable,
 } from '../../../../common/survey/nodeDefLayout'
 
 import { getNodeDefChildren, getSurveyState } from '../../surveyState'
@@ -56,9 +58,11 @@ class NodeDefEntity extends React.Component {
     const columns = getNoColumns(nodeDef)
     const rdgLayout = getLayout(nodeDef)
 
+    console.log('==== ')
+    console.log(nodeDef)
+
     return (
-      this.hasChildren() ?
-        //TODO ? isRenderForm(nodeDef)
+      isRenderForm(nodeDef) && this.hasChildren() ?
         <ResponsiveGridLayout breakpoints={{lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0}}
                               cols={{lg: columns, md: columns, sm: columns, xs: 1, xxs: 1}}
                               rowHeight={edit ? 80 : 60}
@@ -75,7 +79,8 @@ class NodeDefEntity extends React.Component {
                                 sm: rdgLayout,
                               }}
                               isDraggable={edit}
-                              isResizable={edit}>
+                              isResizable={edit}
+                              verticalCompact={false}>
 
           {
             filterInnerPageChildren(children)
@@ -88,7 +93,33 @@ class NodeDefEntity extends React.Component {
 
         </ResponsiveGridLayout>
         //TODO Render table
-        // : '=P'
+
+        : isRenderTable(nodeDef) ?
+
+        <ResponsiveGridLayout breakpoints={{lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0}}
+                              cols={{lg: 3, md: 3, sm: 3, xs: 1, xxs: 1}}
+                              rowHeight={edit ? 80 : 60}
+                              autoSize={false}
+                              containerPadding={[0, 0]}
+          // onLayoutChange={(layout) => console.log(window.innerWidth) ||
+          // window.innerWidth > 1200 && layout.length > 0
+          //   ? putNodeDefProp(nodeDef, nodeDefLayoutProps.layout, layout)
+          //   : null
+          // }
+          // layouts={{
+          //   lg: rdgLayout,
+          //   md: rdgLayout,
+          //   sm: rdgLayout,
+          // }}
+                              isDraggable={edit}
+                              isResizable={false}>
+
+          <div key={'1'} data-grid={{w: 1, h: 1, x: 0, y: 0, i: '1'}}>1</div>
+          <div key={'2'} data-grid={{w: 1, h: 1, x: 1, y: 0, i: '2'}}>2</div>
+          <div key={'3'} data-grid={{w: 1, h: 1, x: 2, y: 0, i: '3'}}>3</div>
+
+
+        </ResponsiveGridLayout>
         : null
     )
   }
