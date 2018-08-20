efe714c8cdefe1fa76e4dc9f86d917a2a81cf5e5
fefrain <fefrain@users.noreply.github.com>
2018-08-20 17:29:32 +0200

    data entry: transform CommandProcessor into EventEmitter

--- server/record/commandProcessor.js
+++ server/record/commandProcessor.js
@@ -1,3 +1,5 @@
+const util = require('util')
+const {EventEmitter} = require('events')
 const R = require('ramda')
 
 const {nodeDefType} = require('../../common/survey/nodeDef')
@@ -6,120 +8,130 @@ const {fetchNodeDef, fetchNodeDefsBySurveyId, fetchNodeDefsByParentId} = require
 const {getSurveyById} = require('../survey/surveyRepository')
 const {createRecord} = require('../record/recordRepository')
 const {insertNode, updateNode, deleteNode} = require('../record/nodeRepository')
-const {insertRecordCreatedLog, insertNodeAddedLog, insertNodeUpdatedLog, insertNodeDeletedLog} = require('../record/recordLogRepository')
 
-const processCommand = async (command) => {
-  switch (command.type) {
-    case commandType.createRecord:
-      return await processCreateRecordCommand(command)
-    case commandType.addNode:
-      return await processAddNodeCommand(command)
-    case commandType.updateNode:
-      return await processUpdateNodeCommand(command)
-    case commandType.deleteNode:
-      return await processDeleteNodeCommand(command)
-    default:
-      return []
+const CommandProcessor = function CommandProcessor () {
+
+  this.processCommand = async (command) => {
+    switch (command.type) {
+      case commandType.createRecord:
+        return await this.processCreateRecordCommand(command)
+      case commandType.addNode:
+        return await this.processAddNodeCommand(command)
+      case commandType.updateNode:
+        return await this.processUpdateNodeCommand(command)
+      case commandType.deleteNode:
+        return await this.processDeleteNodeCommand(command)
+      default:
+        return []
+    }
+  }
+
+  this.processCreateRecordCommand = async (command) => {
+    //TODO do it in transaction
+
+    const events = []
+    const {surveyId, user} = command
+    const survey = await getSurveyById(surveyId)
+    const record = await createRecord(user, survey)
+
+    events.push(this.emitEvent(
+      eventType.recordCreated, {
+        surveyId,
+        user: command.user,
+        record,
+      }
+    ))
+
+    const nodeDefs = await fetchNodeDefsBySurveyId(survey.id)
+    const rootNodeDef = R.find(n => n.parentId === null)(nodeDefs)
+
+    const rootNodeEvents = await this.insertNodeInternal(survey.id, command.user, record.id, null, rootNodeDef)
+    Array.prototype.push.apply(events, rootNodeEvents)
+
+    return events
+  }
+
+  this.processAddNodeCommand = async (command) => {
+    const {surveyId, recordId, nodeDefId, parentNodeId} = command
+
+    const nodeDef = await fetchNodeDef(nodeDefId)
+    return await this.insertNodeInternal(surveyId, command.user, recordId, parentNodeId, nodeDef)
+  }
+
+  this.processUpdateNodeCommand = async (command) => {
+    const {surveyId, nodeId} = command
+
+    const events = []
+
+    const node = await updateNode(surveyId, nodeId, command.value)
+
+    events.push(this.emitEvent(
+      eventType.nodeUpdated, {
+        surveyId,
+        user: command.user,
+        node,
+      }
+    ))
+    return events
+  }
+
+  this.processDeleteNodeCommand = async (command) => {
+    const {surveyId, nodeId} = command
+
+    const events = []
+
+    const node = await deleteNode(surveyId, nodeId)
+
+    events.push(this.emitEvent(
+      eventType.nodeDeleted, {
+        surveyId,
+        user: command.user,
+        node,
+      }
+    ))
+    return events
+  }
+
+  this.insertNodeInternal = async (surveyId, user, recordId, parentId, nodeDef) => {
+    const events = []
+
+    const node = await insertNode(surveyId, createNode(recordId, parentId, nodeDef.id))
+
+    events.push(this.emitEvent(
+      eventType.nodeAdded, {
+        surveyId,
+        user,
+        node,
+      }
+    ))
+
+    if (nodeDefType.entity === nodeDef.type) {
+      const emptyChildrenEvents = await this.insertEmptyChildren(surveyId, user, node)
+      Array.prototype.push.apply(events, emptyChildrenEvents)
+    }
+    return events
+  }
+
+  this.insertEmptyChildren = async (surveyId, user, parentNode) => {
+    const events = []
+    const childrenDefs = await fetchNodeDefsByParentId(parentNode.nodeDefId)
+
+    await Promise.all(childrenDefs.map(async childDef => {
+      const childInsertEvents = await this.insertNodeInternal(surveyId, user, parentNode.recordId, parentNode.id, childDef)
+      Array.prototype.push.apply(events, childInsertEvents)
+    }))
+
+    return events
+  }
+
+  this.emitEvent = (type, event) => {
+    this.emit(type, event)
+    return {type, ...event}
   }
 }
 
-const processCreateRecordCommand = async (command) => {
-  //TODO do it in transaction
-
-  const events = []
-  const {surveyId, user} = command
-  const survey = await getSurveyById(surveyId)
-  const record = await createRecord(user, survey)
-
-  events.push({
-    type: eventType.recordCreated,
-    record,
-  })
-
-  //TODO handle with event observer
-  //LOG
-  insertRecordCreatedLog(surveyId, command.user, record)
-
-  const nodeDefs = await fetchNodeDefsBySurveyId(survey.id)
-  const rootNodeDef = R.find(n => n.parentId === null)(nodeDefs)
-
-  const rootNodeEvents = await insertNodeInternal(survey.id, command.user, record.id, null, rootNodeDef)
-  Array.prototype.push.apply(events, rootNodeEvents)
-
-  return events
-}
-
-const processAddNodeCommand = async (command) => {
-  const {surveyId, recordId, nodeDefId, parentNodeId} = command
-
-  const nodeDef = await fetchNodeDef(nodeDefId)
-  return await insertNodeInternal(surveyId, command.user, recordId, parentNodeId, nodeDef)
-}
-
-const processUpdateNodeCommand = async (command) => {
-  const {surveyId, nodeId} = command
-
-  const events = []
-
-  const node = await updateNode(surveyId, nodeId, command.value)
-  //LOG
-  await insertNodeUpdatedLog(surveyId, command.user, node)
-
-  events.push({
-    type: eventType.nodeUpdated,
-    node,
-  })
-  return events
-}
-
-const processDeleteNodeCommand = async (command) => {
-  const {surveyId, nodeId} = command
-
-  const events = []
-
-  const node = await deleteNode(surveyId, nodeId)
-  //LOG
-  await insertNodeDeletedLog(surveyId, command.user, node)
-
-  events.push({
-    type: eventType.nodeDeleted,
-    node,
-  })
-  return events
-}
-
-const insertNodeInternal = async (surveyId, user, recordId, parentId, nodeDef) => {
-  const events = []
-
-  const node = await insertNode(surveyId, createNode(recordId, parentId, nodeDef.id))
-
-  events.push({
-    type: eventType.nodeAdded,
-    node: node,
-  })
-  //TODO move it to event observer
-  //LOG
-  await insertNodeAddedLog(surveyId, user, node)
-
-  if (nodeDefType.entity === nodeDef.type) {
-    const emptyChildrenEvents = await insertEmptyChildren(surveyId, user, node)
-    Array.prototype.push.apply(events, emptyChildrenEvents)
-  }
-  return events
-}
-
-const insertEmptyChildren = async (surveyId, user, parentNode) => {
-  const events = []
-  const childrenDefs = await fetchNodeDefsByParentId(parentNode.nodeDefId)
-
-  await Promise.all(childrenDefs.map(async childDef => {
-    const childInsertEvents = await insertNodeInternal(surveyId, user, parentNode.recordId, parentNode.id, childDef)
-    Array.prototype.push.apply(events, childInsertEvents)
-  }))
-
-  return events
-}
+util.inherits(CommandProcessor, EventEmitter)
 
 module.exports = {
-  processCommand,
+  CommandProcessor,
 }
