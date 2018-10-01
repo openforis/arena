f7eccc94db1be26a9c96e14dc1569cea2b9f230a
fefrain <fefrain@users.noreply.github.com>
2018-10-01 10:59:25 +0200

    support multiple code list items selection

--- webapp/survey/nodeDef/components/types/nodeDefCodeList.js
+++ webapp/survey/nodeDef/components/types/nodeDefCodeList.js
@@ -16,32 +16,54 @@ import { getSurveyDefaultLanguage, getSurveyCodeListByUUID } from '../../../../.
 import { getNodeValue, newNode } from '../../../../../common/record/node'
 
 import { toQueryString } from '../../../../../server/serverUtils/request'
+import Dropdown from '../../../../commonComponents/form/dropdown'
 
 const CodeListDropdown = props => {
-  const {edit, items} = props
+  const {edit, nodeDef, nodes, items} = props
 
-  const handleSelectedItemsChange = (selectedItems) => {
+  const selectedCodes = R.pipe(
+    R.values,
+    R.reject(node => node.placeholder),
+    R.map(n => getNodeValue(n).code),
+  )(nodes)
+
+  const selectedItems = R.filter(item => R.contains(item.key)(selectedCodes))(items)
+
+  const handleSelectedItemsChange = (newSelectedItems) => {
     const {nodeDef, nodes, parentNode, removeNode, updateNode} = props
 
-    const selectedCodes = selectedItems.map(item => item.key)
-    const removedNode = R.find(n => !R.contains(getNodeValue(n).code, selectedCodes))(nodes)
+    const newSelectedCodes = newSelectedItems.map(item => item.key)
 
-    if (removedNode && removedNode.id) {
-      removeNode(nodeDef, removedNode)
-    } else {
-      const oldCodes = R.pipe(
-        R.values,
-        R.reject(node => node.placeholder),
-        R.map(n => getNodeValue(n).code),
-      )(nodes)
-      const newCodeItem = R.find(item => !R.contains(item.key, oldCodes))(selectedItems)
-      updateNode(nodeDef, newNode(nodeDef.id, parentNode.recordId, parentNode.uuid), {code: newCodeItem.key})
+    if (isNodeDefMultiple(nodeDef)) {
+      //remove deselected node
+      const removedNode = R.find(n => !R.contains(getNodeValue(n).code, newSelectedCodes))(nodes)
+      if (removedNode && removedNode.id) {
+        removeNode(nodeDef, removedNode)
+      }
+    }
+    //add new node or update existing one
+    const newSelectedCode = R.find(code => !R.contains(code, selectedCodes))(newSelectedCodes)
+    if (newSelectedCode) {
+      const placeholder = R.find(R.propEq('placeholder', true))(nodes)
+      const nodeToUpdate = placeholder
+        ? placeholder
+        : nodes.length === 1 && !isNodeDefMultiple(nodeDef)
+          ? nodes[0]
+          : newNode(nodeDef.id, parentNode.recordId, parentNode.uuid)
+      updateNode(nodeDef, nodeToUpdate, {code: newSelectedCode})
     }
   }
 
-  return <InputChips readOnly={edit}
-                     items={items}
-                     onChange={selectedItems => handleSelectedItemsChange(selectedItems)}/>
+  return isNodeDefMultiple(nodeDef)
+    ? <InputChips readOnly={edit}
+                  items={items}
+                  selection={selectedItems}
+                  onChange={selectedItems => handleSelectedItemsChange(selectedItems)}/>
+
+    : <Dropdown readOnly={edit}
+                items={items}
+                selection={R.head(selectedItems)}
+                onChange={item => handleSelectedItemsChange(item ? [item] : [])}/>
 }
 
 const Checkbox = props => {
@@ -127,8 +149,7 @@ class NodeDefCodeList extends React.Component {
       parentId,
     }
     const {data} = await axios.get(`/api/survey/${survey.id}/codeLists/${codeList.id}/items?${toQueryString(queryParams)}`)
-    const {items} = data
-    return items
+    return data.items
   }
 
   render () {
