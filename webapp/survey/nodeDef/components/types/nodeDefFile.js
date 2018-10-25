2cdca2327621bef0bbc9d2174d8cc26487e36fbe
minotogna <mino.togna@gmail.com>
2018-10-25 10:20:59 +0200

    removed margin to file buttons

--- webapp/survey/nodeDef/components/types/nodeDefFile.js
+++ webapp/survey/nodeDef/components/types/nodeDefFile.js
@@ -31,20 +31,20 @@ const getFileName = node => R.pipe(
 const NodeDefFileInput = ({survey, nodeDef, edit, record, node, updateNode, removeNode}) =>
   node
     ? <div className="node-def__file-input">
-        <UploadButton disabled={edit}
-                      showLabel={false}
-                      onChange={files => updateNode(nodeDef, node, {fileName: files[0].name}, files[0])}/>
+      <UploadButton disabled={edit}
+                    showLabel={false}
+                    onChange={files => updateNode(nodeDef, node, {fileName: files[0].name}, files[0])}/>
 
-        <DownloadButton href={`/api/survey/${survey.id}/record/${record.id}/nodes/${node.uuid}/file`}
-                        disabled={edit || R.isEmpty(getNodeValue(node))}
-                        label={getFileName(node)}/>
+      <DownloadButton href={`/api/survey/${survey.id}/record/${record.id}/nodes/${node.uuid}/file`}
+                      disabled={edit || R.isEmpty(getNodeValue(node))}
+                      label={getFileName(node)}/>
 
-        <NodeDefDeleteButton nodeDef={nodeDef}
-                             node={node}
-                             disabled={edit || R.isEmpty(getNodeValue(node))}
-                             showConfirm={true}
-                             removeNode={removeNode}/>
-      </div>
+      <NodeDefDeleteButton nodeDef={nodeDef}
+                           node={node}
+                           disabled={edit || R.isEmpty(getNodeValue(node))}
+                           showConfirm={true}
+                           removeNode={removeNode}/>
+    </div>
     : null
 
 const NodeDefFile = props => {
@@ -76,8 +76,8 @@ const NodeDefFile = props => {
       <NodeDefFormItem {...props}>
         <div className="overflowYAuto" style={{maxHeight: height}}>
           {
-            nodes.map(n =>
-              <NodeDefFileInput {...props} node={n}/>
+            nodes.map((n, i) =>
+              <NodeDefFileInput key={i} {...props} node={n}/>
             )
           }
         </div>
