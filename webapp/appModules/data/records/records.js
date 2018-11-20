2dee56a71a0dbf51d7690cce265bc210a502b23d
minotogna <mino.togna@gmail.com>
2018-11-20 12:13:22 +0100

    added generic table style

--- webapp/appModules/data/records/records.js
+++ webapp/appModules/data/records/records.js
@@ -1,6 +1,11 @@
+import './records.scss'
+
 import React from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 import * as R from 'ramda'
+import { appModuleUri } from '../../appModules'
+import { dataModules } from '../dataModules'
 
 const RecordsList = ({records}) => (
   <div className="records__list"></div>
@@ -13,15 +18,19 @@ class Records extends React.Component {
     const {records} = this.props
 
     return (
-      <div className="records">
+      <div className="records table">
 
-        <div className="records__header">
-          Records
+        <div className="table__header">
+          <h5>Records</h5>
+          <Link to={appModuleUri(dataModules.record)} className="btn btn-s btn-of records__btn-add-record">
+            <span className="icon icon-plus icon-12px icon-left"></span>
+            new
+          </Link>
         </div>
 
         {
           R.isEmpty(records)
-            ? <div className="records__empty">No records added</div>
+            ? <div className="table__empty-rows">No records added</div>
             : <RecordsList records={records}/>
         }
 
--- webapp/appModules/home/components/surveysList.js
+++ webapp/appModules/home/components/surveysList.js
@@ -6,26 +6,13 @@ import * as R from 'ramda'
 import Survey from '../../../../common/survey/survey'
 import { getRelativeDate, compareDatesDesc } from '../../../appUtils/dateUtils'
 
-const SurveyListHeader = () => (
-  <div className="surveys-list__header">
-    <h5 className="header-label">Surveys</h5>
-    <div className="surveys-list__row-header">
-      <div>Name</div>
-      <div>Label</div>
-      <div>Date created</div>
-      <div>Date last modified</div>
-      <div>Status</div>
-    </div>
-  </div>
-)
-
 const SurveyRow = ({surveyInfoRow, surveyInfo, setActiveSurvey}) => {
   const surveyId = surveyInfoRow.id
   const active = surveyInfo && surveyId === surveyInfo.id
   const activeClass = active ? ' active' : ''
 
   return (
-    <div className={`surveys-list__row${activeClass}`}>
+    <div className={`table__row${activeClass}`}>
       <div>{Survey.getName(surveyInfoRow)}</div>
       <div>{Survey.getDefaultLabel(surveyInfoRow)}</div>
       <div>{getRelativeDate(surveyInfoRow.dateCreated)}</div>
@@ -45,27 +32,35 @@ const SurveyList = (props) => {
   const {surveys} = props
   const surveyInfos = surveys.map(Survey.getSurveyInfo)
 
-  return (
-    <div className="surveys-list">
-      <SurveyListHeader {...props}/>
+  return R.isEmpty(surveys)
+    ? null
+    : (
+      <div className="surveys-list table">
+        <div className="table__header">
+          <h5>Surveys</h5>
+        </div>
 
-      {
-        R.isEmpty(surveys)
-          ? null
-          : (
-            <div className="surveys-list__rows">
-              {
-                surveyInfos
-                  .sort((a, b) => compareDatesDesc(a.dateModified, b.dateModified))
-                  .map((surveyInfo, i) =>
-                    <SurveyRow key={i} {...props} surveyInfoRow={surveyInfo}/>
-                  )
-              }
-            </div>
-          )
-      }
-    </div>
-  )
+        <div className="table__row-header">
+          <div>Name</div>
+          <div>Label</div>
+          <div>Date created</div>
+          <div>Date last modified</div>
+          <div>Status</div>
+        </div>
+
+
+        <div className="table__rows">
+
+          {
+            surveyInfos
+              .sort((a, b) => compareDatesDesc(a.dateModified, b.dateModified))
+              .map((surveyInfo, i) =>
+                <SurveyRow key={i} {...props} surveyInfoRow={surveyInfo}/>
+              )
+          }
+        </div>
+      </div>
+    )
 }
 
 export default SurveyList
