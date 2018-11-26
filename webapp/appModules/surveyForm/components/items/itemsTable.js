58966909f7c7c07b546c09bacca9a34bb83fcac4
Stefano Giaccio <stefano.giaccio@gmail.com>
2018-11-26 19:44:11 +0100

    Deleted float: right

--- webapp/appModules/surveyForm/components/items/itemsTable.js
+++ webapp/appModules/surveyForm/components/items/itemsTable.js
@@ -30,36 +30,34 @@ const TableRow = props => {
         }
       </div>
 
-      <div>
-        <div class="align-right">
-          {
-            onSelect && (canSelect || selected) &&
-            <button className={`btn btn-s btn-of-light-xs${selected ? ' active' : ''}`}
-                    onClick={() => onSelect(item)}>
-              <span className={`icon icon-checkbox-${selected ? '' : 'un'}checked icon-12px icon-left`}/>
-              {selected ? 'Selected' : 'Select'}
-            </button>
-          }
-
-          <button className="btn btn-s btn-of-light-xs"
-                  onClick={() => onEdit(item)}>
-            <span className={`icon icon-${readOnly ? 'eye' : 'pencil2'} icon-12px icon-left`}/>
-            {readOnly ? 'View' : 'Edit'}
+      <div className="items__table-row-buttons">
+        {
+          onSelect && (canSelect || selected) &&
+          <button className={`btn btn-s btn-of-light-xs${selected ? ' active' : ''}`}
+                  onClick={() => onSelect(item)}>
+            <span className={`icon icon-checkbox-${selected ? '' : 'un'}checked icon-12px icon-left`}/>
+            {selected ? 'Selected' : 'Select'}
           </button>
+        }
 
-          {
-            !readOnly &&
-            <button className="btn btn-s btn-of-light-xs"
-                    onClick={() => {
-                      if (canDelete(item)) {
-                        onDelete(item)
-                      }
-                    }}>
-              <span className="icon icon-bin2 icon-12px icon-left"/>
-              Delete
-            </button>
-          }
-        </div>
+        <button className="btn btn-s btn-of-light-xs"
+                onClick={() => onEdit(item)}>
+          <span className={`icon icon-${readOnly ? 'eye' : 'pencil2'} icon-12px icon-left`}/>
+          {readOnly ? 'View' : 'Edit'}
+        </button>
+
+        {
+          !readOnly &&
+          <button className="btn btn-s btn-of-light-xs"
+                  onClick={() => {
+                    if (canDelete(item)) {
+                      onDelete(item)
+                    }
+                  }}>
+            <span className="icon icon-bin2 icon-12px icon-left"/>
+            Delete
+          </button>
+        }
       </div>
     </div>
   )
