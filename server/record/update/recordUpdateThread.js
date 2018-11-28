6d9bab6013a4587fde7a9bf1840e31bb91a0b84b
minotogna <mino.togna@gmail.com>
2018-11-28 14:35:01 +0100

    process node updates in queue

--- server/record/update/recordUpdateThread.js
+++ server/record/update/recordUpdateThread.js
@@ -2,24 +2,47 @@ const RecordManager = require('../recordManager')
 const messageTypes = require('./recordThreadMessageTypes')
 const Thread = require('../../threads/thread')
 
-//TODO investigate if queue is needed
+const Queue = require('../../../common/queue')
+
 //TODO move  RecordManager.persistNode / RecordManager.deleteNode
 
 class RecordUpdateThread extends Thread {
 
+  constructor () {
+    super()
+    this.queue = new Queue()
+    this.processing = false
+  }
+
   onMessage (msg) {
-    switch (msg.type) {
-      case messageTypes.updateNode:
-        this.updateNode(msg.surveyId, msg.node, msg.file)
-        break
+    this.queue.enqueue(msg)
+    this.processNext()
+      .then(() => {})
+  }
+
+  async processNext () {
+    if (!(this.processing || this.queue.isEmpty())) {
+
+      this.processing = true
+
+      const msg = this.queue.dequeue()
+
+      switch (msg.type) {
+        case messageTypes.updateNode:
+          await this.updateNode(msg.surveyId, msg.node, msg.file)
+          break
+      }
+
+      this.processing = false
+
+      await this.processNext()
     }
   }
 
-  updateNode (surveyId, node, file) {
-    RecordManager.persistNode(surveyId, node, file)
-      .then(nodes => {
-        this.postMessage(nodes)
-      })
+  async updateNode (surveyId, node, file) {
+    const nodes = await RecordManager.persistNode(surveyId, node, file)
+
+    this.postMessage(nodes)
   }
 
 }
