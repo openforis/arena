e194f4bc0d8303d57813c9d0414b6985c8a26382
minotogna <mino.togna@gmail.com>
2018-11-28 12:25:37 +0100

    added TODOs

--- server/record/update/recordUpdateThread.js
+++ server/record/update/recordUpdateThread.js
@@ -2,6 +2,9 @@ const RecordManager = require('../recordManager')
 const messageTypes = require('./recordThreadMessageTypes')
 const Thread = require('../../threads/thread')
 
+//TODO investigate if queue is needed
+//TODO move  RecordManager.persistNode / RecordManager.deleteNode
+
 class RecordUpdateThread extends Thread {
 
   onMessage (msg) {
