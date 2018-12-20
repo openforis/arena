c42ac422263e4c1d71086479d259e6648bc0f823
fefrain <fefrain@users.noreply.github.com>
2018-12-20 14:51:57 +0100

    Test node def expressions

--- test/it/surveyIntegrationTest.js
+++ test/it/surveyIntegrationTest.js
@@ -4,8 +4,9 @@ require('dotenv').config()
 const {initTestContext, destroyTestContext} = require('./../testContext')
 
 const surveyIntegrationTest = require('./surveyTests/surveyTest')
-const nodeDefTest = require('./surveyTests/nodeDefTest')
 const categoryTest = require('./surveyTests/categoryTest')
+const nodeDefTest = require('./surveyTests/nodeDefTest')
+const nodeDefExpressionTest = require('./surveyTests/nodeDefExpressionTest')
 
 before(initTestContext)
 
@@ -30,6 +31,25 @@ describe('Survey Test', () => {
   it('Create Node Defs', nodeDefTest.createNodeDefsTest)
 
   it('Update Node Def', nodeDefTest.updateNodeDefTest)
+
+  // ==== NODE DEF EXPRESSIONS
+  it('Test "this" expression validation', nodeDefExpressionTest.validateThisExpressionTest)
+
+  it('Test binary expression validation', nodeDefExpressionTest.validateBinaryExpressionTest)
+
+  it('Test "parent()" expression validation', nodeDefExpressionTest.validateParentExpressionTest)
+
+  it('Test "node()" expression validation', nodeDefExpressionTest.validateNodeExpressionTest)
+
+  it('Test "sibling()" expression validation', nodeDefExpressionTest.validateSiblingExpressionTest)
+
+  it('Test syntax error validation', nodeDefExpressionTest.validateSyntaxErrorTest)
+
+  it('Test invalid node expression validation', nodeDefExpressionTest.validateInvalidNodeExpressionTest)
+
+  it('Test invalid node expression on attribute validation', nodeDefExpressionTest.validateInvalidNodeExpressionOnAttributeTest)
+
+  it('Test invalid sibling expression validation', nodeDefExpressionTest.validateInvaldSiblingExpressionTest)
 
 })
 
