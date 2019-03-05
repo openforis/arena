24e1f0c4fedb182d485f7d0c754c2226d8bbcbee
minot <cosimo.togna@fao.org>
2019-03-05 21:31:16 +0100

    removed dashboard module

--- webapp/appModules/appView/components/appModuleSwitch.js
+++ webapp/appModules/appView/components/appModuleSwitch.js
@@ -3,7 +3,6 @@ import { Route, Switch } from 'react-router'
 
 import DesignerView from '../../designer/designerView'
 import HomeView from '../../home/homeView'
-import DashboardView from '../../dashboard/dashboardView'
 import DataView from '../../data/dataView'
 
 import { appModules, appModuleUri } from '../../appModules'
@@ -14,7 +13,6 @@ const AppModuleHOC = Component => props => (
   </div>
 )
 const Home = AppModuleHOC(HomeView)
-const Dashboard = AppModuleHOC(DashboardView)
 const Designer = AppModuleHOC(DesignerView)
 const Data = AppModuleHOC(DataView)
 
@@ -23,7 +21,6 @@ const AppModuleSwitch = (props) => (
 
     <Switch location={props.location}>
       <Route path={appModuleUri(appModules.home)} component={Home}/>
-      <Route path={appModuleUri(appModules.dashboard)} component={Dashboard}/>
       <Route path={appModuleUri(appModules.designer)} component={Designer}/>
       <Route path={appModuleUri(appModules.data)} component={Data}/>
     </Switch>
