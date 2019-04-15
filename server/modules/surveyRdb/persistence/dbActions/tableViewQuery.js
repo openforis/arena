8141c86785808ed5df7ba92c06784834c8030a01
minotogna <mino.togna@gmail.com>
2019-04-15 16:32:48 +0200

    removed unused function

--- server/modules/surveyRdb/persistence/dbActions/tableViewQuery.js
+++ server/modules/surveyRdb/persistence/dbActions/tableViewQuery.js
@@ -28,21 +28,6 @@ const runSelect = async (surveyId, tableName, cols, offset, limit, filter = '', 
   )
 }
 
-const runSelectForEdit = async (surveyId, tableName, cols, offset, limit, filter = '', sort = '', client) => {
-  const schemaName = SchemaRdb.getName(surveyId)
-
-  return await client.any(`
-    SELECT ${cols.join(', ')} 
-    FROM ${schemaName}.${tableName}
-    ${R.isEmpty(filter) ? '' : `WHERE ${filter}`}
-    ${R.isEmpty(sort) ? '' : `ORDER BY ${sort} NULLS LAST`}
-    LIMIT ${limit}
-    OFFSET ${offset}
-    `,
-    []
-  )
-}
-
 const runCount = async (surveyId, tableName, filter = '', client) => {
   const schemaName = SchemaRdb.getName(surveyId)
 
@@ -95,7 +80,6 @@ const queryRootTableByRecordKeys = async (survey, recordUuid, client) => {
 
 module.exports = {
   runSelect,
-  runSelectForEdit,
   runCount,
   queryRootTableByRecordKeys,
 }
