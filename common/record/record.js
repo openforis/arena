d72aaa39de660a2aec8bb0b4489b686b1989fe91
minotogna <mino.togna@gmail.com>
2019-05-07 17:51:55 +0200

    extracted RecordUpdater

--- common/record/record.js
+++ common/record/record.js
@@ -3,13 +3,12 @@ const { uuidv4 } = require('./../uuid')
 
 const SurveyUtils = require('../survey/surveyUtils')
 const Validator = require('../validation/validator')
-const Node = require('./node')
 const User = require('../user/user')
 const RecordStep = require('./recordStep')
 
 const keys = require('./_internal/recordKeys')
-const NodesIndex = require('./_internal/recordNodesIndex')
 const RecordReader = require('./_internal/recordReader')
+const RecordUpdater = require('./_internal/recordUpdater')
 
 // ====== CREATE
 
@@ -19,83 +18,6 @@ const newRecord = (user, preview = false) => ({
   [keys.step]: RecordStep.getDefaultStep(),
   [keys.preview]: preview
 })
-
-// ====== UPDATE
-
-const assocNodes = nodes =>
-  record => {
-    // exclude dirty nodes currently being edited by the user
-
-    const dirtyNodes = RecordReader.findNodesIndexed(Node.isDirty)(record)
-
-    const nodesToUpdate = R.pipe(
-      R.filter(
-        n => {
-          const dirtyNode = R.prop(Node.getUuid(n), dirtyNodes)
-          return !dirtyNode ||
-            Node.isDirty(n) ||
-            R.equals(Node.getValue(dirtyNode), Node.getValue(n)) ||
-            Node.isValueBlank(dirtyNode) && Node.isDefaultValueApplied(n)
-        }),
-      R.map(
-        R.omit([Node.keys.updated, Node.keys.created])
-      )
-    )(nodes)
-
-    const nodesDeletedArray = R.pipe(
-      R.filter(Node.isDeleted),
-      R.values
-    )(nodes)
-
-    return R.pipe(
-      RecordReader.getNodes,
-      R.mergeLeft(nodesToUpdate),
-      mergedNodes => R.assoc(keys.nodes, mergedNodes)(record),
-      deleteNodes(nodesDeletedArray),
-      NodesIndex.addNodes(nodesToUpdate)
-    )(record)
-  }
-
-const deleteNodes = nodesDeletedArray =>
-  record => R.reduce(
-    (updatedRecord, node) => deleteNode(node)(updatedRecord),
-    record,
-    nodesDeletedArray
-  )
-
-// ====== DELETE
-const deleteNode = node =>
-  record => {
-    const nodeUuid = Node.getUuid(node)
-
-    // 1. remove entity children recursively
-    const children = RecordReader.getNodeChildren(node)(record)
-
-    let recordUpdated = R.reduce(
-      (recordAcc, child) => deleteNode(child)(recordAcc),
-      record,
-      children
-    )
-
-    // 2. update validation
-    recordUpdated = R.pipe(
-      Validator.getValidation,
-      Validator.dissocFieldValidation(nodeUuid),
-      newValidation => Validator.assocValidation(newValidation)(recordUpdated)
-    )(recordUpdated)
-
-    // 3. remove node from index
-    recordUpdated = NodesIndex.removeNode(node)(recordUpdated)
-
-    // 4. remove node from record
-    recordUpdated = R.pipe(
-      RecordReader.getNodes,
-      R.dissoc(nodeUuid),
-      newNodes => R.assoc(keys.nodes, newNodes, recordUpdated),
-    )(recordUpdated)
-
-    return recordUpdated
-  }
 
 module.exports = {
   keys,
@@ -115,14 +37,13 @@ module.exports = {
   getRootNode: RecordReader.getRootNode,
   getNodesByDefUuid: RecordReader.getNodesByDefUuid,
 
-
   // ==== hierarchy
   getParentNode: RecordReader.getParentNode,
   getAncestorsAndSelf: RecordReader.getAncestorsAndSelf,
   getAncestorByNodeDefUuid: RecordReader.getAncestorByNodeDefUuid,
 
-  getNodeSiblingsByDefUuid:RecordReader.getNodeSiblingsByDefUuid,
-  getNodeChildrenByDefUuid:RecordReader.getNodeChildrenByDefUuid,
+  getNodeSiblingsByDefUuid: RecordReader.getNodeSiblingsByDefUuid,
+  getNodeChildrenByDefUuid: RecordReader.getNodeChildrenByDefUuid,
   getNodeChildByDefUuid: RecordReader.getNodeChildByDefUuid,
 
   // ==== dependency
@@ -132,14 +53,14 @@ module.exports = {
   getDependentCodeAttributes: RecordReader.getDependentCodeAttributes,
 
   // ====== UPDATE
-  assocNodes,
-  assocNode: node => assocNodes({ [Node.getUuid(node)]: node }),
-  mergeNodeValidations: Validator.mergeValidation,
+  assocNodes: RecordUpdater.assocNodes,
+  assocNode: RecordUpdater.assocNode,
 
   // ====== DELETE
 
-  deleteNode,
+  deleteNode: RecordUpdater.deleteNode,
 
   // ====== VALIDATION
+  mergeNodeValidations: Validator.mergeValidation,
   getValidation: Validator.getValidation,
 }
