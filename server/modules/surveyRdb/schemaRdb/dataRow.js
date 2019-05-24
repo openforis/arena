b8d2663862b9a00931c16b86d4355e5c38b20588
minotogna <mino.togna@gmail.com>
2019-05-24 17:35:51 +0200

    optimized surveyRdbGenerator

--- server/modules/surveyRdb/schemaRdb/dataRow.js
+++ server/modules/surveyRdb/schemaRdb/dataRow.js
@@ -1,36 +1,30 @@
 const R = require('ramda')
-const Promise = require('bluebird')
 
 const NodeDef = require('../../../../common/survey/nodeDef')
-const Record = require('../../../../common/record/record')
-const Node = require('../../../../common/record/node')
 const DataCol = require('./dataCol')
 
-const getNodeCol = (nodeDefCol, record, nodeRow) => {
+const getNodeCol = (nodeDefCol, nodeRow) => {
 
   const nodeDefUuidCol = NodeDef.getUuid(nodeDefCol)
-  const nodeDefUuidRow = Node.getNodeDefUuid(nodeRow)
+  const nodeDefUuidRow = nodeRow['node_def_uuid']
 
   // attribute column in multiple attribute table (value of its own table)
   const nodeCol = nodeDefUuidRow === nodeDefUuidCol ?
     nodeRow :
-    R.pipe(
-      Record.getNodeChildByDefUuid(nodeRow, nodeDefUuidCol),
-      R.defaultTo({})
-    )(record)
+    R.pathOr({}, ['children', nodeDefUuidCol], nodeRow)
 
   return nodeCol
 }
 
-const getValues = async (survey, nodeDefRow, record, nodeRow, nodeDefColumns) => {
-  const values = await Promise.all(nodeDefColumns.map(async nodeDefCol => {
-      const nodeCol = getNodeCol(nodeDefCol, record, nodeRow)
+const getValues = (survey, nodeDefRow, nodeRow, nodeDefColumns) => {
+  const values = nodeDefColumns.map(nodeDefCol => {
+    const nodeCol = getNodeCol(nodeDefCol, nodeRow)
 
-      const nodeColValues = await DataCol.getValues(survey, nodeDefCol, nodeCol)
+    const nodeColValues = DataCol.getValues(survey, nodeDefCol, nodeCol)
 
-      return nodeColValues
-    })
-  )
+    return nodeColValues
+  })
+
   return R.flatten(values)
 }
 
