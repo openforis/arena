5351dd16cd05e82276308c0a216b2287c292c911
minotogna <mino.togna@gmail.com>
2019-06-26 14:27:54 +0200

    added child modules in sidebar expanded view

--- webapp/loggedin/appModules.js
+++ webapp/loggedin/appModules.js
@@ -1,49 +1,87 @@
 const app = 'app'
 
-export const appModuleUri = (module = appModules.home) => `/${[app, module].join('/')}/`
+export const appModuleUri = (module = appModules.home) => `/${[app, module.path].join('/')}/`
 
+//==== App Root modules
 export const appModules = {
-  home: 'home',
-  designer: 'designer',
-  data: 'data',
+  home: {
+    key: 'home',
+    path: 'home',
+    icon: 'home2',
+  },
+  designer: {
+    key: 'designer',
+    path: 'designer',
+    icon: 'quill',
+  },
+  data: {
+    key: 'data',
+    path: 'data',
+    icon: 'table2',
+  },
   // analysis: 'analysis',
   // users: 'users',
 }
 
+//==== Inner modules
+
 export const homeModules = {
-  dashboard: `${appModules.home}/dashboard`,
-
-  surveyInfo: `${appModules.home}/surveyInfo`,
-
-  surveyList: `${appModules.home}/surveys`,
-
-  surveyNew: `${appModules.home}/surveyNew`,
-
-  collectImportReport: `${appModules.home}/collectImportReport`,
+  dashboard: {
+    key: 'dashboard',
+    path: `${appModules.home.path}/dashboard`,
+  },
+  surveyInfo: {
+    key: 'surveyInfo',
+    path: `${appModules.home.path}/surveyInfo`,
+  },
+  surveyList: {
+    key: 'surveys',
+    path: `${appModules.home.path}/surveys`,
+  },
+  surveyNew: {
+    key: 'surveyNew',
+    path: `${appModules.home.path}/surveyNew`,
+  },
+  collectImportReport: {
+    key: 'collectImportReport',
+    path: `${appModules.home.path}/collectImportReport`,
+  },
 }
 
 export const designerModules = {
-
-  formDesigner: `${appModules.designer}/formDesigner`,
-
-  surveyHierarchy: `${appModules.designer}/surveyHierarchy`,
-
-  recordPreview: `${appModules.designer}/formDesigner/preview`,
-
-  categories: `${appModules.designer}/categories`,
-
-  taxonomies: `${appModules.designer}/taxonomies`,
-
+  formDesigner: {
+    key: 'formDesigner',
+    path: `${appModules.designer.path}/formDesigner`,
+  },
+  surveyHierarchy: {
+    key: 'surveyHierarchy',
+    path: `${appModules.designer.path}/surveyHierarchy`,
+  },
+  recordPreview: {
+    key: 'preview',
+    path: `${appModules.designer.path}/preview`,
+  },
+  categories: {
+    key: 'categories',
+    path: `${appModules.designer.path}/categories`,
+  },
+  taxonomies: {
+    key: 'taxonomies',
+    path: `${appModules.designer.path}/taxonomies`,
+  },
 }
 
 export const dataModules = {
-  // edit record form
-  record: `${appModules.data}/record`,
-
-  // records list
-  records: `${appModules.data}/records`,
-
-  dataVis: `${appModules.data}/dataVis`,
-
+  record: {
+    key: 'record',
+    path: `${appModules.data.path}/record`,
+  },
+  records: {
+    key: 'records',
+    path: `${appModules.data.path}/records`,
+  },
+  dataVis: {
+    key: 'dataVis',
+    path: `${appModules.data.path}/dataVis`,
+  },
 }
-
--- webapp/loggedin/appSideBar/appSideBarModules.js
+++ webapp/loggedin/appSideBar/appSideBarModules.js
@@ -2,89 +2,97 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import * as R from 'ramda'
 
-import { appModuleUri, appModules } from '../appModules'
+import { appModuleUri, appModules, designerModules, dataModules } from '../appModules'
 
-const modules = [
-  {
-    module: appModules.home,
-    icon: 'home2',
-    label: 'Home',
-  },
-  {
-    module: appModules.designer,
-    icon: 'quill',
-    label: 'Designer',
-  },
-  {
-    module: appModules.data,
-    icon: 'table2',
-    label: 'Data',
-  }
-  // ,
-  // {
-  //   module: appModules.analysis,
-  //   icon: 'calculator',
-  //   label: 'Analysis',
-  //   disabled: true,
-  // },
-  // {
-  //   module: appModules.users,
-  //   icon: 'users',
-  //   label: 'Users',
-  //   disabled: true,
-  // },
+//==== Modules hierarchy for appSidebar
+const getModule = (module, children = null) => ({
+  key: module.key,
+  uri: appModuleUri(module),
+  icon: module.icon,
+  children: children
+    ? children.map(childModule => getModule(childModule))
+    : null
+})
+
+export const modulesHierarchy = [
+  getModule(appModules.home),
+  getModule(appModules.designer, [designerModules.formDesigner, designerModules.surveyHierarchy, designerModules.categories, designerModules.taxonomies]),
+  getModule(appModules.data, [dataModules.records, dataModules.dataVis]),
 ]
 
-const AppSideBarModule = (props) => {
-  const {
-    pathname, module, surveyInfo,
-    //module props
-    icon, label, sideBarOpened = false
-  } = props
+const ModuleLink = ({ module, showLabel, disabled, className = '' }) => (
+  <Link
+    className={`app-sidebar__module-btn text-uppercase ${className}`}
+    to={module.uri}
+    aria-disabled={disabled}>
+    {
+      module.icon &&
+      <span className={`icon icon-${module.icon} icon-16px${showLabel ? ' icon-left-2x' : ''}`}></span>
+    }
+    {
+      showLabel &&
+      <span>{module.key}</span>
+    }
 
-  const active = R.startsWith(appModuleUri(module), pathname)
-  const requireSurvey = module !== appModules.home
+  </Link>
+)
+
+const AppSideBarModule = (props) => {
+  const { pathname, module, surveyInfo, sideBarOpened = false } = props
+
+  const active = R.startsWith(module.uri, pathname)
+  const isModuleHome = module.key === appModules.home.key
+
+  const requireSurvey = !isModuleHome
+  const disabled = requireSurvey && (R.isEmpty(surveyInfo) || R.isNil(surveyInfo))
 
   return (
-    <React.Fragment>
+    <div className={`app-sidebar__module${active ? ' active' : ''}`}>
 
-      <div className={`app-sidebar__module${active ? ' active' : ''}`}>
-
-        <Link
-          className="app-sidebar__module-btn text-uppercase"
-          to={appModuleUri(module)}
-          aria-disabled={requireSurvey && (R.isEmpty(surveyInfo) || R.isNil(surveyInfo))}>
-          <span className={`icon icon-${icon} icon-16px${sideBarOpened ? ' icon-left-2x' : ''}`}></span>
-          {
-            sideBarOpened &&
-            <span>{label}</span>
-          }
-
-        </Link>
-      </div>
+      <ModuleLink
+        module={module}
+        disabled={disabled}
+        showLabel={sideBarOpened}
+      />
 
       {
-        module === appModules.home &&
-        <div className="separator-of"></div>
+        sideBarOpened && module.children && module.children.map(childModule => (
+          <ModuleLink
+            key={childModule.key}
+            module={childModule}
+            disabled={disabled}
+            showLabel={sideBarOpened}
+            className="app-sidebar__module-child-btn"
+          />
+        ))
       }
 
-    </React.Fragment>
+    </div>
   )
 }
 
 const AppSideBarModules = ({ pathname, surveyInfo, sideBarOpened }) => (
   <div className="app-sidebar__modules">
     {
-      modules.map((m, i) => (
-        <AppSideBarModule key={i}
-                          {...m}
-                          pathname={pathname}
-                          surveyInfo={surveyInfo}
-                          sideBarOpened={sideBarOpened}
-        />
+      modulesHierarchy.map((module, i) => (
+        <React.Fragment>
+          <AppSideBarModule
+            key={module.key}
+            module={module}
+            pathname={pathname}
+            surveyInfo={surveyInfo}
+            sideBarOpened={sideBarOpened}
+          />
+
+          {
+            i !== modulesHierarchy.length - 1 &&
+            <div className="separator-of"></div>
+          }
+
+        </React.Fragment>
       ))
     }
-    <div className="separator-of"></div>
+
   </div>
 )
 
