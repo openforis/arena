f8f68db331cfae65663e6cb387e0c525d768d762
minotogna <cosimo.togna@fao.org>
2019-08-12 17:41:10 +0200

    added logo / user placeholder to appHeader

--- webapp/loggedin/appHeader/appHeader.js
+++ webapp/loggedin/appHeader/appHeader.js
@@ -10,6 +10,17 @@ const AppHeader = () => {
   return (
     <div className="app-header">
 
+      <div className="app-header__logo">
+        <img src="/img/of-logo-small.png"/>
+      </div>
+
+      <div className="app-header__user">
+        <img src="https://cdn0.iconfinder.com/data/icons/user-pictures/100/unknown2-512.png"/>
+        <button className="btn btn-transparent">
+          <span className="icon icon-ctrl"/>
+        </button>
+      </div>
+
     </div>
   )
 
