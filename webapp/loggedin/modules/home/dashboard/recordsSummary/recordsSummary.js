6b810e54a004f588be9e4403bc03c79a2c0aac78
minotogna <mino.togna@gmail.com>
2019-08-20 13:32:27 +0200

    rendering recordsSummary if surveyInfo is valid

--- webapp/loggedin/modules/home/dashboard/recordsSummary/recordsSummary.js
+++ webapp/loggedin/modules/home/dashboard/recordsSummary/recordsSummary.js
@@ -6,31 +6,43 @@ import { connect } from 'react-redux'
 import useI18n from '../../../../../commonComponents/useI18n'
 import RecordsSummaryChart from './chart/recordsSummaryChart'
 
+import Survey from '../../../../../../common/survey/survey'
+import * as SurveyState from '../../../../../survey/surveyState'
+
 import { fetchRecordsAddedSummary } from './actions'
 
 const RecordsSummary = props => {
-  const { fetchRecordsAddedSummary } = props
+  const { surveyInfo, fetchRecordsAddedSummary } = props
+  const surveyInfoValid = Survey.isValid(surveyInfo)
 
   const i18n = useI18n()
 
   useEffect(() => {
-    fetchRecordsAddedSummary()
+    if (surveyInfoValid) {
+      fetchRecordsAddedSummary()
+    }
   }, [])
 
-  return (
-    <div className="home-dashboard__records-summary">
+  return surveyInfoValid
+    ? (
+      <div className="home-dashboard__records-summary">
 
-      <div>
-        <h6 className="text-uppercase">
-          {i18n.t('homeView.recordsSummary.newRecords')}
-        </h6>
+        <div>
+          <h6 className="text-uppercase">
+            {i18n.t('homeView.recordsSummary.newRecords')}
+          </h6>
+        </div>
+
+
+        <RecordsSummaryChart/>
+
       </div>
-
-
-      <RecordsSummaryChart/>
-
-    </div>
-  )
+    )
+    : null
 }
 
-export default connect(null, { fetchRecordsAddedSummary })(RecordsSummary)
+const mapStateToProps = state => ({
+  surveyInfo: SurveyState.getSurveyInfo(state),
+})
+
+export default connect(mapStateToProps, { fetchRecordsAddedSummary })(RecordsSummary)
