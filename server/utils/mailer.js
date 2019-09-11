dddd65df72d4aa72537d9290f9fd4c0bc7968f0d
Stefano Giaccio <stefano.giaccio@gmail.com>
2019-09-11 16:52:52 +0200

    Using i18nFactory server side

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -1,13 +1,13 @@
 const aws = require('../system/aws')
 
-const i18n = require('../system/i18n')
+const i18nFactory = require('../../common/i18n/i18nFactory')
 
 const sendEmail = async (to, msgKey, msgParams = {}, lang) => {
-  i18n.translate(async t => {
-    const mailSubject = t(`${msgKey}.subject`)
-    const msgBody = t(`${msgKey}.body`, msgParams)
-    await aws.sendEmail(process.env.ADMIN_EMAIL, to, mailSubject, msgBody)
-  })
+  const i18n = await i18nFactory.createI18nPromise(lang)
+  const mailSubject = i18n.t(`${msgKey}.subject`)
+  const msgBody = i18n.t(`${msgKey}.body`, msgParams)
+
+  await aws.sendEmail(process.env.ADMIN_EMAIL, to, mailSubject, msgBody)
 }
 
 module.exports = {
