c0962bb12bba43a8fdc555e8a05dc996bbc28040
Stefano Giaccio <stefano.giaccio@gmail.com>
2019-09-20 15:50:32 +0200

    Handling exceptions when deleting expired jwt tokens and and preview records

--- server/system/schedulers/expiredJwtTokensCleanup.js
+++ server/system/schedulers/expiredJwtTokensCleanup.js
@@ -8,13 +8,17 @@ const init = () => {
   const logger = Log.getLogger('ExpiredJwtTokensCleanup')
 
   schedule.scheduleJob('0 1 * * *', async () => {
-    logger.info('Deleting expired jwt tokens from the token blacklist')
+    try {
+      logger.info('Deleting expired jwt tokens from the token blacklist')
 
-    const timeSeconds = Math.floor(new Date().getTime() / 1000)
+      const timeSeconds = Math.floor(new Date().getTime() / 1000)
 
-    // Give one hour margin
-    const deleted = await AuthService.deleteExpiredJwtTokens(timeSeconds - 60 * 60)
-    logger.debug(`${deleted.length} expired jwt tokens deleted`)
+      // Give one hour margin
+      const deleted = await AuthService.deleteExpiredJwtTokens(timeSeconds - 60 * 60)
+      logger.debug(`${deleted.length} expired jwt tokens deleted`)
+    } catch (err) {
+      logger.error(`Error deleting expired jwt tokens: ${err.toString()}`)
+    }
   })
 }
 
--- server/system/schedulers/recordPreviewCleanup.js
+++ server/system/schedulers/recordPreviewCleanup.js
@@ -6,24 +6,32 @@ const RecordService = require('../../modules/record/service/recordService')
 
 const initSchedule = () =>
   schedule.scheduleJob('0 0 * * *', () => {
-    logger.debug('Deleting stale preview records')
+    try {
+      logger.debug('Deleting stale preview records')
 
-    const date = new Date()
-    date.setHours(date.getHours() - 24)
+      const date = new Date()
+      date.setHours(date.getHours() - 24)
 
-    const stalePreviewRecords = RecordService.getStalePreviewRecordUuids(date)
+      const stalePreviewRecords = RecordService.getStalePreviewRecordUuids(date)
 
-    Promise.all(stalePreviewRecords.map(({ user, surveyId, recordUuid }) =>
-      RecordService.checkOut(user, surveyId, recordUuid)
-    ))
+      Promise.all(stalePreviewRecords.map(({ user, surveyId, recordUuid }) =>
+        RecordService.checkOut(user, surveyId, recordUuid)
+      ))
 
-    logger.debug(`${stalePreviewRecords.length} stale preview records deleted`)
+      logger.debug(`${stalePreviewRecords.length} stale preview records deleted`)
+    } catch (err) {
+      logger.error(`Error deleting stale preview records: ${err.toString()}`)
+    }
   })
 
 const init = async () => {
-  logger.debug('Deleting stale preview records')
-  const count = await RecordService.deleteRecordsPreview()
-  logger.debug(`${count} stale preview records deleted`)
+  try {
+    logger.debug('Deleting stale preview records')
+    const count = await RecordService.deleteRecordsPreview()
+    logger.debug(`${count} stale preview records deleted`)
+  } catch (err) {
+    logger.error(`Error deleting stale preview records: ${err.toString()}`)
+  }
 
   initSchedule()
 }
