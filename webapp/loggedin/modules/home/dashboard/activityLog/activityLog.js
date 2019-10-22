2c06daba6760c225ef667410d228b7c8f69f4f05
minotogna <mino.togna@gmail.com>
2019-10-22 14:33:22 +0200

    Home: added activity log item placeholder

--- webapp/loggedin/modules/home/dashboard/activityLog/activityLog.js
+++ webapp/loggedin/modules/home/dashboard/activityLog/activityLog.js
@@ -1,10 +1,19 @@
 import './activityLog.scss'
 
 import React from 'react'
+import { connect } from 'react-redux'
+
+import User from '../../../../../../core/user/user'
+import DateUtils from '../../../../../../core/dateUtils'
 
 import { useI18n } from '../../../../../commonComponents/hooks'
+import ProfilePicture from '../../../../../commonComponents/profilePicture'
+
+import * as AppState from '../../../../../app/appState'
 
 const ActivityLog = props => {
+
+  const { user } = props
 
   const i18n = useI18n()
 
@@ -15,9 +24,38 @@ const ActivityLog = props => {
         {i18n.t('homeView.activityLog.recentActivity')}
       </div>
 
+      <div className="activity-log__content">
+
+        {/*TODO: iterate over items*/}
+        <div className="activity-log__content-item">
+          <div className="activity">
+            <ProfilePicture userUuid={User.getUuid(user)} thumbnail={true}/> created the survey
+          </div>
+          <div className="date">
+            {DateUtils.getRelativeDate(i18n, new Date())}
+          </div>
+        </div>
+
+        <div className="activity-log__content-item-sep"></div>
+        <div className="activity-log__content-item">
+          <div className="activity">
+            <ProfilePicture userUuid={User.getUuid(user)} thumbnail={true}/> created the survey
+          </div>
+          <div className="date">
+            {DateUtils.getRelativeDate(i18n, new Date())}
+          </div>
+        </div>
+        <div className="activity-log__content-item-sep"></div>
+
+      </div>
+
     </div>
   )
 
 }
 
-export default ActivityLog
+const mapStateToProps = state => ({
+  user: AppState.getUser(state)
+})
+
+export default connect(mapStateToProps)(ActivityLog)
