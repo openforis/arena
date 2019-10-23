1dbbe8a03015b86895d9d5bcbd865b6936406de9
minotogna <mino.togna@gmail.com>
2019-10-23 12:25:56 +0200

    AppError: refactored and fixed appError state

--- webapp/app/appErrors/appErrorsState.js
+++ webapp/app/appErrors/appErrorsState.js
@@ -1,29 +1,24 @@
 import * as R from 'ramda'
 
-import * as AppState from '../appState'
-
 export const stateKey = 'appErrors'
 
-export const keys = {
-  errors: 'errors',
-}
+const getState = R.propOr({}, stateKey)
 
 // ==== APP ERRORS
 
 const _getAppErrors = R.pipe(
-  R.prop(keys.errors),
   R.values,
   R.sort((a, b) => +b.id - +a.id)
 )
 
-export const getAppErrors = R.pipe(AppState.getState, _getAppErrors)
+export const getAppErrors = R.pipe(getState, _getAppErrors)
 
 export const assocAppError = error => state => R.pipe(
   _getAppErrors,
   R.head,
   R.defaultTo({ id: -1 }),
   last => 1 + last.id,
-  id => R.assocPath([keys.errors, id + ''], { id, ...error })(state)
+  id => R.assoc(String(id), { id, ...error })(state)
 )(state)
 
-export const dissocAppError = error => R.dissocPath([keys.errors, error.id + ''])
+export const dissocAppError = error => R.dissoc(String(error.id))
