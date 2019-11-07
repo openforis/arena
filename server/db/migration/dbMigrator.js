b5724ae7b92861f40c1d9f9c397804863dc44956
Elias Kunnas <elias@kunnas.com>
2019-11-07 16:55:33 +0200

    db-migrate: Add workaround for EventEmitter leak

--- server/db/migration/dbMigrator.js
+++ server/db/migration/dbMigrator.js
@@ -31,8 +31,12 @@ const migrateSchema = async (schema = publicSchema) => {
   const migrateOptions = {
     config: R.clone(config),
     cwd: `${path.join(__dirname, migrationsFolder)}`,
-    env
-  }
+    env,
+
+    // Required to work around an EventEmitter leak bug.
+    // See: https://github.com/db-migrate/node-db-migrate/issues/421
+    throwUncatched: true,
+}
 
   migrateOptions.config[env].schema = schema
 
