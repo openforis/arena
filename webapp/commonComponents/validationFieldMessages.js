73544298e4a46a6338b882e14da69853afffdde6
Stefano Giaccio <stefano.giaccio@gmail.com>
2019-11-22 10:32:44 +0100

    Improving layout

--- webapp/commonComponents/validationFieldMessages.js
+++ webapp/commonComponents/validationFieldMessages.js
@@ -1,3 +1,5 @@
+import './validationFieldMessages.scss'
+
 import React from 'react'
 import { connect } from 'react-redux'
 
@@ -22,11 +24,22 @@ const getErrorText = i18n => error =>
     ? ValidationResult.getMessage(i18n.lang)(error)
     : i18n.t(ValidationResult.getKey(error), ValidationResult.getParams(error))
 
-const getValidationErrorMessages = i18n => validation => R.pipe(
+const getErrorMessages = i18n => R.pipe(
   Validation.getErrors,
-  R.concat(Validation.getWarnings(validation)),
   R.map(getErrorText(i18n)),
-)(validation)
+  R.join(', '),
+  text => text ? ['error', text] : []
+)
+
+const getWarningMessages = i18n => R.pipe(
+  Validation.getWarnings,
+  R.map(getErrorText(i18n)),
+  R.join(', '),
+  text => text ? ['warning', text] : []
+)
+
+const getValidationErrorMessages = i18n => validation =>
+  R.concat(getWarningMessages(i18n)(validation))(getErrorMessages(i18n)(validation))
 
 const getValidationFieldErrorMessage = (i18n, field) => R.pipe(
   getValidationErrorMessages(i18n),
@@ -38,8 +51,8 @@ const getValidationFieldErrorMessage = (i18n, field) => R.pipe(
         { field }
       )
     ),
-    R.join(', ')
-  )
+    R.identity
+  ),
 )
 
 const getValidationFieldMessages = (i18n, showKeys = true, survey = {}) => validation => R.pipe(
@@ -47,31 +60,35 @@ const getValidationFieldMessages = (i18n, showKeys = true, survey = {}) => valid
   Object.entries,
   // extract invalid fields error messages
   R.map(([field, fieldValidation]) => {
+    let k = field
+    let v = fieldValidation
+
     // if it's a childrenCount validation, extract the actual minCount or maxCount validation
     // and add the nodeDefUuid name as param
     if (field === RecordValidations.keys.childrenCount) {
-      const [nodeDefUuid, nodeValidation] = Object.entries(Validation.getFieldValidations(fieldValidation))[0]
-      const [k, v] = Object.entries(Validation.getFieldValidations(nodeValidation))[0]
+      const [nodeDefUuid, nodeValidation] = Object.entries(Validation.getFieldValidations(v))[0];
+      [k, v] = Object.entries(Validation.getFieldValidations(nodeValidation))[0]
       const nodeDef = Survey.getNodeDefByUuid(nodeDefUuid)(survey)
 
       v.errors[0].params.nodeDefName = NodeDef.getLabel(nodeDef, i18n.lang)
-      return `${showKeys ? `${i18n.t(k)}: ` : ''}${getValidationFieldErrorMessage(i18n, k)(v)}`
-    } else {
-      return `${showKeys ? `${i18n.t(field)}: ` : ''}${getValidationFieldErrorMessage(i18n, field)(fieldValidation)}`
     }
+
+    const [type, message] = getValidationFieldErrorMessage(i18n, k)(v)
+    return [type, `${showKeys ? `${i18n.t(k)}: ` : ''}${message}`]
   }),
   messages => R.pipe(
     getValidationErrorMessages(i18n),
     R.concat(messages)
-  )(validation)
+  )(validation),
 )(validation)
 
 const getValidationFieldMessagesHTML = (i18n, showKeys = true, survey) =>
   validation =>
     R.pipe(
       getValidationFieldMessages(i18n, showKeys, survey),
+      x => console.log('x', x) || x,
       R.addIndex(R.map)(
-        (msg, i) => <Markdown key={i} source={msg}/>
+        ([type, message], i) => <Markdown className={`validation_field_message ${type}`} key={i} source={message}/>
       )
     )(validation)
 
