98f2fa72ca5a408347b383ba3f118961c8f8f1f6
Elias Kunnas <elias@kunnas.com>
2019-11-26 14:29:57 +0200

    shellcheck shell scripts

--- server/db/migration/create-migration.sh
+++ server/db/migration/create-migration.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#! /usr/bin/env bash
+set -e
 
-if [ -z "$1" ]
+if [[ -z $1 ]]
   then
     echo "Give the name of the migration as parameter"
     exit 2
@@ -9,4 +10,8 @@ fi
 type=${2:-public}
 
 . .env
-db-migrate --config server/db/migration/migrationConfig.js --migrations-dir server/db/migration/$type/migrations create $1 --sql-file
+
+db-migrate \
+  --config server/db/migration/migrationConfig.js \
+  --migrations-dir server/db/migration/"$type"/migrations \
+  create "$1" --sql-file
