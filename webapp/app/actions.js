ee89f2d142d01a71b3e86581aea6c5a0dd9b74dd
Stefano Ricci <SteRiccio@users.noreply.github.com>
2019-12-30 10:35:30 +0100

    User login: fixed initApp action

--- webapp/app/actions.js
+++ webapp/app/actions.js
@@ -17,27 +17,19 @@ export const throwSystemError = error => dispatch => dispatch({ type: systemErro
 
 export const initApp = () => async dispatch => {
   const i18n = await i18nFactory.createI18nPromise('en')
-  try {
-    const { user, survey } = await getUserSurvey()
-    dispatch({
-      type: appPropsChange,
-      status: AppState.appStatus.ready,
-      i18n,
-      user,
-      survey,
-    })
-  } catch (error) {
-    dispatch({
-      type: appPropsChange,
-      status: AppState.appStatus.ready,
-      i18n,
-    })
-  }
+  const { user, survey } = await _fetchUserAndSurvey()
+  dispatch({
+    type: appPropsChange,
+    status: AppState.appStatus.ready,
+    i18n,
+    user,
+    survey,
+  })
 }
 
 // ====== USER
 
-const getUserSurvey = async () => {
+const _fetchUserAndSurvey = async () => {
   const {
     data: { user, survey },
   } = await axios.get('/auth/user')
@@ -45,7 +37,7 @@ const getUserSurvey = async () => {
 }
 
 export const initUser = () => async dispatch => {
-  const { user, survey } = await getUserSurvey()
+  const { user, survey } = await _fetchUserAndSurvey()
   dispatch({ type: appPropsChange, user, survey })
 }
 
