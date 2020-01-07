0abdefe2fb896cb8bf510c7aaafd99c0c6fb7731
minotogna <mino.togna@gmail.com>
2020-01-07 15:05:28 +0100

    LoginView: removed unused imports

--- webapp/guest/login/loginView.js
+++ webapp/guest/login/loginView.js
@@ -2,7 +2,7 @@ import './loginView.scss'
 
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Link, useHistory } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 import { useI18n, useFormObject } from '@webapp/commonComponents/hooks'
 import NotLoggedInView from '@webapp/guest/components/notLoggedInView'
