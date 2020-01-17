39e7c90c951839b6b1e8fcb4c5b9f9eb111958a3
Stefano Ricci <SteRiccio@users.noreply.github.com>
2020-01-17 17:21:19 +0100

    Virtual entity: include virtual entity variables in expression editor (WIP)

--- webapp/commonComponents/expression/expressionVariables.js
+++ webapp/commonComponents/expression/expressionVariables.js
@@ -37,14 +37,14 @@ const getSqlVariables = (nodeDef, lang) => {
 
 const getChildDefVariables = (survey, nodeDefContext, nodeDefCurrent, mode, lang) =>
   R.pipe(
-    Survey.getNodeDefChildren(nodeDefContext),
+    Survey.getNodeDefChildren(nodeDefContext, Boolean(nodeDefContext) && NodeDef.isAnalysis(nodeDefContext)),
     R.map(childDef => {
       if (!Expression.isValidExpressionType(childDef)) {
         return null
       }
 
       if (
-        nodeDefCurrent !== null &&
+        Boolean(nodeDefCurrent) &&
         Survey.isNodeDefDependentOn(NodeDef.getUuid(childDef), NodeDef.getUuid(nodeDefCurrent))(survey)
       ) {
         // Exclude nodes that reference the current one
