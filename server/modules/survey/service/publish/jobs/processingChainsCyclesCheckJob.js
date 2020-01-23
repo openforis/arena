7e6b74d70c2b1f310daa2c71f7ec70c28ff6bdce
minotogna <mino.togna@gmail.com>
2020-01-23 17:18:50 +0100

    ProcessingChainsCyclesCheckJob: update nodeDef analysis cycles

--- server/modules/survey/service/publish/jobs/processingChainsCyclesCheckJob.js
+++ server/modules/survey/service/publish/jobs/processingChainsCyclesCheckJob.js
@@ -4,10 +4,9 @@ import Job from '@server/job/job'
 
 import * as Survey from '@core/survey/survey'
 
-import * as SurveyManager from '../../../manager/surveyManager'
-import * as NodeDefManager from '../../../../nodeDef/manager/nodeDefManager'
-import * as RecordManager from '../../../../record/manager/recordManager'
-import * as UserManager from '../../../../user/manager/userManager'
+import * as SurveyManager from '@server/modules/survey/manager/surveyManager'
+import * as NodeDefManager from '@server/modules/nodeDef/manager/nodeDefManager'
+import * as ProcessingChainManager from '@server/modules/analysis/manager/processingChainManager'
 
 export default class ProcessingChainsCyclesCheckJob extends Job {
   constructor(params) {
@@ -17,29 +16,35 @@ export default class ProcessingChainsCyclesCheckJob extends Job {
   async execute() {
     this.total = 4
 
-    // Const {cycleKeys, cycleKeysDeleted} = await _getCycleKeys()
+    const { cycleKeys, cycleKeysDeleted = [] } = await this._getCycleKeys()
     // 1. set all survey cycles to all nodeDef analysis
+    await NodeDefManager.updateNodeDefAnalysisCycles(this.surveyId, cycleKeys, this.tx)
+    this.incrementProcessedItems()
 
-    // 2. dissoc deleted survey  cycles from processing chains
+    // If(!R.isEmpty(cycleKeysDeleted)){
+    //   await ProcessingChainManager.
+    // }
+
+    // 2. dissoc deleted survey cycles from processing chains
 
     // 3. delete processing chains with no cycles
 
     // 4. delete analysis nodeDef if they are not used
   }
 
-  // Async _getCycleKeys() {
-  //   const survey = await SurveyManager.fetchSurveyById(this.surveyId, true, false, this.tx)
-  //   const surveyInfo = Survey.getSurveyInfo(survey)
-  //   const cycleKeys = Survey.getCycleKeys(surveyInfo)
-  //   if (Survey.isPublished(surveyInfo)) {
-  //     const surveyPrev = await SurveyManager.fetchSurveyById(this.surveyId, false, false, this.tx)
-  //     const surveyInfoPrev = Survey.getSurveyInfo(surveyPrev)
-  //     const cycleKeysDeleted = R.difference(Survey.getCycleKeys(surveyInfoPrev), cycleKeys)
-  //     return {cycleKeys, cycleKeysDeleted}
-  //   }
-  //
-  //   return {cycleKeys}
-  // }
+  async _getCycleKeys() {
+    const survey = await SurveyManager.fetchSurveyById(this.surveyId, true, false, this.tx)
+    const surveyInfo = Survey.getSurveyInfo(survey)
+    const cycleKeys = Survey.getCycleKeys(surveyInfo)
+    if (Survey.isPublished(surveyInfo)) {
+      const surveyPrev = await SurveyManager.fetchSurveyById(this.surveyId, false, false, this.tx)
+      const surveyInfoPrev = Survey.getSurveyInfo(surveyPrev)
+      const cycleKeysDeleted = R.difference(Survey.getCycleKeys(surveyInfoPrev), cycleKeys)
+      return { cycleKeys, cycleKeysDeleted }
+    }
+
+    return { cycleKeys }
+  }
 }
 
 ProcessingChainsCyclesCheckJob.type = 'ProcessingChainsCyclesCheckJob'
