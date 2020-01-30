f8423fc439790fb936e57b6b807cc3b399bcd5d3
Stefano Ricci <SteRiccio@users.noreply.github.com>
2020-01-30 16:08:00 +0100

    Processing chain scripts: move scripts generator code to ProcessingStepScriptGenerator

--- server/modules/analysis/service/_processingChainScriptGenerator/processingStepScriptGenerator.js
+++ server/modules/analysis/service/_processingChainScriptGenerator/processingStepScriptGenerator.js
@@ -2,11 +2,16 @@ import * as fs from 'fs'
 import * as path from 'path'
 import * as R from 'ramda'
 
+import * as ProcessUtils from '@core/processUtils'
 import * as Survey from '@core/survey/survey'
 import * as NodeDef from '@core/survey/nodeDef'
+import * as ProcessingChain from '@common/analysis/processingChain'
 import * as ProcessingStep from '@common/analysis/processingStep'
 
-import * as SurveyRdbManager from '../../../surveyRdb/manager/surveyRdbManager'
+import * as FileUtils from '@server/utils/file/fileUtils'
+import * as ProcessingChainManager from '@server/modules/analysis/manager/processingChainManager'
+import * as SurveyManager from '@server/modules/survey/manager/surveyManager'
+import * as SurveyRdbManager from '@server/modules/surveyRdb/manager/surveyRdbManager'
 
 const _generateDataFile = async (survey, cycle, nodeDef, outputDir) => {
   const nodeDefUuidCols = []
@@ -40,12 +45,50 @@ const _generateDataFile = async (survey, cycle, nodeDef, outputDir) => {
   )
 }
 
-export const generateScript = async (survey, cycle, processingStep, outputDir) => {
-  const nodeDefTable = R.pipe(ProcessingStep.getEntityUuid, entityUuid => Survey.getNodeDefByUuid(entityUuid)(survey))(
-    processingStep,
-  )
+export const generateScriptDeprecated = async (surveyId, cycle, processingChain) => {
+  const survey = await SurveyManager.fetchSurveyAndNodeDefsBySurveyId(surveyId, cycle)
 
-  if (nodeDefTable) {
-    await _generateDataFile(survey, cycle, nodeDefTable, outputDir)
+  const outputDir = ProcessUtils.ENV.analysisOutputDir
+  for (const processingStep of ProcessingChain.getProcessingSteps(processingChain)) {
+    const nodeDefTable = R.pipe(ProcessingStep.getEntityUuid, entityUuid =>
+      Survey.getNodeDefByUuid(entityUuid)(survey),
+    )(processingStep)
+
+    if (nodeDefTable) {
+      await _generateDataFile(survey, cycle, nodeDefTable, outputDir)
+    }
+  }
+}
+
+const _generateChainDirs = async path => {
+  const dirSystem = FileUtils.join(path, 'system')
+  const dirUser = FileUtils.join(path, 'user')
+  await Promise.all([FileUtils.mkdir(dirSystem), FileUtils.mkdir(dirUser)])
+  return { dirSystem, dirUser }
+}
+
+export const generateScript = async (surveyId, chainUuid) => {
+  const dirChain = FileUtils.join(ProcessUtils.ENV.analysisOutputDir, chainUuid)
+  await FileUtils.rmdir(dirChain)
+  await FileUtils.mkdir(dirChain)
+  const { dirSystem, dirUser } = await _generateChainDirs(dirChain)
+
+  const fileArena = FileUtils.join(dirChain, 'arena.R')
+  await FileUtils.appendFile(fileArena)
+
+  const fileRStudioProj = FileUtils.join(dirChain, 'r_studio_project.Rproj')
+  await FileUtils.copyFile(FileUtils.join(__dirname, 'chain', 'r_studio_project.Rproj'), fileRStudioProj)
+
+  const fileCommon = FileUtils.join(dirUser, 'common.R')
+  await FileUtils.appendFile(fileCommon)
+
+  const chain = await ProcessingChainManager.fetchChainByUuid(surveyId, chainUuid)
+  const steps = await ProcessingChainManager.fetchStepsByChainUuid(surveyId, chainUuid)
+  for (const step of steps) {
+    const calculations = await ProcessingChainManager.fetchCalculationsByStepUuid(
+      surveyId,
+      ProcessingStep.getUuid(step),
+    )
+    console.log(JSON.stringify(calculations))
   }
 }
