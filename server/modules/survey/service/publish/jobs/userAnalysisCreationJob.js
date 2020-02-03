9b8de292d05e11aec68b582df6f7cce61cdd9da1
Stefano Ricci <SteRiccio@users.noreply.github.com>
2020-02-03 14:44:32 +0100

    User analysis: on survey publish, create or update user_analysis

---
+++ server/modules/survey/service/publish/jobs/userAnalysisCreationJob.js
@@ -0,0 +1,15 @@
+import Job from '@server/job/job'
+
+import * as UserAnalysisManager from '@server/modules/user/manager/userAnalysisManager'
+
+export default class UserAnalysisCreationJob extends Job {
+  constructor(params) {
+    super(UserAnalysisCreationJob.type, params)
+  }
+
+  async execute() {
+    await UserAnalysisManager.insertOrUpdateUserAnalysis(this.surveyId, this.tx)
+  }
+}
+
+UserAnalysisCreationJob.type = 'UserAnalysisCreationJob'
