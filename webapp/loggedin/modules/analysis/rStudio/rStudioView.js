0ce6e7a68789c73431aecab487cf5c079037a43b
Stefano Ricci <SteRiccio@users.noreply.github.com>
2020-03-25 19:15:05 +0100

    RStudio Server setup: changed iFrame default url

--- webapp/loggedin/modules/analysis/rStudio/rStudioView.js
+++ webapp/loggedin/modules/analysis/rStudio/rStudioView.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 import * as ProcessUtils from '@core/processUtils'
 
-const RStudioView = () => <iframe src={ProcessUtils.ENV.rStudioServerURL} style={{ height: '100%', width: '100%' }} />
+const RStudioView = () => {
+  const rStudioServerUrl = ProcessUtils.ENV.rStudioServerURL || `${location.origin}/rstudio/`
+  return <iframe src={rStudioServerUrl} style={{ height: '100%', width: '100%' }} />
+}
 
 export default RStudioView
