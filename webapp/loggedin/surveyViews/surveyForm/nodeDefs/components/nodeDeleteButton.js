0e56be42a829dad4ea064a9ad851f62e88ad56ff
Stefano Ricci <SteRiccio@users.noreply.github.com>
2020-04-07 15:34:54 +0200

    NodeDefEntityTable: hide output variables

--- webapp/loggedin/surveyViews/surveyForm/nodeDefs/components/nodeDeleteButton.js
+++ webapp/loggedin/surveyViews/surveyForm/nodeDefs/components/nodeDeleteButton.js
@@ -1,28 +1,48 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
+import PropTypes from 'prop-types'
 
 import { showDialogConfirm } from '@webapp/app/appDialogConfirm/actions'
+import { removeNode } from '@webapp/loggedin/surveyViews/record/actions'
 
-const NodeDeleteButton = ({ nodeDef, node, disabled = false, showConfirm = true, removeNode }) => {
+const NodeDeleteButton = (props) => {
+  const { nodeDef, node, disabled, showConfirm } = props
+
   const dispatch = useDispatch()
 
   return (
     <button
+      type="button"
       className="btn btn-s btn-transparent"
       style={{
         alignSelf: 'center',
         justifySelf: 'center',
       }}
       aria-disabled={disabled}
-      onClick={() =>
-        showConfirm
-          ? dispatch(showDialogConfirm('surveyForm.confirmNodeDelete', {}, () => removeNode(nodeDef, node)))
-          : removeNode(nodeDef, node)
-      }
+      onClick={() => {
+        const performDelete = () => dispatch(removeNode(nodeDef, node))
+        if (showConfirm) {
+          dispatch(showDialogConfirm('surveyForm.confirmNodeDelete', {}, performDelete))
+        } else {
+          performDelete()
+        }
+      }}
     >
       <span className="icon icon-bin icon-12px" />
     </button>
   )
+}
+
+NodeDeleteButton.propTypes = {
+  nodeDef: PropTypes.any.isRequired,
+  node: PropTypes.any.isRequired,
+  disabled: PropTypes.bool,
+  showConfirm: PropTypes.bool,
+}
+
+NodeDeleteButton.defaultProps = {
+  disabled: false,
+  showConfirm: true,
 }
 
 export default NodeDeleteButton
