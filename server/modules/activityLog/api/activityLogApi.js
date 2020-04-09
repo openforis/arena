d5b07f9ec9cb1db27a8ea56d477eca470b6adaef
Stefano Ricci <SteRiccio@users.noreply.github.com>
2020-04-09 15:28:33 +0200

    ActivityLog: added documentation to API fetch function

--- server/modules/activityLog/api/activityLogApi.js
+++ server/modules/activityLog/api/activityLogApi.js
@@ -4,7 +4,16 @@ import * as ActivityLogService from '../service/activityLogService'
 
 export const init = (app) => {
   // ==== READ
-
+  /**
+   * Fetches a list of activity logs (limited to 30 items) grouped by user, type, content uuid.
+   * Params:
+   * - idGreaterThan : specify it to fetch newest activities
+   *                   (fetched activities will have an id greater than the specified value)
+   * - idLessThan    : specify it to fetch older activities
+   *                   (fetched activities will have an id less than the specified value)
+   *
+   * If neither idGreaterThan nor idLessThan are specified, the last 30 activities will be fetched.
+   */
   app.get('/survey/:surveyId/activity-log', AuthMiddleware.requireSurveyViewPermission, async (req, res, next) => {
     try {
       const { surveyId, idGreaterThan, idLessThan } = Request.getParams(req)
