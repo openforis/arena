290c8df566d311d1862a778ca7ac722030bd5b15
minotogna <mino.togna@gmail.com>
2020-04-13 16:47:37 +0200

    promiseUtils: added each method to sequentially resolve promises

--- core/promiseUtils.js
+++ core/promiseUtils.js
@@ -1,18 +1,37 @@
-const handleGeneratorNext = async (generator) => {
+const handleGeneratorNext = (results) => async (generator) => {
   // result => { done: [Boolean], value: [Object] }
   const { done, value } = generator.next()
-  const result = await value
 
   if (done) {
-    return result
+    return results
   }
 
-  return handleGeneratorNext(generator)
+  results.push(await value)
+
+  return handleGeneratorNext(results)(generator)
 }
 
 /**
- * Transforms the generator passed as input parameter to a Promise that gets resolved when the generator finishes
+ * Transforms the generator passed as input parameter to a Promise that gets resolved when the generator finishes.
  *
- * A useful application is when an array of promises must be resolved in order
+ * A useful application is when an array of promises must be resolved in order.
  */
-export const resolveGenerator = handleGeneratorNext
+export const resolveGenerator = handleGeneratorNext([])
+
+/**
+ * Given an Iterable, iterates serially over all the values in it, executing the given callback on each element.
+ * If the callback returns a Promise, it is awaited before continuing to the next iteration.
+ *
+ * @param {Iterable} iterable - The iterable to iterate.
+ * @param {Function} callback - The callback function to invoke on each element of the iterable.
+ * @returns {Promise<*>} - The returned Promise.
+ */
+export const each = async (iterable, callback) => {
+  function* generator() {
+    for (let i = 0; i < iterable.length; i += 1) {
+      yield callback(iterable[i])
+    }
+  }
+
+  return resolveGenerator(generator())
+}
