f149bd30f0cfc357588e685258bb2fae7d0e1760
Stefano Ricci <SteRiccio@users.noreply.github.com>
2020-05-18 16:17:21 +0200

    RStudio iframe: fixed error accessing RStudio iframe

--- webapp/loggedin/modules/analysis/rStudio/rStudioView.js
+++ webapp/loggedin/modules/analysis/rStudio/rStudioView.js
@@ -1,10 +1,7 @@
 import React from 'react'
 
-import * as ProcessUtils from '@core/processUtils'
-
-const RStudioView = () => {
-  const rStudioServerUrl = ProcessUtils.ENV.rStudioServerURL || `${location.origin}/rstudio/`
-  return <iframe src={rStudioServerUrl} style={{ height: '100%', width: '100%' }} />
-}
+const RStudioView = () => (
+  <iframe title="RStudio" src={`${window.location.origin}/rstudio/`} style={{ height: '100%', width: '100%' }} />
+)
 
 export default RStudioView
