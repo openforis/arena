7e204ca29b6c9145dc0efc07ed18702d58bbaf4b
ramirobg94 <ramirobg94@gmail.com>
2020-06-08 12:58:56 +0200

    fix selector

--- webapp/store/app/error/state.js
+++ webapp/store/app/error/state.js
@@ -1,4 +1,9 @@
 export const appStateKey = '_app'
 export const stateKey = 'error'
 
-export const getSystemError = (state) => (state && state[appStateKey] && state[appStateKey][stateKey]) || null
+const isEmpty = (val) => val === undefined || val == null || val.length <= 0 || Object.values(val).length <= 0
+
+export const getSystemError = (state) =>
+  state && state[appStateKey] && state[appStateKey][stateKey] && !isEmpty(state[appStateKey][stateKey])
+    ? state[appStateKey][stateKey]
+    : ''
