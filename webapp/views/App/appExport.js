22280319a139b6b518b519e2e4aea8a7fa47038e
ramirobg94 <ramirobg94@gmail.com>
2020-06-11 16:41:37 +0200

    remove reducer

--- webapp/views/App/appExport.js
+++ webapp/views/App/appExport.js
@@ -3,14 +3,12 @@ import analysisReducer from '@webapp/loggedin/modules/analysis/reducer'
 import dataReducer from '@webapp/loggedin/modules/data/reducer'
 import surveyViewsReducer from '@webapp/loggedin/surveyViews/reducer'
 import userViewReducer from '@webapp/loggedin/modules/users/user/reducer'
-import userInviteViewReducer from '@webapp/loggedin/modules/users/userInvite/reducer'
 
 import * as HomeState from '@webapp/loggedin/modules/home/homeState'
 import * as AnalysisState from '@webapp/loggedin/modules/analysis/analysisState'
 import * as DataState from '@webapp/loggedin/modules/data/state'
 import * as SurveyViewsState from '@webapp/loggedin/surveyViews/surveyViewsState'
 import * as UserViewState from '@webapp/loggedin/modules/users/user/userViewState'
-import * as UserInviteViewState from '@webapp/loggedin/modules/users/userInvite/userInviteViewState'
 
 import { AppReducer, AppState } from '@webapp/store/app'
 
@@ -23,7 +21,6 @@ const reducers = [
   { name: DataState.stateKey, fn: dataReducer },
   { name: SurveyViewsState.stateKey, fn: surveyViewsReducer },
   { name: UserViewState.stateKey, fn: userViewReducer },
-  { name: UserInviteViewState.stateKey, fn: userInviteViewReducer },
 ]
 
 export { component, reducers }
