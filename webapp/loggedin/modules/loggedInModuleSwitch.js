06c1b95bc5c07ff3b885973d182cd27486799773
ramirobg94 <ramirobg94@gmail.com>
2020-06-11 12:02:57 +0200

    move users

--- webapp/loggedin/modules/loggedInModuleSwitch.js
+++ webapp/loggedin/modules/loggedInModuleSwitch.js
@@ -1,24 +1,28 @@
 import React from 'react'
-import { Route, Switch } from 'react-router'
+import { Route, Switch, useLocation } from 'react-router'
 
 import { appModules, appModuleUri } from '@webapp/app/appModules'
+
+import UsersView from '@webapp/views/Users'
 
 import DesignerView from './designer/designerView'
 import HomeView from './home/homeView'
 import DataViewComponent from './data/dataView'
-import UsersView from './users/usersView'
 import AnalysisView from './analysis/analysisView'
 
-const LoggedInModuleSwitch = props => (
-  <div className="app-module">
-    <Switch location={props.location}>
-      <Route path={appModuleUri(appModules.home)} component={HomeView} />
-      <Route path={appModuleUri(appModules.designer)} component={DesignerView} />
-      <Route path={appModuleUri(appModules.data)} component={DataViewComponent} />
-      <Route path={appModuleUri(appModules.users)} component={UsersView} />
-      <Route path={appModuleUri(appModules.analysis)} component={AnalysisView} />
-    </Switch>
-  </div>
-)
+const LoggedInModuleSwitch = () => {
+  const location = useLocation()
+  return (
+    <div className="app-module">
+      <Switch location={location}>
+        <Route path={appModuleUri(appModules.home)} component={HomeView} />
+        <Route path={appModuleUri(appModules.designer)} component={DesignerView} />
+        <Route path={appModuleUri(appModules.data)} component={DataViewComponent} />
+        <Route path={appModuleUri(appModules.users)} component={UsersView} />
+        <Route path={appModuleUri(appModules.analysis)} component={AnalysisView} />
+      </Switch>
+    </div>
+  )
+}
 
 export default LoggedInModuleSwitch
