2e4c4d8b33bee8b1c2aed8fb8aa800aac244a514
ramirobg94 <ramirobg94@gmail.com>
2020-06-21 12:06:59 +0200

    dismiss with cancel and back, validation

--- webapp/views/App/views/Analysis/Chain/store/actions/useOnUpdate.js
+++ webapp/views/App/views/Analysis/Chain/store/actions/useOnUpdate.js
@@ -1,6 +1,7 @@
 import * as ProcessingChain from '@common/analysis/processingChain'
 
-export const useOnUpdate = ({ chain, setChain }) => ({ name, value }) => {
+export const useOnUpdate = ({ chain, setChain, setDirty }) => ({ name, value }) => {
   const chainUpdated = ProcessingChain.assocProp(name, value)(chain)
+  setDirty(true)
   setChain(chainUpdated)
 }
