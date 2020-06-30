07350e039afd14e148a86eb4c53d2c514638f2e6
ramirobg94 <ramirobg94@gmail.com>
2020-06-30 12:47:07 +0200

    set original

--- webapp/service/storage/analysis/actions.js
+++ webapp/service/storage/analysis/actions.js
@@ -1,7 +1,10 @@
+import * as A from '@core/arena'
+
 import * as AnalysisStorage from './storage'
 
 // ====== chain
 export const persistChain = ({ chain }) => {
+  if (A.isEmpty(chain)) return
   AnalysisStorage.setChain({ chain })
   window.dispatchEvent(new CustomEvent(AnalysisStorage.eventTypes.setChain))
 }
@@ -12,6 +15,7 @@ export const resetChain = () => AnalysisStorage.clearChain()
 
 // ===== step
 export const persistStep = ({ step }) => {
+  if (A.isEmpty(step)) return
   AnalysisStorage.setStep({ step })
   window.dispatchEvent(new CustomEvent(AnalysisStorage.eventTypes.setStep))
 }
@@ -22,6 +26,7 @@ export const resetStep = () => AnalysisStorage.clearStep()
 
 // ===== calculation
 export const persistCalculation = ({ calculation }) => {
+  if (A.isEmpty(calculation)) return
   AnalysisStorage.setCalculation({ calculation })
   window.dispatchEvent(new CustomEvent(AnalysisStorage.eventTypes.setCalculation))
 }
--- webapp/views/App/views/Analysis/Chain/store/calculation/actions/useSelect.js
+++ webapp/views/App/views/Analysis/Chain/store/calculation/actions/useSelect.js
@@ -1,8 +1,10 @@
 import { AnalysisActions } from '@webapp/service/storage'
 
-export const useSelect = ({ setCalculation }) => {
+export const useSelect = ({ setCalculation, setOriginalCalculation, setCalculationDirty }) => {
   return (calculation) => {
     setCalculation(calculation)
+    setOriginalCalculation(calculation)
     AnalysisActions.persistCalculation({ calculation })
+    setCalculationDirty(false)
   }
 }
