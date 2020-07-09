1ea4afc7174d5b7678ba794ca7a48f77800052ce
ramirobg94 <ramirobg94@gmail.com>
2020-07-09 18:48:48 +0200

    update props

--- webapp/views/App/views/Designer/TaxonomyDetail/actions.js
+++ webapp/views/App/views/Designer/TaxonomyDetail/actions.js
@@ -1,10 +1,3 @@
-import axios from 'axios'
-
-import { debounceAction } from '@webapp/utils/reduxUtils'
-import * as Taxonomy from '@core/survey/taxonomy'
-
-import { SurveyState, TaxonomiesActions } from '@webapp/store/survey'
-
 // Taxonomy editor actions
 export const taxonomyViewTaxonomyUpdate = 'taxonomyView/taxonomy/update'
 export const taxonomyViewTaxonomyPropsUpdate = 'taxonomyView/taxonomy/props/update'
@@ -13,16 +6,3 @@ export const taxonomyViewTaxonomyPropsUpdate = 'taxonomyView/taxonomy/props/upda
 
 export const setTaxonomyForEdit = (taxonomyUuid) => (dispatch) =>
   dispatch({ type: taxonomyViewTaxonomyUpdate, taxonomyUuid })
-
-// ====== UPDATE
-export const putTaxonomyProp = (taxonomy, key, value) => async (dispatch, getState) => {
-  dispatch({ type: TaxonomiesActions.taxonomyPropUpdate, taxonomy, key, value })
-
-  const action = async () => {
-    const surveyId = SurveyState.getSurveyId(getState())
-    const { data } = await axios.put(`/api/survey/${surveyId}/taxonomies/${Taxonomy.getUuid(taxonomy)}`, { key, value })
-    dispatch({ type: TaxonomiesActions.taxonomiesUpdate, taxonomies: data.taxonomies })
-  }
-
-  dispatch(debounceAction(action, `${TaxonomiesActions.taxonomyPropUpdate}_${Taxonomy.getUuid(taxonomy)}`))
-}
---
+++ webapp/views/App/views/Designer/TaxonomyDetail/store/actions/useUpdate.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+import { useCallback } from 'react'
+import { useDispatch } from 'react-redux'
+
+import { debounceAction } from '@webapp/utils/reduxUtils'
+import * as Taxonomy from '@core/survey/taxonomy'
+
+import { useSurveyId, TaxonomiesActions } from '@webapp/store/survey'
+
+import { State } from '../state'
+
+export const useUpdate = ({ setState }) => {
+  const dispatch = useDispatch()
+  const surveyId = useSurveyId()
+
+  return useCallback(({ key, value, state }) => {
+    const taxonomy = State.getTaxonomy(state)
+
+    setState(State.assocTaxonomy(State.assocTaxonomyProp({ key, value })(taxonomy))(state))
+
+    const action = async () => {
+      dispatch({ type: TaxonomiesActions.taxonomyPropUpdate, taxonomy, key, value })
+      const { data } = await axios.put(`/api/survey/${surveyId}/taxonomies/${Taxonomy.getUuid(taxonomy)}`, {
+        key,
+        value,
+      })
+      dispatch({ type: TaxonomiesActions.taxonomiesUpdate, taxonomies: data.taxonomies })
+    }
+
+    dispatch(debounceAction(action, `${TaxonomiesActions.taxonomyPropUpdate}_${Taxonomy.getUuid(taxonomy)}`))
+  }, [])
+}
