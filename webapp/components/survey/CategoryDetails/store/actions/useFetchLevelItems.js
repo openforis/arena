6559d1a83b925162b398d5483d89e171a96cdf38
Stefano Ricci <SteRiccio@users.noreply.github.com>
2020-07-16 11:30:02 +0200

    CategoryDetails: fetch draft items only when user can edit survey

--- webapp/components/survey/CategoryDetails/store/actions/useFetchLevelItems.js
+++ webapp/components/survey/CategoryDetails/store/actions/useFetchLevelItems.js
@@ -2,11 +2,13 @@ import { useCallback } from 'react'
 import axios from 'axios'
 
 import { useSurveyId } from '@webapp/store/survey'
+import { useAuthCanEditSurvey } from '@webapp/store/user'
 
 import { State } from '../state'
 
 export const useFetchLevelItems = ({ setState }) => {
   const surveyId = useSurveyId()
+  const canEdit = useAuthCanEditSurvey()
 
   return useCallback(async ({ categoryUuid, levelIndex = 0, parentUuid = null }) => {
     // Reset level items
@@ -15,7 +17,7 @@ export const useFetchLevelItems = ({ setState }) => {
     const {
       data: { items },
     } = await axios.get(`/api/survey/${surveyId}/categories/${categoryUuid}/items`, {
-      params: { draft: true, parentUuid },
+      params: { draft: canEdit, parentUuid },
     })
 
     setState(State.assocLevelItems({ levelIndex, items }))
