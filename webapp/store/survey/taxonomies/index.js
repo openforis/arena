682c3eaa62a39807e4dabf21212198bef472b54d
ramirobg94 <ramirobg94@gmail.com>
2020-07-17 01:59:37 +0200

    remove taxonomies from redux

--- webapp/store/survey/taxonomies/index.js
+++
@@ -1,5 +0,0 @@
-import * as TaxonomiesActions from './actions'
-import * as TaxonomiesState from './state'
-import TaxonomiesReducer from './reducer'
-
-export { TaxonomiesActions, TaxonomiesReducer, TaxonomiesState }
