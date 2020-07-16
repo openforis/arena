07300e7349f4564a73f77aa67d13ee99562f40d4
ramirobg94 <ramirobg94@gmail.com>
2020-07-16 16:35:28 +0200

    add service api categories and taxonomies

--- webapp/service/api/index.js
+++ webapp/service/api/index.js
@@ -1,1 +1,4 @@
 export { fetchUserAndSurvey } from './fetchUserAndSurvey'
+
+export { fetchCategories, fetchCategory } from './categories'
+export { fetchTaxonomies, fetchTaxonomy } from './taxonomies'
