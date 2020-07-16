b20ca69cea088da74b0840bc257979e1c5ba88f6
ramirobg94 <ramirobg94@gmail.com>
2020-07-16 09:56:50 +0200

    remove not necessary condition

--- webapp/components/survey/TaxonomyList/HeaderLeft/HeaderLeft.js
+++ webapp/components/survey/TaxonomyList/HeaderLeft/HeaderLeft.js
@@ -2,7 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { matchPath, useHistory, useLocation } from 'react-router'
 
-import * as A from '@core/arena'
 import * as Taxonomy from '@core/survey/taxonomy'
 
 import * as Taxonomy from '@core/survey/taxonomy'
@@ -11,24 +10,13 @@ import ButtonTaxonomyAdd from '@webapp/components/survey/ButtonTaxonomyAdd'
 import { appModuleUri, designerModules } from '@webapp/app/appModules'
 
 const HeaderLeft = (props) => {
-  const { pathname } = useLocation()
-  const history = useHistory()
-  const inTaxonomiesPath = Boolean(matchPath(pathname, appModuleUri(designerModules.taxonomies)))
-
   const { headerProps } = props
   const { onTaxonomyCreated } = headerProps
+  
   const history = useHistory()
   const { pathname } = useLocation()
-
+  
   const inTaxonomiesPath = Boolean(matchPath(pathname, appModuleUri(designerModules.taxonomies)))
-
-  const onAdd = (taxonomyCreated) => {
-    if (inTaxonomiesPath || A.isNull(onTaxonomyCreated)) {
-      history.push(`${appModuleUri(designerModules.taxonomy)}${Taxonomy.getUuid(taxonomyCreated)}`)
-    } else {
-      onTaxonomyCreated(taxonomyCreated)
-    }
-  }
 
   const onAdd = (taxonomyCreated) => {
     if (inTaxonomiesPath) {
