5fed3dc9faf672af6734020adc39262e95687bd2
ramirobg94 <ramirobg94@gmail.com>
2020-07-20 09:22:12 +0200

    move taxonomyUuid

--- webapp/components/survey/TaxonomyDetails/store/actions/useInit.js
+++ webapp/components/survey/TaxonomyDetails/store/actions/useInit.js
@@ -1,7 +1,4 @@
 import { useCallback } from 'react'
-import { useParams } from 'react-router'
-
-import * as A from '@core/arena'
 
 import * as API from '@webapp/service/api'
 
@@ -10,13 +7,10 @@ import { useSurveyId } from '@webapp/store/survey'
 import { State } from '../state'
 
 export const useInit = ({ setState }) => {
-  const { taxonomyUuid: taxonomyUuidParam } = useParams()
   const surveyId = useSurveyId()
 
   return useCallback(async ({ taxonomyUuid }) => {
-    if (!A.isEmpty(taxonomyUuid) || !A.isEmpty(taxonomyUuidParam)) {
-      const taxonomy = await API.fetchTaxonomy({ surveyId, Uuid: taxonomyUuidParam || taxonomyUuid })
-      setState(State.create({ taxonomy }))
-    }
+    const taxonomy = await API.fetchTaxonomy({ surveyId, Uuid: taxonomyUuid })
+    setState(State.create({ taxonomy }))
   }, [])
 }
--- webapp/components/survey/TaxonomyDetails/store/useTaxonomyDetails.js
+++ webapp/components/survey/TaxonomyDetails/store/useTaxonomyDetails.js
@@ -1,16 +1,18 @@
 import { useState, useEffect } from 'react'
 
+import { useParams } from 'react-router'
+
 import { useActions } from './actions'
 
 export const useTaxonomyDetails = (props) => {
   const { taxonomyUuid } = props
-
+  const { taxonomyUuid: taxonomyUuidParam } = useParams()
   const [state, setState] = useState({})
 
   const Actions = useActions({ setState })
 
   useEffect(() => {
-    Actions.init({ state, taxonomyUuid })
+    Actions.init({ state, taxonomyUuid: taxonomyUuidParam || taxonomyUuid })
   }, [])
 
   return { state, Actions }
