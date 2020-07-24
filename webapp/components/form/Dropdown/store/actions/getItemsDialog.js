64f155ae89d6244dd7fde00042e0123bf405a2c9
ramirobg94 <ramirobg94@gmail.com>
2020-07-24 12:23:56 +0200

    move filter

--- webapp/components/form/Dropdown/store/actions/getItemsDialog.js
+++ webapp/components/form/Dropdown/store/actions/getItemsDialog.js
@@ -2,30 +2,25 @@ import * as StringUtils from '@core/stringUtils'
 import { State } from '../state'
 
 // Utility function used to get or fetch the dialog items
-export const getItemsDialog = async ({ state, value }) => {
+export const getItemsDialog = ({ state, value }) => {
   const autocompleteMinChars = State.getAutocompleteMinChars(state)
+  const items = State.getItems(state)
   const searchValue = value.trim()
+
+  if (autocompleteMinChars <= 0 && searchValue.length === 0) {
+    return items.constructor === Array ? items : items(searchValue)
+  }
 
   if (autocompleteMinChars > 0 && searchValue.length < autocompleteMinChars) {
     return []
   }
 
-  const items = State.getItems(state)
-
-  let itemsDialog = []
-  if (items.constructor === Array) {
-    itemsDialog =
-      autocompleteMinChars <= 0 && searchValue.length === 0
-        ? items
-        : items.filter((item) =>
-            item.constructor === String
-              ? StringUtils.contains(searchValue, item)
-              : StringUtils.contains(searchValue, State.getItemKey(state)(item)) ||
-                StringUtils.contains(searchValue, State.getItemLabel(state)(item))
-          )
-  } else {
-    itemsDialog = await items(searchValue)
-  }
-
-  return itemsDialog
+  return items.constructor === Array
+    ? items.filter((item) =>
+        item.constructor === String
+          ? StringUtils.contains(searchValue, item)
+          : StringUtils.contains(searchValue, State.getItemKey(state)(item)) ||
+            StringUtils.contains(searchValue, State.getItemLabel(state)(item))
+      )
+    : items(value)
 }
