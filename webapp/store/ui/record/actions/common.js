268c3d448caa7f6e024b05c22db36752f5cbaec7
Stefano Ricci <SteRiccio@users.noreply.github.com>
2020-07-30 16:28:25 +0200

    RecordActions: split actions into multiple files

---
+++ webapp/store/ui/record/actions/common.js
@@ -0,0 +1,17 @@
+import * as A from '@core/arena'
+import * as Record from '@core/record/record'
+
+import { LoaderActions } from '@webapp/store/ui'
+
+import * as RecordState from '../state'
+import * as ActionTypes from './actionTypes'
+
+export const recordNodesUpdate = (nodes) => (dispatch, getState) => {
+  const record = RecordState.getRecord(getState())
+  // Hide app loader on record create
+  if (A.isEmpty(Record.getNodes(record))) {
+    dispatch(LoaderActions.hideLoader())
+  }
+
+  dispatch({ type: ActionTypes.nodesUpdate, nodes })
+}
---
+++ webapp/store/ui/record/actions/index.js
@@ -0,0 +1,16 @@
+export {
+  nodeDelete,
+  nodesUpdate,
+  recordCreate,
+  recordDelete,
+  recordLoad,
+  recordUuidPreviewUpdate,
+  validationsUpdate,
+} from './actionTypes'
+
+export { applicationError, cycleChanged, sessionExpired } from './application'
+export { recordNodesUpdate } from './common'
+export { createNodePlaceholder, createRecord } from './create'
+export { nodeValidationsUpdate, nodesUpdateCompleted, updateNode, updateRecordStep } from './update'
+export { checkInRecord, checkOutRecord } from './checkIn'
+export { deleteRecord, deleteRecordUuidPreview, removeNode } from './delete'
