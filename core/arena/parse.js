3d5805d1c18b03ded5af3e683c72740d60c7a3fb
ramirobg94 <ramirobg94@gmail.com>
2020-07-31 09:48:54 +0200

    improve types

--- core/arena/parse.js
+++ core/arena/parse.js
@@ -13,8 +13,8 @@ export const parse = (string) => {
   if (isNull(string)) return null
   if (isEmpty(string)) return ''
   return JSON.parse(string, (key, value) => {
-    if (value.type === 'Map') return new Map(JSON.parse(value.value))
-    if (value.type === 'Set') return new Set(JSON.parse(value.value))
+    if (value.__type === 'Map') return new Map(JSON.parse(value.__values))
+    if (value.__type === 'Set') return new Set(JSON.parse(value.__values))
     return value
   })
 }
