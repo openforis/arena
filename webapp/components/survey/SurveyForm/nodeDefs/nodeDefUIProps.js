7bf445e628897b76cc1c34c8bca59a8b35540c21
ramirobg94 <ramirobg94@gmail.com>
2020-08-04 20:26:20 +0200

    remove validations on nodedef edit

--- webapp/components/survey/SurveyForm/nodeDefs/nodeDefUIProps.js
+++ webapp/components/survey/SurveyForm/nodeDefs/nodeDefUIProps.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import * as R from 'ramda'
+import * as A from '@core/arena'
 
 import * as NodeDef from '@core/survey/nodeDef'
 import * as NodeDefLayout from '@core/survey/nodeDefLayout'
@@ -113,6 +114,7 @@ const propsUI = {
   [file]: {
     component: NodeDefFile,
     icon: <span className="icon icon-file-picture icon-left" />,
+    validations: false,
   },
 
   [entity]: {
@@ -139,6 +141,11 @@ export const getComponent = getProp('component', NodeDefText)
 export const getFormFields = getProp('formFields', ['field'])
 
 export const getDefaultValue = getProp('defaultValue')
+
+export const getValidationsEnabled = (nodeDefType) => {
+  const enabled = getPropByType('validations')(nodeDefType)
+  return !A.isNull(enabled) ? enabled : true
+}
 
 export const getDefaultPropsByType = (type, cycle) => {
   const fn = getPropByType('defaultProps')(type)
