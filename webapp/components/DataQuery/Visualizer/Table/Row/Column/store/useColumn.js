b96f1ed362887ec3afd7aa9ab1d1809197f018c0
Stefano Ricci <SteRiccio@users.noreply.github.com>
2020-08-17 13:25:01 +0200

    DataQuery: add dimensions and measures to data table in view

--- webapp/components/DataQuery/Visualizer/Table/Row/Column/store/useColumn.js
+++ webapp/components/DataQuery/Visualizer/Table/Row/Column/store/useColumn.js
@@ -1,4 +1,5 @@
 import { ColumnNodeDef } from '@common/model/db'
+import * as NodeDef from '@core/survey/nodeDef'
 
 import * as NodeDefUIProps from '@webapp/components/survey/SurveyForm/nodeDefs/nodeDefUIProps'
 
@@ -7,7 +8,14 @@ import { Query } from '@common/model/query'
 export const useColumn = ({ colWidth, query, nodeDef }) => {
   const modeEdit = Query.isModeRawEdit(query)
 
-  const colNames = ColumnNodeDef.getColNames(nodeDef)
+  const colNames = []
+  if (Query.isModeAggregate(query) && Query.getMeasures(query).has(NodeDef.getUuid(nodeDef))) {
+    // for every measure add a column for each aggregate function
+    const aggregateFns = Query.getMeasures(query).get(NodeDef.getUuid(nodeDef))
+    aggregateFns.forEach((aggregateFn) => colNames.push(`${ColumnNodeDef.getColName(nodeDef)}_${aggregateFn}`))
+  } else {
+    colNames.push(...ColumnNodeDef.getColNames(nodeDef))
+  }
   const noCols = modeEdit ? NodeDefUIProps.getFormFields(nodeDef).length : colNames.length
 
   const widthOuter = colWidth * noCols
