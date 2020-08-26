61e78738918b0ff4d43340ddc44b477791711617
Stefano Ricci <SteRiccio@users.noreply.github.com>
2020-08-26 17:39:55 +0200

    Added comments on geometry point parse function

--- core/geo/point.js
+++ core/geo/point.js
@@ -12,6 +12,15 @@ export const newPoint = ({ srsId, x, y }) => ({
   [keys.y]: y,
 })
 
+/**
+ * Parses a point in the format: SRID=SRS_CODE;POINT(X Y)
+ * Valid examples are:
+ * - SRID=EPSG:4326;POINT(12.489060, 41.882788)
+ * - SRID=4326;POINT(12, 41).
+ *
+ * @param {!string} pointText - The point to parse.
+ * @returns {object} - The parsed Point object.
+ */
 export const parsePoint = (pointText) => {
   const match = /SRID=((EPSG:)?(\w+));POINT\((\d+(\.\d+)?) (\d+(\.\d+)?)\)/.exec(pointText)
   return match ? newPoint({ srsId: match[3], x: match[4], y: match[6] }) : null
