e3c946a9b55bc2a8a0ec23821258c89d2eec1314
ramirobg94 <ramirobg94@gmail.com>
2020-08-27 08:22:10 +0200

    improve id

--- test/e2e/utils/ui/sidebar.js
+++ test/e2e/utils/ui/sidebar.js
@@ -4,4 +4,4 @@ const _clickSidebarBtn = async ({ id }) => {
   await click(link({ class: 'sidebar__module-btn', id }))
 }
 
-export const clickSidebarBtnHome = async () => _clickSidebarBtn({ id: 'sidebar_home' })
+export const clickSidebarBtnHome = async () => _clickSidebarBtn({ id: 'sidebar_btn_home' })
