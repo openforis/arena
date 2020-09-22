a3a3ef6f1d2981f64de5402f688a01211059803c
Stefano Ricci <SteRiccio@users.noreply.github.com>
2020-09-22 23:34:36 +0200

    SurveyForm edit: add code attribute (code cleanup)

--- test/e2e/tests/007-surveyFormEditPlotCode.js
+++ test/e2e/tests/007-surveyFormEditPlotCode.js
@@ -12,38 +12,41 @@ import {
   clickCategoryButtonClose,
 } from '../utils/ui'
 
+const nodeDefCode = { type: NodeDef.nodeDefType.code, name: 'country', label: 'Country', isKey: false }
+
+// TODO get it from previous test?
+const nodeDefItemsTree = [
+  { type: 'integer', name: 'tree_id', label: 'Tree id', isKey: true },
+  { type: 'decimal', name: 'tree_dec_1', label: 'Tree decimal 1', isKey: false },
+  { type: 'decimal', name: 'tree_dec_2', label: 'Tree decimal 2', isKey: false },
+]
+
 const nodeDefItems = [
   { type: NodeDef.nodeDefType.integer, name: 'plot_id', label: 'Plot id', isKey: true },
   { type: NodeDef.nodeDefType.text, name: 'plot_text', label: 'Plot text', isKey: false },
   { type: NodeDef.nodeDefType.file, name: 'plot_file', label: 'Plot file', isKey: false },
   { type: NodeDef.nodeDefType.entity, name: 'tree', label: 'Tree', isKey: false },
-  { type: NodeDef.nodeDefType.code, name: 'country', label: 'Country', isKey: false },
+  ...nodeDefItemsTree,
+  nodeDefCode,
 ]
 
-describe('SurveyForm edit Plot: add code attribute', () => {
-  test(
-    'Code attribute create',
-    async () => {
-      const codeValues = { type: NodeDef.nodeDefType.code, name: 'country', label: 'Country' }
-      await addItemToPage({ ...codeValues, saveAndBack: false })
+describe('SurveyForm edit Plot: code attribute', () => {
+  test('Add code attribute "country"', async () => {
+    await addItemToPage({ ...nodeDefCode, saveAndBack: false })
 
-      await clickNodeDefCategoryAdd()
+    await clickNodeDefCategoryAdd()
 
-      // start of category edit
-      await writeCategoryName('administrative_unit')
-      await clickCategoryButtonClose()
-      // end of category edit
+    // start of category edit
+    await writeCategoryName('administrative_unit')
+    await clickCategoryButtonClose()
+    // end of category edit
 
-      await expectNodeDefCategoryIs('administrative_unit')
-      await expectNodeDefCodeParentIsDisabled()
+    await expectNodeDefCategoryIs('administrative_unit')
+    await expectNodeDefCodeParentIsDisabled()
 
-      await clickNodeDefSaveAndClose()
+    await clickNodeDefSaveAndClose()
 
-      await expectItemIsTheLastNodeDef({ item: codeValues })
-      await expectSurveyFormItemsAreInOrder({ items: nodeDefItems })
-    },
-    30 * 1000
-  )
-
-  test('Plot fill form code attribute', async () => {})
+    await expectItemIsTheLastNodeDef({ item: nodeDefCode })
+    await expectSurveyFormItemsAreInOrder({ items: nodeDefItems })
+  })
 })
