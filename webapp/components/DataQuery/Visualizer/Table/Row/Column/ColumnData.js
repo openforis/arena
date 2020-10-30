c6fd80ed35e10d20cf14d39062e861e01de36831
Stefano Ricci <SteRiccio@users.noreply.github.com>
2020-10-30 14:25:12 +0100

    Fixed Explorer date/time column format

--- webapp/components/DataQuery/Visualizer/Table/Row/Column/ColumnData.js
+++ webapp/components/DataQuery/Visualizer/Table/Row/Column/ColumnData.js
@@ -5,20 +5,28 @@ import * as A from '@core/arena'
 
 import * as NodeDef from '@core/survey/nodeDef'
 import * as NumberUtils from '@core/numberUtils'
+import * as DateUtils from '@core/dateUtils'
 
 import { useI18n } from '@webapp/store/system'
 
 import { useColumn } from './store'
 
+const formatters = {
+  [NodeDef.nodeDefType.boolean]: ({ value, i18n, nodeDef }) =>
+    i18n.t(`surveyForm.nodeDefBoolean.labelValue.${NodeDef.getLabelValue(nodeDef)}.${value}`),
+  [NodeDef.nodeDefType.date]: ({ value }) => DateUtils.format(DateUtils.parseISO(value), 'dd/MM/yyyy'),
+  [NodeDef.nodeDefType.decimal]: ({ value }) => NumberUtils.formatDecimal(value),
+  [NodeDef.nodeDefType.integer]: ({ value }) => NumberUtils.formatInteger(value),
+  [NodeDef.nodeDefType.time]: ({ value }) => DateUtils.format(DateUtils.parse(value, 'HH:mm:ss'), 'HH:mm'),
+}
+
 const getColValue = ({ nodeDef, col, row, i18n }) => {
   const value = Object.prototype.hasOwnProperty.call(row, col) ? row[col] : null
-  if (NodeDef.isInteger(nodeDef)) return NumberUtils.formatInteger(value)
-  if (NodeDef.isDecimal(nodeDef)) return NumberUtils.formatDecimal(value)
-  if (NodeDef.isBoolean(nodeDef))
-    return A.isNull(value)
-      ? ''
-      : i18n.t(`surveyForm.nodeDefBoolean.labelValue.${NodeDef.getLabelValue(nodeDef)}.${value}`)
-  return value
+  if (A.isNull(value)) {
+    return ''
+  }
+  const formatter = formatters[NodeDef.getType(nodeDef)]
+  return formatter ? formatter({ i18n, nodeDef, value }) : value
 }
 
 const ColumnData = (props) => {
