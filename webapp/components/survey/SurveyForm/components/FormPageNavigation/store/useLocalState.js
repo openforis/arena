3cbf243b0f8438c90a4a1d991a71b28f55a8c749
ramirobg94 <ramirobg94@gmail.com>
2020-11-12 14:26:41 +0100

    change default

--- webapp/components/survey/SurveyForm/components/FormPageNavigation/store/useLocalState.js
+++ webapp/components/survey/SurveyForm/components/FormPageNavigation/store/useLocalState.js
@@ -19,12 +19,10 @@ export const useLocalState = (props) => {
   const survey = useSurvey()
   const lang = useLang()
   const rootNodeDef = Survey.getNodeDefRoot(survey)
-
   const { edit, nodeDef = rootNodeDef, level, surveyCycleKey, canEditDef, itemLabelFunction = NodeDef.getName } = props
   const childDefs = Survey.getNodeDefChildren(nodeDef)(survey)
   const parentNode = useSelector(SurveyFormState.getFormPageParentNode(nodeDef))
   const expandedFormPageNavigation = useSelector(SurveyFormState.expandedPageNavigation)
-  const label = itemLabelFunction(nodeDef, lang)
   const active = useSelector(SurveyFormState.isNodeDefFormActivePage(nodeDef))
   const outerPageChildDefs = NodeDefLayout.filterNodeDefsWithPage(surveyCycleKey)(childDefs)
 
@@ -41,9 +39,7 @@ export const useLocalState = (props) => {
       outerPageChildDefs,
       canEditDef,
       surveyCycleKey,
-
       itemLabelFunction,
-
       lang,
     })
   )
