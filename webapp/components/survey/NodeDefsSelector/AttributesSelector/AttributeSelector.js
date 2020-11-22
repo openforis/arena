eacf3f99b6f8910641b5a7f3593ef35a7e961112
ramirobg94 <ramirobg94@gmail.com>
2020-11-22 20:09:08 +0100

    simplify

--- webapp/components/survey/NodeDefsSelector/AttributesSelector/AttributeSelector.js
+++ webapp/components/survey/NodeDefsSelector/AttributesSelector/AttributeSelector.js
@@ -35,7 +35,7 @@ const AttributeSelector = (props) => {
         className={`btn btn-s deselectable attribute-selector${isActive ? ' active' : ''}`}
         onClick={() => onToggleAttribute(nodeDefUuid)}
         disabled={!canSelectAttributes}
-        title={NodeDef.getLabelWithType({ nodeDef, lang, type: NodeDef.NodeDefLabelTypes.name })}
+        title={NodeDef.getName(nodeDef)}
       >
         {NodeDef.getLabelWithType({ nodeDef, lang, type: nodeDefLabelType })}
         {NodeDefUIProps.getIconByType(nodeDefType)}
