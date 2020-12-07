c208084476c6e7cd818ef7572919390ca1ac962d
ramirobg94 <ramirobg94@gmail.com>
2020-12-07 14:07:54 +0100

    extract nodedefs functions

--- test/e2e/tests/014-surveyFormPreview.js
+++ test/e2e/tests/014-surveyFormPreview.js
@@ -1,56 +1,88 @@
-import {
-  click,
-  clickParent,
-  writeIntoTextBox,
-  below,
-  toRightOf,
-  textBox,
-  expectInputTextToBe,
-  dropDown,
-} from '../utils/api'
+import * as NodeDef from '@core/survey/nodeDef'
+
+import { above, click, clickParent, dropDown } from '../utils/api'
 import { waitForLoader } from '../utils/ui/loader'
 import { clickSidebarBtnSurveyForm } from '../utils/ui/sidebar'
 import { expectSurveyFormLoaded } from '../utils/ui/surveyForm'
 import { expectSurveyFormHasOnlyAndInOrderThesePages } from '../utils/ui/surveyFormPage'
 import { clickHeaderBtnMySurveys } from '../utils/ui/header'
+import { NodeDefsUtils } from '../utils/ui/nodeDefs'
 
-const enterNodeDefText = async ({ value, label }) => writeIntoTextBox({ text: value, selector: below(label) })
-const enterNodeDefInteger = async ({ value, label }) => writeIntoTextBox({ text: value, selector: below(label) })
-const enterNodeDefDecimal = async ({ value, label }) => writeIntoTextBox({ text: value, selector: below(label) })
-const enterNodeDefDate = async ({ value, label }) => writeIntoTextBox({ text: value, selector: below(label) })
-const enterNodeDefTime = async ({ value, label }) => writeIntoTextBox({ text: value, selector: below(label) })
+const getEnterFunction = ({ type }) => NodeDefsUtils[type].enterValue
+const getEnterParams = ({ type, ...values }) => ({ ...values })
+const getCheckFunction = ({ type }) => NodeDefsUtils[type].expectValue
+const getCheckParams = ({ type, ...values }) => ({ ...values })
 
-const enterNodeDefCode = async ({ label, value }) => {
-  await click(textBox(below(label)))
-  await click(value, below(textBox(below(label))))
+const doSequencial = async ({ items, getFunction, getParams }) =>
+  items.reduce(async (promise, item) => {
+    await promise
+    return getFunction(item)(getParams(item))
+  }, true)
+
+const enterValuesSequencial = async ({ items }) =>
+  doSequencial({ items, getFunction: getEnterFunction, getParams: getEnterParams })
+
+const checkValuesSequencial = async ({ items }) =>
+  doSequencial({ items, getFunction: getCheckFunction, getParams: getCheckParams })
+
+const enterValuesCluster = enterValuesSequencial
+const enterValuesPlot = async ({ items }) => {
+  await click('Plot')
+  await click('Add')
+  await enterValuesSequencial({ items })
 }
-const enterNodeDefCoordinate = async ({ x, y, srs, label }) => {
-  await writeIntoTextBox({ text: x, selector: toRightOf('X', below(label)) })
-  await writeIntoTextBox({ text: y, selector: toRightOf('Y', below(label)) })
 
-  await click(textBox(toRightOf('SRS', below(label))))
-  await click(srs)
+const checkValuesCluster = checkValuesSequencial
+const checkValuesPlot = async ({ id, items }) => {
+  await click('Plot')
+  await dropDown({ class: 'node-select' }).select(`Plot id - ${id}`)
+  await checkValuesSequencial({ items })
 }
 
-const expectNodeDefBase = async ({ value: text, label }) =>
-  expectInputTextToBe({ text: String(text), selector: below(label) })
+const ClusterItems = [
+  { type: NodeDef.nodeDefType.integer, value: 1, label: 'Cluster id' },
+  { type: NodeDef.nodeDefType.decimal, value: 10, label: 'Cluster decimal' },
+  { type: NodeDef.nodeDefType.date, value: '20/11/2020', label: 'Cluster date' },
+  { type: NodeDef.nodeDefType.time, value: '10:30', label: 'Cluster time' },
+  { type: NodeDef.nodeDefType.boolean, value: 'true', label: 'Cluster boolean' },
+  {
+    type: NodeDef.nodeDefType.coordinate,
+    x: 10,
+    y: 15,
+    srs: 'GCS WGS 1984 (EPSG:4326)',
+    label: 'Cluster coordinate',
+  },
+]
 
-const expectNodeDefText = expectNodeDefBase
-const expectNodeDefInteger = expectNodeDefBase
-const expectNodeDefDecimal = expectNodeDefBase
-const expectNodeDefDate = expectNodeDefBase
-const expectNodeDefTime = expectNodeDefBase
+const countryRelativeSelectors = () => above('Region')
+const plots = [
+  [
+    { type: NodeDef.nodeDefType.integer, value: 1, label: 'Plot id' },
+    { type: NodeDef.nodeDefType.text, value: 'text', label: 'Plot text' },
 
-const expectNodeDefCode = expectNodeDefBase
+    {
+      type: NodeDef.nodeDefType.code,
+      value: 'Country',
+      label: 'Country',
+      relativeSelectors: [countryRelativeSelectors],
+    },
+    { type: NodeDef.nodeDefType.code, value: 'Region 01', label: 'Region' },
+    { type: NodeDef.nodeDefType.code, value: 'District 0102', label: 'Province' },
+  ],
+  [
+    { type: NodeDef.nodeDefType.integer, value: 2, label: 'Plot id' },
+    { type: NodeDef.nodeDefType.text, value: 'text', label: 'Plot text' },
 
-const expectNodeDefCoordinate = async ({ x, y, srs, label }) => {
-  await expectInputTextToBe({ text: String(x), selector: toRightOf('X', below(label)) })
-  await expectInputTextToBe({ text: String(y), selector: toRightOf('Y', below(label)) })
-  await expectInputTextToBe({
-    text: srs,
-    selector: toRightOf('SRS', below(label)),
-  })
-}
+    {
+      type: NodeDef.nodeDefType.code,
+      value: 'Country',
+      label: 'Country',
+      relativeSelectors: [countryRelativeSelectors],
+    },
+    { type: NodeDef.nodeDefType.code, value: 'Region 02', label: 'Region' },
+    { type: NodeDef.nodeDefType.code, value: 'District 0203', label: 'Province' },
+  ],
+]
 
 describe('SurveyForm Preview', () => {
   test('Select survey 1', async () => {
@@ -72,72 +104,18 @@ describe('SurveyForm Preview', () => {
     await expectSurveyFormHasOnlyAndInOrderThesePages({ pageLabels: ['Cluster', 'Plot'] })
   })
 
-  test('Enter Cluster nodes', async () => {
-    await enterNodeDefInteger({ value: 1, label: 'Cluster id' })
-    await enterNodeDefDecimal({ value: 10, label: 'Cluster decimal' })
-    await enterNodeDefDate({ value: '20/11/2020', label: 'Cluster date' })
-    await enterNodeDefTime({ value: '10:30', label: 'Cluster time' })
+  test('Enter Cluster nodes', async () => enterValuesCluster({ items: ClusterItems }))
 
-    await click('true', below('Cluster boolean'))
+  test('Enter Plot 1 values', async () => enterValuesPlot({ items: plots[0] }))
 
-    await enterNodeDefCoordinate({ x: 10, y: 10, srs: 'GCS WGS 1984 (EPSG:4326)', label: 'Cluster coordinate' })
-  })
-
-  test('Enter Plot 1 nodes', async () => {
-    await click('Plot')
-    await click('Add')
-
-    await enterNodeDefInteger({ value: 1, label: 'Plot id' })
-    await enterNodeDefText({ value: 'text', label: 'Plot text' })
-
-    await enterNodeDefCode({ value: 'Country', label: 'Country' })
-    await enterNodeDefCode({ value: 'Region 01', label: 'Region' })
-    await enterNodeDefCode({ value: 'District 0102', label: 'Province' })
-  })
-
-  test('Enter Plot 2 nodes', async () => {
-    await click('Add')
-
-    await enterNodeDefInteger({ value: 2, label: 'Plot id' })
-    await enterNodeDefText({ value: 'text', label: 'Plot text' })
-
-    await enterNodeDefCode({ value: 'Country', label: 'Country' })
-    await enterNodeDefCode({ value: 'Region 02', label: 'Region' })
-    await enterNodeDefCode({ value: 'District 0203', label: 'Province' })
-  })
+  test('Enter Plot 2 values', async () => enterValuesPlot({ items: plots[1] }))
 
   test('Check Cluster values', async () => {
     await click('Cluster')
-
-    await expectNodeDefInteger({ value: 1, label: 'Cluster id' })
-    await expectNodeDefDecimal({ value: 10, label: 'Cluster decimal' })
-    await expectNodeDefDate({ value: '20/11/2020', label: 'Cluster date' })
-    await expectNodeDefTime({ value: '10:30', label: 'Cluster time' })
-
-    await expectNodeDefCoordinate({ x: 10, y: 10, srs: 'GCS WGS 1984 (EPSG:4326)', label: 'Cluster coordinate' })
+    await checkValuesCluster({ items: ClusterItems })
   })
 
-  test('Check Plot 1 values', async () => {
-    await click('Plot')
+  test('Check Plot 1 values', async () => checkValuesPlot({ id: 1, items: plots[0] }))
 
-    await dropDown({ class: 'node-select' }).select('Plot id - 1')
-
-    await expectNodeDefInteger({ value: 1, label: 'Plot id' })
-    await expectNodeDefText({ value: 'text', label: 'Plot text' })
-
-    await expectNodeDefCode({ value: 'Country', label: 'Country' })
-    await expectNodeDefCode({ value: 'Region 01', label: 'Region' })
-    await expectNodeDefCode({ value: 'District 0102', label: 'Province' })
-  })
-
-  test('Check Plot 2 values', async () => {
-    await dropDown({ class: 'node-select' }).select('Plot id - 2')
-
-    await expectNodeDefInteger({ value: 2, label: 'Plot id' })
-    await expectNodeDefText({ value: 'text', label: 'Plot text' })
-
-    await expectNodeDefCode({ value: 'Country', label: 'Country' })
-    await expectNodeDefCode({ value: 'Region 02', label: 'Region' })
-    await expectNodeDefCode({ value: 'District 0203', label: 'Province' })
-  })
+  test('Check Plot 2 values', async () => checkValuesPlot({ id: 2, items: plots[1] }))
 })
