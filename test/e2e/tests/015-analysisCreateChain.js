d6d24149db77eda91c8896276a1baf98abafff6d
ramirobg94 <ramirobg94@gmail.com>
2020-12-08 14:33:41 +0100

    add tests

---
+++ test/e2e/tests/015-analysisCreateChain.js
@@ -0,0 +1,90 @@
+import { clickSidebarBtnAnalysisProcessingChains } from '../utils/ui/sidebar'
+import {
+  clearTextBox,
+  click,
+  expectExists,
+  getElement,
+  writeIntoTextBox,
+  toRightOf,
+  button,
+  textBox,
+  above,
+  below,
+} from '../utils/api'
+import { waitForLoader } from '../utils/ui/loader'
+
+describe('Analysis create chain', () => {
+  test("Chain can't be created until survey is published", async () => {
+    await clickSidebarBtnAnalysisProcessingChains()
+    await expectExists({ text: 'This section is available only when survey is published' })
+  })
+
+  test('Publish root_entity error', async () => {
+    await click('Publish')
+    await waitForLoader()
+    await click('Ok')
+    await waitForLoader()
+    await click('Close')
+  })
+
+  test('Chain is empty', async () => {
+    await expectExists({ text: 'No items' })
+  })
+
+  test('Add new chain', async () => {
+    await click('New')
+    await clearTextBox({ selector: toRightOf('Processing chain label') })
+    await writeIntoTextBox({ text: 'Processing chain', selector: toRightOf('Processing chain label') })
+    await clearTextBox({ selector: toRightOf('Description') })
+    await writeIntoTextBox({ text: 'Processing description', selector: toRightOf('Description') })
+  })
+
+  test('Add new step', async () => {
+    await click(button(getElement({ selector: '.icon-plus' })), toRightOf('Processing steps'))
+    await click(textBox(toRightOf('Entity')))
+    await click('Tree')
+  })
+
+  test('Add new calculation', async () => {
+    await click(button(getElement({ selector: '.icon-plus' })), toRightOf('Calculation steps'))
+    await clearTextBox({ selector: toRightOf('Labels') })
+    await writeIntoTextBox({ text: 'Calculation label', selector: above('Quantitative') })
+  })
+
+  test('Add new calculation attribute', async () => {
+    await click('Add', toRightOf('Attribute'))
+
+    await clearTextBox({ selector: toRightOf('Name') })
+    await writeIntoTextBox({ text: 'attribute_name', selector: toRightOf('Name') })
+    await expectExists({ text: 'DECIMAL' })
+    await expectExists({ selector: '.icon-checkbox-checked', relativeSelectors: [toRightOf('Analysis')] })
+
+    await clearTextBox({ selector: toRightOf('Labels') })
+    await writeIntoTextBox({ text: 'Attribute label', selector: toRightOf('Labels') })
+
+    await clearTextBox({ selector: toRightOf('Descriptions') })
+    await writeIntoTextBox({ text: 'Attribute description', selector: toRightOf('Descriptions') })
+
+    await click('Save')
+
+    await waitForLoader()
+
+    await click('Back')
+  })
+
+  test('Check values after Add new calculation attribute', async () => {
+    await expectExists({ text: 'Processing chain', selector: toRightOf('Processing chain label') })
+    await expectExists({ text: 'Tree', selector: toRightOf('1') })
+    await expectExists({ text: 'Calculation label (Attribute label (C))' })
+    await expectExists({ text: 'Calculation label', selector: toRightOf('Labels') })
+    await expectExists({ text: 'Attribute label (C)', selector: toRightOf('Attribute') })
+  })
+
+  test('Save calculation', async () => {
+    await click('Save')
+    await waitForLoader()
+    await expectExists({ text: 'Saved!' })
+    await clickSidebarBtnAnalysisProcessingChains()
+    await expectExists({ text: 'Processing chain', selector: below('Label') })
+  })
+})
