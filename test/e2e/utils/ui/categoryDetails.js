9dc9e67341778dd7c8ac537da63b9ba0dceba842
ramirobg94 <ramirobg94@gmail.com>
2020-12-16 01:44:07 +0100

    remove console logs

--- test/e2e/utils/ui/categoryDetails.js
+++ test/e2e/utils/ui/categoryDetails.js
@@ -60,6 +60,8 @@ export const addCategoryItem = async ({ levelIndex, itemIndex, code, label }) =>
   await expectExists({ selector: selectorsItem.codeId({ levelIndex, itemIndex }) })
   await expectInputTextToBe({ text: '', selector: selectorsItem.code({ levelIndex, itemIndex }) })
   await writeIntoTextBox({ text: String(code), selector: selectorsItem.code({ levelIndex, itemIndex }) })
+  await waitFor(200)
+
   await expectInputTextToBe({ text: String(code), selector: selectorsItem.code({ levelIndex, itemIndex }) })
 
   await waitFor(200)
