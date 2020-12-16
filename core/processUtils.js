f1eadf380f3426eb9b28755bdf43cd5844243799
ramirobg94 <ramirobg94@gmail.com>
2020-12-16 12:27:51 +0100

    set port

--- core/processUtils.js
+++ core/processUtils.js
@@ -13,7 +13,7 @@ const [pgUser, pgPassword, pgHost, pgPort, pgDatabase] = dbUrlMatch
 const ENV = {
   arenaRoot: process.env.ARENA_ROOT,
   arenaDist: process.env.ARENA_DIST,
-  arenaPort: process.env.ARENA_PORT || '9090',
+  arenaPort: process.env.PORT || process.env.ARENA_PORT || '9090',
   nodeEnv: process.env.NODE_ENV || environments.development,
   debug: Boolean(process.env.DEBUG),
   tempFolder: process.env.TEMP_FOLDER || '/tmp/arena_upload',
