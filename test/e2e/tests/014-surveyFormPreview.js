42e4380e67bedc0b32db173adbc137610aab62cc
Ramiro Blázquez <ramirobg94@gmail.com>
2021-01-06 09:45:11 +0100

    add test 14 (#1318)
    * add test 14
    
    * extract nodedefs functions
    
    * Create test.js.yml
    
    * Update test.js.yml
    
    * Update .github/workflows/test.js.yml
    
    * force headless true
    
    * Update browser.js
    
    * Update test.js.yml
    
    * Update test.js.yml
    
    * Update test.js.yml
    
    * Update test.js.yml
    
    * Update test.js.yml
    
    * Update test.js.yml
    
    * Update test.js.yml
    
    * Update browser.js
    
    * Update test.js.yml
    
    * Update browser.js
    
    * Update test.js.yml
    
    * Update test.js.yml
    
    * Update test.js.yml
    
    * Update test.js.yml
    
    * Update test.js.yml
    
    * Update test.js.yml
    
    * Update test.js.yml
    
    * Update test.js.yml
    
    * remove travis
    
    * add chain test
    
    * fix tests
    
    * comment categories job
    
    * reload
    
    * force fail
    
    * survey should be published
    
    * add errors
    
    * add count errors
    
    * remove length
    
    * check errors
    
    * publish without category and taxonomy
    
    * close
    
    * now is published
    
    * allow taxonomies
    
    * uncomment category validation
    
    * get error
    
    * log error
    
    * revert category job
    
    * force category valid
    
    * force valid
    
    * remove force validation
    
    * force valid
    
    * skip one validation
    
    * keep one validation
    
    * comment error test
    
    * check if code is responsible
    
    * check if codeUniqueness is the error
    
    * check is validate item is free
    
    * leave only children
    
    * try with empty array
    
    * add validateRequired
    
    * force null
    
    * force null
    
    * leave codeRequired validation
    
    * left code validations empty
    
    * display logs
    
    * pm2-runtime
    
    * try logs
    
    * force last 1000 lines
    
    * add validations
    
    * add logs
    
    * add logs
    
    * add logs
    
    * sequential
    
    * set right port
    
    * force validation
    
    * remove some consoles
    
    * check if code exists
    
    * wait for 1 secong
    
    * check selectors
    
    * split steps
    
    * move code
    
    * change selectors
    
    * expectInputTextExists
    
    * adjust some selectors
    
    * clean prefix
    
    * move expect
    
    * change getElement button
    
    * now pass
    
    * right port
    
    * expect exists input
    
    * check code
    
    * expect
    
    * remove expect
    
    * remove waitFor
    
    * recover waitfor
    
    * recover watifor
    
    * remove validation
    
    * back to promiseUtils
    
    * remove commented code
    
    * remove console logs
    
    * expect input exists
    
    * remove commented code
    
    * remove checker
    
    * reload before taxa
    
    * check if PROVINCE exists
    
    * be more patient
    
    * right port
    
    * test errors
    
    * show errors
    
    * give more time
    
    * remove erros
    
    * silent --display errors-only
    
    * remove variable timmer
    
    * silent output
    
    * leave waitfor
    
    * deploy
    
    * rename
    
    * remove tests
    
    * check app version
    
    * deploy
    
    * add start command
    
    * change build
    
    * fix type
    
    * set github app version
    
    * remove duplicate
    
    * set port
    
    * dirname
    
    * add dist
    
    * move app user
    
    * Update appCluster.js
    
    * rename
    
    * add branch
    
    * deploy master
    
    * check users avatar
    
    * change path
    
    * set env vars
    
    * process path
    
    * process env
    
    * push master
    
    * change push to pull_request
    
    * on pull request
    
    * be defensive with path vars
    
    * remove logs
    
    * Update .github/workflows/test.js.yml
    
    * Update .github/workflows/test.js.yml
    
    * fix tests
    
    * get more time
    
    * avoid test date
    
    * add timeout
    
    * waitfor
    
    Co-authored-by: Miró Sorja <ping@mirosorja.com>

---
+++ test/e2e/tests/014-surveyFormPreview.js
@@ -0,0 +1,128 @@
+import * as NodeDef from '@core/survey/nodeDef'
+
+import { above, click, clickParent, dropDown, waitFor } from '../utils/api'
+import { waitForLoader } from '../utils/ui/loader'
+import { clickSidebarBtnSurveyForm } from '../utils/ui/sidebar'
+import { expectSurveyFormLoaded } from '../utils/ui/surveyForm'
+import { expectSurveyFormHasOnlyAndInOrderThesePages } from '../utils/ui/surveyFormPage'
+import { clickHeaderBtnMySurveys } from '../utils/ui/header'
+import { NodeDefsUtils } from '../utils/ui/nodeDefs'
+
+const getEnterFunction = ({ type }) => NodeDefsUtils[type].enterValue
+const getEnterParams = ({ type, ...values }) => ({ ...values })
+const getCheckFunction = ({ type }) => NodeDefsUtils[type].expectValue
+const getCheckParams = ({ type, ...values }) => ({ ...values })
+
+const doSequencial = async ({ items, getFunction, getParams }) =>
+  items.reduce(async (promise, item) => {
+    await promise
+    return getFunction(item)(getParams(item))
+  }, true)
+
+const enterValuesSequencial = async ({ items }) =>
+  doSequencial({ items, getFunction: getEnterFunction, getParams: getEnterParams })
+
+const checkValuesSequencial = async ({ items }) =>
+  doSequencial({ items, getFunction: getCheckFunction, getParams: getCheckParams })
+
+const enterValuesCluster = enterValuesSequencial
+const enterValuesPlot = async ({ items }) => {
+  await click('Plot')
+  await waitFor(500)
+  await click('Add')
+  await waitFor(500)
+  await enterValuesSequencial({ items })
+}
+
+const checkValuesCluster = checkValuesSequencial
+const checkValuesPlot = async ({ id, items }) => {
+  await click('Plot')
+  await waitFor(300)
+  await dropDown({ class: 'node-select' }).select(`Plot id - ${id}`)
+  await waitFor(300)
+  await checkValuesSequencial({ items })
+}
+
+const ClusterItems = [
+  { type: NodeDef.nodeDefType.integer, value: 1, label: 'Cluster id' },
+  { type: NodeDef.nodeDefType.decimal, value: 10, label: 'Cluster decimal' },
+  // { type: NodeDef.nodeDefType.date, value: '20/11/2020', label: 'Cluster date' },
+  { type: NodeDef.nodeDefType.time, value: '10:30', label: 'Cluster time' },
+  { type: NodeDef.nodeDefType.boolean, value: 'true', label: 'Cluster boolean' },
+  {
+    type: NodeDef.nodeDefType.coordinate,
+    x: 10,
+    y: 15,
+    srs: 'GCS WGS 1984 (EPSG:4326)',
+    label: 'Cluster coordinate',
+  },
+]
+
+const countryRelativeSelectors = () => above('Region')
+const plots = [
+  [
+    { type: NodeDef.nodeDefType.integer, value: 1, label: 'Plot id' },
+    { type: NodeDef.nodeDefType.text, value: 'text', label: 'Plot text' },
+
+    {
+      type: NodeDef.nodeDefType.code,
+      value: 'Country',
+      label: 'Country',
+      relativeSelectors: [countryRelativeSelectors],
+    },
+    { type: NodeDef.nodeDefType.code, value: 'Region 01', label: 'Region' },
+    { type: NodeDef.nodeDefType.code, value: 'District 0102', label: 'Province' },
+  ],
+  [
+    { type: NodeDef.nodeDefType.integer, value: 2, label: 'Plot id' },
+    { type: NodeDef.nodeDefType.text, value: 'text', label: 'Plot text' },
+
+    {
+      type: NodeDef.nodeDefType.code,
+      value: 'Country',
+      label: 'Country',
+      relativeSelectors: [countryRelativeSelectors],
+    },
+    { type: NodeDef.nodeDefType.code, value: 'Region 02', label: 'Region' },
+    { type: NodeDef.nodeDefType.code, value: 'District 0203', label: 'Province' },
+  ],
+]
+
+describe('SurveyForm Preview', () => {
+  test('Select survey 1', async () => {
+    await waitFor(2000)
+    await clickHeaderBtnMySurveys()
+    await clickParent('Survey')
+
+    await waitFor(1000)
+    await waitForLoader()
+
+    await waitFor(2000)
+    await clickSidebarBtnSurveyForm()
+
+    await expectSurveyFormLoaded()
+
+    await expectSurveyFormHasOnlyAndInOrderThesePages({ pageLabels: ['Cluster', 'Plot'] })
+  })
+
+  test('Open preview', async () => {
+    await click('Preview')
+    await expectSurveyFormHasOnlyAndInOrderThesePages({ pageLabels: ['Cluster', 'Plot'] })
+  })
+
+  test('Enter Cluster nodes', async () => enterValuesCluster({ items: ClusterItems }))
+
+  test('Enter Plot 1 values', async () => enterValuesPlot({ items: plots[0] }), 40000)
+
+  test('Enter Plot 2 values', async () => enterValuesPlot({ items: plots[1] }), 40000)
+
+  test('Check Cluster values', async () => {
+    await click('Cluster')
+    await waitFor(500)
+    await checkValuesCluster({ items: ClusterItems })
+  }, 40000)
+
+  test('Check Plot 1 values', async () => checkValuesPlot({ id: 1, items: plots[0] }), 40000)
+
+  test('Check Plot 2 values', async () => checkValuesPlot({ id: 2, items: plots[1] }), 40000)
+})
