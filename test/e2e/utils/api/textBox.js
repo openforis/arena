b149fd7075ad2546cfa96c91582b0047bc85b4e2
Stefano Ricci <SteRiccio@users.noreply.github.com>
2021-01-14 12:45:43 +0100

    Survey preview test: clear input fields before filling them

--- test/e2e/utils/api/textBox.js
+++ test/e2e/utils/api/textBox.js
@@ -3,7 +3,13 @@ import { clear, into, textBox, write, hover, $ } from 'taiko'
 export const clearTextBox = async ({ selector }) => clear(textBox(selector))
 export const hoverTextBox = async ({ selector }) => hover(textBox(selector))
 
-export const writeIntoTextBox = async ({ text, selector }) => write(text, into(textBox(selector)))
+export const writeIntoTextBox = async ({ text, selector, clearBefore = false }) => {
+  if (clearBefore) {
+    await clearTextBox({ selector })
+  }
+  await write(text, into(textBox(selector)))
+}
+
 export const writeIntoEl = ({ text, element, selector }) => write(text, into(element || $(selector)))
 
 export { textBox }
