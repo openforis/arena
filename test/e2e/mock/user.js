b3e518799fe6a0cd36170df80b225ddb7b5b10a0
Stefano Ricci <SteRiccio@users.noreply.github.com>
2021-04-12 16:49:01 +0200

    insert test user before e2e tests

--- test/e2e/mock/user.js
+++ test/e2e/mock/user.js
@@ -1,6 +1,8 @@
 export const user = {
   email: 'test@arena.com',
+  name: 'Tester',
   password: 'test',
+  passwordEncrypted: '$2a$10$6y2oUZVrQ7aXed.47h4sHeJA8VVA2dW9ObtO/XLveXSzQKBvTOyou',
   authGroup: {
     key: 'systemAdmin',
     label: 'System Administrators',
