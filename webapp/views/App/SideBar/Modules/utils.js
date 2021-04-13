945a7a7e8c927ec945a88b6544729bab752d9b8f
Stefano Ricci <SteRiccio@users.noreply.github.com>
2021-04-13 23:35:47 +0200

    Sidebar: hide data and usres modules for template surveys

--- webapp/views/App/SideBar/Modules/utils.js
+++ webapp/views/App/SideBar/Modules/utils.js
@@ -2,6 +2,7 @@ import { useRef } from 'react'
 import * as R from 'ramda'
 
 import * as Authorizer from '@core/auth/authorizer'
+import * as Survey from '@core/survey/survey'
 
 import {
   analysisModules,
@@ -20,36 +21,52 @@ const keys = {
   root: 'root',
   elementRef: 'elementRef',
   children: 'children',
+  hidden: 'hidden',
 }
 
 // ==== Modules hierarchy
-const getModule = (module, children = null, root = true) => ({
+const getModule = ({ module, children = null, root = true, hidden = false }) => ({
   [keys.key]: module.key,
   [keys.uri]: appModuleUri(module),
   [keys.icon]: module.icon,
   [keys.root]: root,
   [keys.elementRef]: useRef(null),
-  [keys.children]: children ? children.map((childModule) => getModule(childModule, null, false)) : [],
+  [keys.children]: children ? children.map((childModule) => getModule({ module: childModule, root: false })) : [],
+  [keys.hidden]: hidden,
 })
 
 export const getModulesHierarchy = (user, surveyInfo) => [
-  getModule(appModules.home),
-  getModule(appModules.designer, [
-    designerModules.formDesigner,
-    designerModules.surveyHierarchy,
-    designerModules.categories,
-    designerModules.taxonomies,
-  ]),
-  getModule(appModules.data, [
-    dataModules.records,
-    dataModules.explorer,
-    dataModules.export,
-    ...(Authorizer.canCleanseRecords(user, surveyInfo) ? [dataModules.validationReport] : []),
-  ]),
-  ...(Authorizer.canAnalyzeRecords(user, surveyInfo)
-    ? [getModule(appModules.analysis, [analysisModules.processingChains])]
-    : []),
-  getModule(appModules.users, [userModules.users]),
+  // home
+  getModule({ module: appModules.home }),
+  // designer
+  getModule({
+    module: appModules.designer,
+    children: [
+      designerModules.formDesigner,
+      designerModules.surveyHierarchy,
+      designerModules.categories,
+      designerModules.taxonomies,
+    ],
+  }),
+  // data
+  getModule({
+    module: appModules.data,
+    children: [
+      dataModules.records,
+      dataModules.explorer,
+      dataModules.export,
+      ...(Authorizer.canCleanseRecords(user, surveyInfo) ? [dataModules.validationReport] : []),
+    ],
+    hidden: Survey.isTemplate(surveyInfo),
+  }),
+  // analysis
+  getModule({
+    module: appModules.analysis,
+    children: [analysisModules.processingChains],
+    hidden: !Authorizer.canAnalyzeRecords(user, surveyInfo),
+  }),
+  // users
+  getModule({ module: appModules.users, children: [userModules.users], hidden: Survey.isTemplate(surveyInfo) }),
 ]
 
 export const getKey = R.prop(keys.key)
@@ -58,6 +75,7 @@ export const getIcon = R.prop(keys.icon)
 export const getChildren = R.prop(keys.children)
 
 export const isRoot = R.propEq(keys.root, true)
+export const isHidden = R.propEq(keys.hidden, true)
 export const isHome = (module) => getKey(module) === appModules.home.key
 export const isActive = (pathname) => (module) => {
   // Module home is active when page is on dashboard
