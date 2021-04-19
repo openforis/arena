382c79f53d78022bcae8f98ee812ed35dc18cafb
Stefano Ricci <SteRiccio@users.noreply.github.com>
2021-04-20 00:18:02 +0200

    removed unused component

--- webapp/views/App/views/Home/SurveyTemplateList/SurveyTemplateList.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import { Surveys } from '@webapp/components/survey/Surveys'
-
-const SurveyTemplateList = () => (
-  <Surveys module="surveyTemplates" moduleApiUri="/api/surveys" template title="appModules.surveyListTemplates" />
-)
-
-export default SurveyTemplateList
