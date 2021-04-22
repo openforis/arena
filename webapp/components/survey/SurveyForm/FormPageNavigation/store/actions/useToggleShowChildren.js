ab81bfd58524229d5d7f2ff7df9816d2cb1195ff
minotogna <mino.togna@gmail.com>
2021-04-22 09:56:10 +0200

    delete FormPageNavigation

--- webapp/components/survey/SurveyForm/FormPageNavigation/store/actions/useToggleShowChildren.js
+++
@@ -1,9 +0,0 @@
-import { useCallback } from 'react'
-
-import { State } from '../state'
-
-export const useToggleShowChildren = ({ setState }) => {
-  return useCallback(() => {
-    setState((state) => State.assocShowChildren(!State.getShowChildren(state))(state))
-  }, [])
-}
