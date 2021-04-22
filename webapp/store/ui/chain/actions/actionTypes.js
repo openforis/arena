73cbf38fe57bc4b8c600d3e09f8d9d525abb72e8
minotogna <mino.togna@gmail.com>
2021-04-22 09:55:03 +0200

    add selectEntity in ChainNodeDefs

--- webapp/store/ui/chain/actions/actionTypes.js
+++ webapp/store/ui/chain/actions/actionTypes.js
@@ -1,3 +1,4 @@
 export const ChainActionTypes = {
   chainUpdate: 'ui/chain/update',
+  entityDefUuidUpdate: 'ui/chain/entityDefUuid/update',
 }
--- webapp/views/App/views/Analysis/Chain/ChainNodeDefs/ChainNodeDefs.js
+++ webapp/views/App/views/Analysis/Chain/ChainNodeDefs/ChainNodeDefs.js
@@ -1,12 +1,20 @@
 import './ChainNodeDefs.scss'
 import React from 'react'
+import { useDispatch } from 'react-redux'
+
+import { ChainActions, useChainEntityDefUuid } from '@webapp/store/ui/chain'
 
 import { EntitySelectorTree } from '@webapp/components/survey/NodeDefsSelector'
 
 const ChainNodeDefs = () => {
+  const dispatch = useDispatch()
+  const entityDefUuid = useChainEntityDefUuid()
+
+  const selectEntity = (entityDef) => dispatch(ChainActions.updateEntityDefUuid(entityDef.uuid))
+
   return (
     <div className="chain-node-defs">
-      <EntitySelectorTree nodeDefUuidActive={null} onSelect={() => {}} />
+      <EntitySelectorTree nodeDefUuidActive={entityDefUuid} onSelect={selectEntity} />
     </div>
   )
 }
