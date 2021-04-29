ba13909f4ae39e65457cd4b136fe3c55c54c77a8
Stefano Ricci <SteRiccio@users.noreply.github.com>
2021-04-29 10:41:09 +0200

    Taxonomy: added getProps and getPropsDraft methods

--- core/survey/taxonomy.js
+++ core/survey/taxonomy.js
@@ -20,8 +20,8 @@ export const newTaxonomy = (props = {}) => ({
 })
 
 // READ
-export const { getUuid } = ObjectUtils
+export const { getDescriptions, getDescription, getProps, getPropsDraft, getUuid } = ObjectUtils
+
 export const getName = ObjectUtils.getProp(keysProps.name, '')
-export const { getDescriptions, getDescription } = ObjectUtils
 export const getVernacularLanguageCodes = ObjectUtils.getProp(keysProps.vernacularLanguageCodes, [])
 export const isPublished = R.propOr(false, keys.published)
