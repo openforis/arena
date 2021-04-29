adf1f81b89452af64cb2a54fb8fd9d708391314d
Stefano Ricci <SteRiccio@users.noreply.github.com>
2021-04-29 09:28:31 +0200

    ArenaImportJob: added activity log import job

--- server/modules/arenaImport/service/arenaImport/arenaImportJob.js
+++ server/modules/arenaImport/service/arenaImport/arenaImportJob.js
@@ -5,6 +5,7 @@ import Job from '@server/job/job'
 import * as SurveyManager from '@server/modules/survey/manager/surveyManager'
 import * as FileUtils from '@server/utils/file/fileUtils'
 
+import ActivityLogImportJob from './metaImportJobs/activityLogImportJob'
 import ArenaSurveyReaderJob from './metaImportJobs/arenaSurveyReaderJob'
 import SurveyCreatorJob from './metaImportJobs/surveyCreatorJob'
 import CategoriesImportJob from './metaImportJobs/categoriesImportJob'
@@ -16,21 +17,36 @@ import UsersImportJob from './metaImportJobs/usersImportJob'
 import ChainsImportJob from './metaImportJobs/chainsImportJob'
 import CreateRdbJob from './metaImportJobs/createRdb'
 
+const createInnerJobs = ({ backup = true }) => [
+  new ArenaSurveyReaderJob(),
+  new SurveyCreatorJob(),
+  new UsersImportJob(),
+  new TaxonomiesImportJob(),
+  new CategoriesImportJob(),
+  new NodeDefsImportJob(),
+  new ChainsImportJob(),
+  // when not restoring a survey backup, skip activity log, records and files
+  ...(backup ? [new ActivityLogImportJob(), new RecordsImportJob(), new FilesImportJob()] : []),
+  // Needed when the survey is published
+  new CreateRdbJob(),
+]
+
 export default class ArenaImportJob extends Job {
+  /**
+   * Creates a new import job to import a survey in Arena format.
+   * Records, files and activity log will be importin only when restoring a backup.
+   * If not restoring a backup, node definition, categories and taxonomies will be restored as draft.
+   *
+   * @param {!object} params - The import parameters.
+   * @param {!string} [params.filePath] - The file path of the file to import.
+   * @param {!object} [params.user] - The user performing the import.
+   * @param {boolean} [params.backup = true] - If true, props and propsDraft will be imported separately and records, files, activity logs will be imported.
+   * @param {object} [params.surveyInfoTarget = null] - Target survey info (optional).
+   * @returns {ArenaImportJob} - The import job.
+   */
   constructor(params) {
-    super(ArenaImportJob.type, params, [
-      new ArenaSurveyReaderJob(),
-      new SurveyCreatorJob(),
-      new UsersImportJob(),
-      new TaxonomiesImportJob(),
-      new CategoriesImportJob(),
-      new NodeDefsImportJob(),
-      new RecordsImportJob(),
-      new FilesImportJob(),
-      new ChainsImportJob(),
-      // Needed when the survey is published
-      new CreateRdbJob(),
-    ])
+    const { backup = true, ...paramsRest } = params
+    super(ArenaImportJob.type, { ...paramsRest, backup }, createInnerJobs(params))
   }
 
   async onStart() {
