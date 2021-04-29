5f0d92f76e21954aaf9c0e97280795dd777818d9
Stefano Ricci <SteRiccio@users.noreply.github.com>
2021-04-30 00:19:52 +0200

    trying to fix survey export test failing

--- test/e2e/tests/_surveyCreate/index.js
+++ test/e2e/tests/_surveyCreate/index.js
@@ -11,19 +11,16 @@ export const createSurvey = (surveyToAdd) => {
     await page.fill(getSelector(DataTestId.surveyCreate.surveyLabel, 'input'), label)
 
     if (cloneFrom) {
+      // set cloneFrom into Clone from field
       await page.click(`#${DataTestId.surveyCreate.surveyCloneFrom}`)
       await page.click(`text="${cloneFrom}"`)
-      await page.click(getSelector(DataTestId.surveyCreate.submitBtn, 'button'))
-      await Promise.all([
-        page.waitForNavigation(/* { url: `{BASE_URL}/app/home/dashboard/` } */),
-        page.click(DataTestId.modal.close),
-      ])
-    } else {
-      await Promise.all([
-        page.waitForNavigation(/* { url: `{BASE_URL}/app/home/dashboard/` } */),
-        page.click(getSelector(DataTestId.surveyCreate.submitBtn, 'button')),
-      ])
     }
+    // click Create Survey and wait for the job to complete
+    await Promise.all([
+      page.waitForNavigation(/* { url: `{BASE_URL}/app/home/dashboard/` } */),
+      ...(cloneFrom ? [page.click(DataTestId.modal.close)] : []),
+      page.click(getSelector(DataTestId.surveyCreate.submitBtn, 'button')),
+    ])
 
     const labelSelector = getSelector(DataTestId.dashboard.surveyLabel, 'h3')
     await expect(await page.innerText(labelSelector)).toBe(label.toUpperCase())
