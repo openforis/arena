78696496344c3555716a856aa8c3b013b612c358
Stefano Ricci <SteRiccio@users.noreply.github.com>
2021-04-30 10:06:51 +0200

    Survey create test: wait for the job dialog to appear

--- test/e2e/tests/_surveyCreate/index.js
+++ test/e2e/tests/_surveyCreate/index.js
@@ -11,14 +11,21 @@ export const createSurvey = (surveyToAdd) => {
     await page.fill(getSelector(DataTestId.surveyCreate.surveyLabel, 'input'), label)
 
     if (cloneFrom) {
+      // set survey 'Clone from' field
       await page.click(`#${DataTestId.surveyCreate.surveyCloneFrom}`)
       await page.click(`text="${cloneFrom}"`)
+
+      // press "Create survey" and wait for the job to complete
       await page.click(getSelector(DataTestId.surveyCreate.submitBtn, 'button'))
+      await page.waitForSelector(getSelector(DataTestId.modal.modal))
+
+      // close the job dialog and wait fot the navigation to the survey dashboard
       await Promise.all([
         page.waitForNavigation(/* { url: `{BASE_URL}/app/home/dashboard/` } */),
         page.click(DataTestId.modal.close),
       ])
     } else {
+      // press "Create survey" and wait for the navigation to the survey dashboard
       await Promise.all([
         page.waitForNavigation(/* { url: `{BASE_URL}/app/home/dashboard/` } */),
         page.click(getSelector(DataTestId.surveyCreate.submitBtn, 'button')),
