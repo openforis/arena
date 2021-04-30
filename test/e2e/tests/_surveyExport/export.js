4d3ceb9737d20e20ccf51c1d7aa7c9eb3745c55f
Stefano Ricci <SteRiccio@users.noreply.github.com>
2021-04-30 13:30:01 +0200

    added comments

--- test/e2e/tests/_surveyExport/export.js
+++ test/e2e/tests/_surveyExport/export.js
@@ -9,18 +9,24 @@ export const exportSurvey = (survey) =>
     const surveyZipPath = getSurveyZipPath(survey)
     const surveyDirPath = getSurveyDirPath(survey)
 
+    // click on Export button
     await page.click(getSelector(DataTestId.dashboard.surveyExportBtn, 'button'))
+
+    // wait for export job to complete
     await page.waitForSelector(getSelector(DataTestId.modal.modal))
 
+    // click on Donwload button and download generated file
     const [download] = await Promise.all([
       page.waitForEvent('download'),
       page.waitForResponse(/.+\/download\?.*/),
       page.click(DataTestId.surveyExport.downloadBtn),
     ])
 
+    // save and extract generated zip file
     await download.saveAs(surveyZipPath)
     const zip = new AdmZip(surveyZipPath)
     zip.extractAllTo(surveyDirPath, true, '')
 
+    // check that the exported files exist
     await expect(fs.existsSync(surveyDirPath)).toBeTruthy()
   })
