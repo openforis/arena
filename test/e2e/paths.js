f3f12082bf66e54723e35529017da9569ae4e073
Stefano Ricci <SteRiccio@users.noreply.github.com>
2021-04-30 13:13:09 +0200

    Tests: changed temporary downloads path

--- test/e2e/paths.js
+++ test/e2e/paths.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 
-const downloadsPath = process.env.GITHUB_WORKSPACE || path.resolve(__dirname, 'downloads')
+const downloadsPath = path.resolve(process.env.GITHUB_WORKSPACE || __dirname, 'temp')
 const getSurveyZipPath = (survey) => path.resolve(downloadsPath, `survey_${survey.name}.zip`)
 const getSurveyDirPath = (survey) => path.resolve(downloadsPath, `survey_${survey.name}`)
 const getSurveyEntry = (survey, entryPath) => {
