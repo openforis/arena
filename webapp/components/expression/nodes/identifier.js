16ce33d40add318e89aaec519cf946f70a30b283
Stefano Ricci <SteRiccio@users.noreply.github.com>
2021-06-07 13:58:48 +0200

    allow selecting virtual entity children in expression editor

--- webapp/components/expression/nodes/identifier.js
+++ webapp/components/expression/nodes/identifier.js
@@ -1,26 +1,51 @@
 import React from 'react'
-import * as R from 'ramda'
 import PropTypes from 'prop-types'
 
 import * as A from '@core/arena'
 
 import Select from '@webapp/components/form/Select'
 
+const findVariableByValue = ({ variables, value }) => variables.find((variable) => variable.value === value)
+
 const findValue = ({ variables, node }) => {
-  const value = R.find(R.propEq('value', node.name), variables)
+  const value = findVariableByValue({ variables, value: node.name })
   if (!A.isEmpty(value)) return value
+
   const options = variables.reduce((optionsAggregator, group) => [...optionsAggregator, ...(group.options || [])], [])
-  return R.find(R.propEq('value', node.name), options)
+  return findVariableByValue({ variables: options, value: node.name })
 }
 
-const Identifier = ({ node, variables, onChange }) => (
-  <Select
-    className="identifier"
-    options={variables}
-    value={findValue({ variables, node })}
-    onChange={(item) => onChange(R.assoc('name', R.propOr('', 'value', item), node))}
-  />
-)
+const excludeEntityVariables = (variables) => variables.filter((variable) => !variable.entity)
+
+const filterVariablesOrGroups = ({ variables }) => {
+  const variablesUpdated = excludeEntityVariables(variables)
+
+  return variablesUpdated.reduce((groupsAcc, group) => {
+    const groupUpdated = { ...group }
+    if (group.options) {
+      groupUpdated.options = excludeEntityVariables(group.options)
+    }
+    return [...groupsAcc, groupUpdated]
+  }, [])
+}
+
+const Identifier = ({ node, variables, onChange }) => {
+  // exclude entities from basic expression editor identifiers
+  const variablesFiltered = filterVariablesOrGroups({ variables })
+
+  return (
+    <Select
+      className="identifier"
+      options={variablesFiltered}
+      value={findValue({ variables: variablesFiltered, node })}
+      onChange={(item) => {
+        const name = item.value || ''
+        const expressionNodeUpdated = { ...node, name }
+        onChange(expressionNodeUpdated)
+      }}
+    />
+  )
+}
 
 Identifier.propTypes = {
   // Common props
