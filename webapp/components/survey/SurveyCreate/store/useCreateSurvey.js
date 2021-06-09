2519cbe29daa840b13d6d795ba09035e562b6df7
Stefano Ricci <1219739+SteRiccio@users.noreply.github.com>
2021-06-09 22:12:34 +0200

    Enhancement / create survey (#1511)
    * create new survey: added create survey type in UI
    
    * Survey clone/import: always consider imported survey languages and labels
    
    * fixed error cloning survey
    
    * fixed labels
    
    * fixed tests
    
    * fixed tests
    
    * fixed tests
    
    * fixed tests
    
    * fixed tests
    
    Co-authored-by: Stefano Ricci <SteRiccio@users.noreply.github.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- webapp/components/survey/SurveyCreate/store/useCreateSurvey.js
+++ webapp/components/survey/SurveyCreate/store/useCreateSurvey.js
@@ -2,7 +2,14 @@ import { useState } from 'react'
 
 import { useActions } from './actions'
 
-const initialSurvey = {
+export const createTypes = {
+  fromScratch: 'fromScratch',
+  clone: 'clone',
+  import: 'import',
+}
+
+const initialState = {
+  createType: 'fromScratch',
   name: '',
   label: '',
   lang: 'en',
@@ -12,17 +19,32 @@ const initialSurvey = {
 }
 
 export const useCreateSurvey = ({ template = false } = {}) => {
-  const [newSurvey, setNewSurvey] = useState({ ...initialSurvey, template })
+  const [newSurvey, setNewSurvey] = useState({ ...initialState, template })
 
   const { onUpdate, onCreate, onImport } = useActions({
     newSurvey,
     setNewSurvey,
   })
 
+  const onCreateTypeUpdate = (createType) => {
+    const newSurveyUpdated = { ...newSurvey, createType }
+
+    switch (createType) {
+      case createTypes.fromScratch:
+        break
+      default:
+        // reset label and lang (they will be hidden)
+        newSurveyUpdated.label = ''
+        newSurveyUpdated.lang = 'en'
+    }
+    setNewSurvey(newSurveyUpdated)
+  }
+
   return {
     newSurvey,
     onUpdate,
     onCreate,
     onImport,
+    onCreateTypeUpdate,
   }
 }
