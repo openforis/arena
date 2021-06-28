488ed898690d38624364ee6ef4aac7cc0eccbec1
Stefano Ricci <1219739+SteRiccio@users.noreply.github.com>
2021-06-28 17:19:52 +0200

    Show node def edit buttons only on mouse over (#1543)
    * Show node def edit buttons only on mouse over
    
    * fixed DeepScan issues
    
    * Move LabelWithTooltip to its own module
    
    * fixed tests
    
    Co-authored-by: Stefano Ricci <SteRiccio@users.noreply.github.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- test/e2e/tests/_formDesigner/index.js
+++ test/e2e/tests/_formDesigner/index.js
@@ -3,9 +3,22 @@ import { editNodeDefDetails } from '../_nodeDefDetails'
 import { getAtomicAttributeKeys, tree } from '../../mock/nodeDefs'
 import { dragAndDrop } from '../utils/dragDrop'
 
+const makeEditButtonsVisible = async ({ nodeDefName }) => {
+  const wrapperSelector = getSelector(DataTestId.surveyForm.nodeDefWrapper(nodeDefName))
+  await page.waitForSelector(wrapperSelector)
+  const wrapperEl = await page.$(wrapperSelector)
+  await expect(wrapperEl).not.toBeNull()
+
+  // move mouse inside node def wrapper to make edit buttons appear
+  const boundingBox = await wrapperEl.boundingBox()
+  await page.mouse.move(boundingBox.x, boundingBox.y)
+  await page.mouse.move(boundingBox.x + boundingBox.width / 2, boundingBox.y + boundingBox.height / 2, { steps: 2 })
+}
+
 // ==== add
 export const addNodeDef = (nodeDefParent, nodeDefChild, editDetails = true) => {
   test(`${nodeDefParent.label} -> ${nodeDefChild.label} add`, async () => {
+    await makeEditButtonsVisible({ nodeDefName: nodeDefParent.name })
     await page.click(getSelector(DataTestId.surveyForm.nodeDefAddChildBtn(nodeDefParent.name), 'button'))
     await Promise.all([page.waitForNavigation(), page.click(`text="${nodeDefChild.type}"`)])
   })
@@ -15,7 +28,7 @@ export const addNodeDef = (nodeDefParent, nodeDefChild, editDetails = true) => {
   if (nodeDefChild.type === 'entity') {
     test(`Expand ${nodeDefChild.name} table`, async () => {
       // expand table by 3 columns and 4 rows
-      const entityEl = await page.$(getSelector(tree.name))
+      const entityEl = await page.$(getSelector(DataTestId.surveyForm.nodeDefWrapper(tree.name)))
       const entityBBox = await entityEl.boundingBox()
       await dragAndDrop(
         entityBBox.x + entityBBox.width - 5,
@@ -48,10 +61,12 @@ export const addNodeDefSubPage = (nodeDefParent, nodeDefChild) => {
 // ==== edit
 export const editNodeDef = (formName, nodeDef, editDetails = true) => {
   test(`${nodeDef.label} edit`, async () => {
-    await Promise.all([
-      page.waitForNavigation(),
-      page.click(getSelector(DataTestId.surveyForm.nodeDefEditBtn(formName), 'a')),
-    ])
+    await makeEditButtonsVisible({ nodeDefName: formName })
+
+    const editBtnSelector = getSelector(DataTestId.surveyForm.nodeDefEditBtn(formName), 'a')
+    await page.waitForSelector(editBtnSelector)
+
+    await Promise.all([page.waitForNavigation(), page.click(editBtnSelector)])
   })
 
   if (editDetails) editNodeDefDetails(nodeDef)
