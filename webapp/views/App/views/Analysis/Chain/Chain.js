6d700531beb2c0f8440b087d082c54a238ca3b67
ramirobg94 <ramirobg94@gmail.com>
2021-06-29 11:43:42 +0200

    add validation

--- webapp/views/App/views/Analysis/Chain/Chain.js
+++ webapp/views/App/views/Analysis/Chain/Chain.js
@@ -1,17 +1,16 @@
 import './Chain.scss'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { matchPath, useParams, Prompt } from 'react-router'
 import { useDispatch } from 'react-redux'
 import classNames from 'classnames'
 
-import * as A from '@core/arena'
 import * as Validation from '@core/validation/validation'
 import * as Survey from '@core/survey/survey'
 import * as Chain from '@common/analysis/chain'
 
 import { analysisModules, appModuleUri } from '@webapp/app/appModules'
 import { ChainActions, useChain } from '@webapp/store/ui/chain'
-import { useSurveyCycleKeys, useSurveyInfo } from '@webapp/store/survey'
+import { useSurveyCycleKeys, useSurveyInfo, useSurveyLang, useSurvey } from '@webapp/store/survey'
 
 import { useI18n } from '@webapp/store/system'
 
@@ -19,9 +18,26 @@ import { useHistoryListen } from '@webapp/components/hooks'
 import LabelsEditor from '@webapp/components/survey/LabelsEditor'
 import CyclesSelector from '@webapp/components/survey/CyclesSelector'
 import ButtonRStudio from '@webapp/components/ButtonRStudio'
+import * as ChainValidator from '@common/analysis/chainValidator'
 
 import ButtonBar from './ButtonBar'
 import { AnalysisNodeDefs } from './AnalysisNodeDefs'
+
+const useLocalChainValidation = ({ chain }) => {
+  const [validation, setValidation] = useState({})
+  const lang = useSurveyLang()
+  const survey = useSurvey()
+
+  useEffect(() => {
+    const validate = async () => {
+      const _validation = await ChainValidator.validateChain({ chain, defaultLang: lang, survey })
+      setValidation(_validation)
+    }
+    validate()
+  }, [chain, lang, survey])
+
+  return validation
+}
 
 const ChainComponent = () => {
   const i18n = useI18n()
@@ -30,7 +46,7 @@ const ChainComponent = () => {
   const surveyInfo = useSurveyInfo()
   const cycleKeys = useSurveyCycleKeys()
   const chain = useChain()
-  const validation = Chain.getValidation(chain)
+  const validation = useLocalChainValidation({ chain })
 
   const _openRStudio = () => {
     dispatch(ChainActions.openRStudio({ chain }))
@@ -52,7 +68,10 @@ const ChainComponent = () => {
 
   return (
     <div className={classNames('chain', { 'with-cycles': cycleKeys.length > 1 })}>
-      <Prompt when={A.isEmpty(chain.props.labels)} message={i18n.t('chainView.errorNoLabel')} />
+      <Prompt
+        when={!Validation.isValid(Validation.getFieldValidation(Chain.keysProps.labels)(validation))}
+        message={i18n.t('chainView.errorNoLabel')}
+      />
 
       <ButtonRStudio onClick={_openRStudio} disabled={Survey.isDraft(surveyInfo)} />
 
