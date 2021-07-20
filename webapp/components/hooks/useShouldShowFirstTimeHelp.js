83ba883d241e64518965cd3b74cae0d4791d7dda
ramirobg94 <ramirobg94@gmail.com>
2021-07-20 09:44:01 +0200

    avoid fetch

--- webapp/components/hooks/useShouldShowFirstTimeHelp.js
+++ webapp/components/hooks/useShouldShowFirstTimeHelp.js
@@ -1,16 +1,18 @@
 import { useState, useEffect } from 'react'
 
 import * as ActivityLogObject from '@common/activityLog/activityLog'
+import * as Survey from '@core/survey/survey'
 
-export const useShouldShowFirstTimeHelp = ({ useFetchMessages }) => {
+export const useShouldShowFirstTimeHelp = ({ useFetchMessages, surveyInfo }) => {
   const [messages, setMessages] = useState([])
   const [help, setHelp] = useState(false)
 
   const fetchMessages = useFetchMessages({ messages, setMessages })
 
   useEffect(() => {
+    if(Survey.getUuid(surveyInfo))
     fetchMessages({ newest: true })
-  }, [])
+  }, [surveyInfo])
 
   useEffect(() => {
     if (messages.length > 0 && messages.length < 10) {
