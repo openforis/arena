47b1e7807830437cd09e447ca1814109c96b0c64
ramirobg94 <ramirobg94@gmail.com>
2021-08-04 14:49:58 +0200

    close rstudio

--- webapp/views/App/views/Analysis/Instances/Instances.js
+++ webapp/views/App/views/Analysis/Instances/Instances.js
@@ -7,7 +7,6 @@ import * as User from '@core/user/user'
 import * as A from '@core/arena'
 
 import { useI18n } from '@webapp/store/system'
-import ButtonRStudio from '@webapp/components/ButtonRStudio'
 import { useUser } from '@webapp/store/user'
 
 const Instances = () => {
@@ -31,27 +30,13 @@ const Instances = () => {
     }
   }
 
-  const requestRStudioInstances = async () => {
-    try {
-      setLoading(true)
-      const { data = {} } = await axios.post('/api/rstudio')
-      const { instanceId = false, rStudioProxyUrl = false } = data
-      setInstance({ instanceId })
-      setUrl(`${rStudioProxyUrl}${instanceId}_${User.getUuid(user)}`)
-    } catch (err) {
-      return false
-    } finally {
-      setLoading(false)
-    }
-  }
-
   const closeRStudioInstance = async () => {
     try {
       setLoading(true)
       const { instanceId } = instance
       await axios.delete('/api/rstudio', { params: { instanceId } })
       setInstance(null)
-      setUrl(false) 
+      setUrl(false)
     } finally {
       setLoading(false)
     }
@@ -65,7 +50,7 @@ const Instances = () => {
   return (
     <div className="instances-container">
       <p>{i18n.t('instancesView.title')}</p>
-      {!A.isEmpty(instance) ? (
+      {!A.isEmpty(instance) && (
         <div className="instance-row">
           <a href={url} target="_blank">
             <p>
@@ -74,8 +59,6 @@ const Instances = () => {
           </a>
           <button onClick={closeRStudioInstance}>{i18n.t('instancesView.terminate')}</button>
         </div>
-      ) : (
-        <ButtonRStudio onClick={requestRStudioInstances} />
       )}
     </div>
   )
