3d11bd7df72897533b1b4a16207dc0cdb146d89b
Stefano Ricci <1219739+SteRiccio@users.noreply.github.com>
2021-08-30 09:27:09 +0200

    fixed srs not imported from Collect survey (#1721)
    Co-authored-by: Stefano Ricci <SteRiccio@users.noreply.github.com>

--- server/modules/collectImport/service/collectImport/metaImportJobs/surveyCreatorJob.js
+++ server/modules/collectImport/service/collectImport/metaImportJobs/surveyCreatorJob.js
@@ -1,6 +1,9 @@
 import * as R from 'ramda'
 
+import { SRSs } from '@openforis/arena-core'
+
 import * as Survey from '../../../../../../core/survey/survey'
+import * as Srs from '../../../../../../core/geo/srs'
 import * as User from '../../../../../../core/user/user'
 
 import * as ActivityLog from '../../../../../../common/activityLog/activityLog'
@@ -13,6 +16,8 @@ import * as SurveyManager from '../../../../survey/manager/surveyManager'
 
 import * as CollectSurvey from '../model/collectSurvey'
 import { findUniqueSurveyName } from './surveyUniqueNameGenerator'
+
+const SRS_ID_PREFIX = 'EPSG:'
 
 export default class SurveyCreatorJob extends Job {
   constructor(params) {
@@ -36,6 +41,8 @@ export default class SurveyCreatorJob extends Job {
 
     const descriptions = CollectSurvey.toLabels('description', defaultLanguage)(collectSurvey)
 
+    const srss = await this.extractSrss()
+
     const surveyInfo = Survey.newSurvey({
       ownerUuid: User.getUuid(this.user),
       name,
@@ -44,6 +51,7 @@ export default class SurveyCreatorJob extends Job {
       [Survey.infoKeys.collectUri]: collectUri,
       [Survey.infoKeys.descriptions]: descriptions,
       [Survey.infoKeys.labels]: labels,
+      [Survey.infoKeys.srs]: srss,
     })
 
     // insert survey out of transaction to avoid lock and do not update user prefs for the same reason
@@ -63,5 +71,24 @@ export default class SurveyCreatorJob extends Job {
     await ActivityLogManager.insert(this.user, surveyId, ActivityLog.type.surveyCollectImport, null, false, this.tx)
 
     this.setContext({ survey, surveyId, defaultLanguage })
+  }
+
+  async extractSrss() {
+    const { collectSurvey } = this.context
+
+    await SRSs.init()
+
+    const srsElements = CollectSurvey.getElementsByPath(['spatialReferenceSystems', 'spatialReferenceSystem'])(
+      collectSurvey
+    )
+    return srsElements.reduce((srsAcc, srsEl) => {
+      const srsId = CollectSurvey.getAttribute('srid')(srsEl)
+      let srs = SRSs.getSRSByCode(srsId)
+      if (!srs && srsId.startsWith(SRS_ID_PREFIX)) {
+        srs = SRSs.getSRSByCode(srsId.substring(SRS_ID_PREFIX.length))
+      }
+      if (!srs) return srsAcc
+      return [...srsAcc, R.omit([Srs.keys.wkt], srs)]
+    }, [])
   }
 }
