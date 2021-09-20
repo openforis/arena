23f39a3237081281d15a1e6377bee55bd7db8779
Stefano Ricci <SteRiccio@users.noreply.github.com>
2021-09-20 14:17:17 +0200

    fixed errors in browser console

--- webapp/components/survey/SurveyForm/components/FormPageEditButtons/FormPagesEditButtons.js
+++ webapp/components/survey/SurveyForm/components/FormPageEditButtons/FormPagesEditButtons.js
@@ -1,11 +1,9 @@
 import React from 'react'
 
 import { Button } from '@webapp/components/buttons'
-import { useI18n } from '@webapp/store/system'
 import { useFormPageEditButtons } from './useFormPageEditButtons'
 
 export const FormPagesEditButtons = () => {
-  const i18n = useI18n()
   const { onMoveUp, onMoveDown, upDisabled, downDisabled } = useFormPageEditButtons()
 
   return (
@@ -13,14 +11,14 @@ export const FormPagesEditButtons = () => {
       <Button
         size="small"
         iconClassName="icon-arrow-up2 icon-12px"
-        title={i18n.t('surveyForm.movePageUp')}
+        title={'surveyForm.movePageUp'}
         disabled={upDisabled}
         onClick={onMoveUp}
       />
       <Button
         size="small"
         iconClassName="icon-arrow-down2 icon-12px"
-        title={i18n.t('surveyForm.movePageDown')}
+        title={'surveyForm.movePageDown'}
         disabled={downDisabled}
         onClick={onMoveDown}
       />
