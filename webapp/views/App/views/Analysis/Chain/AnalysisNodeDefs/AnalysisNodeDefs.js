965b69019c6abb4b70c24cc96f3948dff6272f95
ramirobg94 <ramirobg94@gmail.com>
2021-09-27 09:50:45 +0200

    add entity column

--- webapp/views/App/views/Analysis/Chain/AnalysisNodeDefs/AnalysisNodeDefs.js
+++ webapp/views/App/views/Analysis/Chain/AnalysisNodeDefs/AnalysisNodeDefs.js
@@ -5,19 +5,16 @@ import { useDispatch } from 'react-redux'
 import * as NodeDef from '@core/survey/nodeDef'
 import * as Survey from '@core/survey/survey'
 import * as Chain from '@common/analysis/chain'
-import { ChainActions, useChainEntityDefUuid, useChain } from '@webapp/store/ui/chain'
+import { useChainEntityDefUuid, useChain } from '@webapp/store/ui/chain'
 import { useSurvey } from '@webapp/store/survey'
 import { useI18n } from '@webapp/store/system'
 import ErrorBadge from '@webapp/components/errorBadge'
-
-import { EntitySelectorTree } from '@webapp/components/survey/NodeDefsSelector'
 
 import { AnalysisNodeDefsHeader } from './AnalysisNodeDefsHeader'
 import { AnalysisNodeDef } from './AnalysisNodeDef'
 import { useSortAnalysisNodeDefs } from './hooks'
 
 const AnalysisNodeDefs = () => {
-  const dispatch = useDispatch()
   const i18n = useI18n()
   const entityDefUuid = useChainEntityDefUuid()
 
@@ -25,16 +22,10 @@ const AnalysisNodeDefs = () => {
   const validation = Chain.getValidation(chain)
   const survey = useSurvey()
 
-  const analysisNodeDefsRef = useRef(null)
-
-  const selectEntity = (entityDef) => dispatch(ChainActions.updateEntityDefUuid(entityDef.uuid))
-  const getLabelSuffix = (entityDef) => {
-    const count = Survey.getAnalysisNodeDefs({ entity: entityDef, chain })(survey).length
-    return count ? ` (${count})` : ''
-  }
+  const analysisNodeDefsRef = useRef(null) 
 
   const _analysisNodeDefsToShow = useMemo(
-    () => Survey.getAnalysisNodeDefs({ chain, entityDefUuid })(survey),
+    () => Survey.getAnalysisNodeDefs({ chain })(survey),
     [chain, survey, entityDefUuid]
   )
 
@@ -42,8 +33,6 @@ const AnalysisNodeDefs = () => {
 
   return (
     <div className="analysis-node-defs-wrapper">
-      <EntitySelectorTree getLabelSuffix={getLabelSuffix} nodeDefUuidActive={entityDefUuid} onSelect={selectEntity} />
-
       <div className="analysis-node-defs" ref={analysisNodeDefsRef}>
         {!entityDefUuid && Survey.getAnalysisNodeDefs({ chain })(survey).length <= 0 && (
           <div className="analysis-node-defs-error">
@@ -51,26 +40,26 @@ const AnalysisNodeDefs = () => {
             <p>{i18n.t('chain.emptyNodeDefs')}</p>
           </div>
         )}
-        {entityDefUuid && (
-          <>
-            <AnalysisNodeDefsHeader />
 
-            {_analysisNodeDefsToShow.length > 0 && (
-              <div className="analysis-node-def__list-header">
-                <div />
-                <div>{i18n.t('common.name')}</div>
-                <div>{i18n.t('common.label')}</div>
-                <div>{i18n.t('common.type')}</div>
-                <div>{i18n.t('common.active')}</div>
-                <div />
-              </div>
-            )}
+        <>
+          <AnalysisNodeDefsHeader />
 
-            {_analysisNodeDefsToShow.map((analysisNodeDef) => (
-              <AnalysisNodeDef key={NodeDef.getUuid(analysisNodeDef)} nodeDefUuid={NodeDef.getUuid(analysisNodeDef)} />
-            ))}
-          </>
-        )}
+          {_analysisNodeDefsToShow.length > 0 && (
+            <div className="analysis-node-def__list-header">
+              <div />
+              <div>{i18n.t('common.entity')}</div>
+              <div>{i18n.t('common.name')}</div>
+              <div>{i18n.t('common.label')}</div>
+              <div>{i18n.t('common.type')}</div>
+              <div>{i18n.t('common.active')}</div>
+              <div />
+            </div>
+          )}
+
+          {_analysisNodeDefsToShow.map((analysisNodeDef) => (
+            <AnalysisNodeDef key={NodeDef.getUuid(analysisNodeDef)} nodeDefUuid={NodeDef.getUuid(analysisNodeDef)} />
+          ))}
+        </>
       </div>
     </div>
   )
