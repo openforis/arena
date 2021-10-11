fa5d61d06ddc95bc43ad6628986166c0f1322b11
Stefano Ricci <SteRiccio@users.noreply.github.com>
2021-10-11 14:36:37 +0200

    fixed DeepScan issue

--- webapp/components/expression/nodes/binary.js
+++ webapp/components/expression/nodes/binary.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types'
 import * as R from 'ramda'
 
 import * as Expression from '@core/expressionParser/expression'
-import * as NodeDef from '@core/survey/nodeDef'
 
 import Dropdown from '../../form/Dropdown'
 import BinaryOperand, { BinaryOperandType } from './binaryOperand'
