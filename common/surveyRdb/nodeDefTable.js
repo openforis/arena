c4d8dad73f3c59c65cf19dd35242968c11817b3e
Ramiro Blázquez <ramirobg94@gmail.com>
2021-10-12 13:51:15 +0200

    code attributes (#1845)
    * code attributes
    
    * fix script
    
    * simpligy code
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- common/surveyRdb/nodeDefTable.js
+++ common/surveyRdb/nodeDefTable.js
@@ -1,7 +1,6 @@
 import * as R from 'ramda'
 import * as toSnakeCase from 'to-snake-case'
 
-import * as Survey from '@core/survey/survey'
 import * as NodeDef from '@core/survey/nodeDef'
 
 const viewSuffix = '_view'
@@ -31,33 +30,26 @@ const getCols = (nodeDef) => R.propOr([], NodeDef.getType(nodeDef), colsByType)
 const getDefaultColumnName = (nodeDef) =>
   NodeDef.isEntity(nodeDef) ? `${NodeDef.getName(nodeDef)}_uuid` : `${NodeDef.getName(nodeDef)}`
 
-export const getColumnNames = (nodeDef) => {
-  const cols = getCols(nodeDef)
+export const getColumnNames = (nodeDef, includeExtendedCols = true ) => {
+  const cols = includeExtendedCols ? getCols(nodeDef) : []
   return R.isEmpty(cols) ? [getDefaultColumnName(nodeDef)] : cols.map((col) => `${NodeDef.getName(nodeDef)}_${col}`)
 }
 
 export const getColumnName = R.pipe(getColumnNames, R.head)
 
-export const getNodeDefsWithColumnNames = (nodeDefs) =>
+export const getNodeDefsWithColumnNames = ({nodeDefs, includeExtendedCols}) =>
   nodeDefs.flatMap((nodeDef) => {
-    const columnNames = getColumnNames(nodeDef)
+    const columnNames = getColumnNames(nodeDef, includeExtendedCols)
     return columnNames.map((colName) => ({ columnName: colName, nodeDef }))
   })
 
-export const getNodeDefsColumnNames = (nodeDefs) =>
-  getNodeDefsWithColumnNames(nodeDefs).flatMap(({ columnName }) => columnName)
+export const getNodeDefsColumnNames = ({nodeDefs, includeExtendedCols}) =>
+  getNodeDefsWithColumnNames({nodeDefs, includeExtendedCols}).flatMap(({ columnName }) => columnName)
 
-export const getNodeDefsByColumnNames = (nodeDefs) =>
-  getNodeDefsWithColumnNames(nodeDefs).reduce(
+export const getNodeDefsByColumnNames = ({nodeDefs, includeExtendedCols}) =>
+  getNodeDefsWithColumnNames({nodeDefs, includeExtendedCols}).reduce(
     (_nodeDefs, { columnName, nodeDef }) => ({ ..._nodeDefs, [columnName]: nodeDef }),
     {}
-  )
-
-export const getColumnNamesByUuids = (nodeDefUuidCols) => (survey) =>
-  R.reduce(
-    (cols, uuid) => R.pipe(Survey.getNodeDefByUuid(uuid), getColumnNames, R.concat(cols))(survey),
-    [],
-    nodeDefUuidCols
   )
 
 export const extractColumnName = (nodeDef, col) =>
