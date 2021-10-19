594a8e677823a4b64a77ac4aa45da4d288597d9a
Stefano Ricci <SteRiccio@users.noreply.github.com>
2021-10-19 14:29:36 +0200

    fixed DeepScan issue

--- webapp/components/form/Dropdown/store/actions/useUpdateSelection.js
+++ webapp/components/form/Dropdown/store/actions/useUpdateSelection.js
@@ -7,15 +7,18 @@ import { useCloseDialog } from './useCloseDialog'
 export const useUpdateSelection = ({ onBeforeChange, onChange, setState }) => {
   const closeDialog = useCloseDialog({ setState })
 
-  return useCallback(async ({ item, selection, state }) => {
-    const keySelection = selection && State.getItemKey(state)(selection)
-    const keyItem = State.getItemKey(state)(item)
+  return useCallback(
+    async ({ item, selection, state }) => {
+      const keySelection = selection && State.getItemKey(state)(selection)
+      const keyItem = State.getItemKey(state)(item)
 
-    if (keyItem !== keySelection && (!onBeforeChange || (await onBeforeChange(item)))) {
-      await onChange(item)
-    }
-    const selectionNew = keyItem === keySelection ? selection : item
+      if (keyItem !== keySelection && (!onBeforeChange || (await onBeforeChange(item)))) {
+        await onChange(item)
+      }
+      const selectionNew = keyItem === keySelection ? selection : item
 
-    closeDialog({ selection: selectionNew })
-  })
+      closeDialog({ selection: selectionNew })
+    },
+    [onBeforeChange, onChange, setState]
+  )
 }
