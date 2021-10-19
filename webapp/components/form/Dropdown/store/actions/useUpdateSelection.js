d761f12608ffd690e9d3d2015a7bff45d6977cda
Stefano Ricci <SteRiccio@users.noreply.github.com>
2021-10-19 12:47:26 +0200

    always close dropdown on selection

--- webapp/components/form/Dropdown/store/actions/useUpdateSelection.js
+++ webapp/components/form/Dropdown/store/actions/useUpdateSelection.js
@@ -1,12 +1,21 @@
+import { useCallback } from 'react'
+
 import { State } from '../state'
 
-export const useUpdateSelection =
-  ({ onBeforeChange, onChange }) =>
-  async ({ item, selection, state }) => {
+import { useCloseDialog } from './useCloseDialog'
+
+export const useUpdateSelection = ({ onBeforeChange, onChange, setState }) => {
+  const closeDialog = useCloseDialog({ setState })
+
+  return useCallback(async ({ item, selection, state }) => {
     const keySelection = selection && State.getItemKey(state)(selection)
     const keyItem = State.getItemKey(state)(item)
 
     if (keyItem !== keySelection && (!onBeforeChange || (await onBeforeChange(item)))) {
       await onChange(item)
     }
-  }
+    const selectionNew = keyItem === keySelection ? selection : item
+
+    closeDialog({ selection: selectionNew })
+  })
+}
