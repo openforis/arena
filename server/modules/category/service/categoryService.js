65dcb3cb6acbe76a8277ee4a9185cec1a606e67b
Stefano Ricci <SteRiccio@users.noreply.github.com>
2021-10-27 10:10:20 +0200

    refactored category export

--- server/modules/category/service/categoryService.js
+++ server/modules/category/service/categoryService.js
@@ -1,13 +1,8 @@
 import * as JobManager from '@server/job/jobManager'
-import * as SurveyManager from '@server/modules/survey/manager/surveyManager'
 
 import * as Category from '@core/survey/category'
 
-import * as R from 'ramda'
-import * as Survey from '@core/survey/survey'
 import * as Response from '@server/utils/response'
-import * as CSVWriter from '@server/utils/file/csvWriter'
-import { db } from '@server/db/db'
 import * as CategoryImportJobParams from './categoryImportJobParams'
 import CategoryImportJob from './categoryImportJob'
 import CategoriesExportJob from './CategoriesExportJob'
@@ -27,38 +22,24 @@ export const importCategory = (user, surveyId, categoryUuid, summary) => {
 }
 
 export const exportCategory = async (surveyId, categoryUuid, draft, res) => {
-  const category = await CategoryManager.fetchCategoryAndLevelsByUuid({ surveyId, categoryUuid, draft })
   const numberOfItemsInCategory = await CategoryManager.countItemsByCategoryUuid(surveyId, categoryUuid)
+  const categoryIsEmpty = numberOfItemsInCategory <= 0
 
-  if (numberOfItemsInCategory <= 0) {
-    return CategoryManager.getCategoryExportTemplate({ res })
+  let fileName
+  if (categoryIsEmpty) {
+    fileName = 'template_category_hierarchical.csv'
+  } else {
+    const category = await CategoryManager.fetchCategoryAndLevelsByUuid({ surveyId, categoryUuid, draft })
+    fileName = `${Category.getName(category) || 'category'}.csv`
   }
-
-  const levels = Category.getLevelsArray(category)
-
-  // get survey languages
-  const survey = await SurveyManager.fetchSurveyById({ surveyId, draft })
-  const languages = R.pipe(Survey.getSurveyInfo, Survey.getLanguages)(survey)
-
-  const {
-    stream: categoryStream,
-    headers = [],
-    extraPropsHeaders = [],
-  } = CategoryManager.getCategoryStreamAndHeaders({
-    surveyId,
-    categoryUuid,
-    levels,
-    languages,
-    category,
-  })
-
-  const fileName = `${Category.getName(category) || 'category'}_code_list_hierarchical.csv`
-
   Response.setContentTypeFile(res, fileName, null, Response.contentTypes.csv)
 
-  return db.stream(categoryStream, (stream) => {
-    stream.pipe(CSVWriter.transformToStream(res, [...headers, ...extraPropsHeaders]))
-  })
+  if (categoryIsEmpty) {
+    // template
+    await CategoryManager.writeCategoryExportTemplateToStream({ outputStream: res })
+  } else {
+    await CategoryManager.exportCategoryToStream({ surveyId, categoryUuid, draft, outputStream: res })
+  }
 }
 
 export const exportAllCategories = async ({ surveyId, draft }) => {
