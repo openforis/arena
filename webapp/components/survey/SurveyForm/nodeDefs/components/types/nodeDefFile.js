a1a1f73a7a8ff4e501154d2b8050ab64ac3fddd5
Ramiro Blázquez <ramirobg94@gmail.com>
2021-10-27 14:03:59 +0200

    preview (#1879)
    * preview
    
    * add type
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- webapp/components/survey/SurveyForm/nodeDefs/components/types/nodeDefFile.js
+++ webapp/components/survey/SurveyForm/nodeDefs/components/types/nodeDefFile.js
@@ -1,14 +1,41 @@
 import './nodeDefFile.scss'
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
 import { uuidv4 } from '@core/uuid'
 
 import UploadButton from '@webapp/components/form/uploadButton'
 import DownloadButton from '@webapp/components/form/downloadButton'
+import Tooltip from '@webapp/components/tooltip'
 
 import * as NodeDef from '@core/survey/nodeDef'
 import * as Node from '@core/record/node'
 import NodeDeleteButton from '../nodeDeleteButton'
+
+const TooltipPreviewImage = ({ path }) => {
+  const [imageUrl, setImageUrl] = useState(false)
+  const [data, setData] = useState(false)
+  const getImage = async () => {
+    const { data } = await axios.get(path, {
+      responseType: 'blob',
+    })
+    setData(data)
+  }
+
+  useEffect(() => {
+    getImage()
+    return () => {}
+  }, [])
+
+  useEffect(() => {
+    if (data) {
+      setImageUrl(URL.createObjectURL(data))
+    }
+  }, [data])
+
+  return <div className="survey-form__node-def-file__preview-image">{imageUrl && <img src={imageUrl} />}</div>
+}
 
 const handleFileChange = (nodeDef, node, file, updateNode) => {
   const value = {
@@ -34,6 +61,15 @@ const FileInput = (props) => {
   const fileName = Node.getFileName(node)
   const fileUploaded = !edit && fileName
 
+  const renderPreviewComponent = () => {
+    if (NodeDef.getFileType(nodeDef) !== NodeDef.fileTypeValues.image) return null
+    return (
+      <TooltipPreviewImage
+        path={`/api/survey/${surveyInfo.id}/record/${Node.getRecordUuid(node)}/nodes/${Node.getUuid(node)}/file`}
+      />
+    )
+  }
+
   return (
     <div className="survey-form__node-def-file">
       <UploadButton
@@ -44,12 +80,14 @@ const FileInput = (props) => {
 
       {fileUploaded && (
         <React.Fragment>
-          <DownloadButton
-            href={`/api/survey/${surveyInfo.id}/record/${Node.getRecordUuid(node)}/nodes/${Node.getUuid(node)}/file`}
-            label={fileName}
-            title={fileName}
-            className="ellipsis"
-          />
+          <Tooltip tooltipComponent={renderPreviewComponent} type="info">
+            <DownloadButton
+              href={`/api/survey/${surveyInfo.id}/record/${Node.getRecordUuid(node)}/nodes/${Node.getUuid(node)}/file`}
+              label={fileName}
+              title={fileName}
+              className="ellipsis"
+            />
+          </Tooltip>
 
           <NodeDeleteButton
             nodeDef={nodeDef}
