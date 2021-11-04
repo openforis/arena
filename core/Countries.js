130170ddb8393f9a5d10c9a2842adde921ba312a
Stefano Ricci <1219739+SteRiccio@users.noreply.github.com>
2021-11-04 13:16:42 +0100

    user access request: store country code in request (#1898)
    Co-authored-by: Stefano Ricci <SteRiccio@users.noreply.github.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- core/Countries.js
+++ core/Countries.js
@@ -1,7 +1,12 @@
 import countriesData from './countries_data.json'
 
-const getCountries = () => Object.entries(countriesData).reduce((acc, [code, item]) => [...acc, { code, ...item }], [])
+const list = () => Object.entries(countriesData).reduce((acc, [code, item]) => [...acc, { code, ...item }], [])
+
+const _getCountry = ({ code }) => countriesData[code]
+
+const getCountryName = ({ code, defaultToCode = true }) => _getCountry({ code })?.name || code
 
 export const Countries = {
-  list: getCountries,
+  list,
+  getCountryName,
 }
