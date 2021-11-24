96fcedd51fecfee7ae6d6eb658817811c45d6f3e
Stefano Ricci <SteRiccio@users.noreply.github.com>
2021-11-24 14:58:52 +0100

    solved DeepScan issue

--- webapp/views/App/views/Data/MapView/MapView.js
+++ webapp/views/App/views/Data/MapView/MapView.js
@@ -1,8 +1,4 @@
-import React, { useEffect } from 'react'
-
-// import * as Survey from '@core/survey/survey'
-
-import { useSurvey } from '@webapp/store/survey'
+import React from 'react'
 
 import { Map } from '@webapp/components/Map'
 import SurveyDefsLoader from '@webapp/components/survey/SurveyDefsLoader'
@@ -10,14 +6,6 @@ import SurveyDefsLoader from '@webapp/components/survey/SurveyDefsLoader'
 import { SamplingPointDataLayer } from './SamplingPointDataLayer'
 
 export const MapView = () => {
-  const survey = useSurvey()
-
-  useEffect(() => {
-    // const samplingPointCategory = Survey.getCategoryByName(Survey.samplingPointDataCategoryName)(survey)
-    // if (samplingPointCategory) {
-    // }
-  }, [])
-
   return (
     <SurveyDefsLoader draft validate={false}>
       <Map layers={<SamplingPointDataLayer />} />
