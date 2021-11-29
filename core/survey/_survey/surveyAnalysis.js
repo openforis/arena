d3ef6e2d7bdcddc76222c2647dab21630f98b52b
Ramiro Blázquez <ramirobg94@gmail.com>
2021-11-29 16:27:17 +0100

    dont show baseunit nodedefs without result variable sibilings

--- core/survey/_survey/surveyAnalysis.js
+++ core/survey/_survey/surveyAnalysis.js
@@ -7,21 +7,43 @@ import { getHierarchy, traverseHierarchyItemSync } from './surveyNodeDefs'
 
 // ====== READ
 export const getAnalysisNodeDefs =
-  ({ chain, entity, entityDefUuid }) =>
+  ({ chain, entity, entityDefUuid, showBaseUnit = true }) =>
   (survey) => {
     let nodeDefs = SurveyNodeDefs.getNodeDefsArray(survey).filter(NodeDef.isAnalysis)
 
+    // nedeDefs in this entity by chain
     if (chain) {
-      nodeDefs = nodeDefs.filter((nodeDef) => {
-        return NodeDef.getPropOrDraftAdvanced(NodeDef.keysPropsAdvanced.chainUuid)(nodeDef) === Chain.getUuid(chain)
-      })
+      nodeDefs = nodeDefs.filter(
+        (nodeDef) =>
+          NodeDef.getPropOrDraftAdvanced(NodeDef.keysPropsAdvanced.chainUuid)(nodeDef) === Chain.getUuid(chain)
+      )
     }
 
+    // nedeDefs in this entity
     if (entity) {
       nodeDefs = nodeDefs.filter((nodeDef) => NodeDef.getParentUuid(nodeDef) === NodeDef.getUuid(entity))
     }
+
+    // nedeDefs in this entity by entityUuid
     if (entityDefUuid) {
       nodeDefs = nodeDefs.filter((nodeDef) => NodeDef.getParentUuid(nodeDef) === entityDefUuid)
+    }
+
+    nodeDefs = nodeDefs.filter((_nodeDef) => showBaseUnit || !NodeDef.isBaseUnit(_nodeDef))
+
+    // show base unit nodeDefs with nodeDef analysis sibilings
+    if (showBaseUnit) {
+      nodeDefs = nodeDefs.filter((nodeDef) => {
+        if (NodeDef.isBaseUnit(nodeDef)) {
+          const hasAnalysisSibilings = nodeDefs.some(
+            (_nodeDef) =>
+              NodeDef.getParentUuid(nodeDef) === NodeDef.getParentUuid(_nodeDef) &&
+              NodeDef.getUuid(nodeDef) !== NodeDef.getUuid(_nodeDef)
+          )
+          return hasAnalysisSibilings
+        }
+        return true
+      })
     }
 
     return nodeDefs.sort((nodeDefA, nodeDefB) => NodeDef.getChainIndex(nodeDefA) - NodeDef.getChainIndex(nodeDefB))
