5909dc1134ded265fd96ab4a4f0bdc51268e33e7
Stefano Ricci <SteRiccio@users.noreply.github.com>
2021-12-03 11:05:43 +0100

    code cleanup

--- webapp/components/DataQuery/Visualizer/Table/Row/LinkRecord/LinkRecord.js
+++ webapp/components/DataQuery/Visualizer/Table/Row/LinkRecord/LinkRecord.js
@@ -4,15 +4,20 @@ import { Link } from 'react-router-dom'
 
 import * as Record from '@core/record/record'
 import { appModuleUri, dataModules } from '@webapp/app/appModules'
+import { useI18n } from '@webapp/store/system'
 
 const LinkRecord = (props) => {
   const { rowNo, row } = props
+
+  const i18n = useI18n()
+
   const { parentUuid, record } = row
+
   const recordUuid = Record.getUuid(record)
   const recordEditUrl = `${appModuleUri(dataModules.record)}${recordUuid}?pageNodeUuid=${parentUuid}`
 
   return (
-    <Link type="button" className="btn-transparent" title="View record" to={recordEditUrl}>
+    <Link type="button" className="btn-transparent" title={i18n.t('dataExplorerView.editRecord')} to={recordEditUrl}>
       {rowNo}
       <span className="icon icon-link icon-right icon-12px" />
     </Link>
