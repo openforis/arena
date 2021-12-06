bf2f9cd85eb0fe46cbce681f9126f4609abe9e0a
Ramiro Blázquez <ramirobg94@gmail.com>
2021-12-06 20:33:52 +0100

    add to the label

--- core/survey/_survey/surveyNodeDefs.js
+++ core/survey/_survey/surveyNodeDefs.js
@@ -38,7 +38,10 @@ export const getNodeDefChildren =
     let childDefs = SurveyNodeDefsIndex.getNodeDefChildren(nodeDef)(surveyIndexed)
     childDefs = includeAnalysis ? childDefs : childDefs.filter((childDef) => !NodeDef.isAnalysis(childDef))
     // the next line filters the sampling analysis nodeDefs without sibilinings
+<<<<<<< HEAD
 
+=======
+>>>>>>> 147ac6a86 (add to the label)
     childDefs = childDefs.filter(
       (childDef) =>
         !NodeDef.isSampling(childDef) ||
@@ -47,7 +50,10 @@ export const getNodeDefChildren =
             NodeDef.isAnalysis(_childDef) && NodeDef.getChainUuid(_childDef) === NodeDef.getChainUuid(childDef)
         ).length > 1
     )
+<<<<<<< HEAD
 
+=======
+>>>>>>> 147ac6a86 (add to the label)
     return childDefs
   }
 
