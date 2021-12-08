395f44262ee74e199ffd80d00938db8599c8a669
Ramiro Blázquez <ramirobg94@gmail.com>
2021-12-08 16:15:37 +0100

    fix bug with nodes without sibilings

--- core/survey/_survey/surveyAnalysis.js
+++ core/survey/_survey/surveyAnalysis.js
@@ -16,7 +16,8 @@ export const getAnalysisNodeDefs =
     hideAreaBasedEstimate = true,
   }) =>
   (survey) => {
-    let nodeDefs = SurveyNodeDefs.getNodeDefsArray(survey).filter((nodeDef) => {
+    const _nodeDefs = SurveyNodeDefs.getNodeDefsArray(survey)
+    let nodeDefs = _nodeDefs.filter((nodeDef) => {
       if (!NodeDef.isAnalysis(nodeDef)) return false
 
       // remove nodeDefs not in this chain
@@ -30,7 +31,7 @@ export const getAnalysisNodeDefs =
       if (entity && NodeDef.getParentUuid(nodeDef) !== NodeDef.getUuid(entity)) return false
 
       // remove nodeDefs not in this entity bu entityUuid
-      if (enentityDefUuidtity && NodeDef.getParentUuid(nodeDef) !== entityDefUuid) return false
+      if (entityDefUuid && NodeDef.getParentUuid(nodeDef) !== entityDefUuid) return false
 
       if (!showSamplingNodeDefs && NodeDef.isSampling(nodeDef)) return false
 
@@ -43,23 +44,20 @@ export const getAnalysisNodeDefs =
         NodeDef.isSampling(nodeDef) &&
         !NodeDef.isBaseUnit(nodeDef)
       ) {
-        const hasAnalysisSibilings = nodeDefs.some(
+        const hasAnalysisSibilings = _nodeDefs.some(
           (_nodeDef) =>
+            NodeDef.isSampling(_nodeDef) &&
             NodeDef.getParentUuid(nodeDef) === NodeDef.getParentUuid(_nodeDef) &&
             NodeDef.getUuid(nodeDef) !== NodeDef.getUuid(_nodeDef)
         )
+
         if (!hasAnalysisSibilings) return false
       }
 
-      return true
-    })
-
-    nodeDefs = nodeDefs.filter((nodeDef) => {
-      if (Boolean(NodeDef.getAreaBasedEstimatedOf(nodeDef))) {
-        if (hideAreaBasedEstimate) {
-          return false
-        }
+      if(hideAreaBasedEstimate && Boolean(NodeDef.getAreaBasedEstimatedOf(nodeDef))){
+        return false
       }
+
       return true
     })
 
