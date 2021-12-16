4a90478dc1f7f5b8711d160bf893fca32ed32d06
Stefano Ricci <SteRiccio@users.noreply.github.com>
2021-12-16 15:13:53 +0100

    code cleanup; added jsdoc to functions

--- core/expressionParser/toSql/toSql.js
+++ core/expressionParser/toSql/toSql.js
@@ -63,11 +63,26 @@ export const unary = (node, params) => {
   }
 }
 
+/**
+ * Returns true if the specified value is a string that starts and ends with a double quote (") char.
+ *
+ * @param {!string} value - The value to test.
+ * @returns {boolean} - True if the value is quoted, false otherwise.
+ */
+const _isQuotedString = (value) =>
+  typeof value === 'string' && value.length >= 2 && value[0] === '"' && value[value.length - 1] === '"'
+
+/**
+ * Expression node value could have been "stringified" (e.g. code/taxon/text attributes)
+ * so it needs to be parsed.
+ * When the value is a string, it will be quoted by pg-promise itself, so there is no need to double quote it
+ * and if it's already quoted, remove the double quotes.
+ *
+ * @param {!string} value - The value to parse.
+ * @returns {object} - The result of the parsing.
+ */
 const _getLiteralParamValue = (value) => {
-  // expression node value could have been "stringified" (e.g. code/taxon/text attributes)
-  // it needs to be parsed
-  // when the value is a string, it will be quoted by pg-promise itself, so there is no need to double quote it
-  if (typeof value === 'string' && value.length >= 2 && value[0] === '"' && value[value.length - 1] === '"') {
+  if (_isQuotedString(value)) {
     try {
       return JSON.parse(value)
     } catch (e) {}
