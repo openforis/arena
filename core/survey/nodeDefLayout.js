6a81e840de7a9664a660c0e0296250d8caf57672
Stefano Ricci <SteRiccio@users.noreply.github.com>
2021-12-22 00:04:13 +0100

    solved DeepScan issue

--- core/survey/nodeDefLayout.js
+++ core/survey/nodeDefLayout.js
@@ -63,7 +63,7 @@ export const getRenderType = (cycle) => _getPropLayout(cycle, keys.renderType)
 export const getLayoutChildren = (cycle) => _getPropLayout(cycle, keys.layoutChildren, [])
 
 export const getLayoutChildrenCompressed =
-  ({ cycle, hiddenDefsByUuid = {}, compressHorizontally = true }) =>
+  ({ cycle, hiddenDefsByUuid = {} }) =>
   (nodeDef) => {
     const layoutChildren = getLayoutChildren(cycle)(nodeDef)
     let itemPrev = { x: 0, y: 0, w: 0, h: 0, xOriginal: 0, yOriginal: 0 }
@@ -78,15 +78,13 @@ export const getLayoutChildrenCompressed =
           const h = hidden ? 0 : hOriginal
           const w = hidden ? 0 : wOriginal
 
-          const sameRowOfPreviousItem = xOriginal > itemPrev?.xOriginal
+          const sameRowOfPreviousItem = xOriginal > itemPrev.xOriginal
 
-          const x = compressHorizontally
-            ? sameRowOfPreviousItem
-              ? // move it to xPrev + wPrev
-                Math.min(itemPrev.x + itemPrev.w, xOriginal)
-              : // item in another row, can have the same x of the previous one
-                Math.min(itemPrev.x, xOriginal)
-            : xOriginal
+          const x = sameRowOfPreviousItem
+            ? // move it to xPrev + wPrev
+              Math.min(itemPrev.x + itemPrev.w, xOriginal)
+            : // item in another row, can have the same x of the previous one
+              Math.min(itemPrev.x, xOriginal)
 
           const y = sameRowOfPreviousItem
             ? // item can have the same y of the previous one
