2c54804d14e4edd8472d14b7c0986c2d78d0dd2a
Ramiro Blázquez <ramirobg94@gmail.com>
2021-12-22 02:09:01 +0100

    Update core/survey/_survey/surveyAnalysis.js

--- core/survey/_survey/surveyAnalysis.js
+++ core/survey/_survey/surveyAnalysis.js
@@ -64,7 +64,7 @@ export const getAnalysisNodeDefs =
       }
 
       if (
-       NodeDef. isAreaBasedEstimatedOf(nodeDef) &&
+       NodeDef.isAreaBasedEstimatedOf(nodeDef) &&
         !showInactiveResultVariables &&
         !NodeDef.getActive(
           _nodeDefs.find((_nodeDef) => NodeDef.getUuid(_nodeDef) === NodeDef.getAreaBasedEstimatedOf(nodeDef))
