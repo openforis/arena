2c7b2a10cbd93bcd29b94a7742bface544d78b35
Stefano Ricci <1219739+SteRiccio@users.noreply.github.com>
2021-12-30 10:50:34 +0100

    added edit and delete button to records list (#2032)
    * added edit and delete button to records list
    
    * records: show update records step in a more compact way
    
    * fixed tests
    
    Co-authored-by: Stefano Ricci <SteRiccio@users.noreply.github.com>

--- webapp/views/App/views/Data/Records/Row/Row.js
+++ webapp/views/App/views/Data/Records/Row/Row.js
@@ -1,20 +1,23 @@
-import React from 'react'
+import React, { useCallback } from 'react'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router'
 import PropTypes from 'prop-types'
-import camelize from 'camelize'
 import * as R from 'ramda'
 
-import * as Survey from '@core/survey/survey'
+import * as A from '@core/arena'
 import * as NodeDef from '@core/survey/nodeDef'
 import * as Record from '@core/record/record'
 import * as Validation from '@core/validation/validation'
-import * as Authorizer from '@core/auth/authorizer'
 import * as DateUtils from '@core/dateUtils'
 import { useI18n } from '@webapp/store/system'
-import { useSurveyInfo, useNodeDefRootKeys } from '@webapp/store/survey'
-import { useUser } from '@webapp/store/user'
+import { useNodeDefRootKeys } from '@webapp/store/survey'
 import { TestId } from '@webapp/utils/testId'
 
 import ErrorBadge from '@webapp/components/errorBadge'
+import { Button, ButtonDelete } from '@webapp/components'
+import { RecordActions } from '@webapp/store/ui/record'
+import { useAuthCanDeleteRecord, useAuthCanEditRecord } from '@webapp/store/user/hooks'
+import { DialogConfirmActions } from '@webapp/store/ui'
 
 const valueFormattersByType = {
   [NodeDef.nodeDefType.date]: ({ value }) =>
@@ -26,13 +29,52 @@ const valueFormattersByType = {
 }
 
 const Row = (props) => {
-  const { row: record, rowNo, selected } = props
+  const { row: record, rowNo, selected, navigateToRecord, onRecordsUpdate } = props
   const nodeDefKeys = useNodeDefRootKeys()
 
   const i18n = useI18n()
-  const surveyInfo = useSurveyInfo()
-  const user = useUser()
-  const canEdit = Survey.isPublished(surveyInfo) && Authorizer.canEditRecord(user, record)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const canEdit = useAuthCanEditRecord(record)
+  const canDelete = useAuthCanDeleteRecord(record)
+
+  const keyValues = nodeDefKeys.map((nodeDef) => {
+    const name = NodeDef.getName(nodeDef)
+    const value = record[A.camelize(name)]
+    const formatter = valueFormattersByType[NodeDef.getType(nodeDef)]
+    return value && formatter ? formatter({ value }) : value
+  })
+
+  const onEditButtonClick = useCallback(
+    (event) => {
+      event.stopPropagation()
+      event.preventDefault()
+
+      navigateToRecord(record)
+    },
+    [record, navigateToRecord]
+  )
+
+  const onDeleteConfirmed = useCallback(() => {
+    dispatch(RecordActions.deleteRecord({ navigate, recordUuid: Record.getUuid(record), goBackOnDelete: false }))
+    onRecordsUpdate()
+  }, [dispatch, navigate, onRecordsUpdate, record])
+
+  const onDeleteButtonClick = useCallback(
+    (event) => {
+      event.stopPropagation()
+      event.preventDefault()
+
+      dispatch(
+        DialogConfirmActions.showDialogConfirm({
+          key: 'dataView.records.confirmDeleteRecord',
+          params: { keyValues },
+          onOk: onDeleteConfirmed,
+        })
+      )
+    },
+    [dispatch, navigate]
+  )
 
   return (
     <>
@@ -43,15 +85,13 @@ const Row = (props) => {
         <ErrorBadge validation={Validation.getValidation(record)} showLabel={false} className="error-badge-inverse" />
         {rowNo}
       </div>
-      {nodeDefKeys.map((nodeDef) => {
+      {nodeDefKeys.map((nodeDef, index) => {
         const name = NodeDef.getName(nodeDef)
         const uuid = NodeDef.getUuid(nodeDef)
-        const value = record[camelize(name)]
-        const formatter = valueFormattersByType[NodeDef.getType(nodeDef)]
-        const valueFormatted = value && formatter ? formatter({ value }) : value
+        const value = keyValues[index]
         return (
           <div key={uuid} data-testid={TestId.records.cellNodeDef(name)} data-value={value}>
-            {valueFormatted}
+            {value}
           </div>
         )
       })}
@@ -62,7 +102,14 @@ const Row = (props) => {
       <div>{R.pipe(Validation.getValidation, Validation.getErrorsCount)(record)}</div>
       <div>{R.pipe(Validation.getValidation, Validation.getWarningsCount)(record)}</div>
       <div>
-        <span className={`icon icon-12px icon-action ${canEdit ? 'icon-pencil2' : 'icon-eye'}`} />
+        <Button
+          iconClassName={`icon-12px icon-action ${canEdit ? 'icon-pencil2' : 'icon-eye'}`}
+          title={`dataView.records.${canEdit ? 'editRecord' : 'viewRecord'}`}
+          onClick={onEditButtonClick}
+        />
+        {canDelete && (
+          <ButtonDelete title="dataView.records.deleteRecord" showLabel={false} onClick={onDeleteButtonClick} />
+        )}
       </div>
     </>
   )
