bb6119f7ffced13664546def488534c64969b117
Stefano Ricci <1219739+SteRiccio@users.noreply.github.com>
2022-01-27 10:38:19 +0100

    Map: format key attribute values in tooltips (#2063)
    * Map: format key attribute values in tooltips
    
    * code cleanup
    
    Co-authored-by: Stefano Ricci <SteRiccio@users.noreply.github.com>

--- webapp/components/DataQuery/index.js
+++ webapp/components/DataQuery/index.js
@@ -1,1 +1,3 @@
 export { default } from './DataQuery'
+
+export { ValueFormatter } from './valueFormatter'
---
+++ webapp/components/DataQuery/valueFormatter.js
@@ -0,0 +1,27 @@
+import * as A from '@core/arena'
+
+import * as NodeDef from '@core/survey/nodeDef'
+import * as NumberUtils from '@core/numberUtils'
+import * as DateUtils from '@core/dateUtils'
+
+const formatters = {
+  [NodeDef.nodeDefType.boolean]: ({ value, i18n, nodeDef }) =>
+    i18n.t(`surveyForm.nodeDefBoolean.labelValue.${NodeDef.getLabelValue(nodeDef)}.${value}`),
+  [NodeDef.nodeDefType.date]: ({ value }) => DateUtils.format(DateUtils.parseISO(value), DateUtils.formats.dateDefault),
+  [NodeDef.nodeDefType.decimal]: ({ value, nodeDef }) =>
+    NumberUtils.formatDecimal(value, NodeDef.getMaxNumberDecimalDigits(nodeDef)),
+  [NodeDef.nodeDefType.integer]: ({ value }) => NumberUtils.formatInteger(value),
+  [NodeDef.nodeDefType.time]: ({ value }) => DateUtils.format(DateUtils.parse(value, 'HH:mm:ss'), 'HH:mm'),
+}
+
+const format = ({ value, i18n, nodeDef }) => {
+  if (A.isNull(value)) {
+    return ''
+  }
+  const formatter = formatters[NodeDef.getType(nodeDef)]
+  return formatter ? formatter({ value, i18n, nodeDef }) : value
+}
+
+export const ValueFormatter = {
+  format,
+}
