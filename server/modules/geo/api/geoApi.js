a14596b938a4855e3820200e080bbcf0ffa8f243
Stefano Ricci <1219739+SteRiccio@users.noreply.github.com>
2022-02-03 14:34:48 +0100

    Map: use system api key (#2070)
    * Map: added API key test button in user profile edit
    
    * code cleanup
    
    * solved DeepScan issues
    
    * Map: use Arena server to proxy requests
    
    * Map / Planet: use provided map for monthly mosaics
    
    * Map: tile requests / forward requests to Planet server
    
    * Allow using env planet api key
    
    * solved DeepScan issue
    
    Co-authored-by: Stefano Ricci <SteRiccio@users.noreply.github.com>

--- server/modules/geo/api/geoApi.js
+++ server/modules/geo/api/geoApi.js
@@ -1,6 +1,18 @@
+import axios from 'axios'
+
 import { SRSs } from '@openforis/arena-core'
 
 import * as Request from '@server/utils/request'
+import * as AuthMiddleware from '@server/modules/auth/authApiMiddleware'
+import { MapUtils } from '@core/map/mapUtils'
+import { PlanetApi } from './planetApi'
+
+const getMapTileForwardUrl = (req) => {
+  const { provider, period, x, y, z } = Request.getParams(req)
+  const apiKey = MapUtils.mapApiKeyByProvider[provider]
+
+  return MapUtils.getMapTileUrl({ provider, x, y, z, period, apiKey })
+}
 
 export const init = (app) => {
   // ==== READ
@@ -10,5 +22,34 @@ export const init = (app) => {
     const srss = SRSs.findSRSByCodeOrName(codeOrName)
 
     res.json({ srss })
+  })
+
+  app.get(
+    '/geo/map/:provider/tile/:z/:y/:x',
+    AuthMiddleware.requireMapUsePermission,
+    // createProxyMiddleware({ router: getForwardUrl, changeOrigin: true, secure: false })
+    async (req, res) => {
+      try {
+        const url = getMapTileForwardUrl(req)
+        const { data: dataStream } = await axios.get(url, { responseType: 'stream' })
+        dataStream.pipe(res)
+      } catch (error) {
+        res.json({})
+      }
+    }
+  )
+
+  app.get('/geo/map/:provider/available_montly_periods', AuthMiddleware.requireMapUsePermission, async (req, res) => {
+    const { provider } = Request.getParams(req)
+    try {
+      if (provider === MapUtils.mapProviders.planet) {
+        const periods = await PlanetApi.fetchAvailableMonthlyMosaicsPeriods()
+        res.json(periods)
+        return
+      }
+    } catch (error) {}
+
+    // error or provider not supported
+    res.json([])
   })
 }
