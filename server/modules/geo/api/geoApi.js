8252f1b76788145289092c21df4f59bd58836aae
Stefano Ricci <1219739+SteRiccio@users.noreply.github.com>
2022-02-04 12:14:19 +0100

    Map: set layer max zoom (#2074)
    Co-authored-by: Stefano Ricci <SteRiccio@users.noreply.github.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- server/modules/geo/api/geoApi.js
+++ server/modules/geo/api/geoApi.js
@@ -30,6 +30,8 @@ export const init = (app) => {
     // createProxyMiddleware({ router: getForwardUrl, changeOrigin: true, secure: false })
     async (req, res) => {
       try {
+        // cache the response for 1 year
+        res.set('Cache-Control', 'private, max-age=31536000')
         const url = getMapTileForwardUrl(req)
         const { data: dataStream } = await axios.get(url, { responseType: 'stream' })
         dataStream.pipe(res)
