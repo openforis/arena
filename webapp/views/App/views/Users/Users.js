e24915b370fa3dbd0dfc35adf2edb3ce8c595ea0
Stefano Ricci <SteRiccio@users.noreply.github.com>
2022-02-07 10:16:01 +0100

    fixed access to User profile

--- webapp/views/App/views/Users/Users.js
+++ webapp/views/App/views/Users/Users.js
@@ -1,8 +1,13 @@
 import React from 'react'
 
+import * as Authorizer from '@core/auth/authorizer'
+import * as Survey from '@core/survey/survey'
+
 import { appModules, userModules } from '@webapp/app/appModules'
 
 import ModuleSwitch from '@webapp/components/moduleSwitch'
+import { useUser } from '@webapp/store/user'
+import { useSurveyInfo } from '@webapp/store/survey'
 
 import { UsersList } from './UsersList'
 import UsersListSurvey from './UsersListSurvey'
@@ -10,33 +15,50 @@ import UserInvite from './UserInvite'
 import UserEdit from './UserEdit'
 import { UsersAccessRequest } from './UsersAccessRequest'
 
-const Users = () => (
-  <ModuleSwitch
-    moduleRoot={appModules.users}
-    moduleDefault={userModules.usersSurvey}
-    modules={[
-      {
-        component: UsersList,
-        path: userModules.users.path,
-      },
-      {
-        component: UsersListSurvey,
-        path: userModules.usersSurvey.path,
-      },
-      {
-        component: UserEdit,
-        path: `${userModules.user.path}/:userUuid`,
-      },
-      {
-        component: UserInvite,
-        path: userModules.userInvite.path,
-      },
-      {
-        component: UsersAccessRequest,
-        path: userModules.usersAccessRequest.path,
-      },
-    ]}
-  />
-)
+const Users = () => {
+  const user = useUser()
+  const surveyInfo = useSurveyInfo()
+
+  return (
+    <ModuleSwitch
+      moduleRoot={appModules.users}
+      moduleDefault={userModules.usersSurvey}
+      modules={[
+        ...(Authorizer.canViewAllUsers(user)
+          ? [
+              {
+                component: UsersList,
+                path: userModules.users.path,
+              },
+            ]
+          : []),
+        ...(Authorizer.canViewSurveyUsers(user, surveyInfo) && !Survey.isTemplate(surveyInfo)
+          ? [
+              {
+                component: UsersListSurvey,
+                path: userModules.usersSurvey.path,
+              },
+              {
+                component: UserInvite,
+                path: userModules.userInvite.path,
+              },
+            ]
+          : []),
+        ...(Authorizer.canViewUsersAccessRequests(user)
+          ? [
+              {
+                component: UsersAccessRequest,
+                path: userModules.usersAccessRequest.path,
+              },
+            ]
+          : []),
+        {
+          component: UserEdit,
+          path: `${userModules.user.path}/:userUuid`,
+        },
+      ]}
+    />
+  )
+}
 
 export default Users
