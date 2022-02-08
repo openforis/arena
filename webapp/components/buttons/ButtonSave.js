0f0f503318c423c760734ed7a23b3aa8cbb7f733
Stefano Ricci <1219739+SteRiccio@users.noreply.github.com>
2022-02-08 15:29:48 +0100

    Survey info description: edit it in textarea (#2091)
    * Survey info description: edit it in textarea
    
    * fixed tests
    
    * fixed tests
    
    * fixed tests
    
    * code cleanup
    
    * fixed tests
    
    Co-authored-by: Stefano Ricci <SteRiccio@users.noreply.github.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- webapp/components/buttons/ButtonSave.js
+++ webapp/components/buttons/ButtonSave.js
@@ -3,11 +3,7 @@ import React from 'react'
 import { Button } from './Button'
 
 export const ButtonSave = (props) => (
-  <Button
-    {...props}
-    className={`btn-primary ${props.className}`}
-    iconClassName="icon-floppy-disk icon-left icon-12px"
-  />
+  <Button {...props} className={`btn-primary ${props.className}`} iconClassName="icon-floppy-disk icon-12px" />
 )
 
 ButtonSave.propTypes = {
