4630f3c401d820dfbecee1a71b715ab57f655806
Stefano Ricci <SteRiccio@users.noreply.github.com>
2022-02-16 15:42:37 +0100

    cleanup code

---
+++ webapp/components/survey/CategoryDetails/store/actions/category/useRefreshCategory.js
@@ -0,0 +1,30 @@
+import { useCallback } from 'react'
+import { useDispatch } from 'react-redux'
+
+import * as A from '@core/arena'
+import { SurveyActions } from '@webapp/store/survey'
+
+import { State } from '../../state'
+import { useFetchLevelItems } from '../item/useFetchLevelItems'
+
+export const useRefreshCategory = ({ setState }) => {
+  const dispatch = useDispatch()
+  const fetchLevelItems = useFetchLevelItems({ setState })
+
+  return useCallback(async ({ category }) => {
+    dispatch(SurveyActions.metaUpdated())
+
+    setState((statePrev) => {
+      const stateUpdated = A.pipe(
+        State.assocCategory({ category }),
+        State.dissocImportSummary,
+        State.dissocItemsActive
+      )(statePrev)
+
+      // Fetch first level items
+      fetchLevelItems({ state: stateUpdated })
+
+      return stateUpdated
+    })
+  })
+}
--- webapp/components/survey/CategoryDetails/store/actions/category/useUpdateCategoryItemExtraPropItem.js
+++ webapp/components/survey/CategoryDetails/store/actions/category/useUpdateCategoryItemExtraPropItem.js
@@ -1,20 +1,16 @@
 import { useCallback } from 'react'
-import { useDispatch } from 'react-redux'
 
 import * as API from '@webapp/service/api'
-import { SurveyActions, useSurveyId } from '@webapp/store/survey'
-
-import { State } from '../../state'
+import { useSurveyId } from '@webapp/store/survey'
+import { useRefreshCategory } from './useRefreshCategory'
 
 export const useUpdateCategoryItemExtraPropItem = ({ setState }) => {
-  const dispatch = useDispatch()
   const surveyId = useSurveyId()
+  const refreshCategory = useRefreshCategory({ setState })
 
   return useCallback(async ({ categoryUuid, name, itemExtraDef = null, deleted = false }) => {
     const category = await API.updateCategoryItemExtraDefItem({ surveyId, categoryUuid, name, itemExtraDef, deleted })
 
-    dispatch(SurveyActions.metaUpdated())
-
-    setState(State.assocCategory({ category }))
+    refreshCategory({ category })
   }, [])
 }
