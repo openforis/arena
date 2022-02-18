b0f4db03a693302a7e44fcf5fe19c3997e5f534e
12rambau <pierrick.rambaud49@gmail.com>
2022-02-18 18:35:01 +0100

    general structure of the doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,52 +4,89 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+from datetime import datetime
 
 
-# -- Project information -----------------------------------------------------
+# -- Project information -------------------------------------------------------
 
-project = 'Arena'
-copyright = '2022, Open Foris'
-author = 'Open Foris'
+project = "Arena"
+copyright = f"2018-{datetime.now().year}, Open Foris"
+author = "Open Foris"
+release = "1.0.24"
 
-# The full version, including alpha/beta/rc tags
-release = '1.0.24'
-
-
-# -- General configuration ---------------------------------------------------
+# -- General configuration -----------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ["**.ipynb_checkpoints"]
 
 
-# -- Options for HTML output -------------------------------------------------
+# -- Options for HTML output ---------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "pydata_sphinx_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = ["css/custom.css"]
+
+# -- Pydata-sphinx-theme parameters --------------------------------------------
+
+# A complete description of the theme can be found on their documentation: 
+# https://pydata-sphinx-theme.readthedocs.io/en/latest/index.html
+
+html_favicon = "_static/img/icon/favicon.ico"
+
+html_logo = "_static/img/icon/arena-logo.png"
+
+html_theme_options = {
+    "show_prev_next": False,
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/openforis/arena",
+            "icon": "fab fa-github"
+        },
+        {
+            "name": "Twitter",
+            "url": "https://twitter.com/OpenForis",
+            "icon": "fab fa-twitter"
+        },
+        {
+             "name": "Website",
+            "url": "https://www.openforis-arena.org/app/home/",
+            "icon": "fas fa-globe"
+        }
+    ],
+    "use_edit_page_button": True,
+    "footer_items": ["copyright", "sphinx-version", "license"]
+}
+
+html_context = {
+    "github_user": "openforis",
+    "github_repo": "arena",
+    "github_version": "master",
+    "doc_path": "docs/source"
+}
+
+# -- Options for images -------------------------------------------------------
+
+images_config = {
+    "download": False
+}
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,20 +1,6 @@
-.. Arena documentation master file, created by
-   sphinx-quickstart on Fri Feb 18 17:10:45 2022.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
-
 Welcome to Arena's documentation!
 =================================
 
 .. toctree::
    :maxdepth: 2
-   :caption: Contents:
-
-
-
-Indices and tables
-==================
-
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
+   :hidden:
