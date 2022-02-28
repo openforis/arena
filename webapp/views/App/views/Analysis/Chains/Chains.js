e8c18441739660d84247385ab3d2a1986421484e
Stefano Ricci <SteRiccio@users.noreply.github.com>
2022-02-28 15:37:39 +0100

    updated chains table (WIP)

--- webapp/views/App/views/Analysis/Chains/Chains.js
+++ webapp/views/App/views/Analysis/Chains/Chains.js
@@ -4,9 +4,10 @@ import { useNavigate } from 'react-router'
 
 import * as Chain from '@common/analysis/chain'
 
-import { useSurveyCycleKey } from '@webapp/store/survey'
+import { useSurveyCycleKey, useSurveyPreferredLang } from '@webapp/store/survey'
 
 import Table from '@webapp/components/Table'
+import ErrorBadge from '@webapp/components/errorBadge'
 
 import { analysisModules, appModuleUri } from '@webapp/app/appModules'
 
@@ -17,19 +18,64 @@ import HeaderLeft from './HeaderLeft'
 const ChainsView = () => {
   const navigate = useNavigate()
   const surveyCycleKey = useSurveyCycleKey()
+  const lang = useSurveyPreferredLang()
 
   const onRowClick = (chain) => navigate(`${appModuleUri(analysisModules.chain)}${Chain.getUuid(chain)}`)
 
   return (
     <Table
       className="chains"
-      gridTemplateColumns="30px repeat(4, 1fr) 50px"
       headerLeftComponent={HeaderLeft}
       module="processing-chains"
       onRowClick={onRowClick}
       restParams={{ surveyCycleKey }}
-      rowHeaderComponent={RowHeader}
-      rowComponent={Row}
+      columns={[
+        {
+          key: 'validation',
+          width: '30px',
+          renderItem: ({ item }) => (
+            <ErrorBadge
+              validation={Chain.getValidation(item)}
+              className="error-badge-inverse"
+              showIcon
+              showLabel={false}
+            />
+          ),
+        },
+        {
+          key: 'label',
+          header: 'common.label',
+          width: '1fr',
+          renderItem: ({ item }) => (
+            <div className="chain-label">
+              <div>{Chain.getLabel(lang)(item)}</div>
+            </div>
+          ),
+        },
+        {
+          key: Chain.keys.dateCreated,
+          header: 'common.dateCreated',
+          renderItem: ({ item }) => DateUtils.getRelativeDate(i18n, Chain.getDateCreated(item)),
+          width: '15rem',
+          sortable: true,
+        },
+        {
+          key: Chain.keys.dateModified,
+          header: 'common.dateLastModified',
+          renderItem: ({ item }) => DateUtils.getRelativeDate(i18n, Chain.getDateModified(item)),
+          width: '15rem',
+          sortable: true,
+        },
+        {
+          key: 'edit',
+          width: '50px',
+          renderItem: () => (
+            <div>
+              <span className="icon icon-12px icon-action icon-pencil2" />
+            </div>
+          ),
+        },
+      ]}
     />
   )
 }
