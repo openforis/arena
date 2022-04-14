8bd62d7234ec32070c8e1c504b302efd9a54727e
Stefano Ricci <SteRiccio@users.noreply.github.com>
2022-04-14 11:30:12 +0200

    Category selector: prevent empty category name specified

--- webapp/components/hooks/useNotifyError.js
+++ webapp/components/hooks/useNotifyError.js
@@ -1,6 +1,5 @@
 import { useNotification } from './useNotification'
 
 export const useNotifyError = () => {
-  const notify = useNotification({ severity: 'error' })
-  return notify
+  return useNotification({ severity: 'error' })
 }
