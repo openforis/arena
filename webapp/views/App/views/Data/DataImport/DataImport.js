b8260dd034d45c21b2b922ba460514917c00821a
Stefano Ricci <SteRiccio@users.noreply.github.com>
2022-05-10 12:01:01 +0200

    hide CSV data import

--- webapp/views/App/views/Data/DataImport/DataImport.js
+++ webapp/views/App/views/Data/DataImport/DataImport.js
@@ -6,7 +6,7 @@ import { useI18n } from '@webapp/store/system'
 
 import TabBar from '@webapp/components/tabBar'
 import { TestId } from '@webapp/utils/testId'
-import { DataImportCsvView } from './DataImportCsvView'
+// import { DataImportCsvView } from './DataImportCsvView'
 import { CollectDataImportView } from './DataImportCollectView'
 
 const DataImport = () => {
@@ -16,11 +16,11 @@ const DataImport = () => {
     <div className="data-import">
       <TabBar
         tabs={[
-          {
-            id: TestId.dataImport.importFromCsvTab,
-            label: i18n.t('dataImportView.importFromCsv'),
-            component: DataImportCsvView,
-          },
+          // {
+          //   id: TestId.dataImport.importFromCsvTab,
+          //   label: i18n.t('dataImportView.importFromCsv'),
+          //   component: DataImportCsvView,
+          // },
           {
             id: TestId.dataImport.importFromCollectTab,
             label: i18n.t('dataImportView.importFromCollect'),
