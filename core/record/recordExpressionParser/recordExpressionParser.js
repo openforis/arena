b7224f4ad68c45a2a55b9a48553bf77919e7592f
Stefano Ricci <SteRiccio@users.noreply.github.com>
2022-05-15 23:43:42 +0200

    code cleanup (record expression parser)

--- core/record/recordExpressionParser/recordExpressionParser.js
+++ core/record/recordExpressionParser/recordExpressionParser.js
@@ -1,16 +1,7 @@
 import { RecordExpressionEvaluator } from '@openforis/arena-core'
 
-import * as Survey from '@core/survey/survey'
-import * as NodeDef from '@core/survey/nodeDef'
-import * as Record from '@core/record/record'
-import * as Node from '@core/record/node'
-
-export const evalNodeQuery = (survey, record, node, query) => {
-  const nodeDef = Survey.getNodeDefByUuid(Node.getNodeDefUuid(node))(survey)
-  const nodeContext = NodeDef.isEntity(nodeDef) ? node : Record.getParentNode(node)(record)
-  const context = { survey, record, nodeContext, object: nodeContext }
-  return new RecordExpressionEvaluator().evaluate(query, context)
-}
+export const evalNodeQuery = (survey, record, node, query) =>
+  new RecordExpressionEvaluator().evalNodeQuery({ survey, record, node, query })
 
 export const evalApplicableExpressions = (survey, record, node, expressions, stopAtFirstFound = false) =>
   new RecordExpressionEvaluator().evalApplicableExpressions({
