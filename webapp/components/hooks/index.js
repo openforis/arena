f6fa136b39d477d68f646929120d013022de43e6
Stefano Ricci <1219739+SteRiccio@users.noreply.github.com>
2022-06-06 09:48:30 +0200

    Dependencies / upgrade (#2265)
    * updated dependencies
    
    * layout adjustments
    
    * updated other dependencies
    
    * updated babel dependencies
    
    * nodeDefCoordinate: fixed errors in console
    
    * nodeDefEntityForm: fixed error in console
    
    * fixed error in console (nodeDefEntityFormGrid)
    
    * updated eslint dependencies
    
    * fixed useIsMountedRef hook
    
    * updated dependencies (1)
    
    * updated dependencies (webpack)
    
    * updated dependencies (2)
    
    * updated dependency (jest-playwright-preset)
    
    Co-authored-by: Stefano Ricci <SteRiccio@users.noreply.github.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- webapp/components/hooks/index.js
+++ webapp/components/hooks/index.js
@@ -1,6 +1,6 @@
 export { default as usePrevious } from './usePrevious'
 export { default as useOnUpdate } from './useOnUpdate'
-export { default as useIsMounted } from './useIsMounted'
+export { default as useIsMountedRef } from './useIsMountedRef'
 export { default as useAsync } from './useAsync'
 export { default as useAsyncGetRequest } from './useAsyncGetRequest'
 export { default as useAsyncPostRequest } from './useAsyncPostRequest'
