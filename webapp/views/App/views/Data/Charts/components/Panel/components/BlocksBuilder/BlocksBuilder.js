39029c59bfe7626e52a97f4756086acc795ece21
persas <diego.persas@gmail.com>
2022-06-24 09:54:50 +0200

    DeepScan issues - Unused imports

--- webapp/views/App/views/Data/Charts/components/Panel/components/BlocksBuilder/BlocksBuilder.js
+++ webapp/views/App/views/Data/Charts/components/Panel/components/BlocksBuilder/BlocksBuilder.js
@@ -1,7 +1,6 @@
-import React, { useCallback, useState } from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
-import Select from 'react-select'
 import { chartsConfig } from '../../../../state/config'
 import './BlocksBuilder.scss'
 
