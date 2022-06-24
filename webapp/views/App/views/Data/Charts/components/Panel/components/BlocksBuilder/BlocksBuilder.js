7470011732c8341c4cd6087ea9699c472a7b458c
persas <diego.persas@gmail.com>
2022-06-24 15:22:10 +0200

    Feat/pie builder (#2324)
    * remove old mocked dimensions
    
    * include Mode Selector in charts
    
    * Pie Builder
    
    * DeepScan issue fixed
    
    * DeepScan issues - Unused imports
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ webapp/views/App/views/Data/Charts/components/Panel/components/BlocksBuilder/BlocksBuilder.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+
+import { chartsConfig } from '../../../../state/config'
+import './BlocksBuilder.scss'
+
+const BlocksBuilder = ({ dimensions, spec, onUpdateSpec }) => {
+  const [type, setType] = useState(null)
+  return (
+    <div className="blocks-builder">
+      {Object.keys(chartsConfig).map((configKey) => (
+        <button
+          key={configKey}
+          onClick={() => {
+            chartsConfig[configKey].selector.onSelect({ spec, onUpdateSpec })()
+            setType(configKey)
+          }}
+        >
+          {chartsConfig[configKey].selector.title}
+        </button>
+      ))}
+      <p>{type}</p>
+    </div>
+  )
+}
+
+BlocksBuilder.propTypes = {
+  spec: PropTypes.string.isRequired,
+  onUpdateSpec: PropTypes.func.isRequired,
+}
+
+export default BlocksBuilder
