9ea4a646fff7327c7e67e332e0afe7253ce2c631
Stefano Ricci <SteRiccio@users.noreply.github.com>
2022-06-29 16:41:09 +0200

    NodeDefDetails: fixed next button brings to blank page

--- webapp/components/survey/NodeDefDetails/store/actions/useGetSiblingNodeDefUuid.js
+++ webapp/components/survey/NodeDefDetails/store/actions/useGetSiblingNodeDefUuid.js
@@ -12,17 +12,32 @@ export const useGetSiblingNodeDefUuid = () => {
   const survey = useSurvey()
   const cycle = useSurveyCycleKey()
 
-  return useCallback(({ state, offset }) => {
-    const nodeDef = State.getNodeDef(state)
-    const nodeDefParent = Survey.getNodeDefParent(nodeDef)(survey)
-    let siblingUuids = null
-    if (NodeDefLayout.isRenderTable(cycle)(nodeDefParent)) {
-      siblingUuids = NodeDefLayout.getLayoutChildren(cycle)(nodeDefParent)
-    } else {
-      const layoutChildren = NodeDefLayout.getLayoutChildrenSorted(cycle)(nodeDefParent)
-      siblingUuids = layoutChildren.map((layoutItem) => layoutItem.i)
-    }
-    const nodeDefIndex = siblingUuids.indexOf(NodeDef.getUuid(nodeDef))
-    return siblingUuids[nodeDefIndex + offset]
-  }, [])
+  const getSinglingNodeDefUuids = useCallback(
+    ({ nodeDef }) => {
+      const nodeDefParent = Survey.getNodeDefParent(nodeDef)(survey)
+
+      let siblingUuids = null
+      if (NodeDefLayout.isRenderTable(cycle)(nodeDefParent)) {
+        siblingUuids = NodeDefLayout.getLayoutChildren(cycle)(nodeDefParent)
+      } else {
+        const layoutChildren = NodeDefLayout.getLayoutChildrenSorted(cycle)(nodeDefParent)
+        siblingUuids = layoutChildren.map((layoutItem) => layoutItem.i)
+      }
+      // filter existing node defs (shown in UI)
+      siblingUuids = siblingUuids.filter((siblingUuid) => Survey.getNodeDefByUuid(siblingUuid)(survey))
+      return siblingUuids
+    },
+    [survey, cycle]
+  )
+
+  return useCallback(
+    ({ state, offset }) => {
+      const nodeDef = State.getNodeDef(state)
+      const siblingUuids = getSinglingNodeDefUuids({ nodeDef })
+
+      const nodeDefIndex = siblingUuids.indexOf(NodeDef.getUuid(nodeDef))
+      return siblingUuids[nodeDefIndex + offset]
+    },
+    [getSinglingNodeDefUuids]
+  )
 }
