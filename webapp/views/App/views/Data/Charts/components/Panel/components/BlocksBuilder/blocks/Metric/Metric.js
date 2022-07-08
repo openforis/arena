b965de0136342768e735483d59658940f34089f4
persas <diego.persas@gmail.com>
2022-07-08 20:38:29 +0200

    Metric transformation

---
+++ webapp/views/App/views/Data/Charts/components/Panel/components/BlocksBuilder/blocks/Metric/Metric.js
@@ -0,0 +1,192 @@
+import React, { useCallback, useMemo, useState, useEffect } from 'react'
+import { uuidv4 } from '@core/uuid'
+import './Metric.scss'
+import { Popover } from 'react-tiny-popover'
+import RenderByType from '../BlockRenderer/BlockRenderer'
+import { ButtonAdd } from '@webapp/components'
+
+const PopoverContent = (props) => {
+  const {
+    config,
+    configItemsByPath,
+    configActions,
+    blockPath,
+    dimensions,
+    block,
+    setIsPopoverOpen,
+    values = {},
+    metric,
+  } = props
+  const { blocks, order } = block
+
+  const [draftValues, setDraftValues] = useState(values || {})
+  const [draftMetric, setDraftMetric] = useState(metric)
+
+  const handleChange = useCallback(
+    (newValue) => {
+      setDraftValues((_draftValues) =>
+        Object.assign({}, _draftValues, {
+          [newValue.blockKey]: newValue.value.map((_val) => Object.assign({}, _val, { parentKey: draftMetric.key })),
+        })
+      )
+    },
+    [draftValues, draftMetric]
+  )
+
+  const handleSaveOrUpdate = useCallback(() => {
+    configActions.addOrUpdateMetric({ blockPath, metric: draftMetric, values: draftValues })
+
+    setIsPopoverOpen(false)
+  }, [blockPath, draftValues, draftMetric])
+
+  useEffect(() => {
+    if (!metric) {
+      setDraftMetric({ key: uuidv4() })
+    }
+  }, [])
+
+  return (
+    <div className="custom-option-modal-container">
+      <div className="custom-option-modal-header">
+        <p>Hi! I'm popover content.</p>
+      </div>
+      <div className="custom-option-modal-blocks">
+        {draftMetric?.key &&
+          order.map((blockKey) =>
+            React.createElement(RenderByType[blocks[blockKey].type], {
+              key: blockKey,
+              dimensions,
+              block: blocks[blockKey],
+              config,
+              configItemsByPath,
+              configActions,
+              blockPath: blockPath ? blockPath.split('.').concat([blockKey]).join('.') : blockKey,
+              onChange: handleChange,
+              values,
+            })
+          )}
+      </div>
+      <div className="custom-option-modal-actions">
+        <button onClick={() => setIsPopoverOpen(false)}></button>
+        <button onClick={handleSaveOrUpdate}>save</button>
+      </div>
+    </div>
+  )
+}
+
+const CustomPopover = (props) => {
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false)
+  const { metric, values, config, configItemsByPath, configActions, blockPath, children, block, dimensions } = props
+
+  return (
+    <Popover
+      padding={8}
+      isOpen={isPopoverOpen}
+      onClickOutside={() => setIsPopoverOpen(false)}
+      positions={['right', 'top', 'left', 'bottom']}
+      align={'center'}
+      content={
+        <PopoverContent
+          config={config}
+          configItemsByPath={configItemsByPath}
+          configActions={configActions}
+          blockPath={blockPath}
+          dimensions={dimensions}
+          block={block}
+          setIsPopoverOpen={setIsPopoverOpen}
+          metric={metric}
+          values={values}
+        />
+      }
+    >
+      <div
+        onClick={(e) => {
+          e.stopPropagation()
+          e.preventDefault()
+          setIsPopoverOpen(!isPopoverOpen)
+        }}
+      >
+        {children}
+      </div>
+    </Popover>
+  )
+}
+
+const Metric = ({ config, configItemsByPath, configActions, blockPath, dimensions, block, metric }) => {
+  const values = useMemo(() => {
+    const blockItems = block.order.reduce((valuesInBlock, orderKey) => {
+      const val = configItemsByPath?.[`${blockPath}.${orderKey}`]?.value?.filter((_val) => {
+        return _val.parentKey === metric.key
+      })
+      return { ...valuesInBlock, [orderKey]: val }
+    }, {})
+
+    return blockItems
+  }, [configItemsByPath, blockPath])
+
+  return (
+    <CustomPopover
+      config={config}
+      configItemsByPath={configItemsByPath}
+      configActions={configActions}
+      blockPath={blockPath}
+      block={block}
+      dimensions={dimensions}
+      metric={metric}
+      values={values}
+    >
+      <div className="block-metric-metrics-item">
+        <p>{block.labelBuilder(values)}</p>
+      </div>
+    </CustomPopover>
+  )
+}
+
+const MetricBlock = ({ config, configItemsByPath, configActions, blockPath, dimensions, block }) => {
+  const { title, subtitle } = block
+
+  const options = useMemo(() => block.options || dimensions, [dimensions, block])
+  const flatOptions = useMemo(() => block.options || options.flatMap((d) => d.options), [options, block])
+
+  const metrics = useMemo(() => configItemsByPath?.[blockPath]?.value || [], [configItemsByPath, blockPath])
+
+  return (
+    <div className="block block-metric">
+      <div className="block-metric-header">
+        <div className="block-metric-header-text">
+          <span className="block__title">{title}</span>
+          <span className="block__subtitle">{subtitle}</span>
+        </div>
+        <div className="block-metric-button-container">
+          <CustomPopover
+            config={config}
+            configItemsByPath={configItemsByPath}
+            configActions={configActions}
+            blockPath={blockPath}
+            block={block}
+            dimensions={dimensions}
+          >
+            <ButtonAdd showLabel={false} size="small" />
+          </CustomPopover>
+        </div>
+      </div>
+      <div className="block-metric-metrics-container">
+        {metrics.map((metric) => (
+          <Metric
+            config={config}
+            configItemsByPath={configItemsByPath}
+            configActions={configActions}
+            blockPath={blockPath}
+            block={block}
+            dimensions={dimensions}
+            key={metric.key}
+            metric={metric}
+          />
+        ))}
+      </div>
+      <span className="block__number-options">{flatOptions.length} Option(s)</span>
+    </div>
+  )
+}
+
+export default MetricBlock
--- webapp/views/App/views/Data/Charts/state/chartTypes/pie/index.js
+++ webapp/views/App/views/Data/Charts/state/chartTypes/pie/index.js
@@ -3,24 +3,14 @@ import * as A from '@core/arena'
 const pie = {
   selector: {
     title: 'Pie',
-    onSelect:
-      ({ spec, onUpdateSpec }) =>
-      () => {
-        const newSpec = {
-          ...spec,
-          layer: [
-            {
-              mark: {
-                type: 'arc',
-                innerRadius: 40,
-                outerRadius: 60,
-              },
-            },
-          ],
-        }
-        delete newSpec.encoding
-        onUpdateSpec(A.stringify(newSpec))
+  },
+  baseSpec: {
+    $schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+    layer: [
+      {
+        mark: { type: 'arc', innerRadius: 40, outerRadius: 60 },
       },
+    ],
   },
   builderBlocks: {
     blocks: {
@@ -33,50 +23,87 @@ const pie = {
             id: 'groupBy',
             title: 'Group by',
             subtitle: 'Select the dimension to group the data',
-            type: 'input',
-            onUpdateSpec:
-              ({ spec, onUpdateSpec }) =>
-              ({ dimension }) => {
-                if (!dimension) {
-                  const newSpec = {
-                    ...spec,
-                  }
-                  delete newSpec.encoding.color
-                  onUpdateSpec(A.stringify(newSpec))
-                  return
-                }
+            type: 'select',
+            optionsParams: { filter: ['nominal', 'temporal'] },
+            isMulti: true,
+            valuesToSpec: ({ value = [], spec = {} }) => {
+              const transform = {
+                calculate: `${value.map((val) => `datum.${val.value}`).join("+','+")}`,
+                as: `${value.map((val) => val.name).join('_')}`,
+              }
 
-                const { name, type } = dimension
+              const color = {
+                field: transform.as,
+                type: 'nominal',
+                legend: {
+                  titleFontSize: 8,
+                  labelFontSize: 5,
+                },
 
-                const newSpec = {
-                  ...spec,
-                  encoding: {
-                    ...(spec.encoding || {}),
-                    color: {
-                      field: name,
-                      type,
-                      legend: {
-                        titleFontSize: 8,
-                        labelFontSize: 5,
-                      },
+                impute: {
+                  value: 'NULL',
+                },
+              }
 
-                      impute: {
-                        value: 'NULL',
-                      },
-                    },
-                  },
-                }
-                onUpdateSpec(A.stringify(newSpec))
-              },
+              const newSpec = {
+                ...spec,
+                transform: [transform],
+                encoding: {
+                  ...(spec.encoding || {}),
+                  color,
+                },
+              }
+
+              return newSpec
+            },
+          },
+
+          size: {
+            id: 'size',
+            title: 'Size',
+            subtitle: 'Select with local options',
+            type: 'select',
+            options: [
+              { value: 1, label: 1, name: 'hello', type: 'nominal' },
+              { value: 10, label: 100, name: 'hello', type: 'nominal' },
+            ],
+            optionsParams: { showIcons: false },
+            isMulti: false,
+            valuesToSpec: ({ value = [], spec = {} }) => {
+              return spec
+            },
           },
           metric: {
             id: 'metric',
             title: 'Metric',
             subtitle: 'Select the measurement to group the data',
-            type: 'input',
-            onUpdateSpec:
-              ({ spec, onUpdateSpec }) =>
-              ({ dimension, aggregateOp = 'sum' }) => {
+            type: 'metric',
+            labelBuilder: (values) => {
+              const aggregation = values.aggregation.map(({ label }) => label)
+              const column = values.column.map(({ label }) => label)
+
+              return `${aggregation}(${column})`
+            },
+            blocks: {
+              column: {
+                id: 'column',
+                title: 'Column',
+                type: 'select',
+              },
+              aggregation: {
+                id: 'aggregation',
+                title: 'Aggregation',
+                type: 'select',
+                options: [
+                  { value: 'sum', label: 'Sum', name: 'sum', type: 'aggregation' },
+                  { value: 'avg', label: 'Avg', name: 'avg', type: 'aggregation' },
+                ],
+                optionsParams: { showIcons: false },
+              },
+            },
+            order: ['column', 'aggregation'],
+            valuesToSpec: ({ value = [], spec = {} }) => {
+              /*
                 if (!dimension) {
                   const newSpec = {
                     ...spec,
@@ -87,6 +114,7 @@ const pie = {
                 }
 
                 const { name, type } = dimension
+                const { aggregateOp = 'sum' } = params
 
                 const newSpec = {
                   ...spec,
@@ -104,13 +132,38 @@ const pie = {
                   },
                 }
                 onUpdateSpec(A.stringify(newSpec))
-              },
+                */
+              return spec
+            },
           },
         },
         order: ['groupBy', 'metric'],
       },
+      other: {
+        title: 'Other',
+        subtitle: 'hello world',
+        type: 'container',
+        blocks: {
+          size: {
+            id: 'size',
+            title: 'Size',
+            subtitle: 'Select with local options',
+            type: 'select',
+            options: [
+              { value: 1, label: 1, name: 'hello', type: 'nominal' },
+              { value: 10, label: 100, name: 'hello', type: 'nominal' },
+            ],
+            optionsParams: { showIcons: false },
+            isMulti: false,
+            valuesToSpec: ({ value = [], spec = {} }) => {
+              return spec
+            },
+          },
+        },
+        order: ['size'],
+      },
     },
-    order: ['query'],
+    order: ['query', 'other'],
   },
 }
 
