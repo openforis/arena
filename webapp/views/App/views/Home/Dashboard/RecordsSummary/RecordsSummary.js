b89382ee2467f76dd3b47b9fdd72e9723359dfe3
Stefano Ricci <SteRiccio@users.noreply.github.com>
2022-08-02 15:04:36 +0200

    fixed records summary dropdown

--- webapp/views/App/views/Home/Dashboard/RecordsSummary/RecordsSummary.js
+++ webapp/views/App/views/Home/Dashboard/RecordsSummary/RecordsSummary.js
@@ -37,7 +37,7 @@ const RecordsSummary = () => {
           <Dropdown
             items={timeRangeItems}
             selection={timeRangeSelection}
-            onChange={(item) => onChangeTimeRange({ timeRange: item.key })}
+            onChange={(item) => onChangeTimeRange({ timeRange: item.value })}
             sourceElement={timeRangeElementRef.current}
             readOnlyInput
           />
