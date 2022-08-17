c96cee63600d9d3583397db782dc9da34c1c641b
Stefano Ricci <SteRiccio@users.noreply.github.com>
2022-08-17 15:30:33 +0200

    Fixed: CSV data export not including _ha variables

--- server/modules/surveyRdb/manager/surveyRdbManager.js
+++ server/modules/surveyRdb/manager/surveyRdbManager.js
@@ -203,7 +203,6 @@ export const fetchEntitiesDataToCsvFiles = async (
 
     await fetchViewData({
       survey,
-      columnNodeDefs: childDefs,
       streamOutput: stream,
       query,
       addCycle: true,
