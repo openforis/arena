f208f8e202e41c16e47ee752041f1ccbf5ee6672
persas <diego.persas@gmail.com>
2022-09-02 19:41:12 +0200

    update types, add filters and table

--- webapp/views/App/views/Data/Charts/components/Chart/Chart.js
+++ webapp/views/App/views/Data/Charts/components/Chart/Chart.js
@@ -1,29 +1,40 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Split from 'react-split'
+import Data from './components/Data'
+
 import './Chart.scss'
 
-const Chart = ({ src, draft, renderChart }) => {
+const Chart = ({ data, draft, renderChart, fullScreen }) => {
   return (
     <div className="charts_chart__container">
-      {src && (
-        <img
-          src={`data:image/svg+xml;base64,${btoa(unescape(encodeURIComponent(src)))}`}
-          alt=""
-          width="100%"
-          height="100%"
-        />
-      )}
       {draft && (
         <div className="charts_chart_draft_overlay">
           <button onClick={renderChart}>rerender</button>
         </div>
+      )}
+      {data && !draft && (
+        <Split sizes={[70, 30]} expandToMin={true} className="wrap wrap_vertical" direction="vertical">
+          <div className="charts_chart__image_container">
+            {data?.svg && (
+              <img
+                src={`data:image/svg+xml;base64,${btoa(unescape(encodeURIComponent(data?.svg)))}`}
+                alt=""
+                width="100%"
+                height="100%"
+              />
+            )}
+          </div>
+          {data?.table && <Data data={data} fullScreen={fullScreen} />}
+        </Split>
       )}
     </div>
   )
 }
 
 Chart.propTypes = {
-  src: PropTypes.string,
+  data: PropTypes.object,
+  fullScreen: PropTypes.bool,
   draft: PropTypes.bool,
   renderChart: PropTypes.func.isRequired,
 }
