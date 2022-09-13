5354cda9e8c2ecc1d4bc76909aa4bef29861f6cd
Stefano Ricci <SteRiccio@users.noreply.github.com>
2022-09-13 11:57:02 +0200

    fixed DeepScan issue

--- webapp/store/surveyRdb/hooks/useEntityDataCount.js
+++ webapp/store/surveyRdb/hooks/useEntityDataCount.js
@@ -7,13 +7,10 @@ import { useDataCountByEntityDefUuid } from './useDataCountByEntityDefUuid'
 export const useEntityDataCount = (entityDefUuid) => {
   const survey = useSurvey()
 
-  const entityDefArray = useMemo(() => {
-    if (entityDefUuid) {
-      const entity = entityDefUuid ? Survey.getNodeDefByUuid(entityDefUuid)(survey) : null
-      return [entity]
-    }
-    return []
-  }, [entityDefUuid, survey])
+  const entityDefArray = useMemo(
+    () => (entityDefUuid ? [Survey.getNodeDefByUuid(entityDefUuid)(survey)] : []),
+    [entityDefUuid, survey]
+  )
 
   const dataCountByEntityDefUuid = useDataCountByEntityDefUuid({ nodeDefs: entityDefArray })
 
