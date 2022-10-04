6e5912090236b5050ef22bbc5f36561dc4905a5d
Daniel Koch <daniel.koch@helsinki.fi>
2022-10-04 13:02:22 +0300

    Geospatial wmts (#2434)
    * add a scalebar
    
    * Fly from point to point on sampling point data
    
    * fix wrong proptype
    
    * finalize code for moving between sampling points
    
    * Clean next/previous (samlpling point data points on map) code a little bit
    
    * Sampling Polygon & editing its properties
    
    * fix typo in comment
    
    * tidy up the code
    
    * add keys to sampling polygon editor items
    
    * Side By Side view for planet data
    
    * remove some copypaste code by merging two functions
    
    * false color option for planet data
    
    * json editor for sampling polygon options
    
    * changing label location (surveyInfo->JSON toggle)
    
    * sampling polygon default values
    
    * make sampling polygon json editor bigger to fit the whole json without a scroll bar
    
    * fix a bug in ten cirle controlpoints
    
    * KML/KMZ/Shapefile support
    
    * opacity slider for kml/kmz/shapefile
    
    * style for kml/kmz/shapefile options
    
    * match maxzoom on righ layer on slider
    
    * modify KML/KMZ/Shapefile options opacity
    
    * add i18n and access control
    
    * fixed code style issues and changed SamplingPolygon data from string to object
    
    * removed isFilePicked and fixed a showZoomLevel import
    
    * refactor SamplingPolygonEditor
    
    * fix comparing to object literal in SamplintPolygonEditor
    
    * remove duplicate display statement
    
    * remove leaflet-side-by-side index.html since it is unused and might be a security risk
    
    * refactor code in imported L.KML to get it pass deep scan
    
    * refactor code in imported L.KML to get it pass deep scan
    
    * remove null catch block, instead use finally block
    
    * fix side-by-side right layer being wrong layer on init when using monthly periods
    
    * remove selected tag from a option
    
    * remove using await and then in the same function
    
    * remove non needed if...else
    
    * handle undefined samplingPolygon
    
    * add jszip
    
    * change sampling polygin properties to camelCase
    
    * Use updated side-by-side module as a dependency and remove it from the source
    
    * import custom wmts
    
    * prepare WMTS component for partial publish
    
    * remove stream
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    Co-authored-by: Stefano Ricci <1219739+SteRiccio@users.noreply.github.com>

---
+++ webapp/components/Map/WmtsComponent.js
@@ -0,0 +1,102 @@
+import React, { useEffect, useState } from 'react'
+import { LayersControl, TileLayer } from 'react-leaflet'
+
+
+import axios from 'axios'
+
+
+/** 
+ * This component is not ready, but can be used for demo purposes.
+ * Just set componentDisabled to false and use any url you want.
+ * The url needs to go to a WMTS getCapabilities xml document, directly or as an api call.
+ * 
+ * basemaps is a list of all available layers.
+ * 
+ * tileMatrixSets is a list of ALL available TileMatrixSets. Some layers may not have support all TileMatrixSets.
+ * TileMatrixSet is basically the projection. TODO is to find a way to find the projection that leaflet uses (EPSG:3857).
+ * (The problem is that no all providers use the same name in the TileMatrixSet list. For some it is with the EPSG code, for some it is custom name...)
+ * 
+ * currentTileMatrixSet is the TileMatrixSet that is used currently...
+*/
+
+export const WmtsComponent = () => {
+    const apiKey = '' 
+    const [baseMaps, setBaseMaps] = useState([])
+    const [tileMatrixSets, setTileMatrixSets] = useState([])
+    const [currentTileMatrixSet, setCurrentTileMatrixSet] = useState('')
+
+    const componentDisabled = true;
+
+    /*  Here we have some example urls, in the final component the url should be provided by the user */
+    //const url = "https://io.apps.fao.org/geoserver/wms/ASIS/VHI_D/v1?service=WMS&version=1.3.0&request=GetCapabilities"
+    //const url = "https://avoin-karttakuva.maanmittauslaitos.fi/avoin/wmts/1.0.0/WMTSCapabilities.xml?api-key=" + apiKey
+    //const url = "https://kartta.hsy.fi/geoserver/gwc/service/wmts?request=getCapabilities"
+    const url = "https://sampleserver6.arcgisonline.com/arcgis/rest/services/WorldTimeZones/MapServer/WMTS/1.0.0/WMTSCapabilities.xml"
+
+    /* Get a list of all available TileMatrixSets. Some layers may not support all of the available TileMatrixSets, each layer has info about the supported sets */
+    const getTileMatrixSets = (json) => {
+        const list = json.map(set => set['ows:Identifier']['_text'])
+        return list
+    }
+
+    /* Prepare a list of {title, url} objects for showing all layers */
+    const makeBaseMaps = (layers, newTileMatrixSet) => {
+        if (!layers.length) layers = [layers]
+        const baseMaps = layers.map((layer) => {
+            const title = layer['ows:Title']._text
+            let url = layer.ResourceURL._attributes?.template || layer.ResourceURL[0]._attributes.template
+            url = url.replace(/{TileRow}/gi, '{y}')
+            url = url.replace(/{TileCol}/gi, '{x}')
+            url = url.replace(/{TileMatrix}/gi, '{z}')
+            url = url.replace(/{TileMatrixSet}/gi, newTileMatrixSet)
+            url = url.replace(/{Style}/gi, layer.Style["ows:Identifier"]._text)
+            //url = url + "?api-key={apiKey}" // If api key is required, different providers might want this different way so find out a way to check what the way is or just try with the common ones
+            const object = {
+                title: title,
+                url: url
+            }
+            return object
+        })
+        return baseMaps
+    }
+
+    useEffect( () => {
+        if (componentDisabled) return
+        let isMounted = true
+        getCapabilities(url).then( (capabilities) => {
+            const newTileMatrixSets = getTileMatrixSets(capabilities.Capabilities.Contents.TileMatrixSet)
+            const base = makeBaseMaps(capabilities.Capabilities.Contents.Layer, newTileMatrixSets[0])
+            if (isMounted) {
+                setTileMatrixSets(newTileMatrixSets)
+                setCurrentTileMatrixSet(newTileMatrixSets[0])
+                setBaseMaps(base)
+            }
+        })
+        return () => { isMounted = false }
+    }, [])
+
+        return (
+            baseMaps.map((layer) => {
+                return (
+                    <LayersControl.BaseLayer key={layer.title} name={layer.title} >
+                        <TileLayer key={layer.title} url={layer.url} apiKey={apiKey} attribution={''}/>
+                    </LayersControl.BaseLayer>
+                )
+            })
+            )
+}
+
+/* Query the user given url through our server to avoid CORS violations. The layer urls are not proxied through our server yet, but maybe they should. 
+   If you get CORS problems, that will be the solution.
+*/
+const getCapabilities = async (url) => {
+    let res, data
+    try {
+        res = await axios.get('/api/geo/map/wmts/getCapabilities/' + encodeURIComponent(url))
+        data =  res.data
+    } catch {
+        return null
+    }
+    return data
+}
+
