70622906647b6699cad8063321c5bf63b61caf0f
Stefano Ricci <SteRiccio@users.noreply.github.com>
2022-11-14 11:35:25 +0100

    prepare Docker build

--- infra/web/start.sh
+++ infra/web/start.sh
@@ -2,13 +2,4 @@
 
 echo "Starting up web app..."
 
-# Disable RStudio Server authentication (authentication managed by NGINX)
-export DISABLE_AUTH=true
-
-# NGINX config: replace PORT and ARENA_PORT placeholders with current environment variables
-sed -i -e 's/$ARENA_PORT/'"$ARENA_PORT"'/g' /etc/nginx/sites-available/default
-sed -i -e 's/$PORT/'"$PORT"'/g' /etc/nginx/sites-available/default
-
-/init & 
-sleep 10 && service nginx start & 
 pm2-runtime server.js 
