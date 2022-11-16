920b7947a043fe2dc77edf20e8d89b283fe1a8b5
Stefano Ricci <SteRiccio@users.noreply.github.com>
2022-11-16 14:04:20 +0100

    SurveysDropdown: show survey description; show items sorted by label

--- webapp/components/survey/SurveyDropdown/useSurveyDropdownOptions.js
+++ webapp/components/survey/SurveyDropdown/useSurveyDropdownOptions.js
@@ -9,7 +9,8 @@ const toOption = (surveyInfo) => {
   if (!surveyInfo) return null
 
   return {
-    label: Survey.getName(surveyInfo),
+    label: Survey.getDefaultLabel(surveyInfo) || Survey.getName(surveyInfo),
+    description: Survey.getDefaultDescription(surveyInfo),
     value: Survey.getIdSurveyInfo(surveyInfo),
   }
 }
@@ -26,12 +27,24 @@ export const useSurveyDropdownOptions = () => {
         API.fetchSurveys({ draft: false, template: true }),
       ])
 
+      const surveyOptions = surveys.map(toOption)
+      const templateOptions = templates.map(toOption)
+
+      // sort surveys and labels by label
+      const sortByLabelFn = (a, b) => {
+        if (a.label < b.label) return -1
+        if (a.label > b.label) return 1
+        return 0
+      }
+      surveyOptions.sort(sortByLabelFn)
+      templateOptions.sort(sortByLabelFn)
+
       setOptions([
         {
-          label: i18n.t('homeView.surveyCreate.template', { count: templates.length }),
-          options: templates.map(toOption),
+          label: i18n.t('homeView.surveyCreate.template', { count: templateOptions.length }),
+          options: templateOptions,
         },
-        { label: i18n.t('homeView.surveyCreate.survey', { count: surveys.length }), options: surveys.map(toOption) },
+        { label: i18n.t('homeView.surveyCreate.survey', { count: surveyOptions.length }), options: surveyOptions },
       ])
     })()
   }, [])
