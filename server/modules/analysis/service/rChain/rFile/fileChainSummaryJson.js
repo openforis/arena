ba856e54eb71c819e1d9d0f195da1c6a4cbeeca9
Stefano Ricci <SteRiccio@users.noreply.github.com>
2022-11-30 21:53:53 +0100

    fixing chain RStudio startup

--- server/modules/analysis/service/rChain/rFile/fileChainSummaryJson.js
+++ server/modules/analysis/service/rChain/rFile/fileChainSummaryJson.js
@@ -6,7 +6,7 @@ const fileName = 'chain_summary.json'
 const initFile = async ({ dir, surveyId, chainUuid, cycle }) => {
   const chainSummary = await AnalysisService.generateChainSummary({ surveyId, chainUuid, cycle })
   const fileDest = FileUtils.join(dir, fileName)
-  await FileUtils.writeFile(fileDest, JSON.stringify(chainSummary), null, 4)
+  await FileUtils.writeFile(fileDest, JSON.stringify(chainSummary, null, 2))
   return fileDest
 }
 
