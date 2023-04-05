540be4721b31ac27e66a09603f5d199047c23443
Stefano Ricci <1219739+SteRiccio@users.noreply.github.com>
2023-04-05 14:21:56 +0200

    Access request: do not allow uppercase letters in email field; layout adjustments (#2762)
    Co-authored-by: Stefano Ricci <SteRiccio@users.noreply.github.com>

--- webapp/views/Guest/views/AccessRequest/AccessRequestField.js
+++ webapp/views/Guest/views/AccessRequest/AccessRequestField.js
@@ -7,46 +7,97 @@ import { Countries } from '@core/Countries'
 import * as UserAccessRequest from '@core/user/userAccessRequest'
 import * as Validation from '@core/validation/validation'
 
-import { FormItem, Input } from '@webapp/components/form/Input'
+import { FormItem } from '@webapp/components/form/Input'
 
 import { useI18n } from '@webapp/store/system'
 import { SurveyTemplateSelect } from './SurveyTemplateSelect'
-import { Dropdown } from '@webapp/components/form'
+import { Dropdown, EmailInput, TextInput } from '@webapp/components/form'
+import ValidationTooltip from '@webapp/components/validationTooltip'
 
-export const AccessRequestField = (props) => {
-  const { field, onChange, request, validation } = props
+const CountryField = (props) => {
+  const { field, onChange, value } = props
 
-  const i18n = useI18n()
+  const { name, defaultValue } = field
 
-  const { name, normalizeFn, required, defaultValue } = field
-
-  const validationFieldName = name.startsWith('props.') ? name.substring(6) : name
-  const value = Objects.path(name.split('.'))(request)
   const selectedValue = value || defaultValue
 
   return (
-    <FormItem key={name} label={i18n.t(`accessRequestView.fields.${name}`)} required={required}>
-      <>
-        {name === `${UserAccessRequest.keys.props}.${UserAccessRequest.keysProps.country}` ? (
-          <Dropdown
-            selection={selectedValue ? Countries.list.find((item) => item.code === selectedValue) : null}
-            items={Countries.list}
-            itemValue="code"
-            itemLabel="name"
-            onChange={(item) => onChange({ name, value: item?.code })}
-          />
-        ) : name === `${UserAccessRequest.keys.props}.${UserAccessRequest.keysProps.templateUuid}` ? (
-          <SurveyTemplateSelect selectedValue={selectedValue} onChange={(value) => onChange({ name, value })} />
-        ) : (
-          <Input
-            value={value}
-            onChange={(val) => {
-              onChange({ name, value: normalizeFn ? normalizeFn(val) : val })
-            }}
-            validation={Validation.getFieldValidation(validationFieldName)(validation)}
-          />
-        )}
-      </>
+    <Dropdown
+      selection={selectedValue ? Countries.list.find((item) => item.code === selectedValue) : null}
+      items={Countries.list}
+      itemValue="code"
+      itemLabel="name"
+      onChange={(item) => onChange({ name, value: item?.code })}
+    />
+  )
+}
+
+const TemplateField = (props) => {
+  const { field, onChange, value } = props
+
+  const { name, defaultValue } = field
+
+  const selectedValue = value || defaultValue
+
+  return <SurveyTemplateSelect selectedValue={selectedValue} onChange={(value) => onChange({ name, value })} />
+}
+
+const EmailField = (props) => {
+  const { field, onChange, value } = props
+
+  const { name } = field
+
+  return (
+    <EmailInput
+      value={value}
+      onChange={(val) => {
+        onChange({ name, value: val })
+      }}
+    />
+  )
+}
+
+const TextField = (props) => {
+  const { field, onChange, value } = props
+
+  const { name, normalizeFn } = field
+
+  return (
+    <TextInput
+      value={value}
+      onChange={(val) => {
+        onChange({ name, value: val })
+      }}
+      textTransformFunction={normalizeFn}
+    />
+  )
+}
+
+const componentsByFieldName = {
+  [`${UserAccessRequest.keys.props}.${UserAccessRequest.keysProps.country}`]: CountryField,
+  [UserAccessRequest.keys.email]: EmailField,
+  [`${UserAccessRequest.keys.props}.${UserAccessRequest.keysProps.templateUuid}`]: TemplateField,
+}
+
+export const AccessRequestField = (props) => {
+  const { field, request, validation } = props
+
+  const i18n = useI18n()
+  const { name, required } = field
+
+  let value = Objects.path(name.split('.'))(request)
+  if (Objects.isEmpty(value)) {
+    value = ''
+  }
+
+  const fieldComponent = React.createElement(componentsByFieldName[name] || TextField, { ...props, value })
+
+  const validationFieldName = name.startsWith('props.') ? name.substring(6) : name
+  const fieldValidation = Validation.getFieldValidation(validationFieldName)(validation)
+
+  return (
+    <FormItem label={i18n.t(`accessRequestView.fields.${name}`)} required={required}>
+      <ValidationTooltip validation={fieldValidation}>{fieldComponent}</ValidationTooltip>
     </FormItem>
   )
 }
