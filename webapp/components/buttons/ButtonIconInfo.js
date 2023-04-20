093f2d9f3c8ed2bcc8f50fa609788d9bd54584f4
Stefano Ricci <SteRiccio@users.noreply.github.com>
2023-04-20 14:05:13 +0200

    Node def coordinate: added info to "Allow only device coordinate" option

--- webapp/components/buttons/ButtonIconInfo.js
+++ webapp/components/buttons/ButtonIconInfo.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import classNames from 'classnames'
+import { Tooltip } from '@mui/material'
+
+import { useI18n } from '@webapp/store/system'
 
 import { Button } from './Button'
 
 export const ButtonIconInfo = (props) => {
-  const { className: classNameProp, ...otherProps } = props
+  const { className: classNameProp, title, ...otherProps } = props
 
+  const i18n = useI18n()
   const className = classNames('btn-transparent', classNameProp)
 
-  return <Button {...otherProps} className={className} iconClassName="icon-info icon-14px" />
+  return (
+    <Tooltip title={i18n.t(title)}>
+      <Button {...otherProps} className={className} iconClassName="icon-info icon-14px" />
+    </Tooltip>
+  )
 }
 
 ButtonIconInfo.propTypes = {
--- webapp/components/survey/NodeDefDetails/CoordinateProps.js
+++ webapp/components/survey/NodeDefDetails/CoordinateProps.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import { useI18n } from '@webapp/store/system'
-import { FormItem } from '@webapp/components/form/Input'
-
 import * as NodeDef from '@core/survey/nodeDef'
 
-import { State } from './store'
+import { useI18n } from '@webapp/store/system'
+import { FormItem } from '@webapp/components/form/Input'
 import { Checkbox } from '@webapp/components/form'
+import { ButtonIconInfo } from '@webapp/components/buttons'
+import { State } from './store'
 
 const CoordinateProps = (props) => {
   const { state, Actions } = props
@@ -18,10 +18,13 @@ const CoordinateProps = (props) => {
 
   return (
     <FormItem label={i18n.t('nodeDefEdit.coordinateProps.allowOnlyDeviceCoordinate')}>
-      <Checkbox
-        checked={NodeDef.isAllowOnlyDeviceCoordinate(nodeDef)}
-        onChange={(value) => Actions.setProp({ state, key: NodeDef.propKeys.allowOnlyDeviceCoordinate, value })}
-      />
+      <div className="form-item_row checkbox-with-info">
+        <ButtonIconInfo title="nodeDefEdit.coordinateProps.allowOnlyDeviceCoordinateInfo" />
+        <Checkbox
+          checked={NodeDef.isAllowOnlyDeviceCoordinate(nodeDef)}
+          onChange={(value) => Actions.setProp({ state, key: NodeDef.propKeys.allowOnlyDeviceCoordinate, value })}
+        />
+      </div>
     </FormItem>
   )
 }
