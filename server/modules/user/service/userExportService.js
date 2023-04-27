45a6e2d146adca25f9ed98895f7da45c7ffb5cf3
Stefano Ricci <1219739+SteRiccio@users.noreply.github.com>
2023-04-27 10:04:30 +0200

    Users export: add country column (#2801)
    Co-authored-by: Stefano Ricci <SteRiccio@users.noreply.github.com>

--- server/modules/user/service/userExportService.js
+++ server/modules/user/service/userExportService.js
@@ -1,5 +1,6 @@
 import i18n from '@core/i18n/i18nFactory'
 import * as AuthGroup from '@core/auth/authGroup'
+import { Countries } from '@core/Countries'
 
 import * as CSVWriter from '@server/utils/file/csvWriter'
 
@@ -16,10 +17,13 @@ const exportUsersIntoStream = async ({ outputStream }) => {
 
   const transformTitle = (title) => (title ? i18n.t(`user.titleValues.${title}`) : '')
 
+  const transformCountry = (countryCode) => (countryCode ? Countries.getCountryName({ code: countryCode }) : '')
+
   const headers = [
     'email',
     'name',
     'title',
+    'country',
     'status',
     'system_administrator',
     'surveys',
@@ -31,6 +35,7 @@ const exportUsersIntoStream = async ({ outputStream }) => {
     ...obj,
     surveys: transformSurveyNames(obj.surveys),
     title: transformTitle(obj.title),
+    country: transformCountry(obj.country),
   })
 
   const transformer = CSVWriter.transformJsonToCsv({ fields: headers, options: { objectTransformer } })
