14852e5cadf9734262239cc570e9c17003d476a1
Stefano Ricci <SteRiccio@users.noreply.github.com>
2023-08-28 10:01:34 +0200

    chain: do not mark survey as draft when opening a node def for edit

--- webapp/components/survey/NodeDefDetails/store/useNodeDefDetails.js
+++ webapp/components/survey/NodeDefDetails/store/useNodeDefDetails.js
@@ -1,15 +1,12 @@
 import { useCallback, useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router'
-import { useDispatch } from 'react-redux'
 
-import * as API from '@webapp/service/api'
 import { useIsDesignerNodeDefRoute, useOnUpdate, useOnBrowserBack, useOnPageUnload } from '@webapp/components/hooks'
 
 import * as Survey from '@core/survey/survey'
-import * as NodeDef from '@core/survey/nodeDef'
 
 import { appModuleUri, analysisModules } from '@webapp/app/appModules'
-import { useSurvey, useSurveyId, useSurveyCycleKey, NodeDefsActions } from '@webapp/store/survey'
+import { useSurvey, useSurveyCycleKey } from '@webapp/store/survey'
 
 import { useActions } from './actions'
 import { State } from './state'
@@ -18,10 +15,8 @@ export const useNodeDefDetails = () => {
   const { nodeDefUuid } = useParams()
 
   const navigate = useNavigate()
-  const dispatch = useDispatch()
 
   const survey = useSurvey()
-  const surveyId = useSurveyId()
   const surveyCycleKey = useSurveyCycleKey()
 
   const [state, setState] = useState({})
@@ -41,22 +36,13 @@ export const useNodeDefDetails = () => {
     const loadNodeDef = async () => {
       // Editing a nodeDef
       if (nodeDefUuid) {
-        let nodeDefSurvey = Survey.getNodeDefByUuid(nodeDefUuid)(survey)
-
-        if (NodeDef.isAnalysis(nodeDefSurvey) && !NodeDef.isTemporary(nodeDefSurvey)) {
-          try {
-            nodeDefSurvey = await API.fetchNodeDef({ surveyId, nodeDefUuid })
-            dispatch(NodeDefsActions.updateNodeDef({ nodeDef: nodeDefSurvey }))
-          } catch (err) {
-            navigate(-1)
-          }
-        }
+        const nodeDefSurvey = Survey.getNodeDefByUuid(nodeDefUuid)(survey)
         const validation = Survey.getNodeDefValidation(nodeDefSurvey)(survey)
         setState(State.create({ nodeDef: nodeDefSurvey, validation }))
       }
     }
     loadNodeDef()
-  }, [nodeDefUuid])
+  }, [nodeDefUuid, survey])
 
   useOnUpdate(() => {
     if (editingFromDesigner) {
