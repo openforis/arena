b3fe5effa7829654cb86bbb1e38e3279cddf6c05
Stefano Ricci <1219739+SteRiccio@users.noreply.github.com>
2023-08-28 19:29:59 +0200

    chain: do not mark survey as draft when opening a node def for edit (#2988)
    * chain: do not mark survey as draft when opening a node def for edit
    
    * fixed node def initialization
    
    * fixed draft node defs fetching
    
    ---------
    
    Co-authored-by: Stefano Ricci <SteRiccio@users.noreply.github.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- webapp/store/survey/status/state.js
+++ webapp/store/survey/status/state.js
@@ -13,9 +13,11 @@ const getStatus = R.pipe(SurveyState.getSurvey, R.propOr(false, stateKey))
 const _areDefsFetched = R.pipe(getStatus, R.propEq(keys.defsFetched, true))
 const _areDefsDraftFetched = R.pipe(getStatus, R.propEq(keys.defsDraftFetched, true))
 
-export const areDefsFetched = (draft) => (state) => (draft && _areDefsDraftFetched(state)) || _areDefsFetched(state)
+export const areDefsFetched = (draft) => (state) => draft ? _areDefsDraftFetched(state) : _areDefsFetched(state)
 
-export const assocDefsFetched = (draft) =>
-  R.pipe(R.assoc(keys.defsFetched, true), R.assoc(keys.defsDraftFetched, draft))
+export const assocDefsFetched = (draft) => (state) =>
+  draft
+    ? R.pipe(R.assoc(keys.defsDraftFetched, true), R.assoc(keys.defsFetched, false))(state)
+    : R.pipe(R.assoc(keys.defsFetched, true), R.assoc(keys.defsDraftFetched, false))(state)
 
 export const resetDefsFetched = R.pipe(R.assoc(keys.defsFetched, false), R.assoc(keys.defsDraftFetched, false))
