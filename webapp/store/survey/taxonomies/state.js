5773265e089991a185debf8a643e614de2678cee
Stefano Ricci <SteRiccio@users.noreply.github.com>
2023-09-07 09:19:44 +0200

    fixed validation error using categoryItemProp function with new category

--- webapp/store/survey/taxonomies/state.js
+++ webapp/store/survey/taxonomies/state.js
@@ -1,7 +1,9 @@
 import * as A from '@core/arena'
 
+import * as Taxonomy from '@core/survey/taxonomy'
+
 export const stateKey = 'taxonomies'
 
-export const assocTaxonomy = (taxonomy) => A.assoc(taxonomy)
+export const assocTaxonomy = (taxonomy) => A.assoc(Taxonomy.getUuid(taxonomy), taxonomy)
 
 export const dissocTaxonomy = (taxonomyUuid) => A.dissoc(taxonomyUuid)
