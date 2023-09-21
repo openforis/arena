46556405bdb2b00f586591fa7578304697c3609b
Stefano Ricci <SteRiccio@users.noreply.github.com>
2023-09-21 14:56:33 +0200

    Data export: added extension to exported files

--- server/modules/survey/service/export/jobs/FilesExportJob.js
+++ server/modules/survey/service/export/jobs/FilesExportJob.js
@@ -1,11 +1,13 @@
 import * as PromiseUtils from '@core/promiseUtils'
 import * as Survey from '@core/survey/survey'
+import * as RecordFile from '@core/record/recordFile'
 
 import Job from '@server/job/job'
 import * as FileUtils from '@server/utils/file/fileUtils'
 
 import * as SurveyRdbService from '@server/modules/surveyRdb/service/surveyRdbService'
 import * as FileService from '@server/modules/record/service/fileService'
+import { Objects } from '@openforis/arena-core'
 
 export default class FilesExportJob extends Job {
   constructor(params) {
@@ -31,12 +33,15 @@ export default class FilesExportJob extends Job {
     // write the files in subfolders by cycle
     await PromiseUtils.each(Object.entries(fileUuidsByCycle), async ([cycle, fileUuids]) => {
       await PromiseUtils.each(fileUuids, async (fileUuid) => {
+        const fileSummary = await FileService.fetchFileSummaryByUuid(surveyId, fileUuid, this.tx)
         const recordFileContent = await FileService.fetchFileContentAsStream({ surveyId, fileUuid }, this.tx)
         const cycleFilesPath = FileUtils.join(outputDir, 'files', cycle)
         if (!FileUtils.exists(cycleFilesPath)) {
           await FileUtils.mkdir(cycleFilesPath)
         }
-        const tempFilePath = FileUtils.join(cycleFilesPath, fileUuid)
+        const extension = RecordFile.getExtension(fileSummary)
+        const exportedFileName = Objects.isEmpty(extension) ? fileUuid : `${fileUuid}.${extension}`
+        const tempFilePath = FileUtils.join(cycleFilesPath, exportedFileName)
         await FileUtils.writeFile(tempFilePath, recordFileContent)
         this.incrementProcessedItems()
       })
