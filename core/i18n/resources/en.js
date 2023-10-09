9718304d0df0b70b5e0c2469a202f90398c59a0f
Stefano Ricci <1219739+SteRiccio@users.noreply.github.com>
2023-10-09 09:39:51 +0200

    Maximum number of surveys user can create: make it editable to system admins (#3056)
    * max surveys editable (server side)
    
    * added "max number of surveys user can create" to UI
    
    * make max surveys editable only for users with survey manager role
    
    ---------
    
    Co-authored-by: Stefano Ricci <SteRiccio@users.noreply.github.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- core/i18n/resources/en.js
+++ core/i18n/resources/en.js
@@ -811,6 +811,7 @@ Are you sure you want to continue?`,
     removeFromSurvey: 'Remove from survey',
     confirmRemove: 'Are you sure you want to revoke access to {{user}} from survey {{survey}}?',
     removeUserConfirmation: 'User {{user}} has been removed from survey {{survey}}',
+    maxSurveysUserCanCreate: 'Max surveys user can create',
   },
 
   userPasswordChangeView: {
--- core/user/_user/userProps.js
+++ core/user/_user/userProps.js
@@ -4,25 +4,38 @@ import * as ObjectUtils from '@core/objectUtils'
 
 import { keys as userKeys } from './userKeys'
 
+const defaultMaxSurveys = 5
+
 export const { mergeProps } = ObjectUtils
 
 export const keysProps = {
   title: 'title',
+  // private props (visible only to the user itself)
   mapApiKeyByProvider: 'mapApiKeyByProvider',
+  // restricted props (editable only by system admins)
+  maxSurveys: 'maxSurveys',
 }
+
+const privateProps = [keysProps.mapApiKeyByProvider]
+const restrictedProps = [keysProps.maxSurveys]
 
 export const titleKeys = ['mr', 'ms', 'preferNotToSay']
 
 // ====== CREATE
-export const newProps = ({ title = null, mapApiKeyByProvider = null }) => ({
-  ...(!A.isEmpty(title) ? { title } : {}),
-  ...(!A.isEmpty(mapApiKeyByProvider) ? { mapApiKeyByProvider } : {}),
-})
+export const newProps = (propsParam) =>
+  Object.keys(keysProps).reduce((acc, propKey) => {
+    const propValue = propsParam[propKey]
+    if (!A.isEmpty(propValue)) {
+      acc[propKey] = propValue
+    }
+    return acc
+  }, {})
 
 // ====== READ
 export const getProps = R.prop(userKeys.props)
 export const getTitle = R.pipe(getProps, R.propOr('', keysProps.title))
 export const getMapApiKey = ({ provider }) => R.pipe(getProps, R.path([keysProps.mapApiKeyByProvider, provider]))
+export const getMaxSurveys = R.pipe(getProps, R.propOr(defaultMaxSurveys, keysProps.maxSurveys))
 
 // ====== UPDATE
 export const assocProps = R.assoc(userKeys.props)
@@ -36,5 +49,10 @@ export const assocMapApiKey =
     const mapApiKeyByProviderUpdated = { ...mapApiKeyByProvider, [provider]: apiKey }
     return assocProp(keysProps.mapApiKeyByProvider)(mapApiKeyByProviderUpdated)(user)
   }
+export const assocMaxSurveys = assocProp(keysProps.maxSurveys)
 
-export const dissocPrivateProps = R.dissocPath([userKeys.props, keysProps.mapApiKeyByProvider])
+const dissocListOfProps = (propsArray) => (user) =>
+  propsArray.reduce((acc, propKey) => R.dissocPath([userKeys.props, propKey])(acc), user)
+
+export const dissocPrivateProps = dissocListOfProps(privateProps)
+export const dissocRestrictedProps = dissocListOfProps(restrictedProps)
