5c7b375f86631777e2e264c4ce8c87100d92fada
Stefano Ricci <SteRiccio@users.noreply.github.com>
2023-10-17 15:11:30 +0200

    Map: show category item keys in coordinate attribute popup

--- webapp/components/DataQuery/valueFormatter.js
+++ webapp/components/DataQuery/valueFormatter.js
@@ -7,18 +7,19 @@ import * as DateUtils from '@core/dateUtils'
 const formatters = {
   [NodeDef.nodeDefType.boolean]: ({ value, i18n, nodeDef }) =>
     i18n.t(`surveyForm.nodeDefBoolean.labelValue.${NodeDef.getLabelValue(nodeDef)}.${value}`),
+  [NodeDef.nodeDefType.code]: ({ value, label }) => label ?? value,
   [NodeDef.nodeDefType.date]: ({ value }) => DateUtils.format(DateUtils.parseISO(value), DateUtils.formats.dateDefault),
   [NodeDef.nodeDefType.decimal]: ({ value, nodeDef }) =>
     NumberUtils.formatDecimal(value, NodeDef.getMaxNumberDecimalDigits(nodeDef)),
   [NodeDef.nodeDefType.integer]: ({ value }) => NumberUtils.formatInteger(value),
 }
 
-const format = ({ value, i18n, nodeDef }) => {
+const format = ({ value, label, i18n, nodeDef }) => {
   if (A.isNull(value)) {
     return ''
   }
   const formatter = formatters[NodeDef.getType(nodeDef)]
-  const formatValue = (v) => (formatter ? formatter({ value: v, i18n, nodeDef }) : value)
+  const formatValue = (v) => (formatter ? formatter({ value: v, label, i18n, nodeDef }) : value)
 
   return NodeDef.isMultiple(nodeDef) && Array.isArray(value) ? value.map(formatValue).join(', ') : formatValue(value)
 }
