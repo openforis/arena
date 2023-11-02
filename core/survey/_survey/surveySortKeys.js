ede72523029c8edffcad3a7086daee2462ef4650
Stefano Ricci <1219739+SteRiccio@users.noreply.github.com>
2023-11-02 15:52:18 +0100

    survey: added date_published (#3125)
    Co-authored-by: Stefano Ricci <SteRiccio@users.noreply.github.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- core/survey/_survey/surveySortKeys.js
+++ core/survey/_survey/surveySortKeys.js
@@ -3,6 +3,7 @@ import * as ObjectUtils from '@core/objectUtils'
 export const sortableKeys = {
   dateCreated: ObjectUtils.keys.dateCreated,
   dateModified: ObjectUtils.keys.dateModified,
+  datePublished: 'datePublished',
   name: ObjectUtils.keys.name,
   ownerName: 'owner_name',
   label: 'label',
