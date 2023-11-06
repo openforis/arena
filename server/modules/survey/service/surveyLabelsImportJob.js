2ca6989bf0e4992ebfa1d66f5ad5c060677233a3
Stefano Ricci <SteRiccio@users.noreply.github.com>
2023-11-06 22:56:45 +0100

    fixed labels import job server side

--- server/modules/survey/service/surveyLabelsImportJob.js
+++ server/modules/survey/service/surveyLabelsImportJob.js
@@ -6,60 +6,34 @@ import Job from '@server/job/job'
 import * as CSVReader from '@server/utils/file/csvReader'
 import * as SurveyManager from '@server/modules/survey/manager/surveyManager'
 import * as NodeDefManager from '@server/modules/nodeDef/manager/nodeDefManager'
-import { Arrays, SystemError } from '@openforis/arena-core'
+import { Arrays } from '@openforis/arena-core'
 
 const errorPrefix = `validationErrors.surveyLabelsImport.`
 const getLabelColumn = (langCode) => `label_${langCode}`
 const getDescriptionColumn = (langCode) => `description_${langCode}`
 
-const getNodeDef = ({ survey, row }) => {
-  const { uuid, path } = row
-  let nodeDef = null
-  if (uuid) {
-    nodeDef = Survey.getNodeDefByUuid(uuid)(survey)
-  } else if (path) {
-    const name = Arrays.last(path)
-    nodeDef = Survey.getNodeDefByName(name)(survey)
-  }
-  if (!nodeDef) {
-    throw new SystemError(`${errorPrefix}.cannotFindNodeDef`, { uuid, path })
-  }
-  return nodeDef
-}
-
-const validateHeaders =
-  ({ survey }) =>
-  (headers) => {
-    const langCodes = Survey.getLanguages(Survey.getSurveyInfo(survey))
-    const mandatoryHeaders = ['uuid']
-    const validHeaders = [...mandatoryHeaders]
-    langCodes.forEach((langCode) => {
-      validHeaders.push(getLabelColumn(langCode), getDescriptionColumn(langCode))
-    })
-    const invalidHeaders = headers.filter((header) => !validHeaders.includes(header)).join(', ')
-    if (invalidHeaders) {
-      throw new SystemError(`${errorPrefix}.invalidHeaders`, { invalidHeaders })
-    }
-  }
-
 export default class SurveyLabelsImportJob extends Job {
   constructor(params) {
     super(SurveyLabelsImportJob.type, params)
+    this.validateHeaders = this.validateHeaders.bind(this)
   }
 
   async execute() {
     const { context, tx } = this
     const { filePath, surveyId } = context
-    const survey = await SurveyManager.fetchSurveyAndNodeDefsBySurveyId({ surveyId, draft: true, advanced: true }, tx)
+    const survey = await SurveyManager.fetchSurveyAndNodeDefsBySurveyId({ surveyId, draft: true }, tx)
+    this.setContext({ survey })
     const langCodes = Survey.getLanguages(Survey.getSurveyInfo(survey))
 
     const nodeDefsUpdated = []
 
-    CSVReader.createReaderFromFile(
+    this.csvReader = CSVReader.createReaderFromFile(
       filePath,
-      validateHeaders({ survey }),
+      this.validateHeaders,
       async (row) => {
-        const nodeDef = getNodeDef({ survey, row })
+        const nodeDef = await this.getNodeDef({ survey, row })
+        if (!nodeDef) return
+
         const labels = langCodes.reduce((acc, langCode) => {
           const label = row[getLabelColumn(langCode)]
           acc[langCode] = label
@@ -80,7 +54,65 @@ export default class SurveyLabelsImportJob extends Job {
       (total) => (this.total = total)
     )
 
-    await NodeDefManager.updateNodeDefPropsInBatch({ surveyId, nodeDef: nodeDefsUpdated }, tx)
+    await this.csvReader.start()
+
+    if (nodeDefsUpdated.length > 0) {
+      await NodeDefManager.updateNodeDefPropsInBatch(
+        {
+          surveyId,
+          nodeDefs: nodeDefsUpdated.map((nodeDefUpdated) => ({
+            nodeDefUuid: NodeDef.getUuid(nodeDefUpdated),
+            props: NodeDef.getProps(nodeDefUpdated),
+          })),
+        },
+        tx
+      )
+    }
+  }
+
+  async validateHeaders(headers) {
+    const { context } = this
+    const { survey } = context
+    const langCodes = Survey.getLanguages(Survey.getSurveyInfo(survey))
+
+    const fixedHeaders = ['uuid', 'path']
+    const dynamicHeaders = []
+    langCodes.forEach((langCode) => {
+      dynamicHeaders.push(getLabelColumn(langCode), getDescriptionColumn(langCode))
+    })
+    const validHeaders = [...fixedHeaders, ...dynamicHeaders]
+    const invalidHeaders = headers.filter((header) => !validHeaders.includes(header)).join(', ')
+    if (invalidHeaders) {
+      await this.addErrorAndStopCsvReader('invalidHeaders', { invalidHeaders })
+    }
+  }
+
+  async getNodeDef({ row }) {
+    const { context } = this
+    const { survey } = context
+    const { uuid, path } = row
+    let nodeDef = null
+    if (uuid) {
+      nodeDef = Survey.getNodeDefByUuid(uuid)(survey)
+    } else if (path) {
+      const name = Arrays.last(path.split('.'))
+      nodeDef = Survey.getNodeDefByName(name)(survey)
+    }
+    if (!nodeDef) {
+      await this.addErrorAndStopCsvReader('cannotFindNodeDef', { uuid, path })
+    }
+    return nodeDef
+  }
+
+  async addErrorAndStopCsvReader(key, params) {
+    this.addError({
+      error: {
+        valid: false,
+        errors: [{ key: `${errorPrefix}${key}`, params }],
+      },
+    })
+    this.csvReader.cancel()
+    await this.setStatusFailed()
   }
 }
 
