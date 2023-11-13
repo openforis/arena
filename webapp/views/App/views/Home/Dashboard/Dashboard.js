37d578d17b5bc39cfc1f04af898807f543b025ac
persas <diego.persas@gmail.com>
2023-11-13 11:00:19 +0100

    Fix/charts (#3138)
    * fix wording in charts
    
    * Wording and name and label switch
    
    * fix dashboard tab
    
    * Added drop down to select users in dashboard
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- webapp/views/App/views/Home/Dashboard/Dashboard.js
+++ webapp/views/App/views/Home/Dashboard/Dashboard.js
@@ -17,6 +17,7 @@ import { useShouldShowFirstTimeHelp } from '@webapp/components/hooks'
 import './Dashboard.scss'
 import RecordsByUser from './UserSummary/RecordsByUser'
 import DailyRecordsByUser from './UserSummary/DailyRecordsByUser'
+import Chart from './RecordsSummary/Chart'
 
 const Dashboard = () => {
   const showFirstTimeHelp = useShouldShowFirstTimeHelp({ useFetchMessages, helperTypes })
@@ -49,12 +50,17 @@ const Dashboard = () => {
             {canEditSurvey && activeTab === 'Dashboard' && !isSurveyInfoEmpty && (
               <div className="chart-container-dashboard">
                 <StorageSummary />
-                <RecordsByUser className="records-by-user" />
+                {recordsSummaryState.counts.length > 0 && <RecordsByUser className="records-by-user" />}
                 {hasSamplingPointData && <SamplingDataChart surveyInfo={surveyInfo} />}
               </div>
             )}
-            {canEditSurvey && activeTab === 'Dashboard' && !isSurveyInfoEmpty && <DailyRecordsByUser />}
-            {canEditSurvey && activeTab === 'Record History' && !isSurveyInfoEmpty && <StorageSummary />}
+            {canEditSurvey &&
+              activeTab === 'Dashboard' &&
+              !isSurveyInfoEmpty &&
+              recordsSummaryState.counts.length > 0 && <DailyRecordsByUser />}
+            {canEditSurvey && activeTab === 'RecordHistory' && !isSurveyInfoEmpty && recordsSummaryState.counts && (
+              <Chart counts={recordsSummaryState.counts} from={recordsSummaryState.from} to={recordsSummaryState.to} />
+            )}
           </RecordsSummaryContext.Provider>
         </div>
       )}
