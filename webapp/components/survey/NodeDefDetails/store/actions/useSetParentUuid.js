e83c1200f3be8fd5f5eef960ebabd70188b6df98
Stefano Ricci <SteRiccio@users.noreply.github.com>
2024-01-23 11:19:44 +0100

    update hiearchy on parent change

--- webapp/components/survey/NodeDefDetails/store/actions/useSetParentUuid.js
+++ webapp/components/survey/NodeDefDetails/store/actions/useSetParentUuid.js
@@ -1,18 +1,30 @@
 import { useCallback } from 'react'
 
+import * as A from '@core/arena'
 import * as NodeDef from '@core/survey/nodeDef'
+import * as Survey from '@core/survey/survey'
 
 import { useValidate } from './useValidate'
 import { State } from '../state'
+import { useSurvey } from '@webapp/store/survey'
 
 export const useSetParentUuid = ({ setState }) => {
   const validateNodeDef = useValidate({ setState })
+  const survey = useSurvey()
 
-  return useCallback(({ state, parentUuid }) => {
-    const nodeDef = State.getNodeDef(state)
+  return useCallback(
+    ({ state, parentUuid }) => {
+      const nodeDef = State.getNodeDef(state)
+      const parentNodeDef = Survey.getNodeDefByUuid(parentUuid)(survey)
+      const metaHierarchy = [...NodeDef.getMetaHierarchy(parentNodeDef), parentUuid]
 
-    const nodeDefUpdated = NodeDef.assocParentUuid(parentUuid)(nodeDef)
+      const nodeDefUpdated = A.pipe(
+        NodeDef.assocParentUuid(parentUuid),
+        NodeDef.assocMetaHierarchy(metaHierarchy)
+      )(nodeDef)
 
-    validateNodeDef({ state, nodeDefUpdated })
-  }, [])
+      validateNodeDef({ state, nodeDefUpdated })
+    },
+    [survey, validateNodeDef]
+  )
 }
