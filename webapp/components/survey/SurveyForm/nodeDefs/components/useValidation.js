cb8167ca787ac1c07553c02296ac5643a6fe2495
Stefano Ricci <SteRiccio@users.noreply.github.com>
2024-02-07 14:45:06 +0100

    refactored useValidation

--- webapp/components/survey/SurveyForm/nodeDefs/components/useValidation.js
+++ webapp/components/survey/SurveyForm/nodeDefs/components/useValidation.js
@@ -1,45 +1,51 @@
 import { useSelector } from 'react-redux'
-import * as R from 'ramda'
+
+import { Objects } from '@openforis/arena-core'
 
 import * as Survey from '@core/survey/survey'
 import * as NodeDef from '@core/survey/nodeDef'
 import * as NodeDefValidations from '@core/survey/nodeDefValidations'
-
 import * as Record from '@core/record/record'
 import * as Node from '@core/record/node'
 import * as RecordValidation from '@core/record/recordValidation'
 import * as Validation from '@core/validation/validation'
 
-import { useSurvey } from '@webapp/store/survey'
+import { SurveyState } from '@webapp/store/survey'
 import { RecordState } from '@webapp/store/ui/record'
 
 export default (props) => {
   const { edit, node, nodeDef, nodes, parentNode } = props
 
-  if (edit) {
-    const survey = useSurvey()
-    return Survey.getNodeDefValidation(nodeDef)(survey)
-  }
-  const record = useSelector(RecordState.getRecord)
-  const recordValidation = Record.getValidation(record)
+  return useSelector((state) => {
+    const survey = SurveyState.getSurvey(state)
+    if (edit) {
+      return Survey.getNodeDefValidation(nodeDef)(survey)
+    }
+    const record = RecordState.getRecord(state)
+    const recordValidation = Record.getValidation(record)
 
-  if (NodeDef.isMultiple(nodeDef)) {
-    // Showing validation for a single node instance of multiple nodeDef
-    if (node) {
-      return RecordValidation.getNodeValidation(node)(recordValidation)
+    if (NodeDef.isMultiple(nodeDef)) {
+      // Showing validation for a single node instance of multiple nodeDef
+      if (node) {
+        return RecordValidation.getNodeValidation(node)(recordValidation)
+      }
+      if (NodeDefValidations.hasMinOrMaxCount(NodeDef.getValidations(nodeDef))) {
+        return RecordValidation.getValidationChildrenCount(
+          Node.getUuid(parentNode),
+          NodeDef.getUuid(nodeDef)
+        )(recordValidation)
+      }
+      if (
+        nodes.some((node) => {
+          const nodeValidation = RecordValidation.getNodeValidation(node)(recordValidation)
+          return Validation.isNotValid(nodeValidation)
+        })
+      ) {
+        return Validation.newInstance(false, {}, [{ key: Validation.messageKeys.record.oneOrMoreInvalidValues }])
+      }
+    } else if (!Objects.isEmpty(nodes)) {
+      return RecordValidation.getNodeValidation(nodes[0])(recordValidation)
     }
-    if (NodeDefValidations.hasMinOrMaxCount(NodeDef.getValidations(nodeDef))) {
-      return RecordValidation.getValidationChildrenCount(
-        Node.getUuid(parentNode),
-        NodeDef.getUuid(nodeDef)
-      )(recordValidation)
-    }
-    if (!R.all(Validation.isValid)(nodes)) {
-      return Validation.newInstance(false, {}, [{ key: Validation.messageKeys.record.oneOrMoreInvalidValues }])
-    }
-  } else if (!R.isEmpty(nodes)) {
-    return RecordValidation.getNodeValidation(nodes[0])(recordValidation)
-  }
-
-  return Validation.newInstance()
+    return Validation.newInstance()
+  }, Objects.isEqual)
 }
