e30a2a1a0b159e40b451e954a51c3f7525bb5c9f
Stefano Ricci <SteRiccio@users.noreply.github.com>
2024-02-22 12:15:54 +0100

    check duplicates

--- webapp/components/form/InputChips/store/actions/useOnItemAddClick.js
+++ webapp/components/form/InputChips/store/actions/useOnItemAddClick.js
@@ -1,15 +1,24 @@
 import { useCallback } from 'react'
+import { useDispatch } from 'react-redux'
 
+import { NotificationActions } from '@webapp/store/ui'
 import { State } from '../state'
 
-export const useOnItemAddClick = ({ onChange, setState }) =>
-  useCallback(
+export const useOnItemAddClick = ({ onChange, setState }) => {
+  const dispatch = useDispatch()
+
+  return useCallback(
     ({ selection }) => {
       setState((statePrev) => {
         const value = State.getInputFieldValue(statePrev)
+        if (selection.indexOf(value) >= 0) {
+          dispatch(NotificationActions.notifyWarning({ key: 'common.itemAlreadyAdded' }))
+          return statePrev
+        }
         onChange([...selection, value])
         return State.assocInputFieldValue('')(statePrev)
       })
     },
-    [onChange, setState]
+    [dispatch, onChange, setState]
   )
+}
