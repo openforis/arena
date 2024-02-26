a0819f0a42f8ca8dc146cdf80d78da013eeb9b21
Stefano Ricci <SteRiccio@users.noreply.github.com>
2024-02-26 22:47:43 +0100

    fixed tests

--- test/e2e/tests/_surveyExport/users.js
+++ test/e2e/tests/_surveyExport/users.js
@@ -1,21 +1,10 @@
 import { getSurveyEntry } from '../../paths'
-import { user } from '../../mock/user'
 import { ExportFile } from '../../../../server/modules/survey/service/surveyExport/exportFile'
 
 export const verifyUsers = (survey) =>
   test(`Verify users`, async () => {
     const usersExport = getSurveyEntry(survey, ExportFile.users)
-    const users = [user]
 
-    await expect(usersExport.length).toBe(users.length)
-
-    await Promise.all(
-      usersExport.map(async (userExport) => {
-        const userMock = users.find((_user) => _user.email === userExport.email)
-        await expect(userMock).toBeTruthy()
-        await expect(userExport.status).toBe('ACCEPTED')
-        await expect(userExport.authGroups.length).toBe(1)
-        await expect(userExport.authGroups[0].name).toBe(userMock.authGroup.key)
-      })
-    )
+    // admin user not included in exported users
+    await expect(usersExport.length).toBe(0)
   })
