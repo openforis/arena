b77afbef059d06b417c4d989c5fe692cf7a3d74b
Stefano Ricci <1219739+SteRiccio@users.noreply.github.com>
2024-03-01 13:03:59 +0100

    Users list: added "copy invitation link to clipboard" button (#3287)
    * Users list: added "copy invitation link to clipboard" button
    
    * copy link to clipboard: code cleanup and added confirmation
    
    * code cleanup
    
    ---------
    
    Co-authored-by: Stefano Ricci <SteRiccio@users.noreply.github.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ webapp/views/App/views/Users/UsersListSurvey/Row/CopyInvitationLinkButton.js
@@ -0,0 +1,54 @@
+import React, { useCallback } from 'react'
+import { useDispatch } from 'react-redux'
+import PropTypes from 'prop-types'
+
+import { Button } from '@webapp/components'
+import * as API from '@webapp/service/api'
+import { useSurveyId } from '@webapp/store/survey'
+import { copyToClipboard } from '@webapp/utils/domUtils'
+import { DialogConfirmActions, NotificationActions } from '@webapp/store/ui'
+
+export const CopyInvitationLinkButton = (props) => {
+  const { email, userUuid } = props
+
+  const surveyId = useSurveyId()
+  const dispatch = useDispatch()
+
+  const doCopyInvitationLinkToKeyboard = useCallback(async () => {
+    const url = await API.fetchUserResetPasswordUrl({ userUuid, surveyId })
+    if (await copyToClipboard(url)) {
+      dispatch(NotificationActions.notifyInfo({ key: 'usersView.invitationLinkCopiedToClipboard' }))
+    }
+  }, [dispatch, surveyId, userUuid])
+
+  const handleCopyInvitationLink = useCallback(
+    (e) => {
+      e.stopPropagation()
+      dispatch(
+        DialogConfirmActions.showDialogConfirm({
+          key: 'usersView.copyInvitationLinkConfirmMessage',
+          headerText: 'usersView.copyInvitationLink',
+          okButtonLabel: 'common.copy',
+          params: { email },
+          onOk: doCopyInvitationLinkToKeyboard,
+        })
+      )
+    },
+    [dispatch, doCopyInvitationLinkToKeyboard, email]
+  )
+
+  return (
+    <Button
+      className="icon-invitation-link"
+      iconClassName="icon-link icon-12px"
+      label="usersView.copyInvitationLink"
+      onClick={handleCopyInvitationLink}
+      showLabel={false}
+    />
+  )
+}
+
+CopyInvitationLinkButton.propTypes = {
+  email: PropTypes.string.isRequired,
+  userUuid: PropTypes.string.isRequired,
+}
