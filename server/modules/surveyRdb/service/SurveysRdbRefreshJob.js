2052499d25b1170496904d4be22b43dc11f1272c
Stefano Ricci <SteRiccio@users.noreply.github.com>
2024-03-18 16:03:01 +0100

    added logging

--- server/modules/surveyRdb/service/SurveysRdbRefreshJob.js
+++ server/modules/surveyRdb/service/SurveysRdbRefreshJob.js
@@ -12,9 +12,15 @@ export default class SurveysRdbRefreshJob extends Job {
     this.total = surveyIds.length
     for await (const surveyId of surveyIds) {
       try {
+        this.logDebug(`refreshing RDB for survey ${surveyId}`)
         const innerJob = new SurveyRdbCreationJob({ surveyId })
         await innerJob.start()
-        this.incrementProcessedItems()
+        if (innerJob.isSucceeded()) {
+          this.logDebug(`RDB for survey ${surveyId} refreshed successfully`)
+          this.incrementProcessedItems()
+        } else if (innerJob.isFailed()) {
+          this.logDebug(`something went wrong updating RDB for survey ${surveyId}`)
+        }
       } catch (error) {
         // ignore it
       }
