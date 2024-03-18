55df975b16ea85ae40e9c0be5899a3836700dbd0
Stefano Ricci <SteRiccio@users.noreply.github.com>
2024-03-18 16:20:01 +0100

    track errors

--- server/modules/surveyRdb/service/SurveysRdbRefreshJob.js
+++ server/modules/surveyRdb/service/SurveysRdbRefreshJob.js
@@ -10,25 +10,32 @@ export default class SurveysRdbRefreshJob extends Job {
   async execute() {
     const { surveyIds } = this.context
     this.total = surveyIds.length
+    const surveysIdsWithErrors = []
     for await (const surveyId of surveyIds) {
+      if (this.isCanceled()) {
+        return
+      }
       try {
         this.logDebug(`refreshing RDB for survey ${surveyId}`)
         const innerJob = new SurveyRdbCreationJob({ surveyId })
         await innerJob.start()
+
         if (innerJob.isSucceeded()) {
           this.logDebug(`RDB for survey ${surveyId} refreshed successfully`)
           this.incrementProcessedItems()
         } else if (innerJob.isFailed()) {
+          surveysIdsWithErrors.push(surveyId)
           this.logDebug(`something went wrong updating RDB for survey ${surveyId}`)
         }
       } catch (error) {
-        // ignore it
+        surveysIdsWithErrors.push(surveyId)
+        this.logDebug(`something went wrong updating RDB for survey ${surveyId}`)
       }
     }
     const message =
       this.processed === this.total
         ? `All RDBs (${this.total}) refreshed successfully`
-        : `Only ${this.processed}/${this.total} RDBs refreshed successully`
+        : `Only ${this.processed}/${this.total} RDBs refreshed successully. Surveys with errors: ${surveysIdsWithErrors}`
     this.logDebug(message)
   }
 }
