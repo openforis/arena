bd0b39ce320aaa9cb96915e1ba30a6861c3f11d3
Stefano Ricci <SteRiccio@users.noreply.github.com>
2024-06-14 09:22:39 +0200

    fixed survey preview tests

--- test/e2e/tests/surveyFormPreview.js
+++ test/e2e/tests/surveyFormPreview.js
@@ -62,7 +62,7 @@ export default () =>
         const startTimePlus1Minute = DateUtils.addMinutes(startTimeDate, 1)
         const possibleDateValues = [new Date(startTime), startTimePlus1Minute]
         const expectedPossibleValues = possibleDateValues.map((possibleDateValue) => {
-          const timezoneOffsetDiff = startTimeDate.getTimezoneOffset()
+          const timezoneOffsetDiff = startTimeDate.getTimezoneOffset() + 60 // browser timezone=Europe/Rome
           const dateWithTimezoneOffset = DateUtils.addMinutes(possibleDateValue, timezoneOffsetDiff)
           return formatTime(dateWithTimezoneOffset)
         })
