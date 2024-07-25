a91b32de85928da7acadb1f5511ee6d1c8cc453e
Stefano Ricci <SteRiccio@users.noreply.github.com>
2024-07-25 15:21:40 +0200

    code cleanup

--- server/modules/dataImport/service/DataImportJob/dataImportCsvFileReader.js
+++ server/modules/dataImport/service/DataImportJob/dataImportCsvFileReader.js
@@ -105,7 +105,7 @@ const valueConverterByNodeDefType = {
 const checkAllHeadersAreValid =
   ({ csvDataExportModel }) =>
   (headers) => {
-    const dataExportModelHeaders = csvDataExportModel.columns.map((col) => col.header)
+    const dataExportModelHeaders = csvDataExportModel.headers
     const invalidHeaders = headers.filter((header) => !dataExportModelHeaders.includes(header))
     if (invalidHeaders.length > 0) {
       throw new SystemError('validationErrors.dataImport.invalidHeaders', { invalidHeaders: invalidHeaders.join(', ') })
