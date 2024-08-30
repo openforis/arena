ef2c645bae52154c4013677f0f76e410c502d31d
Stefano Ricci <1219739+SteRiccio@users.noreply.github.com>
2024-08-30 13:19:57 +0200

    sampling polygon editor: allow inserting decimals; layout ajustments; (#3544)
    * sampling polygon editor: allow inserting decimals; layout ajustments
    
    * code cleanup
    
    * code cleanup
    
    ---------
    
    Co-authored-by: Stefano Ricci <SteRiccio@users.noreply.github.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- webapp/views/App/views/Home/SurveyInfo/SamplingPolygonShapeEditor/SamplingPolygonShapeEditor.js
+++ webapp/views/App/views/Home/SurveyInfo/SamplingPolygonShapeEditor/SamplingPolygonShapeEditor.js
@@ -3,6 +3,12 @@ import PropTypes from 'prop-types'
 
 import { useI18n } from '@webapp/store/system'
 import { FormItem } from '@webapp/components/form/Input'
+import { ButtonGroup } from '@webapp/components/form'
+
+const buttonGroupItems = ['true', 'false'].map((value) => ({
+  key: value,
+  label: `samplingPolygonOptions.${value === 'true' ? 'circle' : 'rectangle'}`,
+}))
 
 const SamplingPolygonShapeEditor = (props) => {
   const { readOnly, isCircle, onChange } = props
@@ -11,10 +17,12 @@ const SamplingPolygonShapeEditor = (props) => {
 
   return (
     <FormItem label={i18n.t('samplingPolygonOptions.shape')}>
-      <select value={isCircle} onChange={onChange} disabled={readOnly}>
-        <option value={false}>{i18n.t('samplingPolygonOptions.rectangle')}</option>
-        <option value={true}>{i18n.t('samplingPolygonOptions.circle')}</option>
-      </select>
+      <ButtonGroup
+        disabled={readOnly}
+        items={buttonGroupItems}
+        onChange={onChange}
+        selectedItemKey={String(isCircle)}
+      />
     </FormItem>
   )
 }
