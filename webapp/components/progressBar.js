4f2db618d7f0a1103059878ba46d8fed155104c8
Stefano Ricci <SteRiccio@users.noreply.github.com>
2024-09-04 11:23:54 +0200

    show label in progress bar when uploading files

--- webapp/components/progressBar.js
+++ webapp/components/progressBar.js
@@ -5,13 +5,19 @@ import classNames from 'classnames'
 import PropTypes from 'prop-types'
 import { LinearProgress } from '@mui/material'
 
+import { useI18n } from '@webapp/store/system'
+
 const ProgressBarWithLabel = (props) => {
-  const { className, color, value } = props
+  const { className, color, textKey = null, value } = props
+
+  const i18n = useI18n()
+  const progressPercent = Math.floor(value)
+  const text = textKey ? i18n.t(textKey, { progressPercent }) : `${progressPercent}%`
 
   return (
     <div className={classNames('progress-bar-with-label', className)}>
       <LinearProgress className={className} color={color} value={value} variant="determinate" />
-      <span>{Math.floor(value)}%</span>
+      <span>{text}</span>
     </div>
   )
 }
@@ -19,11 +25,12 @@ const ProgressBarWithLabel = (props) => {
 ProgressBarWithLabel.propTypes = {
   className: PropTypes.string,
   color: PropTypes.oneOf(['primary', 'secondary', 'error', 'info', 'success', 'warning', 'inherit']),
+  textKey: PropTypes.string,
   value: PropTypes.number,
 }
 
 const ProgressBar = (props) => {
-  const { className: classNameProp, color, indeterminate = true, progress = 0, showText = true } = props
+  const { className: classNameProp, color, indeterminate = true, progress = 0, showText = true, textKey = null } = props
 
   const className = classNames('progress-bar', classNameProp)
 
@@ -31,7 +38,7 @@ const ProgressBar = (props) => {
     return <LinearProgress className={className} color={color} />
   }
   if (showText) {
-    return <ProgressBarWithLabel className={className} color={color} value={progress} />
+    return <ProgressBarWithLabel className={className} color={color} textKey={textKey} value={progress} />
   }
   return <LinearProgress className={className} color={color} variant="determinate" value={progress} />
 }
@@ -42,6 +49,7 @@ ProgressBar.propTypes = {
   indeterminate: PropTypes.bool,
   progress: PropTypes.number,
   showText: PropTypes.bool,
+  textKey: PropTypes.string,
 }
 
 export default ProgressBar
