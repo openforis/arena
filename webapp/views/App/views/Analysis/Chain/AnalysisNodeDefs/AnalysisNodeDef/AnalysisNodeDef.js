f0437e6c5735ea1efd975df652f9d1a9eaa8f294
Stefano Ricci <SteRiccio@users.noreply.github.com>
2024-09-05 10:03:37 +0200

    removing default props

--- webapp/views/App/views/Analysis/Chain/AnalysisNodeDefs/AnalysisNodeDef/AnalysisNodeDef.js
+++ webapp/views/App/views/Analysis/Chain/AnalysisNodeDefs/AnalysisNodeDef/AnalysisNodeDef.js
@@ -15,7 +15,7 @@ import { useI18n } from '@webapp/store/system'
 import InputSwitch from '@webapp/components/form/InputSwitch'
 import WarningBadge from '@webapp/components/warningBadge'
 
-const AnalysisNodeDef = ({ nodeDefUuid, dataCount }) => {
+const AnalysisNodeDef = ({ nodeDefUuid, dataCount = undefined }) => {
   const dispatch = useDispatch()
   const i18n = useI18n()
   const survey = useSurvey()
@@ -97,10 +97,6 @@ const AnalysisNodeDef = ({ nodeDefUuid, dataCount }) => {
 AnalysisNodeDef.propTypes = {
   nodeDefUuid: PropTypes.string.isRequired,
   dataCount: PropTypes.number,
-}
-
-AnalysisNodeDef.defaultProps = {
-  dataCount: undefined,
 }
 
 export { AnalysisNodeDef }
