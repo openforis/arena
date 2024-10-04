107c97708c21b967914b41a447a17224ebabaf07
Stefano Ricci <SteRiccio@users.noreply.github.com>
2024-10-04 15:03:10 +0200

    set user functions as experimental

--- core/expressionParser/helpers/functions.js
+++ core/expressionParser/helpers/functions.js
@@ -18,5 +18,8 @@ export const functionNames = {
   pow: 'pow',
   sum: 'sum',
   taxonProp: 'taxonProp',
+  userEmail: 'userEmail',
+  userName: 'userName',
+  userProp: 'userProp',
   uuid: 'uuid',
 }
