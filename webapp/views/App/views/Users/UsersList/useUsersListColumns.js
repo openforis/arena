4bc643bb9904567ec7c8b1ffca9e373c36d272ad
Stefano Ricci <SteRiccio@users.noreply.github.com>
2024-10-16 15:19:35 +0200

    layout adjustments

--- webapp/views/App/views/Users/UsersList/useUsersListColumns.js
+++ webapp/views/App/views/Users/UsersList/useUsersListColumns.js
@@ -83,12 +83,14 @@ export const useUsersListColumns = () => {
         header: 'usersView.surveysDraft',
         hidden: true,
         renderItem: ({ item }) => User.getSurveysCountDraft(item),
+        width: '10rem',
       },
       {
         key: User.keys.surveysCountPublished,
         header: 'usersView.surveysPublished',
         hidden: true,
         renderItem: ({ item }) => User.getSurveysCountPublished(item),
+        width: '12rem',
       },
     ],
     [goToUserDetails, i18n]
