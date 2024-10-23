c765079d670a52e2b1ca4c9a9963e56e471b0cd5
Stefano Ricci <1219739+SteRiccio@users.noreply.github.com>
2024-10-23 23:34:52 +0200

    Survey clone: divide surveys and templates; sort by name (#3613)
    * Survey clone: divide surveys and templates; sort by name
    
    * fixing tests
    
    * fixing tests
    
    * fixing tests
    
    * fixing tests
    
    ---------
    
    Co-authored-by: Stefano Ricci <SteRiccio@users.noreply.github.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- core/i18n/resources/en/index.js
+++ core/i18n/resources/en/index.js
@@ -2,10 +2,12 @@ import activityLog from './activityLog'
 import appErrors from './appErrors'
 import common from './common'
 import jobs from './jobs'
+import surveyCreate from './surveyCreate'
 
 export default {
   activityLog,
   appErrors,
   common,
   jobs,
+  surveyCreate,
 }
