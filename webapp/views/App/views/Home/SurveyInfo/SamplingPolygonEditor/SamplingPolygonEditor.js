11eee55e151853313a7206b0d5f5415aa45621a8
Stefano Ricci <SteRiccio@users.noreply.github.com>
2024-10-29 12:04:36 +0100

    fixed survey info editor: do not show info to data entry users

--- webapp/views/App/views/Home/SurveyInfo/SamplingPolygonEditor/SamplingPolygonEditor.js
+++ webapp/views/App/views/Home/SurveyInfo/SamplingPolygonEditor/SamplingPolygonEditor.js
@@ -1,15 +1,13 @@
 import './SamplingPolygonEditor.scss'
 
-import React, { useCallback, useState } from 'react'
+import React, { useCallback } from 'react'
 import PropTypes from 'prop-types'
-import Switch from 'react-switch'
 
 import { Objects } from '@openforis/arena-core'
 
 import { getSamplingPolygonDefaults } from '@core/survey/_survey/surveyDefaults'
 
 import { useI18n } from '@webapp/store/system'
-import { FormItem } from '@webapp/components/form/Input'
 
 import SamplingPolygonShapeEditor from '../SamplingPolygonShapeEditor'
 import { FormPropertyItem } from './FormPropertyItem'
@@ -23,9 +21,6 @@ const SamplingPolygonEditor = (props) => {
 
   const samplingPolygonObject = Objects.isEmpty(samplingPolygon) ? getSamplingPolygonDefaults() : samplingPolygon
 
-  const [jsonEditorChecked, setJsonEditorChecked] = useState(false)
-  const [jsonEditorValue, setJsonEditorValue] = useState(JSON.stringify(samplingPolygonObject, null, 2))
-
   const inputPropertiesForAll = [
     { key: 'offsetNorth', labelKey: 'offsetNorth' },
     { key: 'offsetEast', labelKey: 'offsetEast' },
@@ -36,7 +31,6 @@ const SamplingPolygonEditor = (props) => {
   const onSamplingPolygonChange = useCallback(
     (samplingPolygonNext) => {
       setSamplingPolygon(samplingPolygonNext)
-      setJsonEditorValue(JSON.stringify(samplingPolygonNext, null, 2))
     },
     [setSamplingPolygon]
   )
@@ -50,67 +44,46 @@ const SamplingPolygonEditor = (props) => {
     onSamplingPolygonChange({ ...samplingPolygonObject, ['isCircle']: value === 'true' })
   }
 
-  const jsonEditorOnChange = (event) => {
-    const value = event.target.value
-    setJsonEditorValue(value)
-    try {
-      const json = JSON.parse(value)
-      setSamplingPolygon(json)
-    } catch {
-      return //dont save invalid json
-    }
-  }
-
   return (
     <fieldset className="sampling-polygon">
       <legend>{i18n.t('samplingPolygonOptions.samplingPolygon')}</legend>
 
-      <FormItem label="JSON">
-        <div>
-          <Switch checked={jsonEditorChecked} onChange={() => setJsonEditorChecked(!jsonEditorChecked)} height={20} />
-        </div>
-      </FormItem>
-      {jsonEditorChecked && (
-        <textarea className="json-editor" value={jsonEditorValue} rows={12} onChange={jsonEditorOnChange} />
-      )}
-      {!jsonEditorChecked && (
-        <div className="props-editor-wrapper">
-          <SamplingPolygonShapeEditor
-            isCircle={samplingPolygonObject.isCircle}
-            onChange={onShapeChange}
+      <div className="props-editor-wrapper">
+        <SamplingPolygonShapeEditor
+          isCircle={samplingPolygonObject.isCircle}
+          onChange={onShapeChange}
+          readOnly={readOnly}
+          key={'ShapeEditor'}
+        />
+        {samplingPolygonObject.isCircle && (
+          <CircleOnlyItems
+            onPropertyChange={onPropertyChange}
+            samplingPolygonObject={samplingPolygonObject}
+            getFieldValidation={getFieldValidation}
             readOnly={readOnly}
-            key={'ShapeEditor'}
           />
-          {samplingPolygonObject.isCircle && (
-            <CircleOnlyItems
-              onPropertyChange={onPropertyChange}
-              samplingPolygonObject={samplingPolygonObject}
-              getFieldValidation={getFieldValidation}
-              readOnly={readOnly}
-            />
-          )}
-          {!samplingPolygonObject.isCircle && (
-            <RectangleOnlyItems
-              onPropertyChange={onPropertyChange}
-              samplingPolygonObject={samplingPolygonObject}
-              getFieldValidation={getFieldValidation}
-              readOnly={readOnly}
-            />
-          )}
-          {inputPropertiesForAll.map(({ key, labelKey }) => (
-            <FormPropertyItem
-              key={key}
-              objectKey={key}
-              labelKey={labelKey}
-              onPropertyChange={onPropertyChange(key)}
-              value={samplingPolygonObject[key]}
-              samplingPolygonObject={samplingPolygonObject}
-              readOnly={readOnly}
-              getFieldValidation={getFieldValidation}
-            />
-          ))}
-        </div>
-      )}
+        )}
+        {!samplingPolygonObject.isCircle && (
+          <RectangleOnlyItems
+            onPropertyChange={onPropertyChange}
+            samplingPolygonObject={samplingPolygonObject}
+            getFieldValidation={getFieldValidation}
+            readOnly={readOnly}
+          />
+        )}
+        {inputPropertiesForAll.map(({ key, labelKey }) => (
+          <FormPropertyItem
+            key={key}
+            objectKey={key}
+            labelKey={labelKey}
+            onPropertyChange={onPropertyChange(key)}
+            value={samplingPolygonObject[key]}
+            samplingPolygonObject={samplingPolygonObject}
+            readOnly={readOnly}
+            getFieldValidation={getFieldValidation}
+          />
+        ))}
+      </div>
     </fieldset>
   )
 }
--- webapp/views/App/views/Home/SurveyInfo/SurveyInfo.js
+++ webapp/views/App/views/Home/SurveyInfo/SurveyInfo.js
@@ -4,7 +4,7 @@ import React from 'react'
 
 import * as ProcessUtils from '@core/processUtils'
 
-import { useAuthCanEditSurvey, useUserIsSystemAdmin } from '@webapp/store/user'
+import { useAuthCanEditSurvey, useAuthCanUseAnalysis, useUserIsSystemAdmin } from '@webapp/store/user'
 import { TestId } from '@webapp/utils/testId'
 
 import { ButtonSave } from '@webapp/components'
@@ -20,6 +20,7 @@ import { SurveyInfoSampleBasedImageInterpretation } from './SurveyInfoSampleBase
 const SurveyInfo = () => {
   const readOnly = !useAuthCanEditSurvey()
   const isSystemAdmin = useUserIsSystemAdmin()
+  const canUseAnalysis = useAuthCanUseAnalysis()
 
   const {
     userExtraPropDefs,
@@ -41,64 +42,63 @@ const SurveyInfo = () => {
     ...surveyInfoObject
   } = useSurveyInfoForm()
 
+  const tabs = [
+    {
+      key: 'basicInfo',
+      component: SurveyInfoBasicForm,
+      label: 'homeView.surveyInfo.basic',
+      props: {
+        getFieldValidation,
+        setCycles,
+        setDefaultCycleKey,
+        setDescriptions,
+        setFieldManualLinks,
+        setLabels,
+        setLanguages,
+        setName,
+        setSrs,
+        surveyInfoObject,
+      },
+    },
+  ]
+  if (canUseAnalysis) {
+    tabs.push({
+      key: 'sampleBasedInterpretation',
+      component: SurveyInfoSampleBasedImageInterpretation,
+      label: 'homeView.surveyInfo.sampleBasedImageInterpretation',
+      props: {
+        getFieldValidation,
+        sampleBasedImageInterpretationEnabled,
+        samplingPolygon,
+        setSampleBasedImageInterpretationEnabled,
+        setSamplingPolygon,
+      },
+    })
+  }
+  if (!readOnly) {
+    if (ProcessUtils.ENV.experimentalFeatures) {
+      tabs.push({
+        key: 'extraProps',
+        component: SurveyUserExtraPropDefsEditor,
+        label: 'homeView.surveyInfo.userExtraProps.title',
+        props: {
+          extraPropDefs: userExtraPropDefs,
+          onExtraPropDefsChange: setUserExtraPropDefs,
+        },
+      })
+    }
+    if (isSystemAdmin) {
+      tabs.push({
+        key: 'configuration',
+        component: SurveyConfigurationEditor,
+        label: 'homeView.surveyInfo.configuration.title',
+      })
+    }
+  }
+
   return (
     <div className="home-survey-info">
-      <TabBar
-        tabs={[
-          {
-            key: 'basicInfo',
-            component: SurveyInfoBasicForm,
-            label: 'homeView.surveyInfo.basic',
-            props: {
-              getFieldValidation,
-              setCycles,
-              setDefaultCycleKey,
-              setDescriptions,
-              setFieldManualLinks,
-              setLabels,
-              setLanguages,
-              setName,
-              setSrs,
-              surveyInfoObject,
-            },
-          },
-          {
-            key: 'sampleBasedInterpretation',
-            component: SurveyInfoSampleBasedImageInterpretation,
-            label: 'homeView.surveyInfo.sampleBasedImageInterpretation',
-            props: {
-              getFieldValidation,
-              sampleBasedImageInterpretationEnabled,
-              samplingPolygon,
-              setSampleBasedImageInterpretationEnabled,
-              setSamplingPolygon,
-            },
-          },
-          ...(ProcessUtils.ENV.experimentalFeatures
-            ? [
-                {
-                  key: 'extraProps',
-                  component: SurveyUserExtraPropDefsEditor,
-                  label: 'homeView.surveyInfo.userExtraProps.title',
-                  props: {
-                    extraPropDefs: userExtraPropDefs,
-                    onExtraPropDefsChange: setUserExtraPropDefs,
-                  },
-                },
-              ]
-            : []),
-          ...(isSystemAdmin
-            ? [
-                {
-                  key: 'configuration',
-                  component: SurveyConfigurationEditor,
-                  label: 'homeView.surveyInfo.configuration.title',
-                },
-              ]
-            : []),
-        ]}
-      />
-
+      <TabBar showTabs={tabs.length > 1} tabs={tabs} />
       {!readOnly && (
         <ButtonSave className="survey-info-save-btn" onClick={saveProps} testId={TestId.surveyInfo.saveBtn} />
       )}
