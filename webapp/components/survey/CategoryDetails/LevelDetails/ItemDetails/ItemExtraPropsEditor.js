a9c7ffa1153a2e14e934ab20f2347c5dcc45adb3
Stefano Ricci <1219739+SteRiccio@users.noreply.github.com>
2024-12-01 20:14:40 +0100

    fixed error adding new geometry extra prop to category item (#3676)
    * fixed error adding geometry extra prop to category item
    
    * code cleanup
    
    * use latest arena-core
    
    * code cleanup
    
    ---------
    
    Co-authored-by: Stefano Ricci <SteRiccio@users.noreply.github.com>

--- webapp/components/survey/CategoryDetails/LevelDetails/ItemDetails/ItemExtraPropsEditor.js
+++ webapp/components/survey/CategoryDetails/LevelDetails/ItemDetails/ItemExtraPropsEditor.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-import { Points } from '@openforis/arena-core'
+import { Numbers, Objects, Points } from '@openforis/arena-core'
 
 import * as A from '@core/arena'
 import { ExtraPropDef } from '@core/survey/extraPropDef'
@@ -10,17 +11,35 @@ import * as Validation from '@core/validation/validation'
 import { FormItem, Input, NumberFormats } from '@webapp/components/form/Input'
 import SrsDropdown from '@webapp/components/survey/SrsDropdown'
 import { useI18n } from '@webapp/store/system'
+import { useSurveySrsIndex } from '@webapp/store/survey'
+
+const pointNumericFields = ['x', 'y']
+
+const parsePoint = ({ value, srsIndex }) => {
+  const srssArray = Object.values(srsIndex)
+  const surveyDefaultSrs = srssArray.length > 0 ? srssArray[0] : null
+  const surveyDefaultSrsCode = surveyDefaultSrs?.code
+  const point = Points.parse(value) ?? {}
+  // assing default (first) srs if missing in parsed point
+  point.srs = point.srs ?? surveyDefaultSrsCode
+  return point
+}
 
 const GeometryPointExtraPropEditor = (props) => {
   const { extraPropKey, item, readOnly, updateProp, validation } = props
 
+  const srsIndex = useSurveySrsIndex()
+
   const value = CategoryItem.getExtraProp(extraPropKey)(item)
 
-  const point = Points.parse(value) || {}
+  const point = parsePoint({ value, srsIndex })
   const { x, y, srs } = point
 
   const onFieldChange = (field) => (value) => {
-    const pointUpdated = { ...point, [field]: ['x', 'y'].includes(field) ? Number(value) : value }
+    let pointUpdated = { ...point, [field]: pointNumericFields.includes(field) ? Numbers.toNumber(value) : value }
+    if (Objects.isEmpty(pointUpdated.x) && Objects.isEmpty(pointUpdated.y)) {
+      pointUpdated = null
+    }
     const extra = A.pipe(CategoryItem.getExtra, A.assoc(extraPropKey, pointUpdated))(item)
     updateProp({ key: CategoryItem.keysProps.extra, value: extra })
   }
@@ -58,6 +77,14 @@ const GeometryPointExtraPropEditor = (props) => {
   )
 }
 
+GeometryPointExtraPropEditor.propTypes = {
+  extraPropKey: PropTypes.string.isRequired,
+  item: PropTypes.object,
+  readOnly: PropTypes.bool,
+  updateProp: PropTypes.func.isRequired,
+  validation: PropTypes.object,
+}
+
 export const ItemExtraPropsEditor = (props) => {
   const { item, itemExtraDefs, readOnly, updateProp, validation } = props
 
@@ -93,4 +120,12 @@ export const ItemExtraPropsEditor = (props) => {
       )}
     </fieldset>
   )
+}
+
+ItemExtraPropsEditor.propTypes = {
+  item: PropTypes.object,
+  itemExtraDefs: PropTypes.object.isRequired,
+  readOnly: PropTypes.bool,
+  updateProp: PropTypes.func.isRequired,
+  validation: PropTypes.object,
 }
