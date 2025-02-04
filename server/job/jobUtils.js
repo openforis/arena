1b9c851b52a500129fc7ed3263acc0fc99903281
Stefano Ricci <SteRiccio@users.noreply.github.com>
2025-02-04 12:22:16 +0100

    fixed job progress calculation

--- server/job/jobUtils.js
+++ server/job/jobUtils.js
@@ -20,14 +20,20 @@ export const jobThreadMessageTypes = {
 }
 
 const calculateJobProgress = (job) => {
+  const { currentInnerJobIndex, innerJobs, processed, total } = job
   const partialProgress =
-    job.status === jobStatus.succeeded ? 100 : job.total > 0 ? Math.floor((100 * job.processed) / job.total) : 0
+    job.status === jobStatus.succeeded ? 100 : total > 0 ? Math.floor((100 * processed) / total) : 0
 
-  if (job.innerJobs.length === 0 || job.currentInnerJobIndex < 0 || partialProgress === 100) {
+  if (
+    innerJobs.length === 0 ||
+    currentInnerJobIndex < 0 ||
+    partialProgress === 100 ||
+    processed > currentInnerJobIndex
+  ) {
     return partialProgress
   }
-
-  return partialProgress + Math.floor(calculateJobProgress(job.getCurrentInnerJob()) / job.total)
+  const currentInnerJobProgress = calculateJobProgress(job.getCurrentInnerJob())
+  return partialProgress + Math.floor(currentInnerJobProgress / total)
 }
 
 const calculatedElapsedMillis = (job) =>
