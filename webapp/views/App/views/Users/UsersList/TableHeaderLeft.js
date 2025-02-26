e79beee448d21439b6e26b1f6c030ef506101734
Stefano Ricci <SteRiccio@users.noreply.github.com>
2025-02-26 23:59:53 +0100

    layout adjustments

--- webapp/views/App/views/Users/UsersList/TableHeaderLeft.js
+++ webapp/views/App/views/Users/UsersList/TableHeaderLeft.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import { ButtonDownload } from '@webapp/components'
 import { TextInput } from '@webapp/components/form'
@@ -6,14 +7,20 @@ import { TextInput } from '@webapp/components/form'
 export const TableHeaderLeft = (props) => {
   const { search, handleSearch } = props
   return (
-    <div>
-      <ButtonDownload label="common.exportAll" href="/api/users/export" />
+    <div className="users__header-left">
       <TextInput
         className="users__header-left__input-search"
         placeholder="usersView.filterPlaceholder"
         defaultValue={search}
         onChange={handleSearch}
       />
+      <ButtonDownload label="common.exportAll" href="/api/users/export" />
+      <div /> {/* spacer */}
     </div>
   )
+}
+
+TableHeaderLeft.propTypes = {
+  search: PropTypes.string,
+  handleSearch: PropTypes.func.isRequired,
 }
