ffa812b0a4d7853d3b38667c8466cc1f85c6d4d4
Stefano Ricci <1219739+SteRiccio@users.noreply.github.com>
2025-02-27 11:38:13 +0100

    data export: export categories in same data export format (#3742)
    Co-authored-by: Stefano Ricci <SteRiccio@users.noreply.github.com>

--- server/modules/dataExport/service/dataExportJob/jobs/CategoriesExportJob.js
+++ server/modules/dataExport/service/dataExportJob/jobs/CategoriesExportJob.js
@@ -1,9 +1,10 @@
 import Job from '@server/job/job'
 
-import * as PromiseUtils from '@core/promiseUtils'
 import * as Survey from '@core/survey/survey'
 import * as Category from '@core/survey/category'
+import { FileFormats } from '@core/fileFormats'
 
+import { ExportFileNameGenerator } from '@server/utils/exportFileNameGenerator'
 import * as FileUtils from '@server/utils/file/fileUtils'
 import * as CategoryManager from '@server/modules/category/manager/categoryManager'
 
@@ -15,7 +16,8 @@ export default class CategoriesExportJob extends Job {
   }
 
   async execute() {
-    const { survey, outputDir } = this.context
+    const { survey, outputDir, options } = this.context
+    const { fileFormat = FileFormats.csv } = options ?? {}
 
     const categories = Survey.getCategoriesArray(survey)
     this.total = categories.length
@@ -23,18 +25,13 @@ export default class CategoriesExportJob extends Job {
     const categoriesDir = FileUtils.join(outputDir, categoriesOutputFolderName)
     await FileUtils.mkdir(categoriesDir)
 
-    await PromiseUtils.each(categories, async (category) => {
-      const categoryTempFilePath = FileUtils.join(categoriesDir, `${Category.getName(category)}.csv`)
-      const categoryOutputStream = FileUtils.createWriteStream(categoryTempFilePath)
-      await CategoryManager.exportCategoryToStream(
-        {
-          survey,
-          categoryUuid: category.uuid,
-          outputStream: categoryOutputStream,
-        },
-        this.tx
-      )
+    for await (const category of categories) {
+      const { uuid: categoryUuid } = category
+      const extension = ExportFileNameGenerator.getExtensionByFileFormat(fileFormat)
+      const categoryTempFilePath = FileUtils.join(categoriesDir, `${Category.getName(category)}.${extension}`)
+      const outputStream = FileUtils.createWriteStream(categoryTempFilePath)
+      await CategoryManager.exportCategoryToStream({ survey, categoryUuid, outputStream, fileFormat }, this.tx)
       this.incrementProcessedItems()
-    })
+    }
   }
 }
