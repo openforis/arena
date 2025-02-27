c6b22c0c6caf6ced7a0171ad771d51c59a13b47c
Stefano Ricci <SteRiccio@users.noreply.github.com>
2025-02-27 18:00:09 +0100

    fixed schema summary fetch from RStudio

--- common/apiRoutes/survey.js
+++ common/apiRoutes/survey.js
@@ -1,3 +1,6 @@
+import { FileFormats } from '@core/fileFormats'
+
 export const getSurveyPath = (surveyId) => `/survey/${surveyId}`
 
-export const schemaSummary = ({ surveyId, cycle }) => `${getSurveyPath(surveyId)}/schema-summary/?cycle=${cycle}`
+export const schemaSummary = ({ surveyId, cycle }) =>
+  `${getSurveyPath(surveyId)}/schema-summary/?cycle=${cycle}&fileFormat=${FileFormats.csv}`
