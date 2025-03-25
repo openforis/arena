5165982b68c7f603fce2d75db1247672dec0c8b6
Stefano Ricci <1219739+SteRiccio@users.noreply.github.com>
2025-03-25 15:10:07 +0100

    Form header props: show colors as icons (#3794)
    * code cleanup
    
    * show header prop icons

--- server/modules/record/service/update/thread/recordsUpdateThread.js
+++ server/modules/record/service/update/thread/recordsUpdateThread.js
@@ -167,10 +167,7 @@ class RecordsUpdateThread extends Thread {
 
     const { survey, recordsCache } = await this.getOrFetchSurveyData(msg)
 
-    console.log('===init record', recordUuid)
     let record = await RecordManager.fetchRecordAndNodesByUuid({ surveyId, recordUuid })
-
-    console.log('===record1', record)
 
     record = await RecordManager.initNewRecord({
       user,
@@ -180,7 +177,6 @@ class RecordsUpdateThread extends Thread {
       nodesUpdateListener: (updatedNodes) => this.handleNodesUpdated.bind(this)({ record, updatedNodes }),
       nodesValidationListener: (validations) => this.handleNodesValidationUpdated.bind(this)({ record, validations }),
     })
-    console.log('===record2', record)
     recordsCache.set(recordUuid, record)
   }
 
