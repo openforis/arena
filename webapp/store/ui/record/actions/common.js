5aeb2cd4c456ff400d1386364b064845e70070d8
Stefano Ricci <1219739+SteRiccio@users.noreply.github.com>
2025-04-03 09:54:24 +0200

    Support hierarchical code attributes to enumerate entities (#3798)
    * Enumerated entities: support hierarchical categories (WIP)
    
    * code: prevent editing when there are dependent enumerated entities
    
    * code cleanup
    
    * check non empty enumerated entities
    
    * attribute update check: include dependent applicable enumerated enities
    
    * code cleanup
    
    * use arena-core methods
    
    * use latest arena-core
    
    ---------
    
    Co-authored-by: Stefano Ricci <SteRiccio@users.noreply.github.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- webapp/store/ui/record/actions/common.js
+++ webapp/store/ui/record/actions/common.js
@@ -1,17 +1,46 @@
+import { Records } from '@openforis/arena-core'
+
 import * as A from '@core/arena'
+import * as NodeDef from '@core/survey/nodeDef'
 import * as Record from '@core/record/record'
 
-import { LoaderActions } from '@webapp/store/ui'
+import { DialogConfirmActions, LoaderActions } from '@webapp/store/ui'
+import { SurveyState } from '@webapp/store/survey'
 
 import * as RecordState from '../state'
 import * as ActionTypes from './actionTypes'
 
 export const recordNodesUpdate = (nodes) => (dispatch, getState) => {
-  const record = RecordState.getRecord(getState())
+  const state = getState()
+  const record = RecordState.getRecord(state)
   // Hide app loader on record create
   if (A.isEmpty(Record.getNodes(record))) {
     dispatch(LoaderActions.hideLoader())
   }
 
   dispatch({ type: ActionTypes.nodesUpdate, nodes: nodes })
+}
+
+export const checkAndConfirmUpdateNode = ({ dispatch, getState, node, nodeDef, onOk }) => {
+  const state = getState()
+  const survey = SurveyState.getSurvey(state)
+  const record = RecordState.getRecord(state)
+  const lang = SurveyState.getSurveyPreferredLang(state)
+  const dependentEnumeratedEntityDefs = Records.findDependentEnumeratedEntityDefsNotEmpty({ survey, node, nodeDef })(
+    record
+  )
+  const dependentEnumeratedEntityDefsLabel = dependentEnumeratedEntityDefs
+    ?.map((def) => NodeDef.getLabel(def, lang))
+    .join(', ')
+  if (dependentEnumeratedEntityDefsLabel) {
+    dispatch(
+      DialogConfirmActions.showDialogConfirm({
+        key: 'surveyForm.confirmUpdateDependentEnumeratedEntities',
+        params: { entityDefs: dependentEnumeratedEntityDefsLabel },
+        onOk: onOk,
+      })
+    )
+  } else {
+    onOk()
+  }
 }
