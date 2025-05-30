af6baabcd60f53ad44883baedfe2e9e053896350
SteRiccio <1219739+SteRiccio@users.noreply.github.com>
2025-05-30 15:03:53 +0200

    fixed tests

--- webapp/store/system/i18n/hooks.js
+++ webapp/store/system/i18n/hooks.js
@@ -4,7 +4,12 @@ import { initReactI18next, Trans as i18nTrans } from 'react-i18next'
 import i18n from '@core/i18n/i18nFactory'
 
 const browserI18n = i18n.use(detector).use(initReactI18next)
-browserI18n.changeLanguage(navigator.language)
+
+// set i18n language to browser language (English by default)
+const browserLanguage = navigator.language ?? 'en'
+if (browserI18n.language !== browserLanguage) {
+  browserI18n.changeLanguage(browserLanguage)
+}
 
 export const useI18n = () => browserI18n
 
