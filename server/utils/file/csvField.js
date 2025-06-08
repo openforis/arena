384f93abd1d78bc5ab9d6a52cbae51fe37e07367
Stefano Ricci <SteRiccio@users.noreply.github.com>
2025-06-08 23:34:58 +0200

    Data export to Excel: keep correct numeric format

---
+++ server/utils/file/csvField.js
@@ -0,0 +1,11 @@
+const getName = (field) => (typeof field === 'object' ? field.name : field)
+
+const getNames = (fields) => fields.map(getName)
+
+const getType = (field) => (typeof field === 'object' ? field.type : 'string')
+
+export const CsvField = {
+  getName,
+  getNames,
+  getType,
+}
