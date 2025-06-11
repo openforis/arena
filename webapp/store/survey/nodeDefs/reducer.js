dcdf737b035f501d720764dc9a9b6aa53a2d58cc
Stefano Ricci <SteRiccio@users.noreply.github.com>
2025-06-11 23:44:55 +0200

    handle node def dirty state in redux store

--- webapp/store/survey/nodeDefs/reducer.js
+++ webapp/store/survey/nodeDefs/reducer.js
@@ -1,3 +1,5 @@
+import * as A from '@core/arena'
+
 import { exportReducer } from '@webapp/utils/reduxUtils'
 
 import { SystemActions } from '@webapp/store/system'
@@ -25,17 +27,28 @@ const actionHandlers = {
   [NodeDefsActions.nodeDefDelete]: (state, { nodeDef }) => NodeDefsState.dissocNodeDef(nodeDef)(state),
   [NodeDefsActions.nodeDefsDelete]: (state, { nodeDefUuids }) => NodeDefsState.dissocNodeDefs(nodeDefUuids)(state),
 
-  [NodeDefsActions.nodeDefUpdate]: (state, { nodeDef, dirty = false }) =>
-    NodeDefsState.assocNodeDef(nodeDef, dirty)(state),
+  [NodeDefsActions.nodeDefUpdate]: (state, { nodeDef, dirty = false }) => {
+    let stateUpdated = NodeDefsState.assocNodeDef(nodeDef, dirty)(state)
+    if (dirty) {
+      stateUpdated = NodeDefsState.assocDirty(stateUpdated)
+    } else {
+      stateUpdated = NodeDefsState.dissocDirty(stateUpdated)
+    }
+    return stateUpdated
+  },
 
-  [NodeDefsActions.nodeDefSave]: (state, { nodeDef }) => NodeDefsState.assocNodeDef(nodeDef)(state),
+  [NodeDefsActions.nodeDefSave]: (state, { nodeDef }) =>
+    A.pipe(NodeDefsState.assocNodeDef(nodeDef), NodeDefsState.dissocDirty)(state),
 
   [NodeDefsActions.nodeDefsUpdate]: (state, { nodeDefs }) => NodeDefsState.mergeNodeDefs(nodeDefs)(state),
 
-  [NodeDefsActions.nodeDefPropsUpdateCancel]: (state, { nodeDef, nodeDefOriginal, isNodeDefNew }) =>
-    isNodeDefNew
+  [NodeDefsActions.nodeDefPropsUpdateCancel]: (state, { nodeDef, nodeDefOriginal, isNodeDefNew }) => {
+    let stateUpdated = isNodeDefNew
       ? NodeDefsState.dissocNodeDef(nodeDef)(state) // Remove node def from state
-      : NodeDefsState.assocNodeDef(nodeDefOriginal)(state), // Restore original version of node def
+      : NodeDefsState.assocNodeDef(nodeDefOriginal)(state) // Restore original version of node def
+    stateUpdated = NodeDefsState.dissocDirty(stateUpdated)
+    return stateUpdated
+  },
 }
 
 export default exportReducer(actionHandlers)
