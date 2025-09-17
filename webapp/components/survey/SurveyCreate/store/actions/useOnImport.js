1fbc0a8a63ea4d79253388010d9bf568fff432dc
SteRiccio <1219739+SteRiccio@users.noreply.github.com>
2025-09-17 10:31:37 +0200

    code cleanup

--- webapp/components/survey/SurveyCreate/store/actions/useOnImport.js
+++ webapp/components/survey/SurveyCreate/store/actions/useOnImport.js
@@ -12,10 +12,42 @@ const urlBySource = {
   [importSources.arena]: '/api/survey/arena-import',
 }
 
+const onChunk =
+  ({ onUploadProgress, newSurvey, setNewSurvey, resolve }) =>
+  async ({ chunk, totalChunks, content }) => {
+    const { fileId, source, ...surveyObj } = newSurvey
+
+    const formData = objectToFormData({
+      file: content,
+      fileId,
+      chunk,
+      totalChunks,
+      survey: JSON.stringify(surveyObj),
+    })
+
+    const { data } = await axios.post(urlBySource[source], formData, {
+      onUploadProgress: Chunks.onUploadProgress({ totalChunks, chunk, onUploadProgress }),
+    })
+
+    if (chunk === totalChunks) {
+      setNewSurvey((prevNewSurvey) => ({ ...prevNewSurvey, uploading: false }))
+
+      const { job, validation } = data
+      resolve({ job, validation })
+    }
+  }
+
+const onChunkError =
+  ({ setNewSurvey, reject }) =>
+  (error) => {
+    setNewSurvey((prevNewSurvey) => ({ ...prevNewSurvey, uploading: false }))
+    reject(error)
+  }
+
 export const useOnImport = ({ newSurvey, setNewSurvey }) =>
   useCallback(
     ({ startFromChunk = 1, onUploadProgress }) => {
-      const { file, fileId, source, ...surveyObj } = newSurvey
+      const { file } = newSurvey
 
       // reset upload progress (hide progress bar)
       setNewSurvey((prevNewSurvey) => ({ ...prevNewSurvey, uploadProgressPercent: -1, uploading: true }))
@@ -24,31 +56,8 @@ export const useOnImport = ({ newSurvey, setNewSurvey }) =>
       const promise = new Promise((resolve, reject) => {
         fileProcessor = new FileProcessor({
           file,
-          chunkProcessor: async ({ chunk, totalChunks, content }) => {
-            const formData = objectToFormData({
-              file: content,
-              fileId,
-              chunk,
-              totalChunks,
-              survey: JSON.stringify(surveyObj),
-            })
-
-            const { data } = await axios.post(urlBySource[source], formData, {
-              onUploadProgress: Chunks.onUploadProgress({ totalChunks, chunk, onUploadProgress }),
-            })
-
-            if (chunk === totalChunks) {
-              setNewSurvey((prevNewSurvey) => ({ ...prevNewSurvey, uploading: false }))
-
-              const { job, validation } = data
-              resolve({ job, validation })
-            }
-          },
-          onError: (error) => {
-            setNewSurvey((prevNewSurvey) => ({ ...prevNewSurvey, uploading: false }))
-
-            reject(error)
-          },
+          chunkProcessor: onChunk({ onUploadProgress, newSurvey, setNewSurvey, resolve }),
+          onError: onChunkError({ setNewSurvey, reject }),
         })
       })
       fileProcessor.start(startFromChunk)
