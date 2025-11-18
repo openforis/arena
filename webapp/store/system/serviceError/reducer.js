e33576359bd3a923c30be89da23926ab6d6d33d3
SteRiccio <1219739+SteRiccio@users.noreply.github.com>
2025-11-18 14:00:50 +0100

    fixed error at startup

--- webapp/store/system/serviceError/reducer.js
+++ webapp/store/system/serviceError/reducer.js
@@ -2,8 +2,11 @@ import { exportReducer } from '@webapp/utils/reduxUtils'
 
 import * as ServiceErrorActions from './actions'
 import * as ServiceErrorState from './state'
+import * as SystemActions from '../../system/actions'
 
 const actionHandlers = {
+  [SystemActions.SYSTEM_INIT]: () => ({}),
+
   [ServiceErrorActions.SERVICE_ERROR_CREATE]: (state, { error }) => ServiceErrorState.assocAppError(error)(state),
   [ServiceErrorActions.SERVICE_ERROR_DELETE]: (state, { error }) => ServiceErrorState.dissocAppError(error)(state),
 }
