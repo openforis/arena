b6772bac3b0998d9abb346396e9dbe11784d3ccb
Stefano Ricci <1219739+SteRiccio@users.noreply.github.com>
2025-11-24 13:55:48 +0100

    set user preferred language at startup (#4056)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- webapp/store/system/actions.js
+++ webapp/store/system/actions.js
@@ -1,11 +1,15 @@
+import * as User from '@core/user/user'
 import analytics from '@webapp/service/analytics'
 import * as API from '@webapp/service/api'
 import { showJobMonitor } from '../app/job/actions'
+import { useI18n } from './i18n'
 
 export const SYSTEM_INIT = 'system/init'
 export const SYSTEM_RESET = 'system/reset'
 
 export const initSystem = () => async (dispatch) => {
+  const i18n = useI18n()
+
   const { user, survey } = await API.fetchUserAndSurvey()
 
   analytics.identify({
@@ -18,6 +22,11 @@ export const initSystem = () => async (dispatch) => {
   const activeJob = await API.fetchActiveJob()
   if (activeJob) {
     dispatch(showJobMonitor({ job: activeJob }))
+  }
+
+  const userPreferredLanguage = user ? User.getPrefLanguage(user) : null
+  if (userPreferredLanguage) {
+    i18n.changeLanguage(userPreferredLanguage)
   }
 }
 
