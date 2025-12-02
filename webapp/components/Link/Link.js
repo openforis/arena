e8872520f5690167dac7ef7c75c5cb73bb3869b0
Stefano Ricci <SteRiccio@users.noreply.github.com>
2025-12-02 10:30:12 +0100

    Link component: allow children

--- webapp/components/Link/Link.js
+++ webapp/components/Link/Link.js
@@ -3,15 +3,16 @@ import MuiLink from '@mui/material/Link'
 import PropTypes from 'prop-types'
 
 export const Link = (props) => {
-  const { disabled = false, href, label = undefined } = props
+  const { children = null, disabled = false, href, label = undefined } = props
   return (
     <MuiLink onClick={disabled ? () => {} : undefined} href={href} target="_blank" rel="noopener noreferrer">
-      {label ?? href}
+      {children ?? label ?? href}
     </MuiLink>
   )
 }
 
 Link.propTypes = {
+  children: PropTypes.node,
   disabled: PropTypes.bool,
   href: PropTypes.string.isRequired,
   label: PropTypes.string,
