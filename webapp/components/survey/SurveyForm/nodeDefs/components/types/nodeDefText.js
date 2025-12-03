e68e9c46576061ed782c7a7337f1036e6a0a1e2d
Stefano Ricci <SteRiccio@users.noreply.github.com>
2025-12-03 23:24:35 +0100

    fixed text rendering

--- webapp/components/survey/SurveyForm/nodeDefs/components/types/nodeDefText.js
+++ webapp/components/survey/SurveyForm/nodeDefs/components/types/nodeDefText.js
@@ -5,30 +5,37 @@ import classNames from 'classnames'
 
 import { Strings } from '@openforis/arena-core'
 
+import * as A from '@core/arena'
 import * as NodeDef from '@core/survey/nodeDef'
+import * as NodeDefLayout from '@core/survey/nodeDefLayout'
 import * as NodeDefExpression from '@core/survey/nodeDefExpression'
 import * as Node from '@core/record/node'
 
-import { Link } from '@webapp/components'
+import { Link, Markdown } from '@webapp/components'
 import { Input } from '@webapp/components/form/Input'
 import * as NodeDefUIProps from '@webapp/components/survey/SurveyForm/nodeDefs/nodeDefUIProps'
 
 import NodeDefErrorBadge from '../nodeDefErrorBadge'
 import NodeDeleteButton from '../nodeDeleteButton'
 
-const extractConstantHyperlinkValue = (nodeDef) => {
-  if (NodeDef.isReadOnly(nodeDef) && NodeDef.isShownAsHyperlink(nodeDef)) {
-    const defaultValues = NodeDef.getDefaultValues(nodeDef)
-    if (defaultValues?.length === 1) {
-      const defaultValue = defaultValues[0]
-      const expr = NodeDefExpression.getExpression(defaultValue)
-      const validUrlPrefixes = ['http://', 'https://']
-      if (validUrlPrefixes.some((prefix) => expr?.startsWith(`"${prefix}`))) {
-        return Strings.unquoteDouble(expr)
-      }
-    }
+const extractSingleDefaultValueExpression = (nodeDef) => {
+  const defaultValues = NodeDef.getDefaultValues(nodeDef)
+  if (defaultValues?.length === 1) {
+    const defaultValue = defaultValues[0]
+    return NodeDefExpression.getExpression(defaultValue)
   }
   return null
+}
+
+const extractConstantHyperlinkValue = (nodeDef) => {
+  const expr = extractSingleDefaultValueExpression(nodeDef)
+  const validUrlPrefixes = ['http://', 'https://']
+  return validUrlPrefixes.some((prefix) => expr?.startsWith(`"${prefix}`)) ? Strings.unquoteDouble(expr) : null
+}
+
+const extractConstantMarkdownValue = (nodeDef) => {
+  const expr = extractSingleDefaultValueExpression(nodeDef)
+  return A.pipe(Strings.removePrefix(`"`), Strings.removeSuffix('\n'), Strings.removeSuffix('"'))(expr)
 }
 
 const TextInput = ({ nodeDef, readOnly, node, edit, updateNode, canEditRecord }) => {
@@ -75,13 +82,21 @@ const MultipleTextInput = (props) => {
 }
 
 const NodeDefText = (props) => {
-  const { edit, entryDataQuery, nodeDef, nodes } = props
+  const { edit, entryDataQuery, nodeDef, nodes, surveyCycleKey } = props
 
-  const isHyperlink = NodeDef.isReadOnly(nodeDef) && NodeDef.isShownAsHyperlink(nodeDef)
+  const renderType = NodeDefLayout.getRenderType(surveyCycleKey)(nodeDef)
+  const isHyperlink = NodeDef.isReadOnly(nodeDef) && renderType === NodeDefLayout.textRenderType.hyperlink
+  const isMarkdown = NodeDef.isReadOnly(nodeDef) && renderType === NodeDefLayout.textRenderType.markdown
   if (edit) {
     if (isHyperlink) {
       const hyperlink = extractConstantHyperlinkValue(nodeDef) ?? 'https://www.example-link.org'
       return <Link disabled href="#" label={hyperlink} />
+    }
+    if (isMarkdown) {
+      const markdown = extractConstantMarkdownValue(nodeDef)
+      if (markdown) {
+        return <Markdown source={markdown} />
+      }
     }
     return <TextInput {...props} />
   }
@@ -90,6 +105,10 @@ const NodeDefText = (props) => {
   }
   if (isHyperlink) {
     return <Link href={Node.getValue(nodes[0], '')} />
+  }
+  if (isMarkdown) {
+    const text = Node.getValue(nodes[0], '')
+    return <Markdown source={text} />
   }
   return <TextInput {...props} node={nodes[0]} />
 }
