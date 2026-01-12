4870d80d91e07390bc92db769d8eb739fdf64274
SteRiccio <1219739+SteRiccio@users.noreply.github.com>
2026-01-12 14:09:48 +0100

    fixed startup error

--- webapp/store/system/actions.js
+++ webapp/store/system/actions.js
@@ -1,13 +1,16 @@
 import * as User from '@core/user/user'
 import analytics from '@webapp/service/analytics'
 import * as API from '@webapp/service/api'
+
 import { showJobMonitor } from '../app/job/actions'
+import { useI18n } from './i18n'
 
 export const SYSTEM_INIT = 'system/init'
 export const SYSTEM_RESET = 'system/reset'
 
 export const initSystem = () => async (dispatch) => {
   try {
+    const i18n = useI18n()
     const { user, survey } = await API.fetchLoggedInUserAndSurvey()
 
     analytics.identify({ userId: user?.uuid, properties: user })
@@ -19,14 +22,13 @@ export const initSystem = () => async (dispatch) => {
       if (activeJob) {
         dispatch(showJobMonitor({ job: activeJob }))
       }
+      const userPreferredLanguage = User.getPrefLanguage(user)
+      if (userPreferredLanguage) {
+        i18n.changeLanguage(userPreferredLanguage)
+      }
     }
   } catch (error) {
     dispatch({ type: SYSTEM_INIT, user: null, survey: null, errorMessage: String(error) })
-  }
-
-  const userPreferredLanguage = user ? User.getPrefLanguage(user) : null
-  if (userPreferredLanguage) {
-    i18n.changeLanguage(userPreferredLanguage)
   }
 }
 
