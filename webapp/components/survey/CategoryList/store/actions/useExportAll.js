6571544fa32646aa4dc4d44f89b1c6c3362e44ec
SteRiccio <1219739+SteRiccio@users.noreply.github.com>
2026-01-14 10:25:40 +0100

    categories export: fixed file name

--- webapp/components/survey/CategoryList/store/actions/useExportAll.js
+++ webapp/components/survey/CategoryList/store/actions/useExportAll.js
@@ -1,38 +1,50 @@
 import React, { useCallback } from 'react'
 import { useDispatch } from 'react-redux'
 
+import { ExportFileNameGenerator } from '@common/dataExport/exportFileNameGenerator'
 import * as API from '@webapp/service/api'
 
 import { ButtonDownload } from '@webapp/components/buttons'
 
 import { JobActions } from '@webapp/store/app'
-import { useSurveyId } from '@webapp/store/survey'
+import { useSurveyId, useSurveyInfo } from '@webapp/store/survey'
 
 export const useExportAll = () => {
   const surveyId = useSurveyId()
+  const survey = useSurveyInfo()
   const dispatch = useDispatch()
 
   // always export draft properties
   const draft = true
 
-  return useCallback(async ({ fileFormat }) => {
-    const { job } = await API.startExportAllCategoriesJob({ surveyId, draft, fileFormat })
+  return useCallback(
+    async ({ fileFormat }) => {
+      const { job } = await API.startExportAllCategoriesJob({ surveyId, draft, fileFormat })
 
-    dispatch(
-      JobActions.showJobMonitor({
-        job,
-        closeButton: ({ job: jobCompleted }) => {
-          const { tempFileName } = jobCompleted.result
-          return (
-            <ButtonDownload
-              href={`/api/survey/${surveyId}/categories/export/download`}
-              requestParams={{ tempFileName, draft }}
-              onClick={() => dispatch(JobActions.hideJobMonitor())}
-              variant="contained"
-            />
-          )
-        },
+      const downloadFileName = ExportFileNameGenerator.generate({
+        survey,
+        fileType: 'categories',
+        fileFormat,
       })
-    )
-  }, [])
+
+      dispatch(
+        JobActions.showJobMonitor({
+          job,
+          closeButton: ({ job: jobCompleted }) => {
+            const { tempFileName } = jobCompleted.result
+            return (
+              <ButtonDownload
+                fileName={downloadFileName}
+                href={`/api/survey/${surveyId}/categories/export/download`}
+                requestParams={{ tempFileName, draft }}
+                onClick={() => dispatch(JobActions.hideJobMonitor())}
+                variant="contained"
+              />
+            )
+          },
+        })
+      )
+    },
+    [dispatch, draft, survey, surveyId]
+  )
 }
