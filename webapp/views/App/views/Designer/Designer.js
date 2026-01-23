23d9a5b918b8cfd2f5bc2f3164e7a8e38d3e6031
SteRiccio <1219739+SteRiccio@users.noreply.github.com>
2026-01-23 14:45:11 +0100

    code cleanup

--- webapp/views/App/views/Designer/Designer.js
+++ webapp/views/App/views/Designer/Designer.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 
 import SurveyDefsLoader from '@webapp/components/survey/SurveyDefsLoader'
 import ModuleSwitch from '@webapp/components/moduleSwitch'
@@ -20,60 +20,61 @@ import { SurveyDependencyTree } from './SurveyDependencyTree'
 const Designer = () => {
   const canEditSurvey = useAuthCanEditSurvey()
 
+  const modules = useMemo(
+    () => [
+      ...(canEditSurvey
+        ? [
+            {
+              component: FormDesigner,
+              path: designerModules.formDesigner.path,
+            },
+            {
+              component: NodeDefDetails,
+              path: designerModules.nodeDef.path,
+            },
+            {
+              component: NodeDefDetails,
+              path: `${designerModules.nodeDef.path}/:nodeDefUuid/`,
+            },
+          ]
+        : []),
+      {
+        component: SurveyHierarchy,
+        path: designerModules.surveyHierarchy.path,
+      },
+      {
+        component: SurveyDependencyTree,
+        path: designerModules.surveyDependencyTree.path,
+      },
+      // Category
+      {
+        component: CategoryList,
+        path: designerModules.categories.path,
+      },
+      {
+        component: CategoryDetails,
+        path: designerModules.category.path,
+      },
+      {
+        component: CategoryDetails,
+        path: `${designerModules.category.path}/:categoryUuid`,
+      },
+      // Taxonomy
+      {
+        component: TaxonomyList,
+        path: designerModules.taxonomies.path,
+      },
+      {
+        component: TaxonomyDetails,
+        path: `${designerModules.taxonomy.path}/:taxonomyUuid`,
+      },
+    ],
+    [canEditSurvey]
+  )
+
   return (
     <SurveyDefsLoader draft={canEditSurvey} validate={canEditSurvey}>
-      <ModuleSwitch
-        moduleRoot={appModules.designer}
-        moduleDefault={designerModules.formDesigner}
-        modules={[
-          ...(canEditSurvey
-            ? [
-                {
-                  component: FormDesigner,
-                  path: designerModules.formDesigner.path,
-                },
-                {
-                  component: NodeDefDetails,
-                  path: designerModules.nodeDef.path,
-                },
-                {
-                  component: NodeDefDetails,
-                  path: `${designerModules.nodeDef.path}/:nodeDefUuid/`,
-                },
-              ]
-            : []),
-          {
-            component: SurveyHierarchy,
-            path: designerModules.surveyHierarchy.path,
-          },
-          {
-            component: SurveyDependencyTree,
-            path: designerModules.surveyDependencyTree.path,
-          },
-          // Category
-          {
-            component: CategoryList,
-            path: designerModules.categories.path,
-          },
-          {
-            component: CategoryDetails,
-            path: designerModules.category.path,
-          },
-          {
-            component: CategoryDetails,
-            path: `${designerModules.category.path}/:categoryUuid`,
-          },
-          // Taxonomy
-          {
-            component: TaxonomyList,
-            path: designerModules.taxonomies.path,
-          },
-          {
-            component: TaxonomyDetails,
-            path: `${designerModules.taxonomy.path}/:taxonomyUuid`,
-          },
-        ]}
-      />
+      <ModuleSwitch moduleRoot={appModules.designer} moduleDefault={designerModules.formDesigner} modules={modules} />
     </SurveyDefsLoader>
   )
 }
