9d36dc4f318833d49f735a272b14cf021f577b06
SteRiccio <1219739+SteRiccio@users.noreply.github.com>
2026-01-27 12:34:13 +0100

    serviceerrors: code refactor

--- webapp/views/App/ServiceErrors/ServiceError/ServiceError.js
+++ webapp/views/App/ServiceErrors/ServiceError/ServiceError.js
@@ -8,13 +8,14 @@ import { ServiceErrorActions, useI18n } from '@webapp/store/system'
 
 import Markdown from '@webapp/components/markdown'
 
-const ServiceError = ({ error }) => {
+const ServiceError = React.forwardRef((props, ref) => {
+  const { error } = props
   const i18n = useI18n()
   const dispatch = useDispatch()
   const { key, params } = R.path(['response', 'data'], error)
 
   return (
-    <div className="service-errors__error">
+    <div ref={ref} className="service-errors__error">
       <button
         type="button"
         className="btn-s btn-close"
@@ -27,7 +28,9 @@ const ServiceError = ({ error }) => {
       <Markdown className="message" source={i18n.t(key, params)} />
     </div>
   )
-}
+})
+
+ServiceError.displayName = 'ServiceError'
 
 ServiceError.propTypes = {
   error: PropTypes.object.isRequired,
--- webapp/views/App/ServiceErrors/ServiceErrors.js
+++ webapp/views/App/ServiceErrors/ServiceErrors.js
@@ -1,9 +1,8 @@
-import './ServiceErrors.scss'
-
-import React from 'react'
-import { CSSTransition, TransitionGroup } from 'react-transition-group'
-
+/* eslint-disable react-hooks/refs */
 import * as R from 'ramda'
+import React, { useMemo, useRef } from 'react'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
+import './ServiceErrors.scss'
 
 import { useServiceErrors } from '@webapp/store/system'
 
@@ -11,11 +10,37 @@ import ServiceError from './ServiceError'
 
 const ServiceErrors = () => {
   const errors = useServiceErrors()
+
+  const nodeMapRef = useRef(new Map())
+
+  const itemsWithRefs = useMemo(() => {
+    const nodeMap = nodeMapRef.current
+    const errorIds = new Set(errors.map((e) => e.id))
+
+    // 1. Cleanup: Remove refs for errors that no longer exist
+    for (const id of nodeMap.keys()) {
+      if (!errorIds.has(id)) {
+        nodeMap.delete(id)
+      }
+    }
+
+    // 2. Map: Ensure every current error has a stable React ref
+    return errors.map((error) => {
+      if (!nodeMap.has(error.id)) {
+        nodeMap.set(error.id, React.createRef())
+      }
+      return {
+        error,
+        nodeRef: nodeMap.get(error.id),
+      }
+    })
+  }, [errors]) // Only re-runs when the errors array changes
+
   return (
     <TransitionGroup className={`service-errors${R.isEmpty(errors) ? ' hidden-transition' : ''}`} enter appear>
-      {errors.map((error) => (
-        <CSSTransition key={error.id} timeout={500} classNames="fade">
-          <ServiceError error={error} />
+      {itemsWithRefs.map(({ error, nodeRef }) => (
+        <CSSTransition key={error.id} timeout={500} classNames="fade" nodeRef={nodeRef}>
+          <ServiceError ref={nodeRef} error={error} />
         </CSSTransition>
       ))}
     </TransitionGroup>
