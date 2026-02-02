ba6991bf2551ee2a560e1178d77f688a71b24860
Stefano Ricci <1219739+SteRiccio@users.noreply.github.com>
2026-02-02 15:40:43 +0100

    fixing map record edit dialog error (#4110)
    * fixing map record edit dialog error
    
    * updated version number

--- server/system/appCluster.js
+++ server/system/appCluster.js
@@ -46,7 +46,7 @@ export const run = async () => {
   app.use('/guest{/*path}', express.static(dist))
   const imgDir = `${arenaRoot}/web-resources/img`
   app.use('/img', express.static(imgDir))
-  app.use('/noHeader', express.static(dist))
+  app.use('/noHeader{/*path}', express.static(dist))
 
   // ====== APIs
   authApi.init(app)
