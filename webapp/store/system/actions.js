c59c45ba18b357bdfbbce0f6d04109ad9f55d5f4
Stefano Ricci <SteRiccio@users.noreply.github.com>
2026-03-02 23:08:59 +0100

    prepare app info fetch

--- webapp/store/system/actions.js
+++ webapp/store/system/actions.js
@@ -10,13 +10,18 @@ import { SystemActionTypes } from './actionTypes'
 export const initSystem = () => async (dispatch) => {
   try {
     const i18n = useI18n()
+
     const { user, survey } = await API.fetchLoggedInUserAndSurvey()
 
     analytics.identify({ userId: user?.uuid, properties: user })
 
-    dispatch({ type: SystemActionTypes.SYSTEM_INIT, user, survey })
+    const sytemInitPayload = { user, survey }
 
     if (user) {
+      const info = await API.fetchInfo()
+
+      Object.assign(sytemInitPayload, { info })
+
       const activeJob = await API.fetchActiveJob()
       if (activeJob) {
         dispatch(showJobMonitor({ job: activeJob }))
@@ -28,8 +33,9 @@ export const initSystem = () => async (dispatch) => {
 
       dispatch(MessageNotificationActions.fetchMessagesNotifiedToUser({ i18n }))
     }
+    dispatch({ type: SystemActionTypes.SYSTEM_INIT, ...sytemInitPayload })
   } catch (error) {
-    dispatch({ type: SystemActionTypes.SYSTEM_INIT, user: null, survey: null, errorMessage: String(error) })
+    dispatch({ type: SystemActionTypes.SYSTEM_INIT, info: null, user: null, survey: null, errorMessage: String(error) })
   }
 }
 
