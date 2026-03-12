c317429d7513a9fed8fa1590ecde050e7ffe14aa
Stefano Ricci <SteRiccio@users.noreply.github.com>
2026-03-12 22:58:50 +0100

    email service: support Amazon SES

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -11,17 +11,24 @@ const logger = Log.getLogger('Mailer')
 const emailServices = {
   sendgrid: 'sendgrid',
   office365: 'office365',
+  amazonSES: 'amazonSES',
 }
 
 const emailService = ProcessUtils.ENV.emailService
 
+logger.debug(`using ${emailService} email service`)
+
 if (emailService === emailServices.sendgrid) {
-  sgMail.setApiKey(ProcessUtils.ENV.sendGridApiKey)
+  const sendGridApiKey = ProcessUtils.ENV.sendGridApiKey
+  if (!sendGridApiKey) {
+    throw new Error('SENDGRID_API_KEY environment variable is required when using SendGrid email service')
+  }
+  sgMail.setApiKey(sendGridApiKey)
 }
 
 const authUser = ProcessUtils.ENV.emailAuthUser
 const authPass = ProcessUtils.ENV.emailAuthPassword
-const from = ProcessUtils.ENV.adminEmail || authUser
+const from = ProcessUtils.ENV.emailFrom || ProcessUtils.ENV.adminEmail || authUser
 
 const defaultOffice365TransportOptions = {
   host: 'smtp.office365.com',
@@ -31,11 +38,33 @@ const defaultOffice365TransportOptions = {
   tls: { ciphers: 'SSLv3' },
 }
 
+const defaultAmazonSESTransportOptions = {
+  host: ProcessUtils.ENV.emailAmazonSESHost,
+  port: ProcessUtils.ENV.emailAmazonSESPort,
+  auth: { user: authUser, pass: authPass },
+  secure: true,
+  requireTLS: false,
+  tls: {
+    ciphers: 'SSLv3',
+    // rejectUnauthorized: true,
+  },
+}
+
 let office365TransportOptions = null
+let amazonSESTransportOptions = null
 {
   const optionsType = ProcessUtils.ENV.emailTransportOptions ? 'custom' : 'default'
   logger.debug(`using ${optionsType} email transport options`)
-  office365TransportOptions = ProcessUtils.ENV.emailTransportOptions ?? defaultOffice365TransportOptions
+  switch (emailService) {
+    case emailServices.office365:
+      office365TransportOptions = ProcessUtils.ENV.emailTransportOptions ?? defaultOffice365TransportOptions
+      // logger.debug(`Office365 transport config: ${JSON.stringify(office365TransportOptions)}`)
+      break
+    case emailServices.amazonSES:
+      amazonSESTransportOptions = ProcessUtils.ENV.emailTransportOptions ?? defaultAmazonSESTransportOptions
+      // logger.debug(`Amazon SES transport config: ${JSON.stringify(amazonSESTransportOptions)}`)
+      break
+  }
 }
 
 const sendEmailSendgrid = async ({ to, subject, html }) => {
@@ -47,12 +76,24 @@ const sendEmailSendgrid = async ({ to, subject, html }) => {
 }
 
 const sendEmailMSOffice365 = async ({ to, subject, html, text = null }) => {
-  const mailTransport = nodemailer.createTransport(office365TransportOptions)
+  const transporter = nodemailer.createTransport(office365TransportOptions)
 
-  await mailTransport.sendMail({
+  await transporter.sendMail({
     from,
     to,
     replyTo: from,
+    subject,
+    html,
+    text,
+  })
+}
+
+const sendEmailAmazonSES = async ({ to, subject, html, text = null }) => {
+  const transporter = nodemailer.createTransport(amazonSESTransportOptions)
+
+  await transporter.sendMail({
+    from,
+    to,
     subject,
     html,
     text,
@@ -71,15 +112,26 @@ export const sendCustomEmail = async ({ to, subject, html, log = true }) => {
   if (log) {
     logger.debug(`sending ${logMessageCommonPart}`)
   }
-  if (emailService === emailServices.sendgrid) {
-    await sendEmailSendgrid({ to, subject, html })
-  } else if (emailService === emailServices.office365) {
-    await sendEmailMSOffice365({ to, subject, html })
-  } else {
-    throw new Error('Invalid email service specified: ' + emailService)
-  }
-  if (log) {
-    logger.debug(`sent ${logMessageCommonPart}`)
+  try {
+    switch (emailService) {
+      case emailServices.sendgrid:
+        await sendEmailSendgrid({ to, subject, html })
+        break
+      case emailServices.office365:
+        await sendEmailMSOffice365({ to, subject, html })
+        break
+      case emailServices.amazonSES:
+        await sendEmailAmazonSES({ to, subject, html })
+        break
+      default:
+        throw new Error('Invalid email service specified: ' + emailService)
+    }
+    if (log) {
+      logger.debug(`sent ${logMessageCommonPart}`)
+    }
+  } catch (error) {
+    logger.error(`error sending ${logMessageCommonPart}: ${error.message}`)
+    throw error
   }
 }
 
