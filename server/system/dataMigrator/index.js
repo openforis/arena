7c3a5b8c957cf0badbab5c9e3cf60edbb1f8ce53
SteRiccio <1219739+SteRiccio@users.noreply.github.com>
2026-03-24 15:25:52 +0100

    Revert "reverted category import items sorting fix (#4167)"
    This reverts commit 84703802d56f49a9be344f31cc78ac6b35fe62d1.

--- server/system/dataMigrator/index.js
+++ server/system/dataMigrator/index.js
@@ -1,16 +1,17 @@
-import { ServiceType } from '@openforis/arena-core'
+import { ServiceType, Versions } from '@openforis/arena-core'
 
 import * as CategoryService from '@server/modules/category/service/categoryService'
 
-const defaultAppVersion = '2.0.0'
+const versionWithCategoryItemIndexFix = '2.3.20'
 
-const isCategoryItemsMigrationNeeded = (versionInDb) => versionInDb === defaultAppVersion
+const isCategoryItemsMigrationNeeded = (versionInDb) =>
+  Versions.isLessThan(versionInDb, versionWithCategoryItemIndexFix)
 
 const migrateCategoryItemsIfNeeded = async ({ logger, versionInDb }) => {
   if (isCategoryItemsMigrationNeeded(versionInDb)) {
     await CategoryService.initializeAllSurveysCategoryItemIndexes()
   } else {
-    logger.info(`Category item indexes already initialized. App version in DB: ${versionInDb}`)
+    logger.info(`Category item indexes already initialized/fixed. App version in DB: ${versionInDb}`)
   }
 }
 
