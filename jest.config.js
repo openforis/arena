ae30dedc751ef867a18555931c3dad51c00d2382
SteRiccio <1219739+SteRiccio@users.noreply.github.com>
2026-03-25 11:46:45 +0100

    fixing tests

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   roots: ['<rootDir>/'],
   testEnvironment: 'node',
-  setupFiles: ['<rootDir>/test/unit/jest.setup.js'],
+  setupFiles: [require.resolve('./test/unit/jest.setup.js')],
   transformIgnorePatterns: [
     '/node_modules/(?!change-case/|uuid/|@openforis/arena-core/node_modules/uuid/|@scure/|@noble/)',
   ],
