2ff2b8c80433dd16bd820a1340840a17743f2973
Stefano Ricci <1219739+SteRiccio@users.noreply.github.com>
2026-03-25 18:53:51 +0100

    csv data import: fixed error importing data with location altitude_accuracy field (#4169)
    * csv data import: fixed error importing data with location altitude_accuracy field
    
    * fixed tests
    
    * fixing tests
    
    * fixing e2e tests

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   roots: ['<rootDir>/'],
+  testEnvironment: 'node',
+  setupFiles: [require.resolve('./test/unit/jest.setup.js')],
+  transformIgnorePatterns: [
+    '/node_modules/(?!change-case/|uuid/|@openforis/arena-core/node_modules/uuid/|@scure/|@noble/)',
+  ],
   verbose: true,
 }
