5ca436f0060610692a254afa417ebe162b87fb02
SteRiccio <1219739+SteRiccio@users.noreply.github.com>
2026-04-24 15:11:30 +0200

    fixed error (white screen) when server is restarted (chunk load error)

--- webapp/views/Arena/Arena.js
+++ webapp/views/Arena/Arena.js
@@ -1,10 +1,18 @@
-import React, { useEffect } from 'react'
+import React, { useCallback, useEffect } from 'react'
 import { useDispatch } from 'react-redux'
+import { ErrorBoundary } from 'react-error-boundary'
 
 import { SystemActions, useSystemError, useI18n, useSystemStatusReady } from '@webapp/store/system'
 import { ExpansionPanel } from '@webapp/components'
 
 import Routes from './Routes'
+
+const ErrorFallback = ({ error }) => (
+  <div role="alert">
+    <p>Something went wrong:</p>
+    <pre>{error.message}</pre>
+  </div>
+)
 
 const Arena = () => {
   const dispatch = useDispatch()
@@ -15,6 +23,13 @@ const Arena = () => {
   useEffect(() => {
     dispatch(SystemActions.initSystem())
   }, [dispatch])
+
+  const handleError = useCallback((error) => {
+    // reload the page if the error is a chunk load error, which can happen when a new version of the app is deployed while the user has the app open
+    if (error.name === 'ChunkLoadError' || /loading chunk/i.test(error.message)) {
+      globalThis.location.reload()
+    }
+  }, [])
 
   if (!ready) return null
 
@@ -34,7 +49,11 @@ const Arena = () => {
       </div>
     )
 
-  return <Routes />
+  return (
+    <ErrorBoundary FallbackComponent={ErrorFallback} onError={handleError}>
+      <Routes />
+    </ErrorBoundary>
+  )
 }
 
 export default Arena
