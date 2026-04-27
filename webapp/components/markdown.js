e87c21218748d184b671b54a30251ec31843affc
Stefano Ricci <SteRiccio@users.noreply.github.com>
2026-04-27 23:00:37 +0200

    about page: open links in new tab

--- webapp/components/markdown.js
+++ webapp/components/markdown.js
@@ -4,14 +4,14 @@ import PropTypes from 'prop-types'
 import { parseMarkdown } from '@core/markdownUtils'
 
 const Markdown = (props) => {
-  const { container: Container = 'div', className = undefined, source } = props
+  const { container: Container = 'div', className = undefined, source, sanitize = true } = props
 
   const [output, setOutput] = useState('')
 
   useEffect(() => {
-    const parsedSource = parseMarkdown(source)
+    const parsedSource = parseMarkdown(source, sanitize)
     setOutput(parsedSource)
-  }, [source])
+  }, [source, sanitize])
 
   return <Container className={className} dangerouslySetInnerHTML={{ __html: output }} />
 }
@@ -20,6 +20,7 @@ Markdown.propTypes = {
   source: PropTypes.string.isRequired, // Markdown text to render
   container: PropTypes.string, // The container element to use
   className: PropTypes.string, // The class name to apply to the container element
+  sanitize: PropTypes.bool, // Whether to sanitize the HTML output
 }
 
 export default Markdown
