f6c42271ab5a331b8379368f78b39f688f53c00d
SteRiccio <1219739+SteRiccio@users.noreply.github.com>
2026-04-27 15:13:28 +0200

    fixed heic image preview

--- webapp/components/survey/SurveyForm/nodeDefs/components/types/ImagePreview.js
+++ webapp/components/survey/SurveyForm/nodeDefs/components/types/ImagePreview.js
@@ -1,22 +1,35 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react'
 import PropTypes from 'prop-types'
 import axios from 'axios'
+import { heicTo } from 'heic-to'
 
 import { useI18n } from '@webapp/store/system'
 import LoadingBar from '@webapp/components/LoadingBar'
 
 const initialState = { error: false, loading: true }
 
-const determineImageSrc = async ({ file, path }) => {
+const fetchFile = async ({ file, path }) => {
   if (file) {
     // used the file blob if specified, to avoid downloading the file from the path
-    return file
+    return { file, contentType: file.type }
   }
   if (path) {
-    const { data } = await axios.get(path, { responseType: 'blob' })
-    return data
+    const { data, headers } = await axios.get(path, { responseType: 'blob' })
+    const contentType = headers['content-type']
+    return { file: data, contentType }
   }
   return null
+}
+
+// Ensures that HEIC images are converted to JPEG before being displayed, as HEIC is not widely supported by browsers.
+const ensureJpegConversion = async ({ blob, contentType }) => {
+  if (contentType === 'image/heif') {
+    return heicTo({
+      blob,
+      type: 'image/jpeg',
+    })
+  }
+  return blob
 }
 
 export const ImagePreview = ({ path, onLoadComplete = null, file = null }) => {
@@ -26,32 +39,44 @@ export const ImagePreview = ({ path, onLoadComplete = null, file = null }) => {
   // used to store the image src to be able to revoke it on unmount
   const imgSrcRef = useRef(null)
   const [state, setState] = useState(initialState)
+  const isMountedRef = useRef(true)
   const { error, loading } = state
 
+  const onImageLoad = useCallback(async ({ file: blob, contentType }) => {
+    if (!blob || !isMountedRef.current) {
+      return
+    }
+    const convertedBlob = await ensureJpegConversion({ blob, contentType })
+    if (!isMountedRef.current) {
+      const imgUrl = URL.createObjectURL(convertedBlob)
+      imgSrcRef.current = imgUrl
+      imgRef.current.src = imgUrl
+    }
+  }, [])
+
   useEffect(() => {
-    let isCanceled = false
-    determineImageSrc({ file, path })
-      .then((blob) => {
-        if (blob && !isCanceled) {
-          const imgUrl = URL.createObjectURL(blob)
-          imgSrcRef.current = imgUrl
-          imgRef.current.src = imgUrl
-        }
+    fetchFile({ file, path })
+      .then(({ file, contentType }) => {
+        onImageLoad({ file, contentType }).catch(() => {
+          if (!isMountedRef.current) {
+            setState({ loading: false, error: true })
+          }
+        })
       })
       .catch(() => {
-        if (!isCanceled) {
+        if (!isMountedRef.current) {
           setState({ loading: false, error: true })
         }
       })
     return () => {
-      isCanceled = true
+      isMountedRef.current = false
       // revoke object URL to avoid memory leaks
       const imgSrc = imgSrcRef.current
       if (imgSrc?.startsWith('blob:')) {
         URL.revokeObjectURL(imgSrc)
       }
     }
-  }, [path, file])
+  }, [path, file, onImageLoad])
 
   const onLoad = useCallback(() => {
     setState({ loading: false, error: false })
