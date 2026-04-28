5cc2137063222deaa7b2fee8465eca1f12b0d77a
Stefano Ricci <1219739+SteRiccio@users.noreply.github.com>
2026-04-28 12:19:57 +0200

    fixing runtime error starting broken survey (#4209)
    * fixing runtime error starting broken survey
    
    * system errors: show message (WIP)
    
    * code cleanup

--- webapp/store/system/serviceError/actions.js
+++ webapp/store/system/serviceError/actions.js
@@ -1,10 +1,21 @@
-import * as R from 'ramda'
+import * as DomUtils from '@webapp/utils/domUtils'
 
 export const SERVICE_ERROR_CREATE = 'service/error/create'
 export const SERVICE_ERROR_DELETE = 'service/error/delete'
 
 export const closeServiceError = (error) => (dispatch) => dispatch({ type: SERVICE_ERROR_DELETE, error })
-export const createServiceError = ({ error }) => (dispatch) => {
-  const message = R.pathOr(error.message, ['response', 'data', 'error'], error)
-  return dispatch({ type: SERVICE_ERROR_CREATE, error: { ...error, message } })
-}
+export const createServiceError =
+  ({ error }) =>
+  (dispatch) => {
+    let message
+    if (error?.name === 'AxiosError') {
+      const htmlText = error.response?.data
+      if (htmlText) {
+        message = DomUtils.extractPreElementContentFromHtml(htmlText)
+      }
+    }
+    if (!message) {
+      message = error.message
+    }
+    return dispatch({ type: SERVICE_ERROR_CREATE, error: { ...error, message } })
+  }
