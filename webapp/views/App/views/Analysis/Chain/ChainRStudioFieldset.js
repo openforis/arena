da4199b890e0cf2c89c5ee8f427436edb820f4f9
SteRiccio <1219739+SteRiccio@users.noreply.github.com>
2026-05-08 09:53:57 +0200

    code cleanup

--- webapp/views/App/views/Analysis/Chain/ChainRStudioFieldset.js
+++ webapp/views/App/views/Analysis/Chain/ChainRStudioFieldset.js
@@ -7,7 +7,7 @@ import * as RecordStep from '@core/record/recordStep'
 import * as Validation from '@core/validation/validation'
 import * as Chain from '@common/analysis/chain'
 
-import { ChainActions, useChain, useChainRecordsCountByStep } from '@webapp/store/ui/chain'
+import { ChainActions, useChain } from '@webapp/store/ui/chain'
 import { Checkbox } from '@webapp/components/form'
 import ButtonRStudio from '@webapp/components/ButtonRStudio'
 import RecordsDropdown from './RecordsDropdown'
@@ -18,9 +18,6 @@ export const ChainRStudioFieldset = (props) => {
   const dispatch = useDispatch()
   const chain = useChain()
   const validation = Chain.getValidation(chain)
-
-  const recordsCountByStep = useChainRecordsCountByStep()
-  const analysisRecordsAvailable = Number(recordsCountByStep[RecordStep.analysisCode]) > 0
 
   const _openRStudio = useCallback(
     ({ isLocal = false } = {}) => dispatch(ChainActions.openRStudio({ isLocal })),
