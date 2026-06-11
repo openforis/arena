cb3fbc2bd61d5bf6f0c131186a6f2f298530b36f
SteRiccio <1219739+SteRiccio@users.noreply.github.com>
2026-06-11 14:00:56 +0200

    update yarn and arena-server

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   testEnvironment: 'node',
   setupFiles: [require.resolve('./test/unit/jest.setup.js')],
   transformIgnorePatterns: [
-    '/node_modules/(?!change-case/|uuid/|@openforis/arena-core/node_modules/uuid/|@scure/|@noble/|zod/|ai/|@ai-sdk/|eventsource-parser/)',
+    '/node_modules/(?!change-case/|uuid/|@openforis/arena-server/|@openforis/arena-core/node_modules/uuid/|@scure/|@noble/|zod/|ai/|@ai-sdk/|eventsource-parser/)',
   ],
   verbose: true,
 }
