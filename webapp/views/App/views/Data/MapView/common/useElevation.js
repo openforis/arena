1ea25aeced32c26b337fcc11bb6415c4b4a49697
SteRiccio <1219739+SteRiccio@users.noreply.github.com>
2026-07-07 13:02:15 +0200

    map marker: add elevation

--- webapp/views/App/views/Data/MapView/common/useElevation.js
+++ webapp/views/App/views/Data/MapView/common/useElevation.js
@@ -1,30 +1,30 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 
 import { Points } from '@openforis/arena-core'
 
-import * as Survey from '@core/survey/survey'
-
 import * as API from '@webapp/service/api'
+import { useSurveyId, useSurveySrsIndex } from '@webapp/store/survey'
 
-export const useElevation = ({ survey, point, active = true }) => {
-  const surveyId = Survey.getId(survey)
-  const surveyInfo = Survey.getSurveyInfo(survey)
-  const srsIndex = Survey.getSRSIndex(surveyInfo)
+export const useElevation = ({ point, active = true }) => {
+  const surveyId = useSurveyId()
+  const srsIndex = useSurveySrsIndex()
 
-  const pointLatLng = Points.toLatLong(point, srsIndex)
-  const { y: lat, x: lng } = pointLatLng
+  const pointLatLng = useMemo(() => (point ? Points.toLatLong(point, srsIndex) : null), [point, srsIndex])
 
   const [elevation, setElevation] = useState('...')
 
   useEffect(() => {
     const fetchElevation = async () => {
+      const { y: lat, x: lng } = pointLatLng ?? {}
       const _elevation = await API.fetchElevation({ surveyId, lat, lng })
       setElevation(_elevation === null ? 'error' : _elevation)
     }
-    if (active) {
+    if (active && pointLatLng) {
       fetchElevation()
+    } else {
+      setElevation('...')
     }
-  }, [active, lat, lng, surveyId])
+  }, [active, pointLatLng, surveyId])
 
   return elevation
 }
