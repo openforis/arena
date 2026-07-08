2b35bfb9086d7e27e3d9ffa93989b39a2b20774d
SteRiccio <1219739+SteRiccio@users.noreply.github.com>
2026-07-08 11:44:11 +0200

    sort toggle: added tooltip

--- webapp/components/Table/Header/SortToggle/SortToggle.js
+++ webapp/components/Table/Header/SortToggle/SortToggle.js
@@ -2,17 +2,30 @@ import './SortToggle.scss'
 
 import PropTypes from 'prop-types'
 
+import { useI18n } from '@webapp/store/system'
+
+const tooltipKeyByOrder = {
+  asc: 'common.sortDesc',
+  desc: 'common.sortNone',
+}
+
 const SortToggle = ({ disabled = false, sort, field, handleSortBy }) => {
+  const i18n = useI18n()
+
+  const active = sort.by === field
+  const tooltipKey = active ? tooltipKeyByOrder[sort.order] : 'common.sortAsc'
+
   return (
     <button
       type="button"
       className={`
         btn-xs btn-transparent btn-sort-toggle
         ${sort.order || ''}
-        ${sort.by === field ? '' : 'inactive'}
+        ${active ? '' : 'inactive'}
       `}
       disabled={disabled}
       onClick={() => handleSortBy(field)}
+      title={i18n.t(tooltipKey)}
     >
       <span className="icon icon-play3 icon-10px arrow-toggle" />
     </button>
