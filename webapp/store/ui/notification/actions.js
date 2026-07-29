999bf6d861cd031afe47d60d6cb3c18db7b1c635
Stefano Ricci <1219739+SteRiccio@users.noreply.github.com>
2026-07-29 10:35:40 +0200

    User invite: verify email addresses (#4338)
    * user access request: check email address is valid
    
    * user invite: handle invalid email addresses
    
    * improved invalid email notification message
    
    * user access request: handle invalid email error in request creation
    
    * fixed sonarcloud issue
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- webapp/store/ui/notification/actions.js
+++ webapp/store/ui/notification/actions.js
@@ -6,23 +6,23 @@ export const NOTIFICATION_SHOW = 'ui/notification/show'
 export const NOTIFICATION_HIDE = 'ui/notification/hide'
 
 export const showNotification =
-  ({ key, params, severity, autoHide = true, timeout = 10000 }) =>
+  ({ key, params, text, severity, autoHide = true, timeout = 10000 }) =>
   (dispatch) => {
-    const notification = NotificationState.newNotification({ key, params, severity })
+    const notification = NotificationState.newNotification({ key, params, text, severity })
     dispatch({ type: NOTIFICATION_SHOW, notification })
     if (autoHide && timeout > 0) {
       dispatch(debounceAction({ type: NOTIFICATION_HIDE }, NOTIFICATION_HIDE, timeout))
     }
   }
 
-export const notifyInfo = ({ key, params, autoHide = true, timeout = 10000 }) =>
-  showNotification({ key, params, autoHide, timeout, severity: NotificationState.severityType.info })
+export const notifyInfo = ({ key, params, text, autoHide = true, timeout = 10000 }) =>
+  showNotification({ key, params, text, autoHide, timeout, severity: NotificationState.severityType.info })
 
-export const notifyWarning = ({ key, params, autoHide = true, timeout = 10000 }) =>
-  showNotification({ key, params, autoHide, timeout, severity: NotificationState.severityType.warning })
+export const notifyWarning = ({ key, params, text, autoHide = true, timeout = 10000 }) =>
+  showNotification({ key, params, text, autoHide, timeout, severity: NotificationState.severityType.warning })
 
-export const notifyError = ({ key, params, autoHide = true, timeout = 10000 }) =>
-  showNotification({ key, params, autoHide, timeout, severity: NotificationState.severityType.error })
+export const notifyError = ({ key, params, text, autoHide = true, timeout = 10000 }) =>
+  showNotification({ key, params, text, autoHide, timeout, severity: NotificationState.severityType.error })
 
 export const hideNotification = () => (dispatch) => {
   cancelDebouncedAction(NOTIFICATION_HIDE)
