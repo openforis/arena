d7270146c7b61e6aff6e4ae65a4e382d95aea7f9
Stefano Ricci <SteRiccio@users.noreply.github.com>
2026-08-26 23:05:55 +0200

    code cleanup

--- webapp/views/App/views/Designer/SurveyDependencyTree/SurveyDependencyTreeChart.js
+++ webapp/views/App/views/Designer/SurveyDependencyTree/SurveyDependencyTreeChart.js
@@ -1,20 +1,19 @@
-import React, { forwardRef, useCallback, useEffect, useRef } from 'react'
+import React, { forwardRef, useCallback, useEffect } from 'react'
 import PropTypes from 'prop-types'
 
 import * as NodeDef from '@core/survey/nodeDef'
 
 import TreeChart from '@webapp/charts/TreeChart'
 import { highlightClassName } from '@webapp/charts/TreeChart/TreeChart'
-import * as NodeDefUIProps from '@webapp/components/survey/SurveyForm/nodeDefs/nodeDefUIProps'
-import { useSurveyCycleKey, useSurveyPreferredLang } from '@webapp/store/survey'
-import { useI18n } from '@webapp/store/system'
+import { useNodeDefTreeChart } from '@webapp/charts/TreeChart/useNodeDefTreeChart'
 
 export const SurveyDependencyTreeChart = forwardRef((props, ref) => {
   const { data, extraLinksGroups, nodeDefLabelType, onNodeClick, selectedNodeUuid } = props
 
-  const i18n = useI18n()
-  const lang = useSurveyPreferredLang()
-  const cycle = useSurveyCycleKey()
+  const { i18n, wrapperRef, nodeLabelFunction, nodeTooltipFunction, nodeIconFunction } = useNodeDefTreeChart({
+    ref,
+    nodeDefLabelType,
+  })
 
   const nodeClassFunction = useCallback(
     (d) => {
@@ -30,20 +29,6 @@ export const SurveyDependencyTreeChart = forwardRef((props, ref) => {
     },
     [selectedNodeUuid]
   )
-
-  const nodeLabelFunction = useCallback(
-    (d) => NodeDef.getLabelWithType({ nodeDef: d.data, lang, type: nodeDefLabelType }),
-    [lang, nodeDefLabelType]
-  )
-
-  const nodeTooltipFunction = useCallback((d) => NodeDef.getDescription(lang)(d.data), [lang])
-
-  const nodeIconFunction = useCallback(
-    (d) => NodeDefUIProps.getIconDescriptorByNodeDef({ nodeDef: d.data, cycle }),
-    [cycle]
-  )
-
-  const wrapperRef = useRef()
 
   useEffect(() => {
     const domElement = wrapperRef.current
@@ -69,18 +54,6 @@ export const SurveyDependencyTreeChart = forwardRef((props, ref) => {
       selectedNodeUuid,
     })
   }, [data, extraLinksGroups, selectedNodeUuid])
-
-  useEffect(() => {
-    const tree = ref.current
-    return () => tree?.destroy()
-  }, [ref])
-
-  // force updating labels in TreeChart when label and tooltip function change (depending on lang and labelType)
-  useEffect(() => {
-    const tree = ref.current
-    tree.nodeLabelFunction = nodeLabelFunction
-    tree.nodeTooltipFunction = nodeTooltipFunction
-  }, [nodeLabelFunction, nodeTooltipFunction, ref])
 
   useEffect(() => {
     ref.current?.updateSelection()
--- webapp/views/App/views/Designer/SurveyHierarchy/SurveyHierarchyTree.js
+++ webapp/views/App/views/Designer/SurveyHierarchy/SurveyHierarchyTree.js
@@ -1,35 +1,20 @@
-import React, { forwardRef, useCallback, useEffect, useRef } from 'react'
+import React, { forwardRef, useEffect } from 'react'
 import PropTypes from 'prop-types'
 
 import * as NodeDef from '@core/survey/nodeDef'
 
 import TreeChart from '@webapp/charts/TreeChart'
-import * as NodeDefUIProps from '@webapp/components/survey/SurveyForm/nodeDefs/nodeDefUIProps'
-import { useSurveyCycleKey, useSurveyPreferredLang } from '@webapp/store/survey'
-import { useI18n } from '@webapp/store/system'
+import { useNodeDefTreeChart } from '@webapp/charts/TreeChart/useNodeDefTreeChart'
 
 const nodeClassFunction = (d) => `node-grid${NodeDef.isVirtual(d.data) ? ' node-virtual' : ''}`
 
 export const SurveyHierarchyTree = forwardRef((props, ref) => {
   const { data, nodeDefLabelType, onEntityClick } = props
 
-  const i18n = useI18n()
-  const lang = useSurveyPreferredLang()
-  const cycle = useSurveyCycleKey()
-
-  const nodeLabelFunction = useCallback(
-    (d) => NodeDef.getLabelWithType({ nodeDef: d.data, lang, type: nodeDefLabelType }),
-    [lang, nodeDefLabelType]
-  )
-
-  const nodeTooltipFunction = useCallback((d) => NodeDef.getDescription(lang)(d.data), [lang])
-
-  const nodeIconFunction = useCallback(
-    (d) => NodeDefUIProps.getIconDescriptorByNodeDef({ nodeDef: d.data, cycle }),
-    [cycle]
-  )
-
-  const wrapperRef = useRef()
+  const { i18n, wrapperRef, nodeLabelFunction, nodeTooltipFunction, nodeIconFunction } = useNodeDefTreeChart({
+    ref,
+    nodeDefLabelType,
+  })
 
   useEffect(() => {
     const domElement = wrapperRef.current
@@ -47,18 +32,6 @@ export const SurveyHierarchyTree = forwardRef((props, ref) => {
       wrapperClass: 'survey-hierarchy__tree',
     })
   }, [])
-
-  useEffect(() => {
-    const tree = ref.current
-    return () => tree?.destroy()
-  }, [ref])
-
-  // force updating labels in TreeChart when label and tooltip function change (depending on lang and labelType)
-  useEffect(() => {
-    const tree = ref.current
-    tree.nodeLabelFunction = nodeLabelFunction
-    tree.nodeTooltipFunction = nodeTooltipFunction
-  }, [nodeLabelFunction, nodeTooltipFunction, ref])
 
   return <div className="hierarchy__tree survey-hierarchy__tree" ref={wrapperRef} />
 })
