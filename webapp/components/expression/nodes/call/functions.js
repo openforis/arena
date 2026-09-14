3a2994735be32af940dccf66bcd07c196c1d76c3
SteRiccio <1219739+SteRiccio@users.noreply.github.com>
2026-09-14 16:13:12 +0200

    function call builder: sort functions by name

--- webapp/components/expression/nodes/call/functions.js
+++ webapp/components/expression/nodes/call/functions.js
@@ -23,17 +23,9 @@ const complexFunctions = {
 }
 
 export const functions = {
-  [functionNames.isEmpty]: {
-    label: 'isEmpty(...)',
-    component: CallIsEmptyEditor,
-  },
-  [functionNames.isNotEmpty]: {
-    label: 'isNotEmpty(...)',
-    component: CallIsNotEmptyEditor,
-  },
-  [functionNames.numberToWords]: {
-    label: 'numberToWords(...)',
-    component: CallNumberToWordsEditor,
+  [functionNames.categoryItemProp]: {
+    label: 'categoryItemProp(...)',
+    component: CallCategoryItemPropEditor,
   },
   [functionNames.count]: {
     label: 'count(...)',
@@ -59,20 +51,20 @@ export const functions = {
     label: 'includes(...)',
     component: CallIncludesEditor,
   },
+  [functionNames.isEmpty]: {
+    label: 'isEmpty(...)',
+    component: CallIsEmptyEditor,
+  },
+  [functionNames.isNotEmpty]: {
+    label: 'isNotEmpty(...)',
+    component: CallIsNotEmptyEditor,
+  },
   [functionNames.now]: {
     callee: functionNames.now,
   },
-  [functionNames.categoryItemProp]: {
-    label: 'categoryItemProp(...)',
-    component: CallCategoryItemPropEditor,
-  },
-  [functionNames.taxonProp]: {
-    label: 'taxonProp(...)',
-    component: CallTaxonPropEditor,
-  },
-  [functionNames.taxonVernacularName]: {
-    label: 'taxonVernacularName(...)',
-    component: CallTaxonVernacularNameEditor,
+  [functionNames.numberToWords]: {
+    label: 'numberToWords(...)',
+    component: CallNumberToWordsEditor,
   },
   [functionNames.recordCycle]: {
     callee: functionNames.recordCycle,
@@ -92,6 +84,15 @@ export const functions = {
   [functionNames.recordOwnerRole]: {
     callee: functionNames.recordOwnerRole,
   },
+  rowIndex: complexFunctions.rowIndex,
+  [functionNames.taxonProp]: {
+    label: 'taxonProp(...)',
+    component: CallTaxonPropEditor,
+  },
+  [functionNames.taxonVernacularName]: {
+    label: 'taxonVernacularName(...)',
+    component: CallTaxonVernacularNameEditor,
+  },
   [functionNames.userEmail]: {
     callee: functionNames.userEmail,
   },
@@ -108,7 +109,6 @@ export const functions = {
   [functionNames.uuid]: {
     callee: functionNames.uuid,
   },
-  ...complexFunctions,
 }
 
 export const getComplexFunctionNameByExpression = (exprString) => {
